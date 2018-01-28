0ed7436b031f289535fe9c3ac868a0d90ca051e4
Lefteris Karapetsas <lefteris@refu.co>
2018-01-29 00:37:41 +0100

    blockchain.py abstract account modification

--- rotkelchen/blockchain.py
+++ rotkelchen/blockchain.py
@@ -174,38 +174,31 @@ class Blockchain(object):
             }
 
     def add_blockchain_account(self, blockchain, account):
-        if self.balances == {}:
-            raise ValueError('add_blockchain_account called before the first blockchain balance querying')
-
-        if blockchain == 'BTC':
-            if account in self.accounts['BTC']:
-                raise InputError('Tried to add an already existing BTC account')
-            self.modify_btc_account(account, 'append', operator.add)
-
-        elif blockchain == 'ETH':
-            if account in self.accounts['ETH']:
-                raise InputError('Tried to add an already existing ETH account')
-            self.modify_eth_account(account, 'append', operator.add)
-        else:
-            raise InputError('Unsupported blockchain {} provided at add_blockchain_account'.format(blockchain))
-
-        return {'per_account': self.balances, 'totals': self.totals}
+        return self.modify_blockchain_account(blockchain, account, 'append', operator.add)
 
     def remove_blockchain_account(self, blockchain, account):
+        return self.modify_blockchain_account(blockchain, account, 'remove', operator.sub)
+
+    def modify_blockchain_account(self, blockchain, account, append_or_remove, add_or_sub):
         if self.balances == {}:
-            raise ValueError('remove_blockchain_account called before the first blockchain balance querying')
+            raise ValueError(
+                'modify_blockchain_account called before the first blockchain balance querying'
+            )
 
         if blockchain == 'BTC':
             if account not in self.accounts['BTC']:
                 raise InputError('Tried to remove a non existing BTC account')
-            self.modify_btc_account(account, 'remove', operator.sub)
+            self.modify_btc_account(account, append_or_remove, add_or_sub)
 
         elif blockchain == 'ETH':
             if account not in self.accounts['ETH']:
                 raise InputError('Tried to remove a non existing ETH account')
-            self.modify_eth_account(account, 'remove', operator.sub)
+            self.modify_eth_account(account, append_or_remove, add_or_sub)
         else:
-            raise InputError('Unsupported blockchain {} provided at remove_blockchain_account'.format(blockchain))
+            raise InputError(
+                'Unsupported blockchain {} provided at remove_blockchain_account'.format(
+                    blockchain)
+            )
 
         return {'per_account': self.balances, 'totals': self.totals}
 
