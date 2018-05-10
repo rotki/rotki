08669b31e1e3c46db60cd6dd8480bc9a5c00d058
Lefteris Karapetsas <lefteris@refu.co>
2018-05-10 23:50:36 +0200

    Use scm version in setup.py
    Fix #57

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import subprocess
 from setuptools import setup
 
 
@@ -20,40 +19,8 @@ install_requirements = list(set(
 
 version = '0.1.1'  # Do not edit: this is maintained by bumpversion (see .bumpversion.cfg)
 
-
-def read_version_from_git():
-    try:
-        import shlex
-        git_version, _ = subprocess.Popen(
-            shlex.split('git describe --tags --abbrev=8'),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        ).communicate()
-        # Popen returns bytes
-        git_version = git_version.decode()
-
-        if git_version.startswith('v'):
-            git_version = git_version[1:]
-
-        git_version = git_version.strip()
-        # if this is has commits after the tag, it's a prerelease:
-        if git_version.count('-') == 2:
-            _, _, commit = git_version.split('-')
-            if commit.startswith('g'):
-                commit = commit[1:]
-            return '{}+git.r{}'.format(version, commit)
-        elif git_version.count('.') == 2:
-            return git_version
-        else:
-            return version
-    except BaseException as e:
-        print('could not read version from git: {}'.format(e))
-        return version
-
-
 setup(
     name='rotkehlchen',
-    version=read_version_from_git(),
     author='Lefteris Karapetsas',
     author_email='lefteris@refu.co',
     description=('Acccounting, asset management and tax report helper for cryptocurrencies'),
@@ -62,6 +29,8 @@ setup(
     url='https://github.com/rotkehlchenio/rotkehlchen',
     packages=['rotkehlchen'],
     install_requires=install_requirements,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     long_description=read('README.md'),
     classifiers=[
         'Development Status :: 1 - Planning',
