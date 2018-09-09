168569cab3c376cb2565250da11983f905a232c9
Lefteris Karapetsas <lefteris@refu.co>
2018-09-09 13:46:22 +0200

    Test bitmex api withdrawals and deposits

--- rotkehlchen/tests/test_bitmex.py
+++ rotkehlchen/tests/test_bitmex.py
@@ -1,5 +1,11 @@
 import pytest
 from rotkehlchen.bitmex import Bitmex
+from rotkehlchen.fval import FVal
+from rotkehlchen.order_formatting import AssetMovement
+from rotkehlchen.utils import ts_now
+
+TEST_BITMEX_API_KEY = b'XY98JYVL15Zn-iU9f7OsJeVf'
+TEST_BITMEX_API_SECRET = b'671tM6f64bt6KhteDakj2uCCNBt7HhZVEE7H5x16Oy4zb1ag'
 
 
 @pytest.fixture
@@ -13,6 +19,19 @@ def mock_bitmex(accounting_data_dir, inquirer):
     )
 
     bitmex.first_connection_made = True
+    return bitmex
+
+
+@pytest.fixture
+def test_bitmex(accounting_data_dir, inquirer):
+    # API key/secret from tests cases here: https://www.bitmex.com/app/apiKeysUsage
+    bitmex = Bitmex(
+        TEST_BITMEX_API_KEY,
+        TEST_BITMEX_API_SECRET,
+        inquirer,
+        accounting_data_dir,
+    )
+    bitmex.uri = 'https://testnet.bitmex.com'
     return bitmex
 
 
@@ -40,3 +59,38 @@ def test_bitmex_api_signature(mock_bitmex):
         ),
     )
     assert sig == '1749cd2ccae4aa49048ae09f0b95110cee706e0944e6a14ad0b3a8cb45bd336b'
+
+
+def test_bitmex_api_withdrawals_deposit(test_bitmex):
+    result = test_bitmex.query_deposits_withdrawals(
+        start_ts=1536492800,
+        end_ts=1536492976,
+        end_at_least_ts=1536492976,
+    )
+    assert len(result) == 0
+
+    now = ts_now()
+    result = test_bitmex.query_deposits_withdrawals(
+        start_ts=0,
+        end_ts=now,
+        end_at_least_ts=now,
+    )
+    expected_result = [
+        AssetMovement(
+            exchange='bitmex',
+            category='Withdrawal',
+            timestamp=1536486785,
+            asset='BTC',
+            amount=FVal('0.00700000'),
+            fee=FVal('0.00300000')
+        ),
+        AssetMovement(
+            exchange='bitmex',
+            category='Deposit',
+            timestamp=1536486277,
+            asset='BTC',
+            amount=FVal(0.46966992),
+            fee=FVal(0)
+        ),
+    ]
+    assert result == expected_result
