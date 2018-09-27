6846f85221efbd0bdc11125479c6647daefd3777
Lefteris Karapetsas <lefteris@refu.co>
2018-09-27 18:45:00 +0000

    Add version to the rst docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Lefteris Karapetsas'
 author = 'Lefteris Karapetsas'
 
 # The short X.Y version
-version = ''
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
