566db74290792b8839605960981f0db29c0876a5
Kenneth Ascheri <ascherik@lanecc.edu>
2018-10-05 23:15:12 -0700

    Name and fixup window resize function

--- ui/depending_js/sb-admin-2.js
+++ ui/depending_js/sb-admin-2.js
@@ -6,9 +6,10 @@ $(function() {
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).bind("load resize", function() {
-        var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    function resize() {
+        const navBar = $('.navbar');
+        var topOffset = navBar.height();
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
@@ -16,13 +17,17 @@ $(function() {
             $('div.navbar-collapse').removeClass('collapse');
         }
 
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
         height = height - topOffset;
         if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
+        
+        let $page = $("#page-wrapper");
+        $page.css("height", `${height}px`);
+        $page.css("padding-bottom", "10px");
+    }
+
+    $(document).ready(() => resize());
+    $(window).bind("load resize", () => resize());
 
     var url = window.location;
     // var element = $('ul.nav a').filter(function() {
