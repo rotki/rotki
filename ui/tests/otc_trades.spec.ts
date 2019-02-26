151e4f1e1a06bc20001155bcb10fcceccf9ce5c4
Konstantinos Paparas <kelsos86@gmail.com>
2019-02-26 23:12:34 +0100

    Adds e2e test that adds two otc trades

---
+++ ui/tests/otc_trades.spec.ts
@@ -0,0 +1,77 @@
+import {Application, SpectronClient} from 'spectron';
+import {
+    captureOnFailure,
+    createAccount,
+    GLOBAL_TIMEOUT,
+    initialiseSpectron,
+    METHOD_TIMEOUT,
+    setupTest
+} from './common';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import {Guid} from './guid';
+import OtcController, {otcData} from './otc_controller';
+
+const retry = require('promise-retry');
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('otc trades', function () {
+    // @ts-ignore
+    this.timeout(GLOBAL_TIMEOUT);
+
+    const title = this.title;
+    let app: Application;
+    let client: SpectronClient;
+
+    let username: string;
+    const password: string = process.env.PASSWORD as string;
+    let controller: OtcController;
+
+    before(async function () {
+        username = Guid.newGuid().toString();
+        app = initialiseSpectron();
+        await app.start();
+
+        await setupTest(app, title, async () => {
+            await createAccount(app, username, password);
+            client = app.client;
+            controller = new OtcController(client);
+
+            await retry(async () => {
+                await client.click('#side-menu>li:nth-child(2)');
+            });
+
+            await client.waitUntilTextExists('a', 'OTC Trades', METHOD_TIMEOUT);
+
+            await retry(async () => {
+                await client.click('#side-menu>li:nth-child(2)>.nav>li');
+            });
+
+            await client.waitUntilTextExists('.page-header', 'OTC Trades Management', METHOD_TIMEOUT);
+        });
+    });
+
+    after(async () => {
+        if (app && app.isRunning()) {
+            await app.stop();
+        }
+    });
+
+    afterEach(async function () {
+        await captureOnFailure(app, this.currentTest);
+    });
+
+    it('should add two OTC trades and show them on the table', async function () {
+        await controller.addTrade(otcData[0]);
+        await controller.addTrade(otcData[1]);
+        const matchedElements = (await client.elements('#table_otctrades > tbody > tr')).value;
+        const numberOfElements = matchedElements.length;
+        numberOfElements.should.equal(2, 'there should be two entries');
+
+        await client.getText('#table_otctrades > tbody > tr:nth-child(1) > td:nth-child(2)').should.eventually.contain('BTC_EUR');
+        await client.getText('#table_otctrades > tbody > tr:nth-child(1) > td:nth-child(4)').should.eventually.contain('82');
+        await client.getText('#table_otctrades > tbody > tr:nth-child(1) > td:nth-child(5)').should.eventually.contain('268.678317859');
+        await client.getText('#table_otctrades > tbody > tr:nth-child(1) > td:nth-child(6)').should.eventually.contain('08/10/2015 10:48');
+    });
+});
