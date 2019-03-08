18e27098acfa1990461a6277d1c1020fafed3a53
Lefteris Karapetsas <lefteris@refu.co>
2019-03-09 00:30:34 +0100

    Rename AssetKeepet to AssetResolver
    Thanks for the name @rakanalh

--- rotkehlchen/assets/architecture.py
+++ rotkehlchen/assets/architecture.py
@@ -1,31 +1,32 @@
 import os
-from dataclasses import dataclass
 from typing import Any, Dict
+
+from dataclasses import dataclass
 
 from rotkehlchen.errors import UnknownAsset
 from rotkehlchen.utils import rlk_jsonloads_dict
 
 
-class AssetKeeper():
+class AssetResolver():
     __instance = None
     assets: Dict[str, Dict[str, Any]] = None
 
     def __new__(cls):
-        if AssetKeeper.__instance is not None:
-            return AssetKeeper.__instance
+        if AssetResolver.__instance is not None:
+            return AssetResolver.__instance
 
-        AssetKeeper.__instance = object.__new__(cls)
+        AssetResolver.__instance = object.__new__(cls)
 
         dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         with open(os.path.join(dir_path, 'data', 'all_assets.json'), 'r') as f:
             assets = set(rlk_jsonloads_dict(f.read()))
 
-        AssetKeeper.__instance.gateway = assets
-        return AssetKeeper.__instance
+        AssetResolver.__instance.gateway = assets
+        return AssetResolver.__instance
 
     @staticmethod
     def is_name_canonical(asset_name: str) -> bool:
-        return asset_name in AssetKeeper().assets
+        return asset_name in AssetResolver().assets
 
 
 @dataclass(init=False, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
@@ -33,7 +34,7 @@ class Asset():
     name: str
 
     def __init__(self, asset_name: str):
-        if not AssetKeeper().is_name_canonical(asset_name):
+        if not AssetResolver().is_name_canonical(asset_name):
             raise UnknownAsset(asset_name)
 
     def canonical(self) -> str:
