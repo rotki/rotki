d930cab642514866a94248a215e4ba1f26c62f7a
Lefteris Karapetsas <lefteris@refu.co>
2019-03-10 01:11:07 +0100

    Add tests for common asset operations

---
+++ rotkehlchen/tests/test_assets.py
@@ -0,0 +1,39 @@
+import pytest
+
+from rotkehlchen.assets.asset import Asset
+from rotkehlchen.errors import UnknownAsset
+
+
+def test_unknown_asset():
+    """Test than an unknown asset will throw"""
+    with pytest.raises(UnknownAsset):
+        Asset('jsakdjsladjsakdj')
+
+
+def test_asset_hashes_properly():
+    """Test that assets can be hashed and are equivalent to the canonical string"""
+    btc_asset = Asset('BTC')
+    eth_asset = Asset('ETH')
+    mapping = {btc_asset: 100, 'ETH': 200}
+
+    assert btc_asset in mapping
+    assert eth_asset in mapping
+    assert 'BTC' in mapping
+    assert 'ETH' in mapping
+
+    assert mapping[btc_asset] == 100
+    assert mapping[eth_asset] == 200
+    assert mapping['BTC'] == 100
+    assert mapping['ETH'] == 200
+
+
+def test_asset_equals():
+    btc_asset = Asset('BTC')
+    eth_asset = Asset('ETH')
+    other_btc_asset = Asset('BTC')
+
+    assert btc_asset == 'BTC'
+    assert btc_asset != eth_asset
+    assert btc_asset != 'ETH'
+    assert btc_asset == other_btc_asset
+    assert eth_asset == 'ETH'
