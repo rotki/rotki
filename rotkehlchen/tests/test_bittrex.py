0c89d4938b2af73123fa98b9e9b421cec3905ee8
Lefteris Karapetsas <lefteris@refu.co>
2019-03-20 09:13:35 +0100

    Bittrex assets tests and adding more assets categorized thanks to it

--- rotkehlchen/tests/test_bittrex.py
+++ rotkehlchen/tests/test_bittrex.py
@@ -1,7 +1,26 @@
-from rotkehlchen.assets.converters import asset_from_bittrex
+from rotkehlchen.assets.asset import Asset
+from rotkehlchen.assets.converters import BITTREX_TO_WORLD, asset_from_bittrex
+from rotkehlchen.assets.resolver import AssetResolver
 
 
 def test_bittrex_assets_are_known(bittrex):
     currencies = bittrex.get_currencies()
-    for bittrex_asset in currencies:
-        _ = asset_from_bittrex(bittrex_asset['Currency'])
+    # for bittrex_asset in currencies:
+    #     _ = asset_from_bittrex(bittrex_asset['Currency'])
+
+    checking_index = 107
+    for idx, bittrex_asset in enumerate(currencies):
+        if idx >= checking_index:
+            symbol = bittrex_asset['Currency']
+            if not AssetResolver().is_symbol_canonical(symbol):
+                msg = (
+                    f'{idx} - {symbol} is not known. '
+                    f'Bittrex name: {bittrex_asset["CurrencyLong"]}'
+                )
+                assert False, msg
+            else:
+                asset = Asset(symbol)
+                print(
+                    f'{idx} - {symbol} with name {asset.name} '
+                    f'is known. Bittrex name: {bittrex_asset["CurrencyLong"]}',
+                )
