35bbee31a671c8eca446660f7b188e443095dbb2
Konstantinos Paparas <kelsos86@gmail.com>
2019-05-22 00:34:59 +0200

    Rewriting General Settings

---
+++ electron-app/src/components/dialogs/MessageDialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-dialog v-model="message.length > 0" persistent max-width="400">
+    <v-card>
+      <v-card-title
+        :class="{ 'green--text': success, 'red--text': !success }"
+        class="headline"
+      >
+        {{ title }}
+      </v-card-title>
+      <v-layout align-center>
+        <v-flex xs2>
+          <v-icon
+            class="dialog-icon"
+            :class="{ 'green--text': success, 'red--text': !success }"
+          >
+            fa {{ success ? 'fa-check-circle' : 'fa-exclamation-circle' }} fa-3x
+          </v-icon>
+        </v-flex>
+        <v-flex xs10>
+          <v-card-text> {{ message }} </v-card-text>
+        </v-flex>
+      </v-layout>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn :color="success ? 'green' : 'red'" flat @click="dismiss()"
+          >Ok</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class MessageDialog extends Vue {
+  @Prop({ required: true })
+  title!: string;
+  @Prop({ required: true })
+  message!: string;
+
+  @Prop({ default: false })
+  success!: boolean;
+
+  visible(): boolean {
+    return !!this.message;
+  }
+
+  @Emit()
+  dismiss() {}
+}
+</script>
+
+<style scoped>
+.dialog-icon {
+  margin-left: 25px;
+}
+</style>
--- electron-app/src/views/settings/General.vue
+++ electron-app/src/views/settings/General.vue
@@ -1,128 +1,163 @@
-<template>
-  <div id="settings-general">
-    <div class="row">
-      <div class="col-lg-12">
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-container id="settings-general">
+    <v-layout>
+      <v-flex>
         <h1 class="page-header">Settings</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="panel panel-default">
-          <div class="panel-heading">General Settings</div>
-          <div id="general_settings_panel_body" class="panel-body">
-            <div class="form-group input-group">
-              <span class="input-group-addon">Floating Precision:</span>
-              <input
-                id="floating_precision"
-                v-model="floatingPrecision"
-                class="form-control"
-                type="number"
-              />
-            </div>
-            <div class="checkbox">
-              <label>
-                <input
-                  id="anonymized_logs_input"
-                  v-model="anonymizedLogs"
-                  type="checkbox"
-                />
-                Should logs by anonymized?
-              </label>
-            </div>
-            <div class="form-group input-group">
-              <span class="input-group-addon">
-                Date from when to count historical data:
-              </span>
-              <input
-                id="historical_data_start"
-                v-model="historicDataStart"
-                class="form-control"
-                type="text"
-              />
-            </div>
-            <div class="form-group">
-              <label class="form-prompt">
-                Select Main Currency
-              </label>
-              <select
-                id="maincurrencyselector"
-                v-model="selectedCurrency"
-                class="form-control"
-                style="font-family: 'FontAwesome', 'sans-serif';"
-              >
-                <option v-for="currency in currencies" :value="currency">{{
-                  currency
-                }}</option>
-              </select>
-            </div>
-            <div class="form-group input-group">
-              <span class="input-group-addon">
-                Eth RPC Port:
-              </span>
-              <input
-                id="eth_rpc_port"
-                v-model="rpcPort"
-                class="form-control"
-                type="number"
-              />
-            </div>
-            <div class="form-group input-group">
-              <span class="input-group-addon">
-                Balance data saving frequency in hours:
-              </span>
-              <input
-                id="balance_save_frequency"
-                v-model="balanceSaveFrequency"
-                class="form-control"
-                type="number"
-              />
-            </div>
-            <div class="form-group input-group">
-              <span class="input-group-addon">
-                Date display format:
-              </span>
-              <input
-                id="date_display_format"
-                v-model="dateDisplayFormat"
-                class="form-control"
-                type="text"
-              />
-            </div>
-            <button
-              id="settingssubmit"
-              type="submit"
-              class="btn btn-default"
-              @click="save()"
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <v-card>
+          <v-toolbar card>General Settings</v-toolbar>
+          <v-card-text>
+            <v-text-field
+              id="floating_precision"
+              v-model="floatingPrecision"
+              label="Floating Precision"
+              type="number"
+            ></v-text-field>
+            <v-checkbox
+              id="anonymized_logs_input"
+              v-model="anonymizedLogs"
+              label="Should logs by anonymized?"
+            ></v-checkbox>
+
+            <v-menu
+              ref="historicDateMenu"
+              v-model="historicDateMenu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              max-width="290px"
+              min-width="290px"
             >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  id="historical_data_start"
+                  v-model="historicDataStart"
+                  label="Date from when to count historical data"
+                  hint="DD/MM/YYYY format"
+                  persistent-hint
+                  prepend-icon="fa-calendar"
+                  @blur="date = parseDate(historicDataStart)"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="date"
+                no-title
+                @input="historicDateMenu = false"
+              ></v-date-picker>
+            </v-menu>
+
+            <v-select
+              id="maincurrencyselector"
+              v-model="selectedCurrency"
+              label="Select Main Currency"
+              :items="currencies"
+            >
+            </v-select>
+
+            <v-text-field
+              id="eth_rpc_endpoint"
+              v-model="rpcEndpoint"
+              v-validate="{ url: { require_protocol: true } }"
+              label="Eth RPC Endpoint"
+              type="text"
+              data-vv-name="eth_rpc_endpoint"
+            ></v-text-field>
+
+            <v-text-field
+              id="balance_save_frequency"
+              v-model="balanceSaveFrequency"
+              label="Balance data saving frequency in hours"
+              type="number"
+            ></v-text-field>
+
+            <v-text-field
+              id="date_display_format"
+              v-model="dateDisplayFormat"
+              label="Date display format"
+              type="text"
+            ></v-text-field>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn id="settingssubmit" depressed type="submit" @click="save()">
               Save
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <message-dialog
+      title="Settings Error"
+      :message="error"
+      @dismiss="dismiss()"
+    ></message-dialog>
+    <message-dialog
+      :success="true"
+      title="Successfully modified settings."
+      :message="success"
+      @dismiss="dismiss()"
+    ></message-dialog>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import { currencies } from '@/data/currencies';
 import { RotkehlchenState } from '@/store';
-import { showError, showInfo } from '@/legacy/utils';
 import { settings } from '@/legacy/settings';
 import { GeneralSettings } from '@/typing/types';
+import MessageDialog from '@/components/dialogs/MessageDialog.vue';
 
-@Component({})
+@Component({
+  components: { MessageDialog }
+})
 export default class General extends Vue {
   floatingPrecision: number = 0;
   anonymizedLogs: boolean = false;
   historicDataStart: string = '';
-  rpcPort: number = 0;
+  rpcEndpoint: string = 'http://localhost:8546';
   balanceSaveFrequency: number = 0;
   dateDisplayFormat: string = '';
   selectedCurrency: string = '';
 
+  historicDateMenu: boolean = false;
+  date: string = '';
+
+  error: string = '';
+  success: string = '';
+
+  @Watch('date')
+  dateWatch(val: string) {
+    this.historicDataStart = this.formatDate(this.date);
+  }
+
+  formatDate(date: string) {
+    if (!date) return '';
+
+    const [year, month, day] = date.split('-');
+    return `${day}/${month}/${year}`;
+  }
+  parseDate(date: string) {
+    if (!date) return null;
+
+    const [day, month, year] = date.split('/');
+    return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+  }
+
   get currencies(): string[] {
     return currencies.map(x => x.ticker_symbol);
+  }
+
+  dismiss() {
+    this.success = '';
+    this.error = '';
   }
 
   mounted() {
@@ -132,29 +167,18 @@ export default class General extends Vue {
     this.floatingPrecision = settings.floatingPrecision;
     this.anonymizedLogs = settings.anonymizedLogs;
     this.historicDataStart = settings.historicDataStart;
-    this.rpcPort = settings.ethRpcEndpoint;
+    this.rpcEndpoint = settings.ethRpcEndpoint;
     this.balanceSaveFrequency = settings.balanceSaveFrequency;
     this.dateDisplayFormat = settings.dateDisplayFormat;
-
-    $('#historical_data_start').datetimepicker({
-      timepicker: false,
-      format: 'd/m/Y'
-    });
+    this.date = this.parseDate(settings.historicDataStart) || '';
   }
 
   save() {
-    if (this.rpcPort < 1 || this.rpcPort > 65535) {
-      showError(
-        'Invalid port number',
-        'Please ensure that the specified port number is between 1 and 65535'
-      );
-      return;
-    }
     const payload = {
       ui_floating_precision: this.floatingPrecision,
       historical_data_start: this.historicDataStart,
       main_currency: this.selectedCurrency,
-      eth_rpc_port: this.rpcPort,
+      eth_rpc_endpoint: this.rpcEndpoint,
       balance_save_frequency: this.balanceSaveFrequency,
       anonymized_logs: this.anonymizedLogs,
       date_display_format: this.dateDisplayFormat
@@ -163,7 +187,7 @@ export default class General extends Vue {
     const generalSettings: GeneralSettings = {
       floatingPrecision: this.floatingPrecision,
       historicDataStart: this.historicDataStart,
-      ethRpcEndpoint: this.rpcPort,
+      ethRpcEndpoint: this.rpcEndpoint,
       balanceSaveFrequency: this.balanceSaveFrequency,
       anonymizedLogs: this.anonymizedLogs,
       dateDisplayFormat: this.dateDisplayFormat,
@@ -173,18 +197,11 @@ export default class General extends Vue {
     this.$rpc
       .set_settings(payload)
       .then(result => {
-        let message = 'Successfully modified settings.';
-        if ('message' in result && result.message !== '') {
-          message = ` ${message}${result.message}`;
-        }
-        showInfo('Success', message);
+        this.success = result.message || '';
         this.$store.commit('settings', generalSettings);
       })
       .catch((error: Error) => {
-        showError(
-          'Settings Error',
-          `Error at modifying settings: ${error.message}`
-        );
+        this.error = `Error at modifying settings: ${error.message}`;
       });
   }
 }
