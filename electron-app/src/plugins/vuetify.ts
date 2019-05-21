bc709cd302cc876355c73ee439a1d9fa9ffd8d6d
Konstantinos Paparas <kelsos86@gmail.com>
2019-05-21 23:38:13 +0200

    Introduces vuetify for UI components

---
+++ electron-app/src/plugins/vuetify.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#7e4a3b',
+    secondary: '#424242',
+    accent: '#e45325',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  options: {
+    customProperties: true
+  },
+  iconfont: 'fa4'
+});
