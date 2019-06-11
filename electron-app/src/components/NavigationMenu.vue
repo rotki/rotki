c0c8a16ffcdd14d192d1b25c4397e089a2fa4965
Konstantinos Paparas <kelsos86@gmail.com>
2019-06-11 23:55:50 +0200

    Rewrites navigation menu using vuetify

--- electron-app/src/components/NavigationMenu.vue
+++ electron-app/src/components/NavigationMenu.vue
@@ -1,37 +1,29 @@
 <template>
-  <div class="sidebar-nav navbar-collapse">
-    <ul id="side-menu" class="nav">
-      <li>
-        <router-link to="/">
-          <i class="fa fa-dashboard fa-fw"></i>Dashboard
-        </router-link>
-      </li>
-      <li>
-        <a>
-          <i class="fa fa-exchange fa-fw"></i>Trade
-          <span class="fa arrow"></span>
-        </a>
-        <ul class="nav nav-second-level">
-          <li>
-            <router-link to="/otc-trades">OTC Trades</router-link>
-          </li>
-          <li>
-            <a href="#">Exchange Trades</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <router-link to="/statistics">
-          <i class="fa fa-bar-chart fa-fw"></i>Statistics
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/tax-report">
-          <i class="fa fa-calculator fa-fw"></i>Tax Report
-        </router-link>
-      </li>
-    </ul>
-  </div>
+  <v-list>
+    <v-list-tile to="/">
+      <v-list-tile-action><v-icon>fa-dashboard</v-icon></v-list-tile-action>
+      <v-list-tile-title>Dashboard</v-list-tile-title>
+    </v-list-tile>
+    <v-list-group prepend-icon="fa-exchange" value="true">
+      <template v-slot:activator>
+        <v-list-tile>
+          <v-list-tile-title>Trade</v-list-tile-title>
+        </v-list-tile>
+      </template>
+
+      <v-list-tile to="/otc-trades">
+        <v-list-tile-title>OTC Trades</v-list-tile-title>
+      </v-list-tile>
+    </v-list-group>
+    <v-list-tile to="/statistics">
+      <v-list-tile-action><v-icon>fa-bar-chart</v-icon></v-list-tile-action>
+      <v-list-tile-title>Statistics</v-list-tile-title>
+    </v-list-tile>
+    <v-list-tile to="/tax-report">
+      <v-list-tile-action><v-icon>fa-calculator</v-icon></v-list-tile-action>
+      <v-list-tile-title>Tax Report</v-list-tile-title>
+    </v-list-tile>
+  </v-list>
 </template>
 
 <script lang="ts">
