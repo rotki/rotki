975ab8102215d7296cd44535614b53a3f3018542
Lefteris Karapetsas <lefteris@refu.co>
2019-06-26 11:13:23 +0200

    Add binance USDSB to unsupported assets

--- rotkehlchen/assets/converters.py
+++ rotkehlchen/assets/converters.py
@@ -369,8 +369,9 @@ UNSUPPORTED_BITTREX_ASSETS = (
 
 UNSUPPORTED_BINANCE_ASSETS = (
     'ETF',  # ETF is a dead coin given to all ETH holders. Just ignore
-    # BTCB is not yet supported anywhere else
+    # BTCB and USDSB is not yet supported anywhere else
     'BTCB',  # https://www.binance.com/en/support/articles/360029288972
+    'USDSB',  # https://www.binance.com/en/support/articles/360029522132
 )
 
 CRYPTOCOMPARE_TO_WORLD = {v: k for k, v in WORLD_TO_CRYPTOCOMPARE.items()}
