c764b87a8058cf048017a61c386acacdcec15b75
Lefteris Karapetsas <lefteris@refu.co>
2019-07-23 15:13:12 +0200

    special hook for coincurve in windows

---
+++ tools/pyinstaller_hooks/hook-coincurve.py
@@ -0,0 +1,12 @@
+import os
+from PyInstaller.utils.hooks import get_module_file_attribute, collect_dynamic_libs
+
+binaries = []
+if os.name == 'nt':
+    # I don't know why but in Windows the python executable does not seem
+    # to properly copy libsecp256k1.dll
+    # OSError: cannot load library 'C:\Users\lefteris\AppData\Local\Temp\_MEI50802\coincurve\libsecp256k1.dll': error 0x7e.  Additionally, ctypes.util.find_library()did not manage to locate a library called 'C:\\Users\\lefteris\\AppData\\Local\\Temp\\_MEI50802\\coincurve\\libsecp256k1.dll'
+    #[4952] Failed to execute script rotkehlchen-script
+
+    # With this the dll is bundled correctly
+    binaries = collect_dynamic_libs('coincurve')
