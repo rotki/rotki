92178d563ae6c71c0052e1d257a14204157df854
Lefteris Karapetsas <lefteris@refu.co>
2019-08-01 22:34:23 +0200

    Add link to premium from statistics page if no keys are found
    If the user does not have premium then the statistics page now has a
    link for how he can get one.
    
    Also enabled our electron app to open external links in a new window
    and not inside the app's own window.

--- ui/statistics.ts
+++ ui/statistics.ts
@@ -6,7 +6,9 @@ import {showError} from './utils';
 export function create_statistics_ui() {
     let str = page_header('Statistics');
     if (!settings.has_premium) {
-        str += '<div><p>No premium subscription detected. Statistics are only available to premium users.</p></div>';
+        str += `<div><p>No premium subscription detected. Statistics are only available
+to premium users. To get a premium subscription please visit our
+<a href="https://rotkehlchen.io/products" target="_blank">website</a>.</p></div>`;
         $('#page-wrapper').html(str);
     } else {
         service.query_statistics_renderer().then(result => {
