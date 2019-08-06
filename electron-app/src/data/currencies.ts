0f44a484a2acc468661fc51cf40f349f1d5cb9f1
Konstantinos Paparas <kelsos86@gmail.com>
2019-08-06 23:16:45 +0200

    Adds missing currencies

--- electron-app/src/data/currencies.ts
+++ electron-app/src/data/currencies.ts
@@ -5,5 +5,7 @@ export const currencies: Currency[] = [
   new Currency('Euro', 'fa-eur', 'EUR', '€'),
   new Currency('British Pound', 'fa-gbp', 'GBP', '£'),
   new Currency('Japanese Yen', 'fa-jpy', 'JPY', '¥'),
-  new Currency('Chinese Yuan', 'fa-jpy', 'CNY', '¥')
+  new Currency('Chinese Yuan', 'fa-jpy', 'CNY', '¥'),
+  new Currency('Korean Won', 'fa-krw', 'KRW', '₩'),
+  new Currency('Canadian Dollar', 'fa-cad', 'CAD', '$')
 ];
