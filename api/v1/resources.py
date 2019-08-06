c5ee6908b29bfb6566ce8d5a10bdc3eae3cf637e
Lefteris Karapetsas <lefteris@refu.co>
2019-08-06 22:42:53 +0200

    Add an HTTP Rest API server skeleton

---
+++ api/v1/resources.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from flask_restful import Resource
+from webargs.flaskparser import use_kwargs
+
+from rotkehlchen.api.v1.encoding import x
+
+
+def create_blueprint():
+    # Take a look at this SO question on hints how to organize versioned
+    # API with flask:
+    # http://stackoverflow.com/questions/28795561/support-multiple-api-versions-in-flask#28797512
+    return Blueprint("v1_resources", __name__)
+
+
+class BaseResource(Resource):
+    def __init__(self, rest_api_object, **kwargs):
+        super().__init__(**kwargs)
+        self.rest_api = rest_api_object
+
+
+class LogoutResource(BaseResource):
+    def get(self):
+        return self.rest_api.logout()
