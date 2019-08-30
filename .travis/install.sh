9b7e016cfdd673d73505f8746ab89410c2943c73
Lefteris Karapetsas <lefteris@refu.co>
2019-08-30 12:55:22 +0200

    Add pip install -e .
    That's to be able to also use `pkg_resources` in tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -16,6 +16,7 @@ fi
 $PIP_CMD install ${INSTALL_OPT} --upgrade "pip<19.0.0" wheel
 $PIP_CMD install ${INSTALL_OPT} pytest-travis-fold codecov pytest-cov
 $PIP_CMD install ${INSTALL_OPT} -r requirements_dev.txt
+$PIP_CMD install ${INSTALL_OPT} -e .
 
 $PIP_CMD list --outdated
 
