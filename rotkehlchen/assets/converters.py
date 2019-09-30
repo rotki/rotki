f422d803ced399672b502d49d00a1fe207588d69
Lefteris Karapetsas <lefteris@refu.co>
2019-09-30 21:41:39 +0200

    Add Tether CNH to the bittrex ignored assets

--- rotkehlchen/assets/converters.py
+++ rotkehlchen/assets/converters.py
@@ -387,6 +387,9 @@ UNSUPPORTED_BITTREX_ASSETS = (
     # TwelveShips. As of 23/08/2019 no data found outside of Bittrex for this token
     # https://twitter.com/BittrexIntl/status/1164689364997353472
     'TSHP',
+    # Tether CNH. As of 30/09/2019 no data found outside of Bittrex for this token
+    # https://medium.com/bittrex/new-bittrex-international-listing-tether-cnh-cnht-c9ad966ac303
+    'CNHT',
 )
 
 
