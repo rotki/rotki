6c86bdfe5ae6ad740a9b1bd06b48337559e2292f
Lefteris Karapetsas <lefteris@refu.co>
2019-10-04 17:13:36 +0200

    Bump version: 1.0.3 → 1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018-2019, Lefteris Karapetsas'
 author = 'Lefteris Karapetsas'
 
 # The short X.Y version
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
