33d011e09b29a3c9162f525d41376cee25b74266
Jan Steimann <jan.steimann@hhu.de>
2019-10-15 16:18:58 +0200

    Remove DBSettings dict

--- rotkehlchen/datatyping.py
+++ rotkehlchen/datatyping.py
@@ -5,5 +5,4 @@ from rotkehlchen.fval import FVal
 
 # Types used by dbhandler and datahandler
 BalancesData = Dict[Union[str, Asset], Dict[str, Union[FVal, Dict]]]
-DBSettings = Dict[str, Union[int, bool, str, None]]
 ExternalTrade = Dict[str, Union[str, int]]
