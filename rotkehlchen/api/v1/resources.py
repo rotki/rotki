2b73848761f5ad69c93003cc237cdde7b514c04f
Lefteris Karapetsas <lefteris@refu.co>
2019-10-26 10:16:52 +0200

    Add REST API endpoint to query FIAT balances

--- rotkehlchen/api/v1/resources.py
+++ rotkehlchen/api/v1/resources.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Optional
 
-from flask import Blueprint, Flask
+from flask import Blueprint, Response
 from flask_restful import Resource
 from webargs.flaskparser import use_kwargs
 
@@ -32,31 +32,31 @@ class BaseResource(Resource):
 
 class SettingsResource(BaseResource):
 
-    def put(self, settings: Dict[str, Any]) -> Flask.response_class:
+    def put(self, settings: Dict[str, Any]) -> Response:
         return self.rest_api.set_settings(settings)
 
-    def get(self) -> Flask.response_class:
+    def get(self) -> Response:
         return self.rest_api.get_settings()
 
 
 class TaskOutcomeResource(BaseResource):
 
-    def get(self, task_id: int) -> Flask.response_class:
+    def get(self, task_id: int) -> Response:
         return self.rest_api.query_task_outcome(task_id=task_id)
 
 
 class FiatExchangeRatesResource(BaseResource):
 
-    def get(self, currencies: List[str]) -> Flask.response_class:
+    def get(self, currencies: List[str]) -> Response:
         return self.rest_api.get_fiat_exchange_rates(currencies=currencies)
 
 
 class ExchangesResource(BaseResource):
 
-    def put(self, name: str, api_key: str, api_secret: str) -> Flask.response_class:
+    def put(self, name: str, api_key: str, api_secret: str) -> Response:
         return self.rest_api.setup_exchange(name, api_key, api_secret)
 
-    def delete(self, name: str) -> Flask.response_class:
+    def delete(self, name: str) -> Response:
         return self.rest_api.remove_exchange(name=name)
 
 
@@ -65,7 +65,7 @@ class ExchangeBalancesResource(BaseResource):
     get_schema = ExchangeBalanceQuerySchema()
 
     @use_kwargs(get_schema, locations=('json',))
-    def get(self, name: str, async_query: bool) -> Flask.response_class:
+    def get(self, name: str, async_query: bool) -> Response:
         return self.rest_api.query_exchange_balances(name=name, async_query=async_query)
 
 
@@ -74,8 +74,14 @@ class BlockchainBalancesResource(BaseResource):
     get_schema = BlockchainBalanceQuerySchema()
 
     @use_kwargs(get_schema, locations=('json',))
-    def get(self, name: str, async_query: bool) -> Flask.response_class:
+    def get(self, name: str, async_query: bool) -> Response:
         return self.rest_api.query_blockchain_balances(name=name, async_query=async_query)
+
+
+class FiatBalancesResource(BaseResource):
+
+    def get(self) -> Response:
+        return self.rest_api.query_fiat_balances()
 
 
 class TradesResource(BaseResource):
@@ -90,7 +96,7 @@ class TradesResource(BaseResource):
             from_timestamp: Optional[Timestamp],
             to_timestamp: Optional[Timestamp],
             location: Optional[Location],
-    ) -> Flask.response_class:
+    ) -> Response:
         return self.rest_api.get_trades(
             from_ts=from_timestamp,
             to_ts=to_timestamp,
@@ -110,7 +116,7 @@ class TradesResource(BaseResource):
             fee_currency: Asset,
             link: str,
             notes: str,
-    ) -> Flask.response_class:
+    ) -> Response:
         return self.rest_api.add_trade(
             timestamp=timestamp,
             location=location,
@@ -137,7 +143,7 @@ class TradesResource(BaseResource):
             fee_currency: Asset,
             link: str,
             notes: str,
-    ) -> Flask.response_class:
+    ) -> Response:
         return self.rest_api.edit_trade(
             trade_id=trade_id,
             timestamp=timestamp,
@@ -151,7 +157,7 @@ class TradesResource(BaseResource):
             notes=notes,
         )
 
-    def delete(self, trade_id: str) -> Flask.response_class:
+    def delete(self, trade_id: str) -> Response:
         return self.rest_api.delete_external_trade(trade_id=trade_id)
 
 
@@ -159,7 +165,7 @@ class UsersResource(BaseResource):
 
     put_schema = NewUserSchema()
 
-    def get(self) -> Flask.response_class:
+    def get(self) -> Response:
         return self.rest_api.get_users()
 
     @use_kwargs(put_schema, locations=('json',))
@@ -170,7 +176,7 @@ class UsersResource(BaseResource):
             sync_approval: str,
             premium_api_key: str,
             premium_api_secret: str,
-    ) -> Flask.response_class:
+    ) -> Response:
         return self.rest_api.create_new_user(
             name=name,
             password=password,
@@ -192,7 +198,7 @@ class UsersByNameResource(BaseResource):
             sync_approval: str,
             premium_api_key: str,
             premium_api_secret: str,
-    ) -> Flask.response_class:
+    ) -> Response:
         if action is None:
             return self.rest_api.user_set_premium_credentials(
                 name=name,
