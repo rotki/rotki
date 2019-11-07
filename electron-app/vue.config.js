0e6c6488622f3ffb83af04a6bff3487af4edb9d4
Konstantinos Paparas <kelsos86@gmail.com>
2019-11-07 10:00:15 +0100

    Adds initial work on packaging the electron app

--- electron-app/vue.config.js
+++ electron-app/vue.config.js
@@ -11,5 +11,32 @@ module.exports = {
   },
   configureWebpack: {
     devtool: 'source-map'
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'io.rotkehlchen',
+        extraResources: [
+          {
+            from: '../rotkehlchen_py_dist',
+            to: 'rotkehlchen_py_dist',
+            filter: ['**/*']
+          }
+        ],
+        mac: {
+          category: 'public.app-category.finance',
+          icon: 'src/assets/images/rotki.icns'
+        },
+        win: {
+          target: 'nsis',
+          icon: 'src/assets/images/rotki.ico'
+        },
+        linux: {
+          target: ['AppImage'],
+          icon: 'srs/assets/images/rotki_1024x1024.png',
+          category: 'Finance'
+        }
+      }
+    }
   }
 };
