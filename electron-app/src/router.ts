f839ea999ae930067a905575aac09d8057af2e43
Konstantinos Paparas <kelsos86@gmail.com>
2019-11-10 20:06:05 +0100

    Fixes routing to dashboard after login

--- electron-app/src/router.ts
+++ electron-app/src/router.ts
@@ -8,6 +8,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '*',
+      redirect: '/'
+    },
+    {
       path: '/',
       name: 'dashboard',
       component: () => import('./views/Dashboard.vue')
