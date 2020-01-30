5218919a61a5a81a32f3ff64e7e1f4ef51df0b09
Konstantinos Paparas <kelsos86@gmail.com>
2020-01-30 21:46:50 +0100

    Adds test verifying that service key disables

---
+++ electron-app/tests/unit/components/settings/api-keys/service-key.spec.ts
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import { mount, Wrapper } from '@vue/test-utils';
+import store from '@/store/store';
+import ServiceKey from '@/components/settings/api-keys/ServiceKey.vue';
+
+Vue.use(Vuetify);
+
+describe('ServiceKey.vue', () => {
+  let vuetify: typeof Vuetify;
+  let wrapper: Wrapper<ServiceKey>;
+
+  function createWrapper(): Wrapper<ServiceKey> {
+    vuetify = new Vuetify();
+    return mount(ServiceKey, {
+      store,
+      vuetify,
+      attachToDocument: true,
+      propsData: {
+        value: '',
+        title: 'test'
+      }
+    });
+  }
+
+  beforeEach(() => {
+    wrapper = createWrapper();
+    vuetify = new Vuetify();
+  });
+
+  test('component leaves edit mode when updated with non-empty value', async () => {
+    expect.assertions(2);
+    await wrapper.vm.$nextTick();
+    expect(
+      wrapper.find('.service-key__api-key input').attributes('disabled')
+    ).toBeUndefined();
+    wrapper.setProps({
+      value: '1234'
+    });
+    await wrapper.vm.$nextTick();
+    expect(
+      wrapper.find('.service-key__api-key input').attributes('disabled')
+    ).toBe('disabled');
+  });
+});
