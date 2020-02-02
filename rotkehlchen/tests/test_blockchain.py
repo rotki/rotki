50aa05442ad1ba5dbb17ebeb3e799e19b78d6719
Lefteris Karapetsas <lefteris@refu.co>
2020-02-02 23:30:14 +0100

    Add regression test for 678
    Make sure that we don't have extra blockchain balance queries whenever
    we add/remove blockchain accounts or tokens

--- rotkehlchen/tests/test_blockchain.py
+++ rotkehlchen/tests/test_blockchain.py
@@ -1,11 +1,13 @@
 import operator
 import os
+from unittest.mock import patch
 
 import pytest
 from eth_utils.address import to_checksum_address
 
 from rotkehlchen.constants.assets import A_BTC
 from rotkehlchen.tests.utils.blockchain import DEFAULT_BALANCE
+from rotkehlchen.tests.utils.constants import A_GNO
 from rotkehlchen.typing import SupportedBlockchain
 from rotkehlchen.utils.misc import from_wei
 
@@ -69,3 +71,49 @@ def test_add_remove_ethereum_account_saved_as_checksummed(blockchain):
     )
 
     assert checksummed_addr not in blockchain.accounts.eth
+
+
+def test_add_remove_account_assure_all_balances_not_always_queried(blockchain):
+    """Due to a programming mistake at addition and removal of blockchain accounts
+    after the first time all balances were queried every time. That slowed
+    everything down (https://github.com/rotki/rotki/issues/678).
+
+    This is a regression test for that behaviour
+    """
+    addr1 = '0xe188c6BEBB81b96A65aa20dDB9e2aef62627fa4c'
+    blockchain.add_blockchain_accounts(
+        blockchain=SupportedBlockchain.ETHEREUM,
+        accounts=[addr1],
+    )
+    assert addr1 in blockchain.accounts.eth
+
+    with patch.object(blockchain, 'query_balances') as mock:
+        blockchain.remove_blockchain_accounts(
+            blockchain=SupportedBlockchain.ETHEREUM,
+            accounts=[addr1],
+        )
+
+    assert addr1 not in blockchain.accounts.eth
+    assert mock.call_count == 0, 'blockchain.query_balances() should not have been called'
+
+    addr2 = '0x78a087fCf440315b843632cFd6FDE6E5adcCc2C2'
+    with patch.object(blockchain, 'query_balances') as mock:
+        blockchain.add_blockchain_accounts(
+            blockchain=SupportedBlockchain.ETHEREUM,
+            accounts=[addr2],
+        )
+
+    assert addr2 in blockchain.accounts.eth
+    assert mock.call_count == 0, 'blockchain.query_balances() should not have been called'
+
+    with patch.object(blockchain, 'query_balances') as mock:
+        blockchain.track_new_tokens(new_tokens=[A_GNO])
+
+    assert mock.call_count == 0, 'blockchain.query_balances() should not have been called'
+    assert A_GNO in blockchain.owned_eth_tokens
+
+    with patch.object(blockchain, 'query_balances') as mock:
+        blockchain.remove_eth_tokens(tokens=[A_GNO])
+
+    assert A_GNO not in blockchain.owned_eth_tokens
+    assert mock.call_count == 0, 'blockchain.query_balances() should not have been called'
