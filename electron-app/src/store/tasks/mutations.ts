0ff726ce03d444e1fed94c466df173f419ab3e1e
Konstantinos Paparas <kelsos86@gmail.com>
2020-02-05 00:12:44 +0100

    Updates the locking/unlocking mechanism for tasks

--- electron-app/src/store/tasks/mutations.ts
+++ electron-app/src/store/tasks/mutations.ts
@@ -2,24 +2,30 @@ import { MutationTree } from 'vuex';
 import { defaultState, TaskMap, TaskState } from '@/store/tasks/state';
 import { Task, TaskMeta } from '@/model/task';
 
+const unlockTask = (state: TaskState, taskId: number) => {
+  const locked = [...state.locked];
+  const idIndex = locked.findIndex(value => value === taskId);
+  locked.splice(idIndex, 1);
+  return locked;
+};
+
 export const mutations: MutationTree<TaskState> = {
   add: (state: TaskState, task: Task<TaskMeta>) => {
     const update: TaskMap<TaskMeta> = {};
     update[task.id] = task;
     state.tasks = { ...state.tasks, ...update };
   },
-  processing: (state: TaskState, taskId: number) => {
-    state.processingTasks.push(taskId);
+  lock: (state: TaskState, taskId: number) => {
+    state.locked = [...state.locked, taskId];
+  },
+  unlock: (state: TaskState, taskId: number) => {
+    state.locked = unlockTask(state, taskId);
   },
   remove: (state: TaskState, taskId: number) => {
     const tasks = { ...state.tasks };
     delete tasks[taskId];
     state.tasks = tasks;
-
-    const processing = [...state.processingTasks];
-    const idIndex = processing.findIndex(value => value === taskId);
-    processing.splice(idIndex, 1);
-    state.processingTasks = processing;
+    state.locked = unlockTask(state, taskId);
   },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   reset: (state: TaskState) => {
