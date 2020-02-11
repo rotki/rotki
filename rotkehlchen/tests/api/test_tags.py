beb1ec3fa0466c1c050788b9518191a0b5349d10
Lefteris Karapetsas <lefteris@refu.co>
2020-02-11 01:09:43 +0100

    Add tests for editing tags via the REST API

--- rotkehlchen/tests/api/test_tags.py
+++ rotkehlchen/tests/api/test_tags.py
@@ -137,7 +137,7 @@ def test_add_tag_without_description(
     assert db_response['Public'].serialize() == tag1
 
 
-@pytest.mark.parametrize('verb', [('PUT')])
+@pytest.mark.parametrize('verb', ('PUT', 'PATCH'))
 def test_add_edit_tag_errors(
         rotkehlchen_api_server_with_exchanges,
         verb,
@@ -208,21 +208,23 @@ def test_add_edit_tag_errors(
         'foreground_color': '000000',
     }
     for field in ('background_color', 'foreground_color'):
-        # Missing color
-        tag = model_tag.copy()
-        tag.pop(field)
-        response = requests.request(
-            verb,
-            api_url_for(
-                rotkehlchen_api_server_with_exchanges,
-                "tagsresource",
-            ), json=tag,
-        )
-        assert_error_response(
-            response=response,
-            contained_in_msg=f"{field}': ['Missing data for required field",
-            status_code=HTTPStatus.BAD_REQUEST,
-        )
+
+        if verb == 'PUT':
+            # Missing color
+            tag = model_tag.copy()
+            tag.pop(field)
+            response = requests.request(
+                verb,
+                api_url_for(
+                    rotkehlchen_api_server_with_exchanges,
+                    "tagsresource",
+                ), json=tag,
+            )
+            assert_error_response(
+                response=response,
+                contained_in_msg=f"{field}': ['Missing data for required field",
+                status_code=HTTPStatus.BAD_REQUEST,
+            )
         # Invalid color type
         tag = model_tag.copy()
         tag[field] = 55
@@ -292,3 +294,166 @@ def test_add_edit_tag_errors(
             ),
             status_code=HTTPStatus.BAD_REQUEST,
         )
+
+
+def test_edit_tags(
+        rotkehlchen_api_server_with_exchanges,
+):
+    """Test that editing a tag via the REST API works fine"""
+    rotki = rotkehlchen_api_server_with_exchanges.rest_api.rotkehlchen
+
+    # Add two tags
+    tag1 = {
+        'name': 'Public',
+        'description': 'My public accounts',
+        'background_color': 'ffffff',
+        'foreground_color': '000000',
+    }
+    response = requests.put(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=tag1,
+    )
+    assert_proper_response(response)
+    tag2 = {
+        'name': 'private',
+        'description': 'My private accounts',
+        'background_color': '000000',
+        'foreground_color': 'ffffff',
+    }
+    response = requests.put(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=tag2,
+    )
+    assert_proper_response(response)
+
+    # Now try to edit the second tag and change all its fields
+    edit_tag_data = {
+        'name': 'PrIvAtE',  # notice that name should match case insensitive
+        'description': 'My super private accounts',
+        'background_color': '010101',
+        'foreground_color': 'fefefe',
+    }
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_proper_response(response)
+    data = response.json()
+    assert len(data['result']) == 2
+    assert data['result']['Public'] == tag1
+    tag2 = edit_tag_data
+    tag2['name'] = 'private'
+    assert data['result']['private'] == tag2
+
+    # Now try to edit the second tag and change all but description
+    edit_tag_data = {
+        'name': 'private',
+        'background_color': '020202',
+        'foreground_color': 'fafafa',
+    }
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_proper_response(response)
+    data = response.json()
+    assert len(data['result']) == 2
+    assert data['result']['Public'] == tag1
+    edit_tag_data['description'] = tag2['description']
+    assert data['result']['private'] == edit_tag_data
+    tag2 = data['result']['private']
+
+    # Now try to edit the second tag and change only foreground_color
+    edit_tag_data = {
+        'name': 'private',
+        'foreground_color': 'fbfbfb',
+    }
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_proper_response(response)
+    data = response.json()
+    assert len(data['result']) == 2
+    assert data['result']['Public'] == tag1
+    tag2['foreground_color'] = edit_tag_data['foreground_color']
+    assert data['result']['private'] == tag2
+    tag2 = data['result']['private']
+
+    # Now try to edit the second tag and change only background_color
+    edit_tag_data = {
+        'name': 'private',
+        'background_color': '000000',
+    }
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_proper_response(response)
+    data = response.json()
+    assert len(data['result']) == 2
+    assert data['result']['Public'] == tag1
+    tag2['background_color'] = edit_tag_data['background_color']
+    assert data['result']['private'] == tag2
+
+    # Now try to edit a tag without modifying any field and see it's an error
+    edit_tag_data = {'name': 'private'}
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_error_response(
+        response=response,
+        contained_in_msg='No field was given to edit for tag "private"',
+        status_code=HTTPStatus.BAD_REQUEST,
+    )
+
+    # Now try to edit a non-existing tag
+    edit_tag_data = {
+        'name': 'hello',
+        'background_color': '000000',
+    }
+    response = requests.patch(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ), json=edit_tag_data,
+    )
+    assert_error_response(
+        response=response,
+        contained_in_msg='Tried to edit tag with name "hello" which does not exist',
+        status_code=HTTPStatus.CONFLICT,
+    )
+
+    # Query tags and see that both added/edited tags are in the response
+    response = requests.get(
+        api_url_for(
+            rotkehlchen_api_server_with_exchanges,
+            "tagsresource",
+        ),
+    )
+    assert_proper_response(response)
+    data = response.json()
+    assert len(data['result']) == 2
+    assert data['result']['Public'] == tag1
+    assert data['result']['private'] == tag2
+
+    # And finally also check the DB to be certain
+    db_response = rotki.data.db.get_tags()
+    assert len(db_response) == 2
+    assert db_response['Public'].serialize() == tag1
+    assert db_response['private'].serialize() == tag2
