7cdf4104655aafeccab28b4e57b1eee73a1c3376
Konstantinos Paparas <kelsos86@gmail.com>
2020-02-28 22:32:45 +0100

    Fixes API responses and implements basic DSR display

--- electron-app/src/components/defi/Loans.vue
+++ electron-app/src/components/defi/Loans.vue
@@ -3,21 +3,103 @@
     <v-card-title>
       MakerDAO DSR
     </v-card-title>
-    <v-card-text></v-card-text>
+    <v-card-text>
+      <div class="loans__current-dsr">
+        <span>Current DSR:</span> {{ currentDSR.dp(2) }}%
+      </div>
+      <div class="loans__total-dai">
+        <span>Total DAI:</span> {{ totalDai.dp(floatingPrecision) }}
+      </div>
+      <div v-if="premium">
+        <span>Total DAI earned:</span>
+      </div>
+      <v-select
+        v-model="selection"
+        return-object
+        :items="dsrBalances"
+        item-text="address"
+      ></v-select>
+
+      <div v-if="!!selection">
+        <div class="loans__account__dai-locked">
+          <span>DAI locked:</span> {{ selection.balance.dp(floatingPrecision) }}
+        </div>
+      </div>
+    </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
 import Component from 'vue-class-component';
+import { createNamespacedHelpers } from 'vuex';
+import BigNumber from 'bignumber.js';
+import { DSRBalance } from '@/typing/types';
+import { Zero } from '@/utils/bignumbers';
 
-@Component({})
+const { mapState, mapGetters: mapSessionGetters } = createNamespacedHelpers(
+  'session'
+);
+const { mapGetters } = createNamespacedHelpers('balances');
+
+@Component({
+  computed: {
+    ...mapState(['premium']),
+    ...mapSessionGetters(['floatingPrecision']),
+    ...mapGetters(['currentDSR', 'dsrBalances'])
+  }
+})
 export default class Loans extends Vue {
-  async mount() {
-    await this.$store.dispatch('balances/fetchDSRData');
-    console.log('d')
+  premium!: boolean;
+  floatingPrecision!: number;
+  currentDSR!: BigNumber;
+  dsrBalances!: DSRBalance[];
+  selection: DSRBalance | null = null;
+
+  get totalDai(): BigNumber {
+    return this.dsrBalances.reduce(
+      (sum, { balance }) => sum.plus(balance),
+      Zero
+    );
+  }
+
+  async mounted() {
+    await this.$store.dispatch('balances/fetchDSRBalances');
   }
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.loans {
+  &__current-dsr {
+    font-size: 18px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+
+    & span {
+      font-weight: 500;
+    }
+  }
+  &__total-dai {
+    font-size: 18px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+
+    & span {
+      font-weight: 500;
+    }
+  }
+
+  &__account {
+    &__dai-locked {
+      font-size: 18px;
+      padding-top: 8px;
+      padding-bottom: 8px;
+
+      & span {
+        font-weight: 500;
+      }
+    }
+  }
+}
+</style>
--- electron-app/src/services/converters.ts
+++ electron-app/src/services/converters.ts
@@ -1,26 +1,38 @@
 import { ApiDSRBalances, ApiDSRHistory } from '@/services/types-api';
-import { DSRBalances, DSRHistory } from '@/services/types-model';
+import { DSRBalances, DSRHistory, DSRMovement } from '@/services/types-model';
 import { bigNumberify } from '@/utils/bignumbers';
+import BigNumber from 'bignumber.js';
 
-export function convertDSRBalances(balances: ApiDSRBalances): DSRBalances {
-  const data: DSRBalances = {};
+export function convertDSRBalances({
+  balances,
+  current_dsr
+}: ApiDSRBalances): DSRBalances {
+  const data: { [account: string]: BigNumber } = {};
   for (const account of Object.keys(balances)) {
     data[account] = bigNumberify(balances[account]);
   }
-  return data;
+  return {
+    currentDSR: bigNumberify(current_dsr),
+    balances: data
+  };
 }
 
 export function convertDSRHistory(history: ApiDSRHistory): DSRHistory {
-  const data: DSRHistory = {};
+  const data: { [address: string]: DSRMovement } = {};
   for (const account of Object.keys(history)) {
-    const { movement_type, gain_so_far, amount, block_number } = history[
-      account
-    ];
+    const {
+      movement_type,
+      gain_so_far,
+      amount,
+      block_number,
+      timestamp
+    } = history[account];
     data[account] = {
       movementType: movement_type,
       gainSoFar: bigNumberify(gain_so_far),
       amount: bigNumberify(amount),
-      blockNumber: block_number
+      blockNumber: block_number,
+      timestamp
     };
   }
   return data;
