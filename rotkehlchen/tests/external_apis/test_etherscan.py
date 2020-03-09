a7a66858d5c3787dfb39430317d16fc532c9cbe1
Lefteris Karapetsas <lefteris@refu.co>
2020-03-09 19:09:56 +0100

    Add changelog entry for 772

--- rotkehlchen/tests/external_apis/test_etherscan.py
+++ rotkehlchen/tests/external_apis/test_etherscan.py
@@ -44,7 +44,7 @@ def _handle_killed_greenlets(greenlet: gevent.Greenlet) -> None:
 
 @pytest.mark.skipif(
     'TRAVIS' in os.environ,
-    reason='no real etherscan tests in Travis yet due to API key'
+    reason='no real etherscan tests in Travis yet due to API key',
 )
 def test_maximum_rate_limit_reached(temp_etherscan):
     """
