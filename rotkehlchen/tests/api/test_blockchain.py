7472791e69200faef93833a50446b403d2e80abe
Lefteris Karapetsas <lefteris@refu.co>
2020-03-12 21:09:31 +0100

    Fix tests

--- rotkehlchen/tests/api/test_blockchain.py
+++ rotkehlchen/tests/api/test_blockchain.py
@@ -635,7 +635,12 @@ def test_blockchain_accounts_endpoint_errors(rotkehlchen_api_server, api_port, m
         api_url_for(rotkehlchen_api_server, "blockchainsaccountsresource", blockchain='ETH'),
         json=data,
     )
-    message = "'accounts': ['Not a valid list.'"
+    if method == 'GET':
+        message = "'accounts': ['Not a valid list.'"
+    elif method == 'DELETE':
+        message = 'Given value foo is not a checksummed ethereum address'
+    else:
+        message = "'accounts': {0: {'_schema': ['Invalid input type.'"
     assert_error_response(
         response=response,
         contained_in_msg=message,
@@ -1025,7 +1030,7 @@ def test_edit_blockchain_account_errors(
     ), json=request_data)
     assert_error_response(
         response=response,
-        contained_in_msg="'accounts': ['Not a valid list",
+        contained_in_msg='Invalid input type',
         status_code=HTTPStatus.BAD_REQUEST,
     )
 
