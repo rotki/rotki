20006808e677695a1fc8a4354d63d01ecc607198
Lefteris Karapetsas <lefteris@refu.co>
2020-03-28 00:34:52 +0100

    Add test to detect missing __init__.py files in a directory

---
+++ rotkehlchen/tests/test_no_missing_init.py
@@ -0,0 +1,39 @@
+import os
+from typing import Set
+
+
+def find_directories_with_missing_init(path: str) -> Set[str]:
+    package_dirs: Set[str] = set()
+    py_directories: Set[str] = set()
+    for root, dirs, files in os.walk(path):
+        try:
+            dirs.remove("__pycache__")
+        except ValueError:
+            pass
+
+        for name in files:
+            if name == "__init__.py":
+                package_dirs.add(root)
+            if name.endswith(".py"):
+                py_directories.add(root)
+
+    return py_directories - package_dirs
+
+
+def test_no_missing_init():
+    """Test that there is no directories missing an __init__.py file
+
+    The reason for this is some linting tools like mypy and pylint don't check the
+    directories that are missing the files.
+    """
+
+    rotki_path = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", ".."))
+    print(f"\nScanning {rotki_path}")
+    directories_with_missing_init = find_directories_with_missing_init(rotki_path)
+
+    if directories_with_missing_init:
+        print("The following directories are missing '__init__.py' files:")
+        for directory in directories_with_missing_init:
+            print(directory)
+
+    assert not directories_with_missing_init, "some directories are missing __init__.py files"
