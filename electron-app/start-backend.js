63052e3ba59ce5bfdb4487bae97a333b614ab7f6
Konstantinos Paparas <kelsos86@gmail.com>
2020-03-31 22:48:05 +0200

    Adds script to start python backend using temp directory
    [ui tests]

---
+++ electron-app/start-backend.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const { spawn } = require('child_process');
+
+const tmpDir = os.tmpdir();
+let tempPath = path.join(tmpDir, 'rotki');
+
+process.stdout.write(`Using ${tempPath} to start tests`);
+
+if (!fs.existsSync(tempPath)) {
+  fs.mkdirSync(tempPath);
+} else {
+  const contents = fs.readdirSync(tempPath);
+  contents.forEach(name => {
+    const currentPath = path.join(tempPath, name);
+    if (fs.statSync(currentPath).isDirectory()) {
+      fs.rmdirSync(currentPath, { recursive: true });
+    }
+  });
+}
+
+const args = [
+  '-m',
+  'rotkehlchen',
+  '--api-port',
+  '4242',
+  '--api-cors',
+  'http://localhost:8080',
+  '--data-dir',
+  tempPath
+];
+
+spawn('python', args);
