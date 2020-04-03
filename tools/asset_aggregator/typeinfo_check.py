c74230bed0dc243e2ba32dfec4fe339bfa117f98
Lefteris Karapetsas <lefteris@refu.co>
2020-04-04 00:40:09 +0200

    Add support for HIVE token
    https://coinmarketcap.com/currencies/hive-blockchain/

--- tools/asset_aggregator/typeinfo_check.py
+++ tools/asset_aggregator/typeinfo_check.py
@@ -30,6 +30,7 @@ MANUALLY_CHECKED_TYPES = {
     'FIH': 'ethereum token',
     'FLX': 'ethereum token',
     'FORK-2': 'ethereum token',
+    'HIVE': 'own chain',
     'HKG': 'ethereum token',
     'ITM': 'ethereum token',
     'JOY': 'ethereum token',
