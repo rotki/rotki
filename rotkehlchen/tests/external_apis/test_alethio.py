77f0b3d64d40c103549337bb20528afb0353672e
Lefteris Karapetsas <lefteris@refu.co>
2020-04-04 14:40:55 +0200

    If an address holds more than 10 tokens it's now detected
    Thanks to properly implementing processing of paginated alethio
    queries it's now possible to detect via alethio when an address holds
    more than 10 tokens
    
    Fix #830

--- rotkehlchen/tests/external_apis/test_alethio.py
+++ rotkehlchen/tests/external_apis/test_alethio.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+from rotkehlchen.constants.misc import ZERO
 from rotkehlchen.fval import FVal
 from rotkehlchen.tests.utils.alethio import (
     ALETHIO_MULTIPAGE_TOKEN_BALANCES1,
@@ -15,9 +16,14 @@ def patch_alethio(alethio):
         if 'tokenBalances' in url:
             if '0x9531C059098e3d194fF87FebB587aB07B30B1306' in url:
                 data = ALETHIO_SIMPLE_TOKEN_BALANCES
-            elif url == 'https://api.aleth.io/v1/accounts/0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF/tokenBalances':  # noqa: E501
+            elif '0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF' in url:
                 data = ALETHIO_MULTIPAGE_TOKEN_BALANCES1
-            elif 'f432555e5c898f83fc5f00df631bd9c2801fea289' in url:
+            else:
+                raise AssertionError(
+                    'Unexpected Alethio tokenBalance call during mocking in tests',
+                )
+        elif 'token-balances?filter' in url:
+            if 'f432555e5c898f83fc5f00df631bd9c2801fea289' in url:
                 data = ALETHIO_MULTIPAGE_TOKEN_BALANCES2
             elif 'fac9bb427953ac7fddc562adca86cf42d988047fd' in url:
                 data = ALETHIO_MULTIPAGE_TOKEN_BALANCES3
@@ -41,7 +47,7 @@ def test_get_simple_token_balances(alethio):
 
     assert len(balances) == 3
     assert balances['DAI'] == FVal('5.7')
-    assert balances['SAI'] == FVal('0')
+    assert balances['SAI'] == ZERO
     assert balances['USDC'] == FVal('20')
 
 
@@ -51,7 +57,20 @@ def test_get_multipage_token_balances(alethio):
     with alethio_patch:
         balances = alethio.get_token_balances('0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF')
 
-    assert len(balances) == 22
-    assert balances['DAI'] == FVal('5.7')
-    assert balances['SAI'] == FVal('0')
-    assert balances['USDC'] == FVal('20')
+    assert len(balances) == 13
+    expected_balances = {
+        'TUSD': ZERO,
+        'PKG': FVal('100'),
+        'BAT': ZERO,
+        'REP': ZERO,
+        'WBTC': ZERO,
+        'AMB': FVal('0.1'),
+        'LINK': ZERO,
+        'DAI': FVal('106029.46635018318'),
+        'SAI': FVal('24.974762270881137'),
+        'USDC': FVal('1000'),
+        'WETH': FVal('1139.6876129573913'),
+        'KNC': ZERO,
+        'ZRX': ZERO,
+    }
+    assert balances == expected_balances
