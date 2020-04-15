84fb978a3ce4f05362efa080e6bf1fd0e3ce9cb9
Lefteris Karapetsas <lefteris@refu.co>
2020-04-15 22:31:50 +0200

    Tests for handling permission errors at user creation/login/query

--- rotkehlchen/tests/test_users.py
+++ rotkehlchen/tests/test_users.py
@@ -1,6 +1,11 @@
+import os
 import string
+from pathlib import Path
+
+import pytest
 
 from rotkehlchen.data_handler import DataHandler
+from rotkehlchen.errors import SystemPermissionError
 
 
 def _user_creation_and_login(username, password, data_dir, msg_aggregator):
@@ -52,3 +57,35 @@ def test_user_password_with_all_ascii(data_dir, function_scope_messages_aggregat
         data_dir=data_dir,
         msg_aggregator=function_scope_messages_aggregator,
     )
+
+
+@pytest.mark.parametrize('use_clean_caching_directory', [True])
+def test_users_query_permission_error(data_dir, function_scope_messages_aggregator):
+    not_allowed_dir = os.path.join(data_dir, 'notallowed')
+    allowed_user_dir = os.path.join(data_dir, 'allowed_user')
+    os.mkdir(not_allowed_dir)
+    os.chmod(not_allowed_dir, 0o200)
+    os.mkdir(allowed_user_dir)
+    Path(Path(allowed_user_dir) / 'rotkehlchen.db').touch()
+    handler = DataHandler(
+        data_directory=data_dir,
+        msg_aggregator=function_scope_messages_aggregator,
+    )
+    assert handler.get_users() == {'allowed_user': 'loggedout'}
+    # Change permissions back to that pytest cleanup can clean it
+    os.chmod(not_allowed_dir, 0o777)
+
+
+@pytest.mark.parametrize('use_clean_caching_directory', [True])
+def test_new_user_permission_error(data_dir, function_scope_messages_aggregator):
+    not_allowed_dir = os.path.join(data_dir, 'notallowed')
+    os.mkdir(not_allowed_dir)
+    os.chmod(not_allowed_dir, 0o200)
+    handler = DataHandler(
+        data_directory=not_allowed_dir,
+        msg_aggregator=function_scope_messages_aggregator,
+    )
+    with pytest.raises(SystemPermissionError):
+        handler.unlock(username='someuser', password='123', create_new=True)
+    # Change permissions back to that pytest cleanup can clean it
+    os.chmod(not_allowed_dir, 0o777)
