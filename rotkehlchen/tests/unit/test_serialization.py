b2cc224e11fddd8b178a9a4f3cf19fb74d5f9138
Lefteris Karapetsas <lefteris@refu.co>
2020-04-15 15:04:12 +0200

    Add test for serialize/deserialization of location also including DB

--- rotkehlchen/tests/unit/test_serialization.py
+++ rotkehlchen/tests/unit/test_serialization.py
@@ -1,10 +1,11 @@
 import pytest
 
+from rotkehlchen.balances.manual import ManuallyTrackedBalance, add_manually_tracked_balances
 from rotkehlchen.constants.assets import A_BTC, A_ETH
 from rotkehlchen.errors import DeserializationError
 from rotkehlchen.fval import FVal
-from rotkehlchen.serialization.deserialize import deserialize_trade_type
-from rotkehlchen.typing import TradeType
+from rotkehlchen.serialization.deserialize import deserialize_location, deserialize_trade_type
+from rotkehlchen.typing import Location, TradeType
 from rotkehlchen.utils.serialization import (
     pretty_json_dumps,
     rkl_decode_value,
@@ -76,3 +77,30 @@ def test_deserialize_trade_type():
 
     with pytest.raises(DeserializationError):
         deserialize_trade_type(1)
+
+
+@pytest.mark.parametrize('use_clean_caching_directory', [True])
+def test_deserialize_location(database):
+    balances = []
+    for idx, data in enumerate(Location):
+        assert deserialize_location(str(data)) == data
+        balances.append(ManuallyTrackedBalance(
+            asset=A_BTC,
+            label='Test' + str(idx),
+            amount=FVal(1),
+            location=data,
+            tags=None,
+        ))
+
+    with pytest.raises(DeserializationError):
+        deserialize_location('dsadsad')
+
+    with pytest.raises(DeserializationError):
+        deserialize_location(15)
+
+    # Also write and read each location to DB to make sure that
+    # location.serialize_for_db() and deserialize_location_from_db work fine
+    add_manually_tracked_balances(database, balances)
+    balances = database.get_manually_tracked_balances()
+    for data in Location:
+        assert data in (x.location for x in balances)
