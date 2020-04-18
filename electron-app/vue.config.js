070b674da83e5513677b816943c56b29a4358c84
Konstantinos Paparas <kelsos86@gmail.com>
2020-04-19 01:00:28 +0200

    Fixes sourcemaps to enable coverage for Vue SFCs
    [ui tests]

--- electron-app/vue.config.js
+++ electron-app/vue.config.js
@@ -1,7 +1,36 @@
 // vue.config.js
 module.exports = {
-  configureWebpack: {
-    devtool: 'source-map'
+  configureWebpack: config => {
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.CODE_COVERAGE === 'true'
+    ) {
+      config.devtool = 'eval-source-map';
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        'webpack:///[resource-path]?[hash]';
+      config.output.devtoolModuleFilenameTemplate = info => {
+        const isVue = info.resourcePath.match(/\.vue$/);
+        const isScript = info.query.match(/type=script/);
+        const hasModuleId = info.moduleId !== '';
+
+        // Detect generated files, filter as webpack-generated
+        if (
+          // Must result from vue-loader
+          isVue &&
+          // Must not be 'script' files (enough for chrome), or must have moduleId (firefox)
+          (!isScript || hasModuleId)
+        ) {
+          let pathParts = info.resourcePath.split('/');
+          const baseName = pathParts[pathParts.length - 1];
+          // prepend 'generated-' to filename as well, so it's easier to find desired files via Ctrl+P
+          pathParts.splice(-1, 1, `generated-${baseName}`);
+          return `webpack-generated:///${pathParts.join('/')}?${info.hash}`;
+        }
+
+        // If not generated, filter as webpack-vue
+        return `webpack-vue:///${info.resourcePath}`;
+      };
+    }
   },
   pluginOptions: {
     electronBuilder: {
