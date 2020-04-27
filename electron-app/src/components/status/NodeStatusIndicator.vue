1e00d0bf0351bdfec3d1d7e7729a32632de8e00e
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-04-27 12:31:59 +0200

    More issues fixed (previous commit broke UI tests)

--- electron-app/src/components/status/NodeStatusIndicator.vue
+++ electron-app/src/components/status/NodeStatusIndicator.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-menu class="node-status-indicator" transition="slide-y-transition" bottom>
+  <v-menu
+    class="node-status-indicator"
+    transition="slide-y-transition"
+    bottom
+    z-index="105"
+  >
     <template #activator="{ on }">
       <v-btn color="primary" dark icon text v-on="on">
         <v-icon
