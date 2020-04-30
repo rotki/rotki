abac32e992201bf1e8bb665bc47b457cc5a0f155
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-04-30 18:38:48 +0200

    Updates unit tests for AmountDisplay
    * Fixes unit tests for change from fiat->fiatCurrency prop
    * Adds unit tests for scrambleData session flag
    
    [ui tests]

--- electron-app/tests/unit/components/display/amount-display.spec.ts
+++ electron-app/tests/unit/components/display/amount-display.spec.ts
@@ -5,12 +5,12 @@ import Vuetify from 'vuetify';
 import AmountDisplay from '@/components/display/AmountDisplay.vue';
 import { currencies } from '@/data/currencies';
 import store from '@/store/store';
-import { bigNumberify } from '@/utils/bignumbers';
+import { Zero, bigNumberify } from '@/utils/bignumbers';
 import '@/filters';
 
 Vue.use(Vuetify);
 
-function createWrapper(value: BigNumber, fiat: boolean = false) {
+function createWrapper(value: BigNumber, amount: BigNumber, fiatCurrency: string | null) {
   let vuetify: typeof Vuetify = new Vuetify();
   return mount(AmountDisplay, {
     store,
@@ -18,7 +18,8 @@ function createWrapper(value: BigNumber, fiat: boolean = false) {
     stubs: ['v-tooltip'],
     propsData: {
       value,
-      fiat
+      fiatCurrency,
+      amount
     }
   });
 }
@@ -26,27 +27,68 @@ function createWrapper(value: BigNumber, fiat: boolean = false) {
 describe('AmountDisplay.vue', () => {
   let wrapper: Wrapper<AmountDisplay>;
 
-  beforeEach(() => {
-    store.commit('session/defaultCurrency', currencies[1]);
-    store.commit('balances/usdToFiatExchangeRates', { EUR: 1.2 });
-    store.commit('session/settings', { floatingPrecision: 2 });
+  describe('Common case', () => {
+    beforeEach(() => {
+      store.commit('session/defaultCurrency', currencies[1]);
+      store.commit('balances/usdToFiatExchangeRates', { EUR: 1.2 });
+      store.commit('session/settings', { floatingPrecision: 2 });
+    });
+
+    afterEach(() => {
+      store.commit('session/reset');
+    });
+
+    test('displays amount converted to selected fiat currency', async () => {
+      wrapper = createWrapper(bigNumberify(1.20440001), Zero, 'USD');
+      expect(wrapper.find('.amount-display__value').text()).toMatch('1.45');
+      expect(wrapper.find('.amount-display__full-value').exists()).toBe(false);
+    });
+
+    test('displays amount converted to selected fiat currency (does not double-convert)', async () => {
+      wrapper = createWrapper(bigNumberify(1.20440001), bigNumberify(1.20440001), 'EUR');
+      expect(wrapper.find('.amount-display__value').text()).toMatch('1.20');
+      expect(wrapper.find('.amount-display__full-value').exists()).toBe(false);
+    });
+
+    test('displays amount as it is without fiat conversion', async () => {
+      wrapper = createWrapper(bigNumberify(1.20540001), Zero, null);
+      expect(wrapper.find('.amount-display__value').text()).toMatch('1.21');
+      expect(wrapper.find('.amount-display__full-value').text()).toMatch(
+        '1.20540001'
+      );
+    });
   });
 
-  afterEach(() => {
-    store.commit('session/reset');
-  });
+  describe('Scramble data', () => {
+    beforeEach(() => {
+      store.commit('session/defaultCurrency', currencies[1]);
+      store.commit('balances/usdToFiatExchangeRates', { EUR: 1.2 });
+      store.commit('session/settings', { floatingPrecision: 2 });
+      store.commit('session/scrambleData', true);
+    });
 
-  test('displays amount converted to selected currency on fiat flag', async () => {
-    wrapper = createWrapper(bigNumberify(1.20440001), true);
-    expect(wrapper.find('.amount-display__value').text()).toMatch('1.45');
-    expect(wrapper.find('.amount-display__full-value').exists()).toBe(false);
-  });
+    afterEach(() => {
+      store.commit('session/reset');
+    });
 
-  test('displays amount as it is without the fiat flag', async () => {
-    wrapper = createWrapper(bigNumberify(1.20540001));
-    expect(wrapper.find('.amount-display__value').text()).toMatch('1.21');
-    expect(wrapper.find('.amount-display__full-value').text()).toMatch(
-      '1.20540001'
-    );
+    test('displays amount converted to selected fiat currency as scrambled', async () => {
+      wrapper = createWrapper(bigNumberify(1.20440001), Zero, 'USD');
+      expect(wrapper.find('.amount-display__value').text()).not.toMatch('1.45');
+      expect(wrapper.find('.amount-display__full-value').exists()).toBe(false);
+    });
+
+    test('displays amount converted to selected fiat currency (does not double-convert) as scrambled', async () => {
+      wrapper = createWrapper(bigNumberify(1.20440001), bigNumberify(1.20440001), 'EUR');
+      expect(wrapper.find('.amount-display__value').text()).not.toMatch('1.20');
+      expect(wrapper.find('.amount-display__full-value').exists()).toBe(false);
+    });
+
+    test('displays amount as it is without fiat conversion as scrambled', async () => {
+      wrapper = createWrapper(bigNumberify(1.20540001), Zero, null);
+      expect(wrapper.find('.amount-display__value').text()).not.toMatch('1.21');
+      expect(wrapper.find('.amount-display__full-value').text()).not.toMatch(
+        '1.20540001'
+      );
+    });
   });
 });
