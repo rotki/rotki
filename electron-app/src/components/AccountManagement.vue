edcdb8f21ceffc6647274ba69972bc2b7e70467b
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-05-03 00:44:37 +0200

    Improvements to Welcome Screen
    * Added the flying rotki background to the Welcome Screen
    * Cleaned up some of the component code between Sign In and Create New
    Account screens
    * Added transitions between the two screens
    * Added "rotki" header to the Welcome Dialog
    * De-emphasized "Create New Account" button in Sign In screen
    * De-emphasized "Cancel" button in Create New Account screen
    * Moved tooltip for "Enable Premium" from the checkbox to a new
    (i) info icon

--- electron-app/src/components/AccountManagement.vue
+++ electron-app/src/components/AccountManagement.vue
@@ -1,46 +1,73 @@
 <template>
   <div>
-    <v-overlay v-if="!connected" class="account_management__loading">
-      <v-row align="center" justify="center">
-        <v-col cols="12" class="account_management__loading__content">
-          <v-progress-circular indeterminate size="72"></v-progress-circular>
-          <span class="account_management__loading__content__text">
-            Please wait...
-          </span>
-        </v-col>
-      </v-row>
-    </v-overlay>
-    <div class="account_management__privacy_notice">
-      <v-alert
-        id="privacy_notice__message"
-        outlined
-        dense
-        color="primary"
-        class="account_management__privacy_notice__message"
-      >
-        <div>Rotki is a local application that respects your privacy.</div>
-        <div>
-          Rotki accounts are encrypted using your password and saved in your
-          local filesystem.
+    <v-overlay class="account_management__loading">
+      <v-dialog :value="true && !premiumVisible" persistent max-width="450">
+        <v-card class="rotki-welcome pb-6">
+          <div
+            class="pt-6 pb-3 display-3 font-weight-black primary white--text"
+          >
+            <span class="px-6">rotki</span>
+            <span style="display: block;" class="pl-6 mb-3 caption">
+              the opensource portfolio manager that respects your privacy
+            </span>
+          </div>
+          <v-row
+            v-if="!connected"
+            no-gutters
+            align="center"
+            justify="center"
+            class="my-3"
+          >
+            <v-col
+              class="account_management__loading__content grey-darken-2--text"
+            >
+              <span
+                class="account_management__loading__content__text my-3 pb-6"
+              >
+                Connecting to rotki backend
+              </span>
+              <v-progress-circular
+                color="grey"
+                indeterminate
+                size="56"
+              ></v-progress-circular>
+            </v-col>
+          </v-row>
+          <v-slide-y-transition>
+            <login
+              :loading="loading"
+              :displayed="!accountCreation && connected"
+              :sync-conflict="syncConflict"
+              @login="login($event)"
+              @new-account="accountCreation = true"
+            ></login>
+          </v-slide-y-transition>
+          <v-slide-y-transition>
+            <create-account
+              :loading="loading"
+              :displayed="accountCreation && connected"
+              @cancel="accountCreation = false"
+              @confirm="createAccount($event)"
+            ></create-account>
+          </v-slide-y-transition>
+        </v-card>
+        <div class="account_management__privacy_notice">
+          <v-alert
+            id="privacy_notice__message"
+            outlined
+            dense
+            color="primary"
+            class="account_management__privacy_notice__message"
+          >
+            <div>rotki is a local application that respects your privacy.</div>
+            <div>
+              rotki accounts are encrypted using your password and saved in your
+              local filesystem.
+            </div>
+          </v-alert>
         </div>
-      </v-alert>
-    </div>
-
-    <login
-      v-if="connected"
-      :displayed="!message && !logged && !accountCreation"
-      :loading="loading"
-      :sync-conflict="syncConflict"
-      @login="login($event)"
-      @new-account="accountCreation = true"
-    ></login>
-
-    <create-account
-      :loading="loading"
-      :displayed="!message && !logged && accountCreation"
-      @cancel="accountCreation = false"
-      @confirm="createAccount($event)"
-    ></create-account>
+      </v-dialog>
+    </v-overlay>
 
     <message-overlay
       class="account_management__premium"
@@ -53,7 +80,7 @@
         Upgrade to Premium
       </template>
 
-      Rotki is open source software and needs your support! Please consider
+      rotki is open source software and needs your support! Please consider
       upgrading to premium by purchasing a premium subscription. This way you
       can help us further develop the software and you can also enjoy additional
       premium-only features.
@@ -164,6 +191,32 @@ export default class AccountManagement extends Vue {
 </script>
 
 <style lang="scss">
+@keyframes scrollLarge {
+  0% {
+    transform: rotate(-13deg) translateY(0);
+  }
+  100% {
+    transform: rotate(-13deg) translateY(-1200px);
+  }
+}
+
+.account_management__loading {
+  height: 400%;
+  width: 400%;
+  top: -25%;
+  left: -100%;
+  background-color: #42210b;
+  background: url(~@/assets/images/rotkipattern2.png);
+  -webkit-animation-name: scrollLarge;
+  animation-name: scrollLarge;
+  -webkit-animation-duration: 35s;
+  animation-duration: 35s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+}
+
 .v-overlay {
   z-index: 200 !important;
 }
@@ -188,9 +241,14 @@ export default class AccountManagement extends Vue {
   }
   &__privacy_notice {
     width: 100%;
+    max-width: 600px;
     position: absolute;
+    left: 0;
+    right: 0;
+    margin-right: auto;
+    margin-left: auto;
     bottom: 20px;
-    z-index: 8100;
+    z-index: -1;
     align-items: center;
     display: flex;
     flex-direction: column;
