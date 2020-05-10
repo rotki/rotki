c1301e991032d83e20b78222ded9e88c9be02fbd
Aleister Coinley <coinyon@uberhax0r.de>
2020-05-10 15:40:41 +0200

    Import/export missing exchanges in submodule

--- rotkehlchen/exchanges/__init__.py
+++ rotkehlchen/exchanges/__init__.py
@@ -1,6 +1,9 @@
 from rotkehlchen.exchanges.binance import Binance  # noqa: F401
 from rotkehlchen.exchanges.bitmex import Bitmex  # noqa: F401
 from rotkehlchen.exchanges.bittrex import Bittrex  # noqa: F401
+from rotkehlchen.exchanges.coinbase import Coinbase  # noqa: F401
+from rotkehlchen.exchanges.coinbasepro import Coinbasepro  # noqa: F401
 from rotkehlchen.exchanges.exchange import ExchangeInterface  # noqa: F401
+from rotkehlchen.exchanges.gemini import Gemini  # noqa: F401
 from rotkehlchen.exchanges.kraken import Kraken  # noqa: F401
 from rotkehlchen.exchanges.poloniex import Poloniex  # noqa: F401
