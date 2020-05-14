f18c90c6c5f54385a4e8e3d4cc981081570d36e3
Lefteris Karapetsas <lefteris@refu.co>
2020-05-14 22:45:36 +0200

    Use a CSV Dict reader, map the fields properly and use new CT format
    Fix #995 by adjusting the import of cointracking data to the new
    format they have as shown in the bug report.

--- rotkehlchen/data/importer.py
+++ rotkehlchen/data/importer.py
@@ -1,6 +1,7 @@
 import csv
+from itertools import count
 from pathlib import Path
-from typing import List
+from typing import Any, Dict
 
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.constants.assets import A_USD
@@ -14,6 +15,12 @@ from rotkehlchen.serialization.deserialize import (
     deserialize_timestamp_from_date,
 )
 from rotkehlchen.typing import AssetAmount, Fee, Location, Price, TradePair, TradeType
+
+
+def remap_header(fieldnames):
+    cur_count = count(1)
+    mapping = {1: 'Buy', 2: 'Sell', 3: 'Fee'}
+    return [f'Cur.{mapping[next(cur_count)]}' if f.startswith('Cur.') else f for f in fieldnames]
 
 
 class UnsupportedCointrackingEntry(Exception):
@@ -65,7 +72,7 @@ class DataImporter():
     def __init__(self, db: DBHandler) -> None:
         self.db = db
 
-    def _consume_cointracking_entry(self, csv_row: List[str]) -> None:
+    def _consume_cointracking_entry(self, csv_row: Dict[str, Any]) -> None:
         """Consumes a cointracking entry row from the CSV and adds it into the database
         Can raise:
             - DeserializationError if something is wrong with the format of the expected values
@@ -73,18 +80,20 @@ class DataImporter():
             - IndexError if the CSV file is corrupt
             - UnknownAsset if one of the assets founds in the entry are not supported
         """
-        row_type = csv_row[1]  # Type
+        row_type = csv_row["Type"]
         timestamp = deserialize_timestamp_from_date(
-            date=csv_row[9],
+            date=csv_row["Date"],
             formatstr='%d.%m.%Y %H:%M',
             location='cointracking.info',
         )
-        notes = csv_row[8]
-        location = exchange_row_to_location(csv_row[6])
+        notes = csv_row["Comment"]
+        location = exchange_row_to_location(csv_row["Exchange"])
+
+        # TODO: Fees
 
         if row_type == 'Gift/Tip' or row_type == 'Trade':
-            base_asset = Asset(csv_row[3])
-            quote_asset = None if csv_row[5] == '' else Asset(csv_row[5])
+            base_asset = Asset(csv_row['Cur.Buy'])
+            quote_asset = None if csv_row['Cur.Sell'] == '' else Asset(csv_row['Cur.Sell'])
             if not quote_asset and row_type != 'Gift/Tip':
                 raise DeserializationError('Got a trade entry with an empty quote asset')
 
@@ -92,9 +101,9 @@ class DataImporter():
                 # Really makes no difference as this is just a gift and the amount is zero
                 quote_asset = A_USD
             pair = TradePair(f'{base_asset.identifier}_{quote_asset.identifier}')
-            base_amount_bought = deserialize_asset_amount(csv_row[2])
-            if csv_row[4] != '-':
-                quote_amount_sold = deserialize_asset_amount(csv_row[4])
+            base_amount_bought = deserialize_asset_amount(csv_row["Buy"])
+            if csv_row["Sell"] != '-':
+                quote_amount_sold = deserialize_asset_amount(csv_row["Sell"])
             else:
                 quote_amount_sold = AssetAmount(ZERO)
             rate = Price(quote_amount_sold / base_amount_bought)
@@ -115,11 +124,11 @@ class DataImporter():
         elif row_type == 'Deposit' or row_type == 'Withdrawal':
             category = deserialize_asset_movement_category(row_type.lower())
             if category == AssetMovementCategory.DEPOSIT:
-                amount = deserialize_asset_amount(csv_row[2])
-                asset = Asset(csv_row[3])
+                amount = deserialize_asset_amount(csv_row["Buy"])
+                asset = Asset(csv_row["Cur.Buy"])
             else:
-                amount = deserialize_asset_amount(csv_row[4])
-                asset = Asset(csv_row[5])
+                amount = deserialize_asset_amount(csv_row["Sell"])
+                asset = Asset(csv_row["Cur.Sell"])
 
             asset_movement = AssetMovement(
                 location=location,
@@ -139,12 +148,12 @@ class DataImporter():
             )
 
     def import_cointracking_csv(self, filepath: Path) -> None:
-        with open(filepath, 'r') as csvfile:
+        with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.reader(csvfile, delimiter=',', quotechar='"')
-            next(data)  # skip header row
+            header = remap_header(next(data))
             for row in data:
                 try:
-                    self._consume_cointracking_entry(row)
+                    self._consume_cointracking_entry(dict(zip(header, row)))
                 except UnknownAsset as e:
                     self.db.msg_aggregator.add_warning(
                         f'During cointracking CSV import found action with unknown '
@@ -153,8 +162,8 @@ class DataImporter():
                     continue
                 except IndexError:
                     self.db.msg_aggregator.add_warning(
-                        f'During cointracking CSV import found entry with '
-                        f'unexpected number of columns',
+                        'During cointracking CSV import found entry with '
+                        'unexpected number of columns',
                     )
                     continue
                 except DeserializationError as e:
