2c565d152841a8eba46648991d3c26cced7b0349
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-05-27 00:05:31 +0200

    Various improvements/fixes to vault UI
    * Adds a 'loading' property to the AmountDisplay component which displays a small skeleton loader
    * Some fixes to not get errors due to accessing methods/properties of objects not yet loaded
    * Grouped common things together
    * Liquidation & Liquidation Events are now in one larger vertical card

--- frontend/app/src/components/defi/Vault.vue
+++ frontend/app/src/components/defi/Vault.vue
@@ -3,11 +3,8 @@
     <v-col cols="12">
       <v-row>
         <v-col class="vault__header">
-          <v-btn color="primary" @click="premium2 = !premium2">
-            Toggle Premium
-          </v-btn>
           <div class="vault__header__identifier">
-            Vault #{{ vault.identifier }} ({{ vault.name }})
+            Vault #{{ vault.identifier }} ({{ vault.collateralType }})
           </div>
           <div class="vault__header__owner secondary--text text--lighten-2">
             Owned by:
@@ -43,54 +40,21 @@
                 ></amount-display>
               </div>
             </div>
-            <v-divider class="my-3"></v-divider>
+            <v-divider class="my-4"></v-divider>
             <div class="d-flex justify-space-between">
               <div>
                 Current ratio
               </div>
               <div>
-                {{ vault.collateralizationRatio }}
+                {{
+                  vault.collateralizationRatio
+                    ? vault.collateralizationRatio
+                    : 'NaN'
+                }}
               </div>
             </div>
           </stat-card>
-        </v-col>
-        <v-col cols="12" md="6">
-          <stat-card title="Liquidation">
-            <div class="d-flex justify-space-between">
-              <div>
-                Liquidation price
-              </div>
-              <div>
-                <amount-display
-                  fiat-currency="USD"
-                  show-currency="ticker"
-                  :value="vault.liquidationPrice"
-                ></amount-display>
-              </div>
-            </div>
-            <v-divider class="my-3"></v-divider>
-            <div class="d-flex justify-space-between font-weight-light">
-              <div>
-                Minimum ratio
-              </div>
-              <div>
-                {{ vault.liquidationRatio }}
-              </div>
-            </div>
-            <div class="d-flex justify-space-between font-weight-light">
-              <div>
-                Stability fee
-              </div>
-              <div>
-                XX.XX%
-              </div>
-            </div>
-          </stat-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12" md="6">
-          <stat-card title="Debt">
+          <stat-card class="vault__debt mt-5" title="Debt">
             <div class="d-flex justify-space-between">
               <div>
                 Outstanding debt
@@ -107,19 +71,33 @@
                 xx.xx XYZ
               </div>
             </div>
-            <v-divider class="my-3"></v-divider>
+            <v-divider class="my-4"></v-divider>
+            <div
+              class="vault__debt__stability-fee d-flex justify-space-between mb-2"
+            >
+              <div>
+                Stability fee
+              </div>
+              <div>
+                {{ vault.stabilityFee }}
+              </div>
+            </div>
             <div class="d-flex justify-space-between">
               <div>
                 Total interest owed
               </div>
-              <div v-if="premium2 && vault.totalInterestOwed">
+              <div v-if="premium">
                 <amount-display
-                  v-if="!vault.totalInterestOwed.isNegative()"
+                  v-if="
+                    vault.totalInterestOwed &&
+                    !vault.totalInterestOwed.isNegative()
+                  "
                   :value="vault.totalInterestOwed"
                   asset="DAI"
                 ></amount-display>
                 <amount-display
                   v-else
+                  :loading="typeof vault.totalInterestOwed === 'undefined'"
                   :value="'0.00'"
                   asset="DAI"
                 ></amount-display>
@@ -128,65 +106,113 @@
                 <premium-lock></premium-lock>
               </div>
             </div>
-            <div v-if="premium2" class="text-right">
+            <div v-if="premium" class="text-right">
               <div class="font-weight-light">
                 xx.xx XYZ
               </div>
             </div>
           </stat-card>
         </v-col>
-        <v-col
-          v-if="
-            vault.totalLiquidatedAmount && vault.totalLiquidatedAmount.gt(0)
-          "
-          cols="12"
-          md="6"
-        >
-          <stat-card :locked="!premium2" title="Liquidation Events">
-            <div class="d-flex justify-space-between font-weight-light">
-              <div>
-                Liquidated collateral
+        <v-col cols="12" md="6">
+          <stat-card title="Liquidation" class="vault__liquidation">
+            <div class="vault__liquidation__upper pb-5">
+              <div class="d-flex justify-space-between">
+                <div>
+                  Liquidation price
+                </div>
+                <div>
+                  <amount-display
+                    fiat-currency="USD"
+                    show-currency="ticker"
+                    :value="vault.liquidationPrice"
+                  ></amount-display>
+                </div>
               </div>
-              <div>
-                <amount-display
-                  :value="vault.totalLiquidatedAmount"
-                  :asset="vault.collateralAsset"
-                ></amount-display>
-              </div>
-            </div>
-            <div class="d-flex justify-end font-weight-light">
-              <div>
-                <amount-display
-                  :value="vault.totalLiquidatedUsd"
-                  show-currency="ticker"
-                  fiat-currency="USD"
-                ></amount-display>
+              <v-divider class="my-4"></v-divider>
+              <div class="d-flex justify-space-between font-weight-light">
+                <div>
+                  Minimum ratio
+                </div>
+                <div>
+                  {{ vault.liquidationRatio }}
+                </div>
               </div>
             </div>
-            <div class="d-flex justify-space-between font-weight-light">
-              <div>
-                Oustanding debt at liquidation
-              </div>
-              <div>
-                <amount-display
-                  :value="vault.totalInterestOwed"
-                  asset="DAI"
-                ></amount-display>
-              </div>
-            </div>
-            <v-divider class="my-3"></v-divider>
-            <div class="d-flex justify-space-between">
-              <div>
-                Total value lost
-              </div>
-              <div>
-                <amount-display
-                  :value="
-                    vault.totalLiquidatedUsd.plus(vault.totalInterestOwed)
+            <div>
+              <span class="vault__liquidation__liquidation-events__header">
+                Liquidation Events
+              </span>
+              <v-skeleton-loader
+                v-if="premium"
+                :loading="typeof vault.totalLiquidatedAmount === 'undefined'"
+                class="mx-auto pt-3"
+                max-width="450"
+                type="paragraph"
+              >
+                <div
+                  v-if="
+                    vault.totalLiquidatedAmount &&
+                    vault.totalLiquidatedAmount.gt(0)
                   "
-                  show-currency="ticker"
-                  fiat-currency="USD"
-                ></amount-display>
+                  class="vault__liquidation-events__content"
+                >
+                  <div
+                    class="vault__liquidation-events__content__liquidated-collateral mb-2"
+                  >
+                    <div class="d-flex justify-space-between">
+                      <div>
+                        Liquidated collateral
+                      </div>
+                      <div>
+                        <amount-display
+                          :value="vault.totalLiquidatedAmount"
+                          :asset="vault.collateralAsset"
+                        ></amount-display>
+                      </div>
+                    </div>
+                    <div class="d-flex justify-end font-weight-light">
+                      <div>
+                        <amount-display
+                          :value="vault.totalLiquidatedUsd"
+                          show-currency="ticker"
+                          fiat-currency="USD"
+                        ></amount-display>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="d-flex justify-space-between">
+                    <div>
+                      Outsanding debt at liquidation
+                    </div>
+                    <div>
+                      <amount-display
+                        :value="vault.totalInterestOwed"
+                        asset="DAI"
+                      ></amount-display>
+                    </div>
+                  </div>
+                  <v-divider class="my-4"></v-divider>
+                  <div class="d-flex justify-space-between">
+                    <div>
+                      Total value lost
+                    </div>
+                    <div>
+                      <amount-display
+                        :value="
+                          vault.totalLiquidatedUsd.plus(vault.totalInterestOwed)
+                        "
+                        show-currency="ticker"
+                        fiat-currency="USD"
+                      ></amount-display>
+                    </div>
+                  </div>
+                </div>
+                <div v-else>
+                  No liquidation events have occurred for this vault.
+                </div>
+              </v-skeleton-loader>
+              <div v-else class="text-right">
+                <premium-lock></premium-lock>
               </div>
             </div>
           </stat-card>
@@ -248,7 +274,6 @@ export default class Vault extends Vue {
   vault!: MakerDAOVault | MakerDAOVaultModel | null;
 
   premium!: boolean;
-  premium2: boolean = true;
   dateDisplayFormat!: string;
 
   openTransaction(url: string) {
@@ -263,6 +288,30 @@ export default class Vault extends Vue {
     &__identifier {
       font-size: 24px;
       font-weight: bold;
+    }
+  }
+
+  &__liquidation {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    &__upper {
+      min-height: 100px;
+      height: 45%;
+    }
+
+    ::v-deep .v-card__text {
+      display: flex;
+      flex-direction: column;
+    }
+
+    &__liquidation-events {
+      &__header {
+        font-size: 20px;
+        font-weight: 500;
+        color: rgba(0, 0, 0, 0.87);
+      }
     }
   }
 }
