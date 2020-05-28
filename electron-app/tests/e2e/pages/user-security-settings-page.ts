ca48bc568bc8df33058cd0a91e1342db6b8cf988
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-05-28 11:16:47 +0200

    Revert "Moves files for PR into new folder structure"
    This reverts commit 2788a7ea1326f5b7d9afbf3af98429f0455e306e.

---
+++ electron-app/tests/e2e/pages/user-security-settings-page.ts
@@ -0,0 +1,38 @@
+export class UserSecuritySettingsPage {
+  visit() {
+    cy.get('.user-dropdown').click();
+    cy.get('.user-dropdown__settings').click();
+    cy.get('a.settings__usersecurity').click();
+  }
+
+  changePassword(currentPassword: string, newPassword: string) {
+    cy.get('.user-security-settings__fields__current-password input').clear();
+    cy.get('.user-security-settings__fields__current-password input').type(
+      currentPassword
+    );
+
+    cy.get('.user-security-settings__fields__new-password input').clear();
+    cy.get('.user-security-settings__fields__new-password input').type(
+      newPassword
+    );
+
+    cy.get(
+      '.user-security-settings__fields__new-password-confirm input'
+    ).clear();
+    cy.get('.user-security-settings__fields__new-password-confirm input').type(
+      newPassword
+    );
+
+    cy.get('.user-security-settings__buttons__change-password').click();
+  }
+
+  confirmSuccess() {
+    cy.get('.message-dialog__title').should('include.text', 'Success');
+    cy.get('.message-dialog__buttons__confirm').click();
+  }
+
+  confirmFailure() {
+    cy.get('.message-dialog__title').should('include.text', 'Settings Error');
+    cy.get('.message-dialog__buttons__confirm').click();
+  }
+}
