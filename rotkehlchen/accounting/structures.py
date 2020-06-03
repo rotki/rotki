4094488980a4361f0c5d6be0c19ba29baacbe369
Lefteris Karapetsas <lefteris@refu.co>
2020-06-03 16:39:35 +0200

    Move Balance structure to a generic position and more
    Also add some tests for the structure and enable addition of balances
    and balances with their dict serialization

--- rotkehlchen/accounting/structures.py
+++ rotkehlchen/accounting/structures.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Any, Dict
 
 from rotkehlchen.assets.asset import Asset
+from rotkehlchen.constants.misc import ZERO
+from rotkehlchen.errors import InputError
 from rotkehlchen.fval import FVal
 from rotkehlchen.typing import Timestamp
 
@@ -31,3 +34,42 @@ class DefiEvent:
     event_type: DefiEventType
     asset: Asset
     amount: FVal
+
+
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
+class Balance:
+    amount: FVal = ZERO
+    usd_value: FVal = ZERO
+
+    def serialize(self) -> Dict[str, str]:
+        return {'amount': str(self.amount), 'usd_value': str(self.usd_value)}
+
+    def to_dict(self) -> Dict[str, FVal]:
+        return {'amount': self.amount, 'usd_value': self.usd_value}
+
+    def __add__(self, other: Any) -> 'Balance':
+        other = _evaluate_balance_input(other, 'addition')
+        return Balance(
+            amount=self.amount + other.amount,
+            usd_value=self.usd_value + other.usd_value,
+        )
+
+
+def _evaluate_balance_input(other: Any, operation: str) -> Balance:
+    transformed_input = other
+    if isinstance(other, dict):
+        if len(other) == 2 and 'amount' in other and 'usd_value' in other:
+            try:
+                amount = FVal(other['amount'])
+                usd_value = FVal(other['usd_value'])
+            except ValueError:
+                raise InputError(
+                    f'Found valid dict object but with invalid values during Balance {operation}',
+                )
+            transformed_input = Balance(amount=amount, usd_value=usd_value)
+        else:
+            raise InputError(f'Found invalid dict object during Balance {operation}')
+    elif not isinstance(other, Balance):
+        raise InputError(f'Found a {type(other)} object during Balance {operation}')
+
+    return transformed_input
