d1d32362e3082af911ad6004227285f3f348e405
Lefteris Karapetsas <lefteris@refu.co>
2020-06-11 01:34:38 +0200

    Write tests for non boolean not operator pylint checker

---
+++ rotkehlchen/tests/pylint/test_disallow_not.py
@@ -0,0 +1,70 @@
+import astroid
+from tools.pylint import NotBooleanChecker
+
+
+def test_detect_list_as_nonboolean_not(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    a = []
+    not a #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 1
+
+
+def test_detect_dict_as_nonboolean_not(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    a = {}
+    not a #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 1
+
+
+def test_boolean_does_not_trigger_checker(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    a = False
+    not a #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 0
+
+
+def test_isinstance_does_not_trigger_checker(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    a = 5
+    not isinstance(a, str) #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 0
+
+
+def test_boolean_function_does_not_trigger_checker(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    def foo() -> bool:
+        return True
+    not foo() #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 0
+
+
+def test_subsscript_function_does_not_crash_checker(pylint_test_linter):
+    checker = NotBooleanChecker(linter=pylint_test_linter)
+    node = astroid.extract_node("""
+    def foo() -> Optional[object]:
+        return object()
+    not foo() #@
+    """)
+    checker.visit_unaryop(node)
+    messages = checker.linter.release_messages()
+    assert len(messages) == 0
