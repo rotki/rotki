c9648ac8613b3d96112fe6e60eac2f30eee5ea7c
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-06-16 19:28:06 +0200

    Fix integration test
    [ui tests]

--- frontend/app/tests/e2e/pages/user-security-settings-page.ts
+++ frontend/app/tests/e2e/pages/user-security-settings-page.ts
@@ -2,7 +2,7 @@ export class UserSecuritySettingsPage {
   visit() {
     cy.get('.user-dropdown').click();
     cy.get('.user-dropdown__settings').click();
-    cy.get('a.settings__usersecurity').click();
+    cy.get('a.settings__user-security').click();
   }
 
   changePassword(currentPassword: string, newPassword: string) {
