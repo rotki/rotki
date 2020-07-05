fa462c3139b9c12e153e18fd4d4b2f2bfa35eeed
Lefteris Karapetsas <lefteris@refu.co>
2020-07-05 13:00:37 +0200

    Aave history also should include deposit/withdrawals (plus interest)

--- rotkehlchen/tests/unit/test_aave.py
+++ rotkehlchen/tests/unit/test_aave.py
@@ -2,7 +2,7 @@ import pytest
 
 from rotkehlchen.accounting.structures import Balance
 from rotkehlchen.assets.asset import EthereumToken
-from rotkehlchen.chain.ethereum.aave import Aave, AaveInterestPayment
+from rotkehlchen.chain.ethereum.aave import Aave, AaveEvent
 from rotkehlchen.fval import FVal
 from rotkehlchen.premium.premium import Premium
 
@@ -53,45 +53,100 @@ def test_get_lending_profit_for_address(aave, price_historian):  # pylint: disab
     )
 
     expected_events = [
-        AaveInterestPayment(
-            balance=Balance(
+        AaveEvent(
+            event_type='deposit',
+            value=Balance(
+                amount=FVal('102.926986169186236436'),
+                usd_value=FVal('104.367963975554843746104'),
+            ),
+            block_number=9963767,
+            timestamp=1588114293,
+            tx_hash='0x8b72307967c4f7a486c1cb1b6ebca5e549de06e02930ece0399e2096f1a132c5',
+        ), AaveEvent(
+            event_type='deposit',
+            value=Balance(
+                amount=FVal('160'),
+                usd_value=FVal('161.440'),
+            ),
+            block_number=9987395,
+            timestamp=1588430911,
+            tx_hash='0x78ae48d93e0284d1f9a5e1cd4a7e5f2e3daf65ab5dafb0c4bd626aa90e783d60',
+        ), AaveEvent(
+            event_type='interest',
+            value=Balance(
                 amount=FVal('0.037901731034995483'),
                 usd_value=FVal('0.038242846614310442347'),
             ),
             block_number=9987395,
             timestamp=1588430911,
-        ), AaveInterestPayment(
-            balance=Balance(
+            tx_hash='0x78ae48d93e0284d1f9a5e1cd4a7e5f2e3daf65ab5dafb0c4bd626aa90e783d60',
+        ), AaveEvent(
+            event_type='deposit',
+            value=Balance(
+                amount=FVal('390'),
+                usd_value=FVal('393.510'),
+            ),
+            block_number=9989872,
+            timestamp=1588463542,
+            tx_hash='0xb9999b06b706dcc973bcf381d69f12620f1bef887082bce9679cf256f7e8023c',
+        ), AaveEvent(
+            event_type='interest',
+            value=Balance(
                 amount=FVal('0.013768655195843925'),
                 usd_value=FVal('0.013892573092606520325'),
             ),
             block_number=9989872,
             timestamp=1588463542,
-        ), AaveInterestPayment(
-            balance=Balance(
+            tx_hash='0xb9999b06b706dcc973bcf381d69f12620f1bef887082bce9679cf256f7e8023c',
+        ), AaveEvent(
+            event_type='deposit',
+            value=Balance(
+                amount=FVal('58.985239852398524415'),
+                usd_value=FVal('59.398136531365314085905'),
+            ),
+            block_number=10041636,
+            timestamp=1589155650,
+            tx_hash='0x28054d29620515337b8ffb2f7f2dda5b2033beae9844b42359893f4f73d855bc',
+        ), AaveEvent(
+            event_type='interest',
+            value=Balance(
                 amount=FVal('0.598140134217201945'),
                 usd_value=FVal('0.602327115156722358615'),
             ),
             block_number=10041636,
             timestamp=1589155650,
-        ), AaveInterestPayment(
-            balance=Balance(
+            tx_hash='0x28054d29620515337b8ffb2f7f2dda5b2033beae9844b42359893f4f73d855bc',
+        ), AaveEvent(
+            event_type='interest',
+            value=Balance(
                 amount=FVal('1.13704264707898858'),
                 usd_value=FVal('1.14045377502022554574'),
             ),
             block_number=10160566,
             timestamp=1590753905,
-        ), AaveInterestPayment(
-            balance=Balance(
+            tx_hash='0x07ac09cc06c7cd74c7312f3a82c9f77d69ba7a89a4a3b7ded33db07e32c3607c',
+        ), AaveEvent(
+            event_type='deposit',
+            value=Balance(
+                amount=FVal('168.84093462143338681'),
+                usd_value=FVal('171.03586677151202083853'),
+            ),
+            block_number=10266740,
+            timestamp=1592175763,
+            tx_hash='0x90b818ba8d3b55f332b64f3df58bf37f33addcbfc1f27bd1ec6102ae4bf2d871',
+        ), AaveEvent(
+            event_type='interest',
+            value=Balance(
                 amount=FVal('3.948991286917379003'),
                 usd_value=FVal('4.000328173647304930039'),
             ),
             block_number=10266740,
             timestamp=1592175763,
+            tx_hash='0x90b818ba8d3b55f332b64f3df58bf37f33addcbfc1f27bd1ec6102ae4bf2d871',
         ),
     ]
     assert len(profit_map) == 1
     assert profit_map['aDAI'].events == expected_events
     # comparison is greater than since interest is always accruing since writing the test
-    assert profit_map['aDAI'].total_earned.amount >= FVal('14.639592665598308240')
-    assert profit_map['aDAI'].total_earned.usd_value >= FVal('14.855619384119985440682')
+    assert profit_map['aDAI'].total_earned.amount >= FVal('24.207179802347627414')
+    assert profit_map['aDAI'].total_earned.usd_value >= FVal('24.580592532348742989192')
