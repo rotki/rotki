a53e578d7c8e70a05d6d2bae3675a00f7157dcbd
Lefteris Karapetsas <lefteris@refu.co>
2020-07-09 01:18:23 +0200

    Default linux data directory is now $XDG_CONFIG_HOME/rotki/data
    Also if new data directory does not exist and if old directory exists
    then copy everything to the new directory.
    
    Part of #1105

--- rotkehlchen/config.py
+++ rotkehlchen/config.py
@@ -1,16 +1,45 @@
-import errno
+import logging
 import os
+import platform
+import shutil
+from pathlib import Path
 
-from rotkehlchen.typing import FilePath
+log = logging.getLogger(__name__)
 
 
-def default_data_directory() -> FilePath:
+def get_xdg_config_home() -> Path:
+    directory = os.environ.get('XDG_CONFIG_HOME', None)
+    if directory is None:
+        home = os.path.expanduser("~")
+        directory = os.path.join(home, '.config')
+
+    return Path(directory)
+
+
+def old_data_directory() -> Path:
     home = os.path.expanduser("~")
-    data_directory = os.path.join(home, '.rotkehlchen')
-    try:
-        os.makedirs(data_directory)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    directory = os.path.join(home, '.rotkehlchen')
+    return Path(directory)
 
-    return FilePath(data_directory)
+
+def default_data_directory() -> Path:
+    if platform.system() == 'Linux':
+        xdgconfig = get_xdg_config_home()
+        datadir = xdgconfig / 'rotki' / 'data'
+    elif platform.system() == 'Windows':
+        # TODO
+        pass
+    elif platform.system() == 'Darwin':
+        # TODO
+        pass
+    else:
+        raise AssertionError(f'Rotki running in unknown system: {platform.system()}')
+
+    # If old data directory exists and new does not exist copy stuff
+    old_dir = old_data_directory()
+    if old_dir.exists() and not datadir.exists():
+        log.info(f'First time using standard data directory. Copying from {old_dir} to {datadir}')
+        shutil.copytree(old_dir, datadir)
+
+    datadir.mkdir(parents=True, exist_ok=True)
+    return datadir
