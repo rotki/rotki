bf09b6845db213ccd275c156507447656027b623
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-07-15 17:05:49 +0200

    Adds a css-based scrollbar to the app and certain dialogs
    * Adds a css-based scrollbar to the app (main window) and the tagmanager dialog (which often scrolled
    * This ensures consistency across OSes and also blends nicer with the rest of the app

--- frontend/app/src/components/tags/TagManager.vue
+++ frontend/app/src/components/tags/TagManager.vue
@@ -137,4 +137,18 @@ export default class TagManager extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+::-webkit-scrollbar {
+    width: 12px;
+}
+ 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+    border-radius: 10px;
+}
+ 
+::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+}
+</style>
