4f107a287370c5968bb3b7ff9a10900f76796766
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-07-20 11:31:55 +0200

    Update location of fiat balances
    * Moves fiatBalances to manualBalances sub-page in Accounts & Balances (temporary until fiatbalances is deprecated).

--- frontend/app/src/components/accounts/ManualBalances.vue
+++ frontend/app/src/components/accounts/ManualBalances.vue
@@ -1,48 +1,57 @@
 <template>
-  <v-card class="manual-balances mt-8">
-    <v-card-title>Manually Tracked Balances</v-card-title>
-    <v-card-text>
-      <v-btn
-        absolute
-        fab
-        top
-        right
-        dark
-        color="primary"
-        class="manual-balances__addBalance"
-        @click="newBalance()"
-      >
-        <v-icon>
-          fa fa-plus
-        </v-icon>
-      </v-btn>
-      <big-dialog
-        :display="openDialog"
-        :title="dialogTitle"
-        :sub-title="dialogSubTitle"
-        primary-action="Save"
-        @confirm="save()"
-        @cancel="openDialog = false"
-      >
-        <manual-balances-form
-          ref="dialogChild"
-          :edit="balanceToEdit"
-        ></manual-balances-form>
-      </big-dialog>
-      <manual-balances-list @editBalance="edit($event)"></manual-balances-list>
-    </v-card-text>
-  </v-card>
+  <fragment>
+    <v-card class="manual-balances mt-8">
+      <v-card-title>Manually Tracked Balances</v-card-title>
+      <v-card-text>
+        <v-btn
+          absolute
+          fab
+          top
+          right
+          dark
+          color="primary"
+          class="manual-balances__addBalance"
+          @click="newBalance()"
+        >
+          <v-icon>
+            fa fa-plus
+          </v-icon>
+        </v-btn>
+        <big-dialog
+          :display="openDialog"
+          :title="dialogTitle"
+          :sub-title="dialogSubTitle"
+          primary-action="Save"
+          @confirm="save()"
+          @cancel="openDialog = false"
+        >
+          <manual-balances-form
+            ref="dialogChild"
+            :edit="balanceToEdit"
+          ></manual-balances-form>
+        </big-dialog>
+        <manual-balances-list @editBalance="edit($event)"></manual-balances-list>
+      </v-card-text>
+    </v-card>
+    <fiat-balances></fiat-balances>
+  </fragment>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import FiatBalances from '@/components/accounts/FiatBalances.vue';
 import ManualBalancesForm from '@/components/accounts/ManualBalancesForm.vue';
 import ManualBalancesList from '@/components/accounts/ManualBalancesList.vue';
 import BigDialog from '@/components/dialogs/BigDialog.vue';
 import { ManualBalance } from '@/services/types-model';
 
 @Component({
-  components: { ManualBalancesList, ManualBalancesForm, BigDialog }
+  components: {
+    ManualBalancesList,
+    ManualBalancesForm,
+    BigDialog,
+    FiatBalances
+  }
 })
 export default class ManualBalances extends Vue {
   balanceToEdit: ManualBalance | null = null;
