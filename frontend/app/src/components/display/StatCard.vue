751da35281b5c148bba21aa63576af09abe5c078
Isidoros Passadis <isidoros.passadis@gmail.com>
2020-07-21 22:48:38 +0200

    Fix and tweak DeFi Overview Page
    * Convert DeFi Overview page to use i18n #999
    * Remove unsupported tags by vuetify #1431
    * Add Compound Icon
    * Add list of "whitelisted" protocol icons that we prefer over Zerion icons
    * Fix fiat amounts in Defi Overview Details to be expressed correctly & converted to user's profit currency (Fix #1423)
    
    [ui tests]

--- frontend/app/src/components/display/StatCard.vue
+++ frontend/app/src/components/display/StatCard.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-card class="stat-card">
-    <v-card-title>
-      <v-img
-        v-if="protocolIcon"
-        contain
-        alt="Protocol Logo"
-        max-height="32px"
-        :src="protocolIcon"
-      />
-      <span v-if="title">
-        {{ title }}
-      </span>
-      <v-spacer v-if="locked" />
-      <premium-lock v-if="locked" />
-    </v-card-title>
-    <v-card-text class="flex">
-      <span v-if="!locked && loading">
-        <v-progress-linear indeterminate color="primary" />
-      </span>
-      <slot v-else-if="!locked" />
-    </v-card-text>
+  <v-card class="stat-card d-flex flex-columns pa-0">
+    <div v-if="bordered" class="stat-card__border">
+      <div class="stat-card__image ma-2">
+        <v-img
+          v-if="protocolIcon"
+          contain
+          alt="Protocol Logo"
+          height="36px"
+          width="36px"
+          :src="protocolIcon"
+        />
+      </div>
+      <div class="stat-card__border__gradient" />
+    </div>
+    <div class="stat-card__content flex-grow-1">
+      <v-card-title class="py-2">
+        <span v-if="title">
+          {{ title }}
+        </span>
+        <v-spacer v-if="locked" />
+        <premium-lock v-if="locked" />
+      </v-card-title>
+      <v-card-text>
+        <span v-if="!locked && loading">
+          <v-progress-linear indeterminate color="primary" />
+        </span>
+        <slot v-else-if="!locked" />
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 
@@ -39,6 +47,8 @@ export default class StatCard extends Vue {
   loading!: boolean;
   @Prop({ required: false, type: String })
   protocolIcon!: string;
+  @Prop({ required: false, type: Boolean, default: false })
+  bordered!: boolean;
 }
 </script>
 
@@ -53,6 +63,22 @@ export default class StatCard extends Vue {
         padding-top: 12px;
         font-size: 1em;
       }
+    }
+  }
+
+  &__border {
+    width: 48px;
+    border-radius: 4px 0 0 4px !important;
+    background-color: var(--v-rotki-light-grey-darken1);
+
+    &__gradient {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0px;
+      height: 100%;
+      width: 48px;
+      background: linear-gradient(115deg, rgba(255, 0, 0, 0), white);
     }
   }
 }
