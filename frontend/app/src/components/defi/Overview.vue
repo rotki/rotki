05d7327b5c410feb630f260430ab4f728265383a
Konstantinos Paparas <kelsos86@gmail.com>
2020-07-21 22:13:58 +0200

    Introduces a detail dialog for protocol assets
    [ui tests]

--- frontend/app/src/components/defi/Overview.vue
+++ frontend/app/src/components/defi/Overview.vue
@@ -56,23 +56,70 @@
       fiat
       :value="summary.balanceUsd"
     />
+    <v-divider class="my-4" />
+    <h4 class="pb-2 d-flex flex-row justify-space-between">
+      Details
+      <v-dialog v-model="details" scrollable max-width="450px">
+        <template #activator="{ on, attrs }">
+          <v-btn
+            x-small
+            icon
+            max-width="32px"
+            color="primary"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>fa-external-link-square</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <v-img
+              aspect-ratio="1"
+              :src="icon"
+              max-width="32px"
+              max-height="32px"
+              class="mb-2"
+              contain
+            />
+            <span class="ml-2">
+              {{ summary.tokenInfo.tokenName }}
+            </span>
+          </v-card-title>
+          <v-card-subtitle>
+            Protocol asset details
+          </v-card-subtitle>
+          <v-card-text class="overview__details">
+            <div v-for="asset in summary.assets" :key="asset.tokenAddress">
+              <defi-asset :asset="asset" />
+              <v-divider />
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </h4>
   </stat-card>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import CryptoIcon from '@/components/CryptoIcon.vue';
+import DefiAsset from '@/components/defi/DefiAsset.vue';
 import InfoRow from '@/components/defi/display/InfoRow.vue';
+import AmountDisplay from '@/components/display/AmountDisplay.vue';
 import StatCard from '@/components/display/StatCard.vue';
 import { DefiProtocolSummary } from '@/store/defi/types';
 
 @Component({
-  components: { StatCard, InfoRow }
+  components: { DefiAsset, CryptoIcon, AmountDisplay, StatCard, InfoRow }
 })
 export default class Overview extends Vue {
   @Prop({ required: true, type: Boolean })
   loading!: boolean;
   @Prop({ required: true })
   summary!: DefiProtocolSummary;
+
+  details: boolean = false;
 
   get icon(): string {
     const protocol = this.summary.protocol;
@@ -88,5 +135,9 @@ export default class Overview extends Vue {
 .overview {
   min-height: 250px;
   min-width: 300px;
+
+  &__details {
+    height: 300px;
+  }
 }
 </style>
