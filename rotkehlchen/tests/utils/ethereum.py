2f6b6565bb024148b72c9c8d5ae2a0cc421d7f01
Lefteris Karapetsas <lefteris@refu.co>
2020-07-21 18:15:21 +0200

    Use infura instead of blockscout for real query tests
    It seems blockscout is having server problems at the moment and as
    such we are replacing with an infura address made specifically for
    rotki CI tests.
    
    Fix #1184

--- rotkehlchen/tests/utils/ethereum.py
+++ rotkehlchen/tests/utils/ethereum.py
@@ -1,6 +1,7 @@
 ETHEREUM_TEST_PARAMETERS = ['ethrpc_endpoint,ethereum_manager_connect_at_start', [
     # Query etherscan
     ('', False),
-    # For real node querying let's use blockscout
-    ('https://mainnet-nethermind.blockscout.com', True),
+    # For real node querying use infura for now since blockscout is having server problems
+    # ('https://mainnet-nethermind.blockscout.com', True),
+    ('https://mainnet.infura.io/v3/66302b8fb9874614905a3cbe903a0dbb', True),
 ]]
