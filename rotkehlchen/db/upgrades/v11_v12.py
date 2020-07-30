e6bab2a97e4c671f7a24794048006a9b4404364a
Lefteris Karapetsas <lefteris@refu.co>
2020-07-30 14:19:33 +0200

    Upgrade DB to v12, deleting all bittrex data
    Deleting all bittrex data from the DB. They have to be re-queried from
    the exchange as we can't trust the data we have due to the old
    v1.1. API not returning data from some time.

---
+++ rotkehlchen/db/upgrades/v11_v12.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from rotkehlchen.db.dbhandler import DBHandler
+
+
+def _delete_bittrex_data(db: 'DBHandler') -> None:
+    cursor = db.conn.cursor()
+    # This is the user credentiaals trades table at v10
+    cursor.execute('DELETE FROM trades WHERE location="D";')
+    cursor.execute('DELETE FROM used_query_ranges WHERE name LIKE "bittrex_%";')
+    db.conn.commit()
+
+
+def upgrade_v11_to_v12(db: 'DBHandler') -> None:
+    """Upgrades the DB from v11 to v12
+
+    - Deletes all bittrex related DB data
+    """
+    _delete_bittrex_data(db)
