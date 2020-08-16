aa4cb736d3f09e8a76fb0266239a4c4ae2dff573
Lefteris Karapetsas <lefteris@refu.co>
2020-08-16 12:27:01 +0200

    Add small assets.json editing script for bulk edits

---
+++ tools/assets_editing/main.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parent.parent.parent
+ASSETS_FILE = Path(f'{root_dir}/rotkehlchen/data/all_assets.json')
+with open(ASSETS_FILE, 'r') as f:
+    assets = json.loads(f.read())
+
+
+new_assets = {}
+for key, entry in assets.items():
+    # here edit
+    pass
+
+with open(ASSETS_FILE, 'w') as f:
+    f.write(
+        json.dumps(
+            new_assets, sort_keys=True, indent=4,
+        ),
+    )
