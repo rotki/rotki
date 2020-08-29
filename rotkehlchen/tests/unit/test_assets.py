6ed25bac5ae52debae9016492541776ff3b14a23
Lefteris Karapetsas <lefteris@refu.co>
2020-08-29 10:00:51 +0200

    Changelog entry and test adjustment

--- rotkehlchen/tests/unit/test_assets.py
+++ rotkehlchen/tests/unit/test_assets.py
@@ -121,7 +121,7 @@ def test_coingecko_identifiers_are_reachable():
 def test_assets_json_meta():
     """Test that all_assets.json md5 matches and that if md5 changes since last
     time then version is also bumped"""
-    last_meta = {'md5': '597b156fa32dde013580c2f453fd6370', 'version': 3}
+    last_meta = {'md5': '94cc4fc39da7c66b190ced86573c6835', 'version': 4}
     data_dir = Path(__file__).resolve().parent.parent.parent / 'data'
     data_md5 = file_md5(data_dir / 'all_assets.json')
 
