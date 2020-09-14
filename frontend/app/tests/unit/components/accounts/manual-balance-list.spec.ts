194243cbbfcdca14adb50adfe775953b5154376e
Konstantinos Paparas <kelsos86@gmail.com>
2020-09-14 14:24:36 +0200

    Adjusts manual balance tests to work with i18n
    [ui tests]

--- frontend/app/tests/unit/components/accounts/manual-balance-list.spec.ts
+++ frontend/app/tests/unit/components/accounts/manual-balance-list.spec.ts
@@ -26,11 +26,11 @@ describe('ManualBalanceList.vue', () => {
   });
 
   test('currency header is properly updated', async () => {
-    expect(wrapper.find('th:nth-child(4)').text()).toMatch('USD Value');
+    expect(wrapper.find('th:nth-child(4)').text()).toContain('USD');
     store.commit('session/generalSettings', {
       selectedCurrency: findCurrency(currencies[1].ticker_symbol)
     });
     await wrapper.vm.$nextTick();
-    expect(wrapper.find('th:nth-child(4)').text()).toMatch('EUR Value');
+    expect(wrapper.find('th:nth-child(4)').text()).toContain('EUR');
   });
 });
--- frontend/app/tests/unit/i18n.ts
+++ frontend/app/tests/unit/i18n.ts
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 
+function stringify(value: { [key: string]: any }): string {
+  return Object.values(value)
+    .map(value => value.toString())
+    .join(', ');
+}
+
 function I18n(vue: typeof Vue): void {
-  vue.prototype.$t = (key: string) => key;
-  vue.prototype.$tc = (key: string) => key;
+  vue.prototype.$t = (key: string, args?: object) =>
+    args ? `${key}::${stringify(args)}` : key;
+  vue.prototype.$tc = (key: string, choice?: number, args?: object) =>
+    args ? `${key}::${choice}::${stringify(args)}` : key;
 }
 
 Vue.use(I18n);
