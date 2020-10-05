887ae6f89a6d4e24676de590735c2e2dcfed3840
Konstantinos Paparas <kelsos86@gmail.com>
2020-10-05 11:54:29 +0200

    Fixes trimming of whitespace when pasting api keys

--- frontend/app/src/components/account-management/PremiumCredentials.vue
+++ frontend/app/src/components/account-management/PremiumCredentials.vue
@@ -23,6 +23,7 @@
         :rules="apiKeyRules"
         :label="$t('premium_credentials.label_api_key')"
         @input="apiKeyChanged"
+        @paste="onApiKeyPaste"
       />
       <revealable-input
         :value="apiSecret"
@@ -32,6 +33,7 @@
         :label="$t('premium_credentials.label_api_secret')"
         :rules="apiSecretRules"
         @input="apiSecretChanged"
+        @paste="onApiSecretPaste"
       />
     </div>
   </div>
@@ -40,6 +42,7 @@
 <script lang="ts">
 import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator';
 import RevealableInput from '@/components/inputs/RevealableInput.vue';
+import { trimOnPaste } from '@/utils/event';
 
 @Component({
   components: { RevealableInput }
@@ -82,5 +85,19 @@ export default class PremiumCredentials extends Vue {
 
   @Emit()
   enabledChanged(_enabled: boolean) {}
+
+  onApiKeyPaste(_event: ClipboardEvent) {
+    const paste = trimOnPaste(_event);
+    if (paste) {
+      this.apiKeyChanged(paste);
+    }
+  }
+
+  onApiSecretPaste(_event: ClipboardEvent) {
+    const paste = trimOnPaste(_event);
+    if (paste) {
+      this.apiSecretChanged(paste);
+    }
+  }
 }
 </script>
