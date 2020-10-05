c104ce137d7772758b4dc224db98d1d6deb1ec51
Lefteris Karapetsas <lefteris@refu.co>
2020-10-05 16:24:02 +0200

    Support Rarible
    RARI - https://www.coingecko.com/en/coins/rarible

--- rotkehlchen/externalapis/cryptocompare.py
+++ rotkehlchen/externalapis/cryptocompare.py
@@ -117,6 +117,7 @@ CRYPTOCOMPARE_SPECIAL_CASES_MAPPING = {
     Asset('GHST'): Asset('USDT'),
     Asset('MEXP'): Asset('USDT'),
     Asset('POLS'): Asset('USDT'),
+    Asset('RARI'): Asset('USDT'),
 }
 CRYPTOCOMPARE_SPECIAL_CASES = CRYPTOCOMPARE_SPECIAL_CASES_MAPPING.keys()
 
