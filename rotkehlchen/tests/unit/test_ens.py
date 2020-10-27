dd181023c3c1f99f7217d2a06a314a10bf29635a
Lefteris Karapetsas <lefteris@refu.co>
2020-10-27 10:02:22 +0100

    Add timeout on the ENS unit test

--- rotkehlchen/tests/unit/test_ens.py
+++ rotkehlchen/tests/unit/test_ens.py
@@ -1,21 +1,18 @@
 import warnings as test_warnings
 
-import gevent
 import pytest
 
-from rotkehlchen.chain.ethereum.manager import NodeName
 from rotkehlchen.chain.ethereum.zerion import ZERION_ADAPTER_ADDRESS
-from rotkehlchen.tests.utils.ethereum import ETHEREUM_TEST_PARAMETERS
+from rotkehlchen.tests.utils.ethereum import (
+    ETHEREUM_TEST_PARAMETERS,
+    wait_until_all_nodes_connected,
+)
 
 
 @pytest.mark.parametrize(*ETHEREUM_TEST_PARAMETERS)
-def test_ens_lookup(ethereum_manager, call_order):
+def test_ens_lookup(ethereum_manager, call_order, ethereum_manager_connect_at_start):
     """Test that ENS lookup works. Both with etherscan and with querying a real node"""
-    # Wait until all nodes are connected
-    if NodeName.OWN in call_order:
-        while NodeName.OWN not in ethereum_manager.web3_mapping:
-            gevent.sleep(2)
-            continue
+    wait_until_all_nodes_connected(ethereum_manager_connect_at_start, ethereum_manager)
     result = ethereum_manager.ens_lookup('api.zerion.eth', call_order)
     assert result is not None
     if result != ZERION_ADAPTER_ADDRESS:
