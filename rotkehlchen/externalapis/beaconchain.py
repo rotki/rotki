6c3af5b7072d9ef96b68def6836a87c5bcef1ce3
Lefteris Karapetsas <lefteris@refu.co>
2020-11-28 18:07:46 +0100

    Implement tests for all beaconcha.in endpoints

--- rotkehlchen/externalapis/beaconchain.py
+++ rotkehlchen/externalapis/beaconchain.py
@@ -1,18 +1,20 @@
 from json.decoder import JSONDecodeError
-from typing import Any, Dict, List, NamedTuple
+from typing import TYPE_CHECKING, Any, Dict, List, NamedTuple, Union
 
 import gevent
 import requests
 from typing_extensions import Literal
 
 from rotkehlchen.constants.timing import QUERY_RETRY_TIMES
-from rotkehlchen.db.dbhandler import DBHandler
 from rotkehlchen.errors import RemoteError
 from rotkehlchen.externalapis.interface import ExternalServiceWithApiKey
-from rotkehlchen.typing import ExternalService
+from rotkehlchen.typing import ChecksumEthAddress, ExternalService
 from rotkehlchen.user_messages import MessagesAggregator
-from rotkehlchen.utils.misc import get_chunks
+from rotkehlchen.utils.misc import from_wei, get_chunks
 from rotkehlchen.utils.serialization import rlk_jsonloads_dict
+
+if TYPE_CHECKING:
+    from rotkehlchen.db.dbhandler import DBHandler
 
 
 class ValidatorBalance(NamedTuple):
@@ -24,12 +26,26 @@ class ValidatorBalance(NamedTuple):
 class ValidatorPerformance(NamedTuple):
     balance: int  # in wei
     performance_1d: int  # in wei
-    performance_7d: int  # in wei
+    performance_1w: int  # in wei
     performance_1m: int  # in wei
     performance_1y: int  # in wei
 
+    def serialize(self):
+        return {
+            'balance': from_wei(self.balance),
+            'performance_1d': from_wei(self.performance_1d),
+            'performance_1w': from_wei(self.performance_1w),
+            'performance_1m': from_wei(self.performance_1m),
+            'performance_1y': from_wei(self.performance_1y),
+        }
 
-class Beaconchain(ExternalServiceWithApiKey):
+
+class ValidatorID(NamedTuple):
+    index: int
+    public_key: str
+
+
+class BeaconChain(ExternalServiceWithApiKey):
     """Even though the beaconchain allows for API keys we don't implement it yet.
     We do extend ExternalServiceWithApiKey though so that it becomes easier to add
     in the future.
@@ -37,7 +53,7 @@ class Beaconchain(ExternalServiceWithApiKey):
     https://beaconcha.in/api/v1/docs
     """
 
-    def __init__(self, database: DBHandler, msg_aggregator: MessagesAggregator) -> None:
+    def __init__(self, database: 'DBHandler', msg_aggregator: MessagesAggregator) -> None:
         super().__init__(database=database, service_name=ExternalService.ETHERSCAN)
         self.msg_aggregator = msg_aggregator
         self.session = requests.session()
@@ -48,14 +64,17 @@ class Beaconchain(ExternalServiceWithApiKey):
     def _query(
             self,
             module: Literal['validator'],
-            endpoint: Literal['balanceHistory', 'performance'],
+            endpoint: Literal['balanceHistory', 'performance', 'eth1'],
             encoded_args: str,
-    ) -> List[Dict[str, Any]]:
+    ) -> Union[List[Dict[str, Any]], Dict[str, Any]]:
         """
         May raise:
         - RemoteError due to problems querying beaconcha.in API
         """
-        query_str = f'{self.url}{module}/{encoded_args}/{endpoint}'
+        if endpoint == 'eth1':
+            query_str = f'{self.url}{module}/{endpoint}/{encoded_args}'
+        else:
+            query_str = f'{self.url}{module}/{encoded_args}/{endpoint}'
         times = QUERY_RETRY_TIMES
         backoff_in_seconds = 10
 
@@ -110,24 +129,33 @@ class Beaconchain(ExternalServiceWithApiKey):
         chunks = list(get_chunks(validator_indices, n=100))
         data = []
         for chunk in chunks:
-            data.extend(self._query(
+            result = self._query(
                 module='validator',
-                endpoint='balanceHistory',
+                endpoint='balancehistory',
                 encoded_args=','.join(str(x) for x in chunk),
-            ))
+            )
+            if isinstance(result, list):
+                data.extend(result)
+            else:
+                data.append(result)
 
         # We are only interested in last epoch, so get its value
         balances: Dict[int, ValidatorBalance] = {}
-        for entry in data:
-            index = entry['validatorindex']
-            epoch = entry['epoch']
-            if index in balances and balances[index].epoch >= epoch:
-                continue
+        try:
+            for entry in data:
+                index = entry['validatorindex']
+                epoch = entry['epoch']
+                if index in balances and balances[index].epoch >= epoch:
+                    continue
 
-            balances[index] = ValidatorBalance(
-                epoch=epoch,
-                balance=entry['balance'],
-                effective_balance=entry['effectivebalance'],
+                balances[index] = ValidatorBalance(
+                    epoch=epoch,
+                    balance=entry['balance'],
+                    effective_balance=entry['effectivebalance'],
+                )
+        except KeyError as e:
+            raise RemoteError(
+                f'Beaconchai.in balance response processing error. Missing key entry {str(e)}',
             )
 
         return balances
@@ -143,21 +171,56 @@ class Beaconchain(ExternalServiceWithApiKey):
         chunks = list(get_chunks(validator_indices, n=100))
         data = []
         for chunk in chunks:
-            data.extend(self._query(
+            result = self._query(
                 module='validator',
                 endpoint='performance',
                 encoded_args=','.join(str(x) for x in chunk),
-            ))
+            )
+            if isinstance(result, list):
+                data.extend(result)
+            else:
+                data.append(result)
 
-        performance = {}
-        for entry in data:
-            index = entry['validatorindex']
-            performance[index] = ValidatorPerformance(
-                balance=entry['balance'],
-                performance_1d=entry['performance1d'],
-                performance_7d=entry['performance7d'],
-                performance_1m=entry['performance31d'],
-                performance_1y=entry['performance365d'],
+        try:
+            performance = {}
+            for entry in data:
+                index = entry['validatorindex']
+                performance[index] = ValidatorPerformance(
+                    balance=entry['balance'],
+                    performance_1d=entry['performance1d'],
+                    performance_1w=entry['performance7d'],
+                    performance_1m=entry['performance31d'],
+                    performance_1y=entry['performance365d'],
+                )
+        except KeyError as e:
+            raise RemoteError(
+                f'Beaconchai.in performance response processing error. Missing key entry {str(e)}',
             )
 
         return performance
+
+    def get_eth1_address_validators(self, address: ChecksumEthAddress) -> List[ValidatorID]:
+        """Get a list of Validators that are associated with the given eth1 address
+
+        May raise:
+        - RemoteError due to problems querying beaconcha.in API
+        """
+        result = self._query(
+            module='validator',
+            endpoint='eth1',
+            encoded_args=address,
+        )
+        if isinstance(result, list):
+            data = result
+        else:
+            data = [result]
+
+        try:
+            validators = [
+                ValidatorID(index=x['validatorindex'], public_key=x['publickey']) for x in data
+            ]
+        except KeyError as e:
+            raise RemoteError(
+                f'Beaconchai.in eth1 response processing error. Missing key entry {str(e)}',
+            )
+        return validators
