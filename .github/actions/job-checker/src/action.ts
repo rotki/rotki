5e9e35c7bbf2c88a547e0539b877dfa9862c1302
Konstantinos Paparas <kelsos86@gmail.com>
2020-11-30 19:30:59 +0100

    Introduces local action that detects changes

---
+++ .github/actions/job-checker/src/action.ts
@@ -0,0 +1,44 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+export async function getPullRequestFiles(): Promise<string[]> {
+  const token = core.getInput('token', {required: true})
+  const client = github.getOctokit(token)
+  const {context} = github
+  if (!context.payload.pull_request) {
+    throw new Error('not a pr')
+  }
+  const {number} = context.payload.pull_request
+
+  const {data} = await client.pulls.listFiles({
+    ...context.repo,
+    pull_number: number
+  })
+
+  return data.map(value => value.filename)
+}
+
+export function changeDetected(
+  monitored: string[],
+  changed: string[]
+): boolean {
+  for (const path of monitored) {
+    for (const detected of changed) {
+      if (detected.startsWith(path) || detected === path) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+export function getInputAsArray(
+  name: string,
+  options?: core.InputOptions
+): string[] {
+  return core
+    .getInput(name, options)
+    .split('\n')
+    .map(s => s.trim())
+    .filter(x => x !== '')
+}
---
+++ .github/actions/job-checker/src/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import {changeDetected, getInputAsArray, getPullRequestFiles} from './action'
+
+async function run(): Promise<void> {
+  try {
+    const options = {required: true}
+    const backendPaths = getInputAsArray('backend_paths', options)
+    const frontendPaths = getInputAsArray('frontend_paths', options)
+    const documentationPaths = getInputAsArray('documentation_paths', options)
+
+    const changes = await getPullRequestFiles()
+
+    if (changeDetected(frontendPaths, changes)) {
+      core.setOutput('frontend_tasks', true)
+    }
+
+    if (changeDetected(backendPaths, changes)) {
+      core.setOutput('backend_tasks', true)
+    }
+
+    if (changeDetected(documentationPaths, changes)) {
+      core.setOutput('documentation_tasks', true)
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
