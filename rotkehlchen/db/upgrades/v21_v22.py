4902a2d791351575c5f2a815d12361e2a17a59a6
Lefteris Karapetsas <lefteris@refu.co>
2020-12-16 09:55:44 +0100

    Fix the v21->v22 DB upgrade
    The price historian is not instantiated during the upgrade so
    assertion fails. Was not caught in the tests due to price query mocking.

--- rotkehlchen/db/upgrades/v21_v22.py
+++ rotkehlchen/db/upgrades/v21_v22.py
@@ -1,9 +1,5 @@
 from typing import TYPE_CHECKING
 
-from rotkehlchen.constants.assets import A_ETH
-from rotkehlchen.fval import FVal
-from rotkehlchen.history.price import query_usd_price_zero_if_error
-
 if TYPE_CHECKING:
     from rotkehlchen.db.dbhandler import DBHandler
 
@@ -12,36 +8,12 @@ def upgrade_v21_to_v22(db: 'DBHandler') -> None:
     """Upgrades the DB from v21 to v22
 
     Changes the ETH2 deposit table to properly name the deposit index column
-    and transfers all the data to the new table
+    and deletes all old data so they can be populated again.
     """
     cursor = db.conn.cursor()
-    query = cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='eth2_deposits';",
-    )
-    results = query.fetchall()
-
-    # Check if table exists. IF not we are coming from an older DB
-    entries = []
-    if len(results) != 0:
-        count = cursor.execute('SELECT COUNT(*) FROM eth2_deposits;').fetchone()[0]
-        if count != 0:
-            # Also query only if we have something. If we don't the table may have
-            # just been created and is already with the new schema
-            query = cursor.execute(
-                'SELECT tx_hash, '
-                'log_index, '
-                'from_address, '
-                'timestamp, '
-                'pubkey, '
-                'withdrawal_credentials, '
-                'validator_index, '
-                'value from eth2_deposits;',
-            )
-            for q in query:
-                entries.append(q)
-
     # delete old table and create new one
     cursor.execute('DROP TABLE IF EXISTS eth2_deposits;')
+    cursor.execute('DELETE from used_query_ranges WHERE name LIKE "eth2_deposits_%";')
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS eth2_deposits (
     tx_hash VARCHAR[42] NOT NULL,
@@ -55,30 +27,4 @@ def upgrade_v21_to_v22(db: 'DBHandler') -> None:
     deposit_index INTEGER NOT NULL,
     PRIMARY KEY (tx_hash, log_index)
 );""")
-
-    # and finally put all data to the new table
-    new_entries = []
-    for e in entries:
-        usd_price = query_usd_price_zero_if_error(
-            asset=A_ETH,
-            time=e[3],
-            location='v21 -> v22 DB upgrade',
-            msg_aggregator=db.msg_aggregator,
-        )
-        new_entries.append(list(e) + [str(FVal(e[7]) * usd_price)])
-
-    query = ("""
-        INSERT INTO eth2_deposits (
-        tx_hash,
-        log_index,
-        from_address,
-        timestamp,
-        pubkey,
-        withdrawal_credentials,
-        deposit_index,
-        amount,
-        usd_value
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """)
-    cursor.executemany(query, new_entries)
     db.conn.commit()
