35e34cfb34814ed9a9ce17a1a75a6ff1a9c2a23b
Lefteris Karapetsas <lefteris@refu.co>
2021-01-07 22:04:30 +0100

    Adjust the price history tests to the new querying method

--- rotkehlchen/tests/integration/test_price_history.py
+++ rotkehlchen/tests/integration/test_price_history.py
@@ -1,70 +1,32 @@
-import random
-
 import pytest
 
-from rotkehlchen.constants.assets import A_BTC, A_ETH, A_EUR, A_USD
+from rotkehlchen.assets.asset import Asset
+from rotkehlchen.constants.assets import A_BTC, A_EUR, A_USD
 from rotkehlchen.fval import FVal
-from rotkehlchen.tests.utils.constants import A_BSV, A_DASH, A_IOTA
-from rotkehlchen.tests.utils.history import prices
+from rotkehlchen.tests.utils.constants import A_DASH, A_XMR
+from rotkehlchen.utils.misc import get_or_make_price_history_dir
+from rotkehlchen.externalapis.cryptocompare import PRICE_HISTORY_FILE_PREFIX, Cryptocompare
 
 
 @pytest.mark.parametrize('should_mock_price_queries', [False])
-def test_inconsistent_prices_double_checking(price_historian):
-    """ This is a regression test for the inconsistent DASH/EUR and DASH/USD prices
-    that were returned on 02/12/2018. Issue:
-    https://github.com/rotki/rotki/issues/221
-    """
+def test_price_queries(price_historian, data_dir, database):
+    """Test some historical price queries. Make sure that we test some
+    assets not in cryptocompare but in coigecko so the backup mechanism triggers and works"""
 
-    # Note the prices are not the same as in the issue because rotkehlchen uses
-    # hourly rates while in the issue we showcased query of daily average
-    usd_price = price_historian.query_historical_price(A_DASH, A_USD, 1479200704)
-    assert usd_price.is_close(FVal('9.6125'))
-    eur_price = price_historian.query_historical_price(A_DASH, A_EUR, 1479200704)
-    assert eur_price.is_close(FVal('9.0015'), max_diff=0.001)
-
-    inv_usd_price = price_historian.query_historical_price(A_USD, A_DASH, 1479200704)
-    assert inv_usd_price.is_close(FVal('0.10385'), max_diff=0.0001)
-    inv_eur_price = price_historian.query_historical_price(A_EUR, A_DASH, 1479200704)
-    assert inv_eur_price.is_close(FVal('0.1111'), max_diff=0.0001)
-
-
-def do_queries_for(from_asset, to_asset, price_historian):
-    queries = 5
-
-    pair_map = prices[from_asset][to_asset]
-    keys = random.sample(pair_map.keys(), min(queries, len(pair_map)))
-    for timestamp in keys:
-        price = price_historian.query_historical_price(from_asset, to_asset, timestamp)
-        msg = (
-            f'Unexpected price for {from_asset} -> {to_asset} for {timestamp}. '
-            f'Got {price} expected {pair_map[timestamp]}'
-        )
-        assert price.is_close(pair_map[timestamp]), msg
-
-
-@pytest.mark.parametrize('should_mock_price_queries', [False])
-def test_price_queries(price_historian):
-    """Test some cryptocompare price queries making sure our querying mechanism works"""
-    # TODO: Get rid of the cache here and then try again with the cache
-    # TODO2: Once historical price of DASH and other token stabilize perhaps also
-    #        include them in the tests
-    do_queries_for(A_BTC, A_EUR, price_historian)
-    do_queries_for(A_ETH, A_EUR, price_historian)
-
-
-@pytest.mark.parametrize('should_mock_price_queries', [False])
-def test_cryptocompare_iota_query(price_historian):
-    """
-    Test that IOTA can be properly queried from cryptocompare
-
-    Issue: https://github.com/rotki/rotki/issues/299
-    """
-    usd_price = price_historian.query_historical_price(A_IOTA, A_USD, 1511793374)
-    assert usd_price.is_close(FVal('0.954'))
-
-
-@pytest.mark.parametrize('should_mock_price_queries', [False])
-def test_cryptocompare_bchsv_query(price_historian):
-    """Test that BCHSV can be properly queried from cryptocompare (it's BSV there)"""
-    btc_price = price_historian.query_historical_price(A_BSV, A_BTC, 1550945818)
-    assert btc_price.is_close(FVal('0.01633'))
+    # These should hit cryptocompare
+    assert price_historian.query_historical_price(A_BTC, A_EUR, 1479200704) == FVal('663.66')
+    assert price_historian.query_historical_price(A_XMR, A_BTC, 1579200704) == FVal('0.007526')
+    # this should hit the cryptocompare cache we are creating here
+    contents = """{"start_time": 0, "end_time": 1439390800,
+    "data": [{"time": 1438387200, "close": 10, "high": 10, "low": 10, "open": 10,
+    "volumefrom": 10, "volumeto": 10}, {"time": 1438390800, "close": 20, "high": 20,
+    "low": 20, "open": 20, "volumefrom": 20, "volumeto": 20}]}"""
+    price_historian.cryptocompare = Cryptocompare(data_directory=data_dir, database=database)
+    price_history_dir = get_or_make_price_history_dir(data_dir)
+    with open(price_history_dir / f'{PRICE_HISTORY_FILE_PREFIX}DASH_USD.json', 'w') as f:
+        f.write(contents)
+    assert price_historian.query_historical_price(A_DASH, A_USD, 1438387700) == FVal('10')
+    # this should hit coingecko, since cornichon is not in cryptocompare
+    cornichon = Asset('CORN-2')
+    expected_price = FVal('0.07830444726516915')
+    assert price_historian.query_historical_price(cornichon, A_USD, 1608854400) == expected_price
