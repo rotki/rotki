b1db3112751038cca48316d1b9f20b0fd0429375
Lefteris Karapetsas <lefteris@refu.co>
2021-01-25 16:51:56 +0100

    Just log a warning and not show warning msg for duplicate DB entry

--- rotkehlchen/db/dbhandler.py
+++ rotkehlchen/db/dbhandler.py
@@ -1901,8 +1901,8 @@ class DBHandler:
                         continue
 
                     string_repr = db_tuple_to_str(entry, tuple_type)
-                    self.msg_aggregator.add_warning(
-                        f'Failed to add "{string_repr}" to the DB. It already exists.',
+                    logger.warning(
+                        f'Did not add "{string_repr}" to the DB. It already exists.',
                     )
                 except sqlcipher.InterfaceError:  # pylint: disable=no-member
                     log.critical(f'Interface error with tuple: {entry}')
