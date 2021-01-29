60274434b07b83c789976788566ca48d2b0d16cb
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-29 13:56:02 +0100

    Updates purge mechanism

--- frontend/app/src/components/settings/data-security/DataManagement.vue
+++ frontend/app/src/components/settings/data-security/DataManagement.vue
@@ -6,85 +6,28 @@
         <v-card-subtitle v-text="$t('data_management.subtitle')" />
         <v-form ref="form">
           <v-card-text>
-            <div
-              class="title"
-              v-text="$t('data_management.exchange_data.title')"
-            />
-            <v-row align="center">
+            <v-row>
               <v-col>
-                <v-select
-                  v-model="exchange"
-                  class="data-management__fields__exchange"
-                  :items="exchanges"
-                  :success-messages="purgeSuccessMessages"
-                  :error-messages="exchangePurgeError"
-                  label="Exchange"
-                >
-                  <template #item="{ item, attrs, on }">
-                    <v-list-item
-                      :class="`exchange__${item}`"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      {{ item }}
-                    </v-list-item>
-                  </template>
-                </v-select>
-              </v-col>
-              <v-col cols="auto">
-                <v-tooltip top>
-                  <template #activator="{ on, attrs }">
-                    <v-btn
-                      class="data-management__purge-exchange"
-                      icon
-                      :disabled="!!!exchange"
-                      v-bind="attrs"
-                      v-on="on"
-                      @click="confirmationType = 'single'"
-                    >
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                  </template>
-                  <span
-                    v-text="$t('data_management.exchange_data.purge_tooltip')"
-                  />
-                </v-tooltip>
+                <purge-selector
+                  v-model="source"
+                  :status="status"
+                  :pending="pending"
+                  @purge="showConfirmation($event)"
+                />
               </v-col>
             </v-row>
-            <status-button
-              class="data-management__purge-all-exchange"
-              :tooltip="$t('data_management.exchange_data.purge_all_tooltip')"
-              :success-message="purgeAllSuccessMessage"
-              :error-message="purgeAllErrorMessage"
-              @click="confirmationType = 'exchanges'"
-            >
-              {{ $t('data_management.exchange_data.purge_all_button') }}
-            </status-button>
-            <div
-              class="title"
-              v-text="$t('data_management.ethereum_transactions.title')"
-            />
-            <status-button
-              class="data-management__purge-transactions"
-              :tooltip="
-                $t('data_management.ethereum_transactions.purge_tooltip')
-              "
-              :success-message="transactionPurgeSuccessMessage"
-              :error-message="transactionPurgeErrorMessage"
-              @click="confirmationType = 'transactions'"
-            >
-              {{ $t('data_management.ethereum_transactions.purge_button') }}
-            </status-button>
           </v-card-text>
         </v-form>
       </v-card>
       <confirm-dialog
-        v-if="!!confirmationType"
+        v-if="confirm"
         display
-        :title="confirmations[confirmationType].title"
-        :message="confirmations[confirmationType].message"
-        @confirm="confirmations[confirmationType].confirm"
-        @cancel="confirmationType = ''"
+        :title="$t('data_management.confirm.title')"
+        :message="
+          $t('data_management.confirm.message', { source: sourceLabel })
+        "
+        @confirm="purge(source)"
+        @cancel="confirm = false"
       />
     </v-col>
   </v-row>
@@ -92,149 +35,74 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import ConfirmDialog from '@/components/dialogs/ConfirmDialog.vue';
+import PurgeSelector, {
+  ALL_TRANSACTIONS,
+  ALL_MODULES,
+  ALL_EXCHANGES,
+  Purgable,
+  PurgeParams
+} from '@/components/settings/data-security/PurgeSelector.vue';
 import StatusButton from '@/components/settings/data-security/StatusButton.vue';
 import { SUPPORTED_EXCHANGES } from '@/data/defaults';
 import { SupportedExchange } from '@/services/balances/types';
-
-type ConfirmationData = {
-  readonly title: string;
-  readonly message: string;
-  readonly confirm: () => Promise<void>;
-};
-
-type AllConfirmations = { [key: string]: ConfirmationData };
-
-type ConfirmationType = 'transactions' | 'exchanges' | 'single' | '';
+import { MODULES } from '@/services/session/consts';
+import { SupportedModules } from '@/services/session/types';
+import { ActionStatus } from '@/store/types';
 
 @Component({
-  components: { StatusButton, ConfirmDialog }
+  components: { PurgeSelector, StatusButton, ConfirmDialog }
 })
 export default class DataManagement extends Vue {
-  exchange: SupportedExchange = SUPPORTED_EXCHANGES[0];
-  exchanges = SUPPORTED_EXCHANGES;
+  source: Purgable = ALL_TRANSACTIONS;
+  status: ActionStatus | null = null;
+  confirm: boolean = false;
+  pending: boolean = false;
+  sourceLabel: string = '';
 
-  transactionPurgeSuccess: boolean = false;
-  exchangePurgeSuccess: SupportedExchange | '' = '';
-  allExchangePurgeSuccess: boolean = false;
-
-  exchangePurgeError: string[] = [];
-  transactionPurgeError: string = '';
-  allExchangePurgeError: string = '';
-
-  confirmationType: ConfirmationType = '';
-
-  get purgeSuccessMessages(): string[] {
-    const exchangePurgeSuccess = this.exchangePurgeSuccess;
-    if (exchangePurgeSuccess) {
-      const message = this.$t('data_management.exchange_data.purge_success', {
-        exchangePurgeSuccess
-      });
-      return [message.toString()];
-    }
-    return [];
+  showConfirmation(source: PurgeParams) {
+    this.sourceLabel = source.text;
+    this.confirm = true;
   }
 
-  get purgeAllSuccessMessage(): string | null {
-    return this.allExchangePurgeSuccess
-      ? this.$t('data_management.exchange_data.purge_all_success').toString()
-      : null;
-  }
-
-  get purgeAllErrorMessage(): string | null {
-    return this.allExchangePurgeError
-      ? this.$t('data_management.exchange_data.purge_all_error').toString()
-      : null;
-  }
-
-  get transactionPurgeSuccessMessage(): string | null {
-    return this.transactionPurgeSuccess
-      ? this.$t(
-          'data_management.ethereum_transactions.purge_success'
-        ).toString()
-      : null;
-  }
-
-  get transactionPurgeErrorMessage(): string | null {
-    return this.transactionPurgeError
-      ? this.$t('data_management.ethereum_transactions.purge_error').toString()
-      : null;
-  }
-
-  readonly confirmations: AllConfirmations = {
-    transactions: {
-      title: this.$t('data_management.confirm.transactions.title').toString(),
-      message: this.$t(
-        'data_management.confirm.transactions.message'
-      ).toString(),
-      confirm: this.purgeEthereumTransactions
-    },
-    exchanges: {
-      title: this.$t('data_management.confirm.all_exchanges.title').toString(),
-      message: this.$t(
-        'data_management.confirm.all_exchanges.message'
-      ).toString(),
-      confirm: this.purgeAllExchangeData
-    },
-    single: {
-      title: this.$t('data_management.confirm.exchange.title').toString(),
-      message: this.$t('data_management.confirm.exchange.message').toString(),
-      confirm: this.purgeExchangeData
-    }
-  };
-
-  async purgeExchangeData() {
-    for (let i = 0; i < this.exchangePurgeError.length; i++) {
-      this.exchangePurgeError.pop();
-    }
-
-    this.confirmationType = '';
-    const name = this.exchange;
+  async purge(source: string) {
+    this.confirm = false;
     try {
-      await this.$api.balances.deleteExchangeData(name);
-      this.exchangePurgeSuccess = name;
-      setTimeout(() => {
-        this.exchangePurgeSuccess = '';
-      }, 5000);
+      this.pending = true;
+      await this.purgeSource(source);
+      this.status = {
+        message: this.$t('data_management.success', {
+          source: this.sourceLabel
+        }).toString(),
+        success: true
+      };
+      setTimeout(() => (this.status = null), 5000);
     } catch (e) {
-      this.exchangePurgeError.push(
-        this.$t('data_management.errors.exchange', {
-          name,
-          message: e.message
-        }).toString()
-      );
+      this.status = {
+        message: this.$t('data_management.error', {
+          source: this.sourceLabel
+        }).toString(),
+        success: false
+      };
+    } finally {
+      this.pending = false;
     }
   }
 
-  async purgeAllExchangeData() {
-    this.confirmationType = '';
-    try {
-      await this.$api.balances.deleteExchangeData();
-      this.allExchangePurgeSuccess = true;
-      setTimeout(() => {
-        this.allExchangePurgeSuccess = false;
-      }, 5000);
-    } catch (e) {
-      this.allExchangePurgeError = this.$t(
-        'data_management.errors.all_exchanges',
-        { message: e.message }
-      ).toString();
-    }
-  }
-
-  async purgeEthereumTransactions() {
-    this.transactionPurgeError = '';
-    this.confirmationType = '';
-    try {
+  private async purgeSource(source: string) {
+    if (source === ALL_TRANSACTIONS) {
       await this.$api.balances.deleteEthereumTransactions();
-      this.transactionPurgeSuccess = true;
-      setTimeout(() => {
-        this.transactionPurgeSuccess = false;
-      }, 5000);
-    } catch (e) {
-      this.transactionPurgeError = this.$t(
-        'data_management.errors.transactions',
-        { message: e.message }
-      ).toString();
+    } else if (source === ALL_MODULES) {
+      await this.$api.balances.deleteModuleData();
+    } else if (source === ALL_EXCHANGES) {
+      await this.$api.balances.deleteExchangeData();
+    } else {
+      if (SUPPORTED_EXCHANGES.includes(source as any)) {
+        await this.$api.balances.deleteExchangeData(
+          source as SupportedExchange
+        );
+      } else if (MODULES.includes(source as any)) {
+        await this.$api.balances.deleteModuleData(source as SupportedModules);
+      }
     }
   }
 }
---
+++ frontend/app/src/components/settings/data-security/PurgeSelector.vue
@@ -0,0 +1,120 @@
+<template>
+  <fragment>
+    <v-row align="center">
+      <v-col>
+        <v-autocomplete
+          outlined
+          :value="value"
+          :label="$t('purge_selector.label')"
+          :items="purgable"
+          item-text="text"
+          item-value="id"
+          :disabled="pending"
+          @input="input"
+        />
+      </v-col>
+      <v-col cols="auto">
+        <div class="pb-6">
+          <v-tooltip open-delay="400" top>
+            <template #activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                icon
+                :disabled="!value || pending"
+                :loading="pending"
+                v-on="on"
+                @click="purge({ source: value, text: text(value) })"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+            <span> {{ $t('purge_selector.tooltip') }} </span>
+          </v-tooltip>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <action-status-indicator :status="status" />
+      </v-col>
+    </v-row>
+  </fragment>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import { DEFI_MODULES } from '@/components/defi/wizard/consts';
+import ActionStatusIndicator from '@/components/error/ActionStatusIndicator.vue';
+import Fragment from '@/components/helper/Fragment';
+import { tradeLocations } from '@/components/history/consts';
+import { SUPPORTED_EXCHANGES } from '@/data/defaults';
+import { MODULES } from '@/services/session/consts';
+import { ActionStatus } from '@/store/types';
+
+export const ALL_EXCHANGES = 'all_exchanges';
+export const ALL_MODULES = 'all_modules';
+export const ALL_TRANSACTIONS = 'ethereum_transactions';
+
+const purgable = [
+  ALL_EXCHANGES,
+  ALL_MODULES,
+  ALL_TRANSACTIONS,
+  ...SUPPORTED_EXCHANGES,
+  ...MODULES
+];
+
+export type Purgable = typeof purgable[number];
+export type PurgeParams = { readonly source: Purgable; readonly text: string };
+
+@Component({
+  components: { ActionStatusIndicator, Fragment }
+})
+export default class PurgeSelector extends Vue {
+  readonly purgable = purgable.map(id => ({
+    id: id,
+    text: this.text(id)
+  }));
+
+  @Prop({ required: true })
+  value!: Purgable;
+
+  @Prop({ required: false })
+  status!: ActionStatus | null;
+
+  @Prop({ required: false, type: Boolean, default: false })
+  pending!: boolean;
+
+  @Emit()
+  input(_value: Purgable) {}
+
+  @Emit()
+  purge(_payload: PurgeParams) {}
+
+  text(source: Purgable) {
+    const location = tradeLocations.find(
+      ({ identifier }) => identifier === source
+    );
+    if (location) {
+      return this.$t('purge_selector.exchange', {
+        name: location.name
+      }).toString();
+    }
+
+    const module = DEFI_MODULES.find(({ identifier }) => identifier === source);
+    if (module) {
+      return this.$t('purge_selector.module', { name: module.name }).toString();
+    }
+
+    if (source === ALL_TRANSACTIONS) {
+      return this.$t('purge_selector.ethereum_transactions').toString();
+    } else if (source === ALL_EXCHANGES) {
+      return this.$t('purge_selector.all_exchanges').toString();
+    } else if (source === ALL_MODULES) {
+      return this.$t('purge_selector.all_modules').toString();
+    }
+    return source;
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
