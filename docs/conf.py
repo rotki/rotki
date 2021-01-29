6af89e6f27af8efb5ea9931b2d5f080e9379d4c6
Lefteris Karapetsas <lefteris@refu.co>
2021-01-29 20:35:41 +0100

    Bump version: 1.12.2 → 1.13.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018-2020, Lefteris Karapetsas'
 author = 'Lefteris Karapetsas'
 
 # The short X.Y version
-version = '1.12.2'
+version = '1.13.0'
 # The full version, including alpha/beta/rc tags
-release = '1.12.2'
+release = '1.13.0'
 
 
 # -- General configuration ---------------------------------------------------
