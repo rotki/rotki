1145de784d5f61d1bd4c749618dad1bad20e1021
Lefteris Karapetsas <lefteris@refu.co>
2021-03-08 21:30:17 +0100

    frontend: Use symbol if exists in display of asset details page

--- frontend/app/src/views/Assets.vue
+++ frontend/app/src/views/Assets.vue
@@ -5,13 +5,13 @@
         <crypto-icon :symbol="identifier" size="48px" />
       </v-col>
       <v-col class="d-flex flex-column">
-        <span class="text-h5 font-weight-medium">{{ identifier }}</span>
+        <span class="text-h5 font-weight-medium">{{ symbol }}</span>
         <span class="text-subtitle-2 text--secondary">
           {{ assetName }}
         </span>
       </v-col>
     </v-row>
-    <asset-value-row class="mt-8" :identifier="identifier" />
+    <asset-value-row class="mt-8" :identifier="identifier" :symbol="symbol" />
     <asset-amount-and-value-over-time
       v-if="premium"
       class="mt-8"
@@ -46,6 +46,11 @@ export default class Assets extends Mixins(PremiumMixin) {
   get assetName(): string {
     const asset = this.assetInfo(this.identifier);
     return asset ? asset.name : '';
+  }
+
+  get symbol(): string {
+    const asset = this.assetInfo(this.identifier);
+    return asset.symbol ? asset.symbol : this.identifier;
   }
 }
 </script>
