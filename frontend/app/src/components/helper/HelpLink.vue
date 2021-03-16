2eb3fccb8f893aed7d5b383dd6b87fec666325d2
Konstantinos Paparas <kelsos86@gmail.com>
2021-03-16 11:38:53 +0100

    Introduces a basic help link on asset-manager
    Provides a direct link to help on locating CryptoCompare and CoinGecko
    identifiers for assets.

---
+++ frontend/app/src/components/helper/HelpLink.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <v-tooltip open-delay="400" top>
+    <template #activator="{ attrs, on }">
+      <v-btn
+        icon
+        :small="small"
+        v-bind="attrs"
+        :href="$interop.isPackaged ? undefined : url"
+        target="_blank"
+        v-on="on"
+        @click="$interop.isPackaged ? openLink() : undefined"
+      >
+        <v-icon :small="small">mdi-help-circle</v-icon>
+      </v-btn>
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class HelpLink extends Vue {
+  @Prop({ required: true, type: String })
+  url!: string;
+  @Prop({ required: true, type: String })
+  tooltip!: string;
+  @Prop({ required: false, default: false, type: Boolean })
+  small!: boolean;
+
+  openLink() {
+    this.$interop.openUrl(this.url);
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
