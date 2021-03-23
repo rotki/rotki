b16946444f4ca635115137f38ed4b47fcb4e5bbd
Konstantinos Paparas <kelsos86@gmail.com>
2021-03-23 11:47:38 +0100

    Fixes wrong value being displayed in assets page

---
+++ frontend/app/src/utils/setup-formatter.ts
@@ -0,0 +1,21 @@
+import { default as BigNumber } from 'bignumber.js';
+
+export function setupFormatter() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  // @ts-ignore
+  window.devtoolsFormatters = [
+    {
+      header: function (obj: any) {
+        if (!(obj instanceof BigNumber)) {
+          return null;
+        }
+        return ['div', {}, obj.toString()];
+      },
+      hasBody: function () {
+        return false;
+      }
+    }
+  ];
+}
