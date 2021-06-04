f2ae3e25655313552fb69771cb65b27b7edb5c07
Lefteris Karapetsas <lefteris@refu.co>
2021-06-04 15:02:44 +0200

    Bump version: 1.17.1 → 1.17.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018-2021, Rotki Solutions GmbH'
 author = 'The rotki team'
 
 # The short X.Y version
-version = '1.17.1'
+version = '1.17.2'
 # The full version, including alpha/beta/rc tags
-release = '1.17.1'
+release = '1.17.2'
 
 
 # -- General configuration ---------------------------------------------------
