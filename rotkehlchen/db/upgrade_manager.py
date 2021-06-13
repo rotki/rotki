0c2ea2167c006919f4e4dd3c750db5b7a2c28b79
Lefteris Karapetsas <lefteris@refu.co>
2021-06-13 12:07:52 +0200

    Upgrade DB to v27, deleting unused tables and upgrading some tables

--- rotkehlchen/db/upgrade_manager.py
+++ rotkehlchen/db/upgrade_manager.py
@@ -28,6 +28,7 @@ from rotkehlchen.db.upgrades.v22_v23 import upgrade_v22_to_v23
 from rotkehlchen.db.upgrades.v23_v24 import upgrade_v23_to_v24
 from rotkehlchen.db.upgrades.v24_v25 import upgrade_v24_to_v25
 from rotkehlchen.db.upgrades.v25_v26 import upgrade_v25_to_v26
+from rotkehlchen.db.upgrades.v26_v27 import upgrade_v26_to_v27
 from rotkehlchen.errors import DBUpgradeError
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.utils.misc import ts_now
@@ -183,6 +184,10 @@ UPGRADES_LIST = [
     UpgradeRecord(
         from_version=25,
         function=upgrade_v25_to_v26,
+    ),
+    UpgradeRecord(
+        from_version=26,
+        function=upgrade_v26_to_v27,
     ),
 ]
 
