4744482550309f40d28d668f4f9cff2706de5da8
Konstantinos Paparas <kelsos86@gmail.com>
2021-11-05 13:45:13 +0100

    Introduces the new api for handling backups

---
+++ frontend/app/src/services/backup/backup-api.ts
@@ -0,0 +1,96 @@
+import { ActionResult } from '@rotki/common/lib/data';
+import { AxiosInstance } from 'axios';
+import { axiosCamelCaseTransformer } from '@/services/axios-tranformers';
+import {
+  CreateDatabaseResponse,
+  DatabaseInfo,
+  DatabaseInfoResponse,
+  DeleteDatabaseResponse
+} from '@/services/backup/types';
+import {
+  handleResponse,
+  validStatus,
+  validWithSessionStatus
+} from '@/services/utils';
+import { ActionStatus } from '@/store/types';
+import { assert } from '@/utils/assertions';
+
+export class BackupApi {
+  private static transformer = [axiosCamelCaseTransformer];
+  constructor(private readonly axios: AxiosInstance) {}
+
+  async info(): Promise<DatabaseInfo> {
+    const response = await this.axios.get<DatabaseInfoResponse>(
+      '/history/database/info',
+      {
+        validateStatus: validWithSessionStatus,
+        transformResponse: BackupApi.transformer
+      }
+    );
+
+    return handleResponse(response, response =>
+      DatabaseInfoResponse.parse(response.data)
+    );
+  }
+
+  async createBackup(): Promise<string> {
+    const response = await this.axios.get<CreateDatabaseResponse>(
+      '/history/database/backups',
+      {
+        validateStatus: validWithSessionStatus
+      }
+    );
+
+    return handleResponse(response, response =>
+      CreateDatabaseResponse.parse(response.data)
+    );
+  }
+
+  async deleteBackup(file: string): Promise<boolean> {
+    const response = await this.axios.delete<DeleteDatabaseResponse>(
+      '/history/database/backups',
+      {
+        data: {
+          file
+        },
+        validateStatus: validWithSessionStatus
+      }
+    );
+
+    return handleResponse(response, response =>
+      DeleteDatabaseResponse.parse(response.data)
+    );
+  }
+
+  async downloadFile(file: string): Promise<ActionStatus> {
+    try {
+      const response = await this.axios.get('', {
+        params: {
+          file: file
+        },
+        responseType: 'blob',
+        validateStatus: validStatus
+      });
+      if (response.status === 200) {
+        const url = window.URL.createObjectURL(response.data);
+        const link = document.createElement('a');
+        link.id = 'db-download-link';
+        link.href = url;
+        const filename = new URL(file).pathname.split('/').pop();
+        assert(filename);
+        link.setAttribute('download', filename);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        return { success: true };
+      }
+
+      const body = await (response.data as Blob).text();
+      const result: ActionResult<null> = JSON.parse(body);
+
+      return { success: false, message: result.message };
+    } catch (e: any) {
+      return { success: false, message: e.message };
+    }
+  }
+}
