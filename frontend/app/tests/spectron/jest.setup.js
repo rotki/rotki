b9d2bac0bf8d301aeab8f5dac4a30925c50bdc7f
Konstantinos Paparas <kelsos86@gmail.com>
2021-11-23 20:34:14 +0100

    Removes spectron

--- frontend/app/tests/spectron/jest.setup.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-// eslint-disable-next-line no-undef
-jasmine.getEnv().addReporter({
-  specStarted: result => (window.currentTest = result),
-  specDone: result => (window.currentTest = result)
-});
-
-dotenv.config({ path: './.env.test' });
