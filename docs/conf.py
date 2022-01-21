ece72fb41a97751055a8df2ae47edad41683c559
Lefteris Karapetsas <lefteris@refu.co>
2022-01-21 11:40:09 +0100

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'rotki'
-copyright = '2018-2021, Rotki Solutions GmbH'
+copyright = '2018-2020, Eleftherios Karapetsas. 2020-2022, Rotki Solutions GmbH'
 author = 'The rotki team'
 
 # The short X.Y version
