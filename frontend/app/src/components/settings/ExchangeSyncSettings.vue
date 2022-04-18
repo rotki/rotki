c2cdb93ae883e87a92eb9eb1123e1c67ed300ea3
Luki Centuri <lukicenturi@gmail.com>
2022-04-18 19:44:13 +0700

    feat: add option to toggle exchanges sync

---
+++ frontend/app/src/components/settings/ExchangeSyncSettings.vue
@@ -0,0 +1,13 @@
+<template>
+  <setting-category>
+    <template #title>
+      {{ $t('exchange_sync_settings.title') }}
+    </template>
+
+    <v-row>
+      <v-col>
+
+      </v-col>
+    </v-row>
+  </setting-category>
+</template>
