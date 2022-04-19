b224e7606bbd89c59dc0659f1f8038f498531563
nebolax <alexey.nebolsin.jr@gmail.com>
2022-04-19 22:21:39 +0400

    Add snapshot currency conversion

--- rotkehlchen/db/snapshots.py
+++ rotkehlchen/db/snapshots.py
@@ -6,8 +6,11 @@ from zipfile import ZIP_DEFLATED, ZipFile
 from rotkehlchen.accounting.export.csv import CSVWriteError, _dict_to_csv_file
 from rotkehlchen.accounting.structures.balance import BalanceType
 from rotkehlchen.assets.asset import Asset
+from rotkehlchen.constants.assets import A_USD
 from rotkehlchen.db.dbhandler import DBHandler
 from rotkehlchen.db.utils import DBAssetBalance, LocationData
+from rotkehlchen.fval import FVal
+from rotkehlchen.history.price import PriceHistorian
 from rotkehlchen.types import Timestamp
 
 BALANCES_FILENAME = 'balances_snapshot.csv'
@@ -18,13 +21,24 @@ class DBSnapshot:
     def __init__(self, db_handler: DBHandler) -> None:
         self.db = db_handler
 
-    def get_timed_balances(self, timestamp: Timestamp) -> List[DBAssetBalance]:
-        """Retrieves the timed_balances from the db for a given timestamp"""
+    def get_timed_balances(
+        self,
+        timestamp: Timestamp,
+        main_currency: Asset,
+    ) -> List[DBAssetBalance]:
+        """Retrieves the timed_balances from the db for a given timestamp
+           Sets the usd_value to the value in the main currency
+        """
         balances_data = []
         cursor = self.db.conn.cursor()
         timed_balances_result = cursor.execute(
             'SELECT category, time, amount, currency, usd_value FROM timed_balances '
             'WHERE time=?', (timestamp,),
+        )
+        main_currency_price = PriceHistorian.query_historical_price(
+            from_asset=A_USD,
+            to_asset=main_currency,
+            timestamp=timestamp,
         )
         for data in timed_balances_result:
             balances_data.append(
@@ -33,13 +47,19 @@ class DBSnapshot:
                     time=data[1],
                     amount=data[2],
                     asset=Asset(data[3]),
-                    usd_value=data[4],
+                    usd_value=str(FVal(data[4]) * main_currency_price),
                 ),
             )
         return balances_data
 
-    def get_timed_location_data(self, timestamp: Timestamp) -> List[LocationData]:
-        """Retrieves the timed_location_data from the db for a given timestamp"""
+    def get_timed_location_data(
+        self,
+        timestamp: Timestamp,
+        main_currency: Asset,
+    ) -> List[LocationData]:
+        """Retrieves the timed_location_data from the db for a given timestamp
+           Sets the usd_value to the value in the main currency
+        """
         location_data = []
         cursor = self.db.conn.cursor()
         timed_location_data = cursor.execute(
@@ -47,12 +67,17 @@ class DBSnapshot:
             'WHERE time=?',
             (timestamp,),
         )
+        main_currency_price = PriceHistorian.query_historical_price(
+            from_asset=A_USD,
+            to_asset=main_currency,
+            timestamp=timestamp,
+        )
         for data in timed_location_data:
             location_data.append(
                 LocationData(
                     time=data[0],
                     location=data[1],
-                    usd_value=data[2],
+                    usd_value=str(FVal(data[2]) * main_currency_price),
                 ),
             )
         return location_data
@@ -61,6 +86,7 @@ class DBSnapshot:
         self,
         timed_balances: List[DBAssetBalance],
         timed_location_data: List[LocationData],
+        main_currency: Asset,
     ) -> Tuple[bool, str]:
         """Creates a zip file of csv files containing timed_balances and timed_location_data."""
         dirpath = Path(mkdtemp())
@@ -68,6 +94,7 @@ class DBSnapshot:
             timed_balances=timed_balances,
             timed_location_data=timed_location_data,
             directory=dirpath,
+            main_currency=main_currency,
         )
         if not success:
             return False, msg
@@ -99,8 +126,15 @@ class DBSnapshot:
         Otherwise, a zip file is created and the snapshot generated is written to the file
         and a path to the zip file is returned.
         """
-        timed_balances = self.get_timed_balances(timestamp)
-        timed_location_data = self.get_timed_location_data(timestamp)
+        main_currency = self.db.get_settings().main_currency
+        timed_balances = self.get_timed_balances(
+            timestamp=timestamp,
+            main_currency=main_currency,
+        )
+        timed_location_data = self.get_timed_location_data(
+            timestamp=timestamp,
+            main_currency=main_currency,
+        )
 
         if len(timed_balances) == 0 or len(timed_location_data) == 0:
             return False, 'No snapshot data found for the given timestamp.'
@@ -109,12 +143,14 @@ class DBSnapshot:
             return self.create_zip(
                 timed_balances=timed_balances,
                 timed_location_data=timed_location_data,
+                main_currency=main_currency,
             )
 
         return self._export(
             timed_balances=timed_balances,
             timed_location_data=timed_location_data,
             directory=directory_path,
+            main_currency=main_currency,
         )
 
     @staticmethod
@@ -122,12 +158,13 @@ class DBSnapshot:
         timed_balances: List[DBAssetBalance],
         timed_location_data: List[LocationData],
         directory: Path,
+        main_currency: Asset,
     ) -> Tuple[bool, str]:
         """Serializes the balances and location_data snapshots into a dictionary.
         It then writes the serialized data to a csv file.
         """
-        serialized_timed_balances = [balance.serialize() for balance in timed_balances]
-        serialized_timed_location_data = [loc_data.serialize() for loc_data in timed_location_data]
+        serialized_timed_balances = [balance.serialize(currency=main_currency) for balance in timed_balances]  # noqa: E501
+        serialized_timed_location_data = [loc_data.serialize(currency=main_currency) for loc_data in timed_location_data]  # noqa: E501
 
         try:
             directory.mkdir(parents=True, exist_ok=True)
--- rotkehlchen/tests/api/test_snapshots.py
+++ rotkehlchen/tests/api/test_snapshots.py
@@ -7,52 +7,56 @@ from pathlib import Path
 
 import requests
 
+from rotkehlchen.assets.asset import Asset
+from rotkehlchen.constants.assets import A_ETH, A_EUR, A_USD
+from rotkehlchen.db.settings import ModifiableDBSettings
 from rotkehlchen.db.snapshots import BALANCES_FILENAME, LOCATION_DATA_FILENAME
 from rotkehlchen.tests.utils.api import api_url_for, assert_simple_ok_response
-from rotkehlchen.types import Location
+from rotkehlchen.types import Location, Timestamp
+from rotkehlchen.utils.misc import ts_now
 
 
-def _populate_db_with_balances(connection):
+def _populate_db_with_balances(connection, ts: Timestamp):
     cursor = connection.cursor()
     cursor.execute(
         """
         INSERT INTO "timed_balances" ("category", "time", "currency", "amount", "usd_value") VALUES
         (?, ?, ?, ?, ?);
-        """, ('A', 1, 'ADA', '1.00', '178.44'),
+        """, ('A', ts, 'ADA', '1.00', '178.44'),
     )
     cursor.execute(
         """
         INSERT INTO "timed_balances" ("category", "time", "currency", "amount", "usd_value") VALUES
         (?, ?, ?, ?, ?);
-        """, ('A', 1, 'AVAX', '1.00', '87'),
+        """, ('A', ts, 'AVAX', '1.00', '87'),
     )
     connection.commit()
 
 
-def _populate_db_with_location_data(connection):
+def _populate_db_with_location_data(connection, ts: Timestamp):
     cursor = connection.cursor()
     cursor.execute(
         """
         INSERT INTO "timed_location_data" ("time", "location", "usd_value") VALUES
         (?, ?, ?);
-        """, (1, 'A', '100.00'),
+        """, (ts, 'A', '100.00'),
     )
     cursor.execute(
         """
         INSERT INTO "timed_location_data" ("time", "location", "usd_value") VALUES
         (?, ?, ?);
-        """, (1, 'B', '200.00'),
+        """, (ts, 'B', '200.00'),
     )
     cursor.execute(
         """
         INSERT INTO "timed_location_data" ("time", "location", "usd_value") VALUES
         (?, ?, ?);
-        """, (1, 'H', '50.00'),
+        """, (ts, 'H', '50.00'),
     )
     connection.commit()
 
 
-def assert_csv_export_response(response, csv_dir, is_download=False):
+def assert_csv_export_response(response, csv_dir, main_currency: Asset, is_download=False):
     if is_download:
         assert response.status_code == HTTPStatus.OK
     else:
@@ -68,7 +72,7 @@ def assert_csv_export_response(response, csv_dir, is_download=False):
                 'liability',
             )
             assert row['amount'] is not None
-            assert row['usd_value'] is not None
+            assert row[f'{main_currency.symbol.lower()}_value'] is not None
             count += 1
         assert count == 2
 
@@ -78,56 +82,73 @@ def assert_csv_export_response(response, csv_dir, is_download=False):
         for row in reader:
             assert len(row) == 2
             assert Location.deserialize(row['location']) is not None
-            assert row['usd_value'] is not None
+            assert row[f'{main_currency.symbol.lower()}_value'] is not None
             count += 1
         assert count == 3
 
 
 def test_export_snapshot(rotkehlchen_api_server, tmpdir_factory):
     conn = rotkehlchen_api_server.rest_api.rotkehlchen.data.db.conn
-
+    ts = Timestamp(ts_now())
     csv_dir = str(tmpdir_factory.mktemp('test_csv_dir'))
     csv_dir2 = str(tmpdir_factory.mktemp('test_csv_dir2'))
-    _populate_db_with_balances(conn)
-    _populate_db_with_location_data(conn)
+    _populate_db_with_balances(conn, ts)
+    _populate_db_with_location_data(conn, ts)
 
+    rotkehlchen_api_server.rest_api.rotkehlchen.data.db.set_settings(ModifiableDBSettings(main_currency=A_EUR))  # noqa: E501
     response = requests.post(
         api_url_for(
             rotkehlchen_api_server,
             'dbsnapshotexportingresource',
         ),
         json={
-            'timestamp': 1,
+            'timestamp': ts,
             'path': csv_dir,
         },
     )
-    assert_csv_export_response(response, csv_dir, is_download=False)
+    assert_csv_export_response(response, csv_dir, main_currency=A_EUR, is_download=False)
+
+    rotkehlchen_api_server.rest_api.rotkehlchen.data.db.set_settings(ModifiableDBSettings(main_currency=A_ETH))  # noqa: E501
     response = requests.post(
         api_url_for(
             rotkehlchen_api_server,
             'dbsnapshotexportingresource',
         ),
         json={
-            'timestamp': 1,
+            'timestamp': ts,
             'path': csv_dir2,
         },
     )
-    assert_csv_export_response(response, csv_dir2, is_download=False)
+    assert_csv_export_response(response, csv_dir2, main_currency=A_ETH, is_download=False)
+
+    rotkehlchen_api_server.rest_api.rotkehlchen.data.db.set_settings(ModifiableDBSettings(main_currency=A_USD))  # noqa: E501
+    response = requests.post(
+        api_url_for(
+            rotkehlchen_api_server,
+            'dbsnapshotexportingresource',
+        ),
+        json={
+            'timestamp': ts,
+            'path': csv_dir2,
+        },
+    )
+    assert_csv_export_response(response, csv_dir2, main_currency=A_USD, is_download=False)
 
 
 def test_download_snapshot(rotkehlchen_api_server):
     conn = rotkehlchen_api_server.rest_api.rotkehlchen.data.db.conn
+    ts = Timestamp(ts_now())
+    _populate_db_with_balances(conn, ts)
+    _populate_db_with_location_data(conn, ts)
 
-    _populate_db_with_balances(conn)
-    _populate_db_with_location_data(conn)
-
+    rotkehlchen_api_server.rest_api.rotkehlchen.data.db.set_settings(ModifiableDBSettings(main_currency=A_EUR))  # noqa: E501
     response = requests.post(
         api_url_for(
             rotkehlchen_api_server,
             'dbsnapshotdownloadingresource',
         ),
         json={
-            'timestamp': 1,
+            'timestamp': ts,
         },
     )
     with tempfile.TemporaryDirectory() as tmpdirname:
@@ -136,4 +157,4 @@ def test_download_snapshot(rotkehlchen_api_server):
         tempzipfile.write_bytes(response.content)
         with zipfile.ZipFile(tempzipfile, 'r') as zip_ref:
             zip_ref.extractall(extractdir)
-        assert_csv_export_response(response, extractdir, is_download=True)
+        assert_csv_export_response(response, extractdir, main_currency=A_EUR, is_download=True)
