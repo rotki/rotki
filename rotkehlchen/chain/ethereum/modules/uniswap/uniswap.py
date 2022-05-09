baa62aae479304783945472384e1f44078ff292c
Isaac Adewumi <ienioladewumi@gmail.com>
2022-05-09 07:51:20 +0100

    switch from amm subgraph to uniswap v3 oracle for unknown assets price

--- rotkehlchen/chain/ethereum/modules/uniswap/uniswap.py
+++ rotkehlchen/chain/ethereum/modules/uniswap/uniswap.py
@@ -11,15 +11,22 @@ from rotkehlchen.chain.ethereum.interfaces.ammswap.types import (
     AddressEvents,
     AddressEventsBalances,
     AddressToLPBalances,
-    AddressToV3LPBalances,
     AddressTrades,
     AssetToPrice,
     DDAddressEvents,
     EventType,
     ProtocolBalance,
-    V3ProtocolBalance,
 )
 from rotkehlchen.chain.ethereum.interfaces.ammswap.utils import SUBGRAPH_REMOTE_ERROR_MSG
+from rotkehlchen.chain.ethereum.modules.uniswap.utils import (
+    get_latest_lp_addresses,
+    uniswap_lp_token_balances,
+)
+from rotkehlchen.chain.ethereum.modules.uniswap.v3.types import (
+    AddressToUniswapV3LPBalances,
+    UniswapV3ProtocolBalance,
+)
+from rotkehlchen.chain.ethereum.modules.uniswap.v3.utils import uniswap_v3_lp_token_balances
 from rotkehlchen.chain.ethereum.trades import AMMSwap, AMMTrade
 from rotkehlchen.constants import ZERO
 from rotkehlchen.errors.misc import ModuleInitializationFailure, RemoteError
@@ -36,7 +43,6 @@ from rotkehlchen.user_messages import MessagesAggregator
 from rotkehlchen.utils.interfaces import EthereumModule
 
 from .graph import V3_SWAPS_QUERY
-from .utils import get_latest_lp_addresses, uniswap_lp_token_balances, uniswap_v3_lp_token_balances
 
 if TYPE_CHECKING:
     from rotkehlchen.accounting.structures.balance import AssetBalance
@@ -114,28 +120,27 @@ class Uniswap(AMMSwapPlatform, EthereumModule):
         )
         return protocol_balance
 
-    def get_v3_balances_chain(self, addresses: List[ChecksumEthAddress]) -> V3ProtocolBalance:
+    def get_v3_balances_chain(self, addresses: List[ChecksumEthAddress]) -> UniswapV3ProtocolBalance:  # noqa: 501
         """Get the addresses' Uniswap V3 pools data via chain queries."""
-        known_assets: Set[EthereumToken] = set()
-        unknown_assets: Set[EthereumToken] = set()
+        price_known_assets: Set[EthereumToken] = set()
+        price_unknown_assets: Set[EthereumToken] = set()
 
         address_mapping = {}
         for address in addresses:
             pool_balances = uniswap_v3_lp_token_balances(
                 userdb=self.database,
-                premium=self.premium,
                 address=address,
                 ethereum=self.ethereum,
-                known_assets=known_assets,
-                unknown_assets=unknown_assets,
+                price_known_assets=price_known_assets,
+                price_unknown_assets=price_unknown_assets,
             )
             if len(pool_balances) != 0:
                 address_mapping[address] = pool_balances
 
-        protocol_balance = V3ProtocolBalance(
+        protocol_balance = UniswapV3ProtocolBalance(
             address_balances=address_mapping,
-            known_assets=known_assets,
-            unknown_assets=unknown_assets,
+            known_assets=price_known_assets,
+            unknown_assets=price_unknown_assets,
         )
         return protocol_balance
 
@@ -535,12 +540,8 @@ class Uniswap(AMMSwapPlatform, EthereumModule):
     def get_v3_balances(
         self,
         addresses: List[ChecksumEthAddress],
-    ) -> AddressToV3LPBalances:
-        """Get the addresses' balances in the Uniswap V3 protocol
-
-        Premium users can request balances either via the Uniswap subgraph or
-        on-chain.
-        """
+    ) -> AddressToUniswapV3LPBalances:
+        """Get the addresses' balances in the Uniswap V3 protocol."""
         protocol_balance = self.get_v3_balances_chain(addresses)
         known_assets = protocol_balance.known_assets
         unknown_assets = protocol_balance.unknown_assets
@@ -550,8 +551,7 @@ class Uniswap(AMMSwapPlatform, EthereumModule):
         )
 
         unknown_asset_price: AssetToPrice = {}
-        if self.premium:
-            unknown_asset_price = self._get_unknown_asset_price_graph(unknown_assets=unknown_assets)  # noqa:E501
+        unknown_asset_price = self._get_unknown_asset_price_chain(unknown_assets=unknown_assets)
 
         self._update_assets_prices_in_address_balances(
             address_balances=protocol_balance.address_balances,
