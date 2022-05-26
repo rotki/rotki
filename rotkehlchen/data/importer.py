c99344131e31cf804281438bc688603a08d4e73e
nebolax <alexey.nebolsin.jr@gmail.com>
2022-05-26 20:49:20 +0400

    Add context switching for csv imports

--- rotkehlchen/data/importer.py
+++ rotkehlchen/data/importer.py
@@ -6,6 +6,7 @@ from itertools import count
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple
 
+import gevent
 from pysqlcipher3 import dbapi2 as sqlcipher
 
 from rotkehlchen.accounting.ledger_actions import LedgerAction, LedgerActionType
@@ -47,6 +48,7 @@ logger = logging.getLogger(__name__)
 log = RotkehlchenLogsAdapter(logger)
 
 SAI_TIMESTAMP = 1574035200
+ROWS_PER_YIELD = 500
 
 SINGLE_BINANCE_ENTRIES = [
     BinanceDepositWithdrawEntry(),
@@ -212,7 +214,9 @@ class DataImporter():
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.reader(csvfile, delimiter=',', quotechar='"')
             header = remap_header(next(data))
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_cointracking_entry(dict(zip(header, row)), **kwargs)
                 except UnknownAsset as e:
@@ -570,7 +574,9 @@ class DataImporter():
                 asset = row['Currency']
                 investments_withdrawals[asset].append(row)
 
-        for timestamp in multiple_rows:
+        for idx, timestamp in enumerate(multiple_rows):
+            if idx % ROWS_PER_YIELD == 0:
+                gevent.sleep(0.1)
             # When we convert multiple assets dust to CRO
             # in one time, it will create multiple debited rows with
             # the same timestamp
@@ -643,7 +649,9 @@ class DataImporter():
 
         # Compute investments profit
         if len(investments_withdrawals) != 0:
-            for asset in investments_withdrawals:
+            for idx, asset in enumerate(investments_withdrawals):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 asset_object = asset_from_cryptocom(asset)
                 if asset not in investments_deposits:
                     log.error(
@@ -747,7 +755,9 @@ class DataImporter():
                 )
                 # continue, since they already are in DB
 
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_cryptocom_entry(row, **kwargs)
                 except UnknownAsset as e:
@@ -873,7 +883,9 @@ class DataImporter():
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_blockfi_entry(row, **kwargs)
                 except UnknownAsset as e:
@@ -947,7 +959,9 @@ class DataImporter():
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_blockfi_trade(row, **kwargs)
                 except UnknownAsset as e:
@@ -1082,7 +1096,9 @@ class DataImporter():
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_nexo(row, **kwargs)
                 except UnknownAsset as e:
@@ -1178,7 +1194,9 @@ Trade from ShapeShift with ShapeShift Deposit Address:
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_shapeshift_trade(row, **kwargs)
                 except UnknownAsset as e:
@@ -1348,7 +1366,9 @@ Activity from uphold with uphold transaction id:
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_uphold_transaction(row, **kwargs)
                 except UnknownAsset as e:
@@ -1442,7 +1462,9 @@ Activity from uphold with uphold transaction id:
         """
         with open(filepath, 'r', encoding='utf-8-sig') as csvfile:
             data = csv.DictReader(csvfile)
-            for row in data:
+            for idx, row in enumerate(data):
+                if idx % ROWS_PER_YIELD == 0:
+                    gevent.sleep(0.1)
                 try:
                     self._consume_bisq_trade(row, **kwargs)
                 except UnknownAsset as e:
@@ -1535,7 +1557,9 @@ Activity from uphold with uphold transaction id:
     def _process_binance_rows(self, multi: Dict[Timestamp, List[BinanceCsvRow]]) -> None:
         stats: Dict[BinanceEntry, int] = defaultdict(int)
         skipped_rows: List[Any] = []
-        for timestamp, rows in multi.items():
+        for idx, (timestamp, rows) in enumerate(multi.items()):
+            if idx % ROWS_PER_YIELD == 0:
+                gevent.sleep(0.1)
             single_processed, rows_without_single = self._process_single_binance_entries(
                 timestamp=timestamp,
                 rows=rows,
