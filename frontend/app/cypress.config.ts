dd65998e77fc32097583a8f112f02d2c0266bf88
Konstantinos Paparas <kelsos86@gmail.com>
2022-06-02 16:06:33 +0200

    updates cypress, vite, vitest and vite plugins

---
+++ frontend/app/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    fixturesFolder: 'tests/e2e/fixtures',
+    specPattern: 'tests/e2e/specs/*.spec.ts',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    videosFolder: 'tests/e2e/videos',
+    supportFile: 'tests/e2e/support/index.ts',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      // include any other plugin code...
+      return config;
+    }
+  },
+  defaultCommandTimeout: 60000,
+  responseTimeout: 60000,
+  pageLoadTimeout: 120000
+});
