4a7eb1f4cd6c33f828ef33696dbec65762cf287d
nebolax <alexey.nebolsin.jr@gmail.com>
2022-06-21 14:54:18 +0300

    Handle genesis block ethereum transactions

--- rotkehlchen/chain/ethereum/constants.py
+++ rotkehlchen/chain/ethereum/constants.py
@@ -1,4 +1,5 @@
 from rotkehlchen.chain.ethereum.types import string_to_ethereum_address
+from rotkehlchen.types import Timestamp, deserialize_evm_tx_hash
 
 RANGE_PREFIX_ETHTX = 'ethtxs'
 RANGE_PREFIX_ETHINTERNALTX = 'ethinternaltxs'
@@ -9,5 +10,7 @@ MODULES_PREFIX = MODULES_PACKAGE + '.'
 MODULES_PREFIX_LENGTH = len(MODULES_PREFIX)
 
 ZERO_ADDRESS = string_to_ethereum_address('0x0000000000000000000000000000000000000000')
+GENESIS_HASH = deserialize_evm_tx_hash('0x' + '0' * 64)  # hash for transactions in genesis block
+ETHEREUM_BEGIN = Timestamp(1438269973)
 
 CPT_KRAKEN = 'kraken'
