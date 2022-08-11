8f06cd9fb753e91303c84982e1f614d06a06601b
Konstantinos Paparas <kelsos86@gmail.com>
2022-08-11 09:48:58 +0200

    Moves premium feature setup to watcher

--- frontend/app/src/premium/setup-premium.ts
+++ frontend/app/src/premium/setup-premium.ts
@@ -1,12 +1,14 @@
 import * as CompositionAPI from '@vue/composition-api';
+import { set } from '@vueuse/core';
 import * as BigNumber from 'bignumber.js';
 import * as Chart from 'chart.js';
 import ChartJsPluginZoom from 'chartjs-plugin-zoom';
+import { storeToRefs } from 'pinia';
 import Vue from 'vue';
 import * as zod from 'zod';
-import { registerComponents } from '@/premium/register-components';
+import { usePremiumStore } from '@/store/session/premium';
 
-export const setupPremium = () => {
+export const setupPremium = async () => {
   window.Vue = Vue;
   window.Chart = Chart;
   window['chartjs-plugin-zoom'] = ChartJsPluginZoom;
@@ -14,5 +16,9 @@ export const setupPremium = () => {
   window['@vue/composition-api'] = CompositionAPI;
   window.zod = zod;
   window.bn = BigNumber;
+  const { componentsLoaded } = storeToRefs(usePremiumStore());
+  set(componentsLoaded, false);
+  const { registerComponents } = await import('@/premium/register-components');
   registerComponents();
+  set(componentsLoaded, true);
 };
