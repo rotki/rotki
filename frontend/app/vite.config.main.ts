970a31a0b96894e460678f8f8147182a0efd37ea
Konstantinos Paparas <kelsos86@gmail.com>
2022-08-26 15:28:54 +0200

    Adds small vite-plugin to copy ps-list binary on windows

--- frontend/app/vite.config.main.ts
+++ frontend/app/vite.config.main.ts
@@ -1,9 +1,43 @@
+import * as fs from 'fs';
 import { builtinModules } from 'module';
-import { join } from 'path';
-import { defineConfig } from 'vite';
+import { platform } from 'os';
+import { join, parse } from 'path';
+import { defineConfig, Plugin } from 'vite';
 
 const PACKAGE_ROOT = __dirname;
 const isDevelopment = process.env.NODE_ENV === 'development';
+
+const binaryDependencyPlugin = (): Plugin => {
+  let outDir = '';
+  let rootDir = '';
+  return {
+    name: 'vite-plugin-binary-dependency',
+    configResolved(config) {
+      outDir = config.build.outDir;
+      rootDir = config.root;
+    },
+    async writeBundle() {
+      if (platform() !== 'win32') {
+        console.log('ps-list vendor skipped');
+        return;
+      }
+      const parsedPath = parse(rootDir);
+      const psList = join(parsedPath.dir, 'node_modules', 'ps-list', 'vendor');
+      const files = fs.readdirSync(psList);
+      const output = join(rootDir, outDir, 'vendor');
+      if (!fs.existsSync(output)) {
+        fs.mkdirSync(output);
+      }
+
+      for (const file of files) {
+        const src = join(psList, file);
+        const dest = join(output, file);
+        console.log(`${file} -> ${dest}`);
+        fs.copyFileSync(src, dest);
+      }
+    }
+  };
+};
 
 export default defineConfig({
   root: PACKAGE_ROOT,
@@ -13,6 +47,7 @@ export default defineConfig({
       '@': join(PACKAGE_ROOT, 'src') + '/'
     }
   },
+  plugins: [binaryDependencyPlugin()],
   build: {
     sourcemap: isDevelopment ? 'inline' : false,
     outDir: 'dist',
