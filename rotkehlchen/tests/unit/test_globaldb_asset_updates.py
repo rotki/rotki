499722108774a46ac4cb31be0c54e6713250604a
nebolax <alexey.nebolsin.jr@gmail.com>
2022-09-27 15:49:38 +0200

    Use savepoints in assets updates

--- rotkehlchen/tests/unit/test_globaldb_asset_updates.py
+++ rotkehlchen/tests/unit/test_globaldb_asset_updates.py
@@ -3,6 +3,7 @@ import pytest
 from rotkehlchen.assets.types import AssetData, AssetType
 from rotkehlchen.chain.ethereum.types import string_to_evm_address
 from rotkehlchen.errors.serialization import DeserializationError
+from rotkehlchen.globaldb.handler import GlobalDBHandler
 from rotkehlchen.globaldb.updates import AssetsUpdater
 from rotkehlchen.types import ChainID, EvmTokenKind, Timestamp
 
@@ -295,3 +296,45 @@ def test_parse_full_insert(assets_updater, text, expected_data, error_msg):
             assets_updater._parse_full_insert(text)
 
         assert error_msg in str(excinfo.value)
+
+
+def test_some_updates_are_malformed(assets_updater):
+    """
+    Checks the folowing cases:
+    1. If some of the updates are broken, others are not affected.
+    2. If an update is broken, info about asset stays the same in all tables as before the update.
+
+    Checks both insertion of a new asset and an update of an existing one.
+    """
+    # In "ETH" update swapped_for is incorrect
+    # In "BTC" update everything is good
+    # In "NEW-ASSET-1" insertion swapped_for is incorrect
+    # In "NEW-ASSET-2" insertion everything is good
+    update_text = """INSERT INTO assets(identifier, name, type) VALUES("ETH", "name1", "B"); INSERT INTO common_asset_details(identifier, symbol, coingecko, cryptocompare, forked, started, swapped_for) VALUES("ETH", "symbol1", "", "", NULL, NULL, "NONEXISTENT");
+*
+INSERT INTO assets(identifier, name, type) VALUES("BTC", "name2", "B"); INSERT INTO common_asset_details(identifier, symbol, coingecko, cryptocompare, forked, started, swapped_for) VALUES("BTC", "symbol2", "", "", NULL, NULL, NULL);
+*
+INSERT INTO assets(identifier, name, type) VALUES("NEW-ASSET-1", "name3", "B"); INSERT INTO common_asset_details(identifier, symbol, coingecko, cryptocompare, forked, started, swapped_for) VALUES("NEW-ASSET-1", "symbol3", "", "", NULL, NULL, "LOLKEK");
+*
+INSERT INTO assets(identifier, name, type) VALUES("NEW-ASSET-2", "name4", "B"); INSERT INTO common_asset_details(identifier, symbol, coingecko, cryptocompare, forked, started, swapped_for) VALUES("NEW-ASSET-2", "symbol4", "", "", NULL, NULL, NULL);
+*
+"""  # noqa: E501
+    connection = GlobalDBHandler().conn
+    assets_updater._apply_single_version_update(
+        connection=connection,
+        version=999,  # doesn't matter
+        text=update_text,
+        conflicts={'ETH': 'remote', 'BTC': 'remote'},
+    )
+
+    # Should have stayed unchanged since one of the insertions was incorrect
+    assert connection.execute('SELECT name FROM assets WHERE identifier="ETH"').fetchone()[0] == 'Ethereum'  # noqa: E501
+    assert connection.execute('SELECT symbol FROM common_asset_details WHERE identifier="ETH"').fetchone()[0] == 'ETH'  # noqa: E501
+    # BTC should have been updated since the insertion were correct
+    assert connection.execute('SELECT name FROM assets WHERE identifier="BTC"').fetchone()[0] == 'name2'  # noqa: E501
+    assert connection.execute('SELECT symbol FROM common_asset_details WHERE identifier="BTC"').fetchone()[0] == 'symbol2'  # noqa: E501
+    # NEW-ASSET-1 should have not been added since the insertions were incorrect
+    assert connection.execute('SELECT * FROM assets WHERE identifier="NEW-ASSET-1"').fetchone() is None  # noqa: E501
+    # NEW-ASSET-2 should have been added since the insertions were correct
+    assert connection.execute('SELECT name FROM assets WHERE identifier="NEW-ASSET-2"').fetchone()[0] == 'name4'  # noqa: E501
+    assert connection.execute('SELECT symbol FROM common_asset_details WHERE identifier="NEW-ASSET-2"').fetchone()[0] == 'symbol4'  # noqa: E501
