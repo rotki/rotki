6bd2cf8096123839d3ee9308eae0913e4516dbb4
Lefteris Karapetsas <lefteris@refu.co>
2022-10-01 17:24:29 +0200

    Remove unused pytest fixtures

--- rotkehlchen/tests/fixtures/exchanges/bitcoinde.py
+++ rotkehlchen/tests/fixtures/exchanges/bitcoinde.py
@@ -3,18 +3,6 @@ import pytest
 from rotkehlchen.tests.utils.exchanges import create_test_bitcoinde
 
 
-@pytest.fixture(scope='session')
-def bitcoinde(
-        session_inquirer,  # pylint: disable=unused-argument
-        messages_aggregator,
-        session_database,
-):
-    return create_test_bitcoinde(
-        database=session_database,
-        msg_aggregator=messages_aggregator,
-    )
-
-
 @pytest.fixture(scope='function')
 def function_scope_bitcoinde(
         inquirer,  # pylint: disable=unused-argument
--- rotkehlchen/tests/fixtures/exchanges/coinbasepro.py
+++ rotkehlchen/tests/fixtures/exchanges/coinbasepro.py
@@ -8,20 +8,6 @@ def fixture_coinbasepro_passphrase():
     return 'supersecretpassphrase'
 
 
-@pytest.fixture(scope='session')
-def coinbasepro(
-        session_database,
-        session_inquirer,  # pylint: disable=unused-argument
-        messages_aggregator,
-        coinbasepro_passphrase,
-):
-    return create_test_coinbasepro(
-        database=session_database,
-        msg_aggregator=messages_aggregator,
-        passphrase=coinbasepro_passphrase,
-    )
-
-
 @pytest.fixture(scope='function')
 def function_scope_coinbasepro(
         database,
