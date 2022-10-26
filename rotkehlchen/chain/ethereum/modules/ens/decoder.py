89baaddaf145f0abdb508aee62c8120c3e0d0a0c
Lefteris Karapetsas <lefteris@refu.co>
2022-10-27 01:47:51 +0200

    ENS Decoder for registering an ENS name
    Also passing along some extra data for ERC721 transfers in the decoded events

--- rotkehlchen/chain/ethereum/modules/ens/decoder.py
+++ rotkehlchen/chain/ethereum/modules/ens/decoder.py
@@ -29,6 +29,8 @@ log = RotkehlchenLogsAdapter(logger)
 
 ENS_REGISTRAR_CONTROLLER = string_to_evm_address('0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5')
 
+NAME_REGISTERED = b'\xcaj\xbb\xe9\xd7\xf1\x14"\xcbl\xa7b\x9f\xbfo\xe9\xef\xb1\xc6!\xf7\x1c\xe8\xf0+\x9f*#\x00\x97@O'  # noqa: E501
+NAME_REGISTERED_ABI = '{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"name","type":"string"},{"indexed":true,"internalType":"bytes32","name":"label","type":"bytes32"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"cost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"expires","type":"uint256"}],"name":"NameRegistered","type":"event"}'  # noqa: E501
 NAME_RENEWED = b'=\xa2L\x02E\x82\x93\x1c\xfa\xf8&}\x8e\xd2M\x13\xa8*\x80h\xd5\xbd3}0\xecE\xce\xa4\xe5\x06\xae'  # noqa: E501
 NAME_RENEWED_ABI = '{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"name","type":"string"},{"indexed":true,"internalType":"bytes32","name":"label","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"cost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"expires","type":"uint256"}],"name":"NameRenewed","type":"event"}'  # noqa: E501
 
@@ -41,6 +43,87 @@ class EnsDecoder(DecoderInterface, CustomizableDateMixin):  # lgtm[py/missing-ca
             msg_aggregator: 'MessagesAggregator',  # pylint: disable=unused-argument
     ) -> None:
         CustomizableDateMixin.__init__(self, base_tools.database)
+        self.eth = A_ETH.resolve_to_crypto_asset()
+
+    def _decode_ens_registrar_event(
+            self,
+            tx_log: EthereumTxReceiptLog,
+            transaction: EvmTransaction,  # pylint: disable=unused-argument
+            decoded_events: List[HistoryBaseEntry],  # pylint: disable=unused-argument
+            all_logs: List[EthereumTxReceiptLog],  # pylint: disable=unused-argument
+            action_items: Optional[List[ActionItem]],  # pylint: disable=unused-argument
+    ) -> Tuple[Optional[HistoryBaseEntry], List[ActionItem]]:
+        if tx_log.topics[0] == NAME_REGISTERED:
+            return self._decode_name_registered(
+                tx_log=tx_log,
+                transaction=transaction,
+                decoded_events=decoded_events,
+                all_logs=all_logs,
+                action_items=action_items,
+            )
+
+        if tx_log.topics[0] == NAME_RENEWED:
+            return self._decode_name_renewed(
+                tx_log=tx_log,
+                transaction=transaction,
+                decoded_events=decoded_events,
+                all_logs=all_logs,
+                action_items=action_items,
+            )
+
+        return None, []
+
+    def _decode_name_registered(
+            self,
+            tx_log: EthereumTxReceiptLog,
+            transaction: EvmTransaction,  # pylint: disable=unused-argument
+            decoded_events: List[HistoryBaseEntry],  # pylint: disable=unused-argument
+            all_logs: List[EthereumTxReceiptLog],  # pylint: disable=unused-argument
+            action_items: Optional[List[ActionItem]],  # pylint: disable=unused-argument
+    ) -> Tuple[Optional[HistoryBaseEntry], List[ActionItem]]:
+        try:
+            _, decoded_data = decode_event_data_abi_str(tx_log, NAME_REGISTERED_ABI)
+        except DeserializationError as e:
+            log.debug(f'Failed to decode ENS name registered event due to {str(e)}')
+            return None, []
+
+        name = decoded_data[0]
+        amount = from_wei(decoded_data[1])
+        expires = decoded_data[2]
+
+        refund_from_registrar = None
+        to_remove_indices = []
+        for event_idx, event in enumerate(decoded_events):
+            if event.event_type == HistoryEventType.RECEIVE and event.asset == A_ETH and event.counterparty == ENS_REGISTRAR_CONTROLLER:  # noqa: E501
+                # remove ETH refund event
+                refund_from_registrar = event.balance.amount
+                to_remove_indices.append(event_idx)
+
+            # Find the ETH transfer event which should be before the registered event
+            if event.event_type == HistoryEventType.SPEND and event.asset == A_ETH and event.counterparty == tx_log.address:  # noqa: E501
+                expected_amount = amount
+                if refund_from_registrar:
+                    expected_amount = amount + refund_from_registrar
+                if event.balance.amount != expected_amount:
+                    return None, []  # registration amount did not match
+
+                event.event_type = HistoryEventType.TRADE
+                event.event_subtype = HistoryEventSubType.SPEND
+                event.counterparty = CPT_ENS
+                event.notes = f'Register ENS name {name}.eth for {amount} ETH until {self.timestamp_to_date(expires)}'  # noqa: E501
+
+            # Find the ENS ERC721 receive event which should be before the registered event
+            if event.event_type == HistoryEventType.RECEIVE and event.asset.identifier == 'eip155:1/erc721:0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85':  # noqa: E501
+                assert event.extra_data is not None, 'All ERC721 events should have extra data'
+                event.event_type = HistoryEventType.TRADE
+                event.event_subtype = HistoryEventSubType.RECEIVE
+                event.counterparty = CPT_ENS
+                event.notes = f'Receive ENS name ERC721 token for {name}.eth with id {event.extra_data["token_id"]}'  # noqa: E501
+
+        for index in to_remove_indices:
+            del decoded_events[index]
+
+        return None, []
 
     def _decode_name_renewed(
             self,
@@ -49,15 +132,12 @@ class EnsDecoder(DecoderInterface, CustomizableDateMixin):  # lgtm[py/missing-ca
             decoded_events: List[HistoryBaseEntry],  # pylint: disable=unused-argument
             all_logs: List[EthereumTxReceiptLog],  # pylint: disable=unused-argument
             action_items: Optional[List[ActionItem]],  # pylint: disable=unused-argument
-    ) -> Tuple[Optional[HistoryBaseEntry], Optional[ActionItem]]:
-        if tx_log.topics[0] != NAME_RENEWED:
-            return None, None
-
+    ) -> Tuple[Optional[HistoryBaseEntry], List[ActionItem]]:
         try:
             _, decoded_data = decode_event_data_abi_str(tx_log, NAME_RENEWED_ABI)
         except DeserializationError as e:
             log.debug(f'Failed to decode ENS name renewed event due to {str(e)}')
-            return None, None
+            return None, []
 
         name = decoded_data[0]
         amount = from_wei(decoded_data[1])
@@ -71,13 +151,13 @@ class EnsDecoder(DecoderInterface, CustomizableDateMixin):  # lgtm[py/missing-ca
                 event.counterparty = CPT_ENS
                 event.notes = f'Renew ENS name {name} for {amount} ETH until {self.timestamp_to_date(expires)}'  # noqa: E501
 
-        return None, None
+        return None, []
 
     # -- DecoderInterface methods
 
     def addresses_to_decoders(self) -> Dict[ChecksumEvmAddress, Tuple[Any, ...]]:
         return {
-            ENS_REGISTRAR_CONTROLLER: (self._decode_name_renewed,),  # noqa: E501
+            ENS_REGISTRAR_CONTROLLER: (self._decode_ens_registrar_event,),  # noqa: E501
         }
 
     def counterparties(self) -> List[str]:
