24eed4798312af79828e941c311b9baac3816f46
Lefteris Karapetsas <lefteris@refu.co>
2022-10-27 20:29:40 +0200

    Bump version: 1.25.3 → 1.26.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018-2020, Eleftherios Karapetsas. 2020-2022, Rotki Solutions GmbH'
 author = 'The rotki team'
 
 # The short X.Y version
-version = '1.25.3'
+version = '1.26.0'
 # The full version, including alpha/beta/rc tags
-release = '1.25.3'
+release = '1.26.0'
 
 
 # -- General configuration ---------------------------------------------------
