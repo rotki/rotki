be0a5d8fa00661986ca604b4a90e8889798e0d0f
Konstantinos Paparas <kelsos86@gmail.com>
2022-11-21 18:34:11 +0100

    Adds wait on manual balance price refresh and reload block

--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -13,16 +13,28 @@ import { storePiniaPlugins } from '@/store/debug';
 import { StoreResetPlugin, StoreTrackPlugin } from '@/store/plugins';
 import { setupDayjs } from '@/utils/date';
 import { checkIfDevelopment } from '@/utils/env-utils';
+import { logger } from '@/utils/logging';
 import { setupFormatter } from '@/utils/setup-formatter';
 import i18n from './i18n';
 import router from './router';
-import './utils/logging';
 
 const isDevelopment = checkIfDevelopment() && !import.meta.env.VITE_TEST;
 Vue.config.productionTip = false;
 Vue.config.devtools = isDevelopment;
 
 Vue.use(PiniaVuePlugin);
+
+// This should disable vite page reloads on CI.
+// Monitor e2e tests for this and if this doesn't work remove it.
+if (import.meta.env.MODE === 'production' && import.meta.env.VITE_TEST) {
+  logger.info('disabling vite:reload');
+  if (import.meta.hot) {
+    import.meta.hot.on('vite:beforeFullReload', () => {
+      logger.info('vite:reload detected');
+      throw '(skipping full reload)';
+    });
+  }
+}
 
 Vue.directive('blur', {
   inserted: function (el) {
--- frontend/app/tests/e2e/support/utils.ts
+++ frontend/app/tests/e2e/support/utils.ts
@@ -13,3 +13,30 @@ export function selectLocation(element: string, value: string) {
   const identifier = value.toLocaleLowerCase();
   cy.get(`#balance-location__${identifier}`).click();
 }
+
+/**
+ * Used to wait for an async query to complete.
+ * The method will intercept the original query, get the task id
+ *
+ * @param url {@see cypress/types/net-stubbing#RouteMatcher} The url that returns the pending task id
+ * @param timeout How long the util will wait for the /api/1/tasks/task_id to
+ * complete
+ */
+export const waitForAsyncQuery = (
+  url: { method: 'POST' | 'GET'; url: string },
+  timeout = 120000
+) => {
+  cy.intercept(url).as('asyncQuery');
+
+  cy.wait('@asyncQuery', { timeout: 30000 })
+    .its('response.body')
+    .then(body => {
+      cy.intercept({
+        method: 'GET',
+        url: `/api/1/tasks/${body.result.task_id}`
+      }).as('task');
+      cy.wait('@task', { timeout })
+        .its('response.statusCode')
+        .should('equal', 200);
+    });
+};
