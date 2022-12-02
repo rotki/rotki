61d2e6c412cf4179e586816376c397c1e66656bf
Lefteris Karapetsas <lefteris@refu.co>
2022-12-02 20:35:00 +0100

    Introduce the non-complete optimism classes

---
+++ rotkehlchen/chain/optimism/constants.py
@@ -0,0 +1,16 @@
+from rotkehlchen.chain.evm.types import NodeName, WeightedNode
+from rotkehlchen.constants.misc import ONE
+from rotkehlchen.types import SupportedBlockchain
+
+OPTIMISM_ETHERSCAN_NODE_NAME = 'optimism etherscan'
+
+OPTIMISM_ETHERSCAN_NODE = WeightedNode(
+    node_info=NodeName(
+        name=OPTIMISM_ETHERSCAN_NODE_NAME,
+        endpoint='',
+        owned=False,
+        blockchain=SupportedBlockchain.OPTIMISM,
+    ),
+    weight=ONE,
+    active=True,
+)
---
+++ rotkehlchen/chain/optimism/node_inquirer.py
@@ -0,0 +1,67 @@
+import logging
+from typing import TYPE_CHECKING, Sequence, cast
+
+from eth_typing import BlockNumber
+
+from rotkehlchen.chain.constants import DEFAULT_EVM_RPC_TIMEOUT
+from rotkehlchen.chain.evm.contracts import EvmContracts
+from rotkehlchen.chain.evm.node_inquirer import EvmNodeInquirer
+from rotkehlchen.chain.evm.types import WeightedNode
+from rotkehlchen.greenlets import GreenletManager
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import ChainID, SupportedBlockchain, Timestamp
+
+from .constants import OPTIMISM_ETHERSCAN_NODE, OPTIMISM_ETHERSCAN_NODE_NAME
+
+if TYPE_CHECKING:
+    from rotkehlchen.db.dbhandler import DBHandler
+
+    from .etherscan import OptimismEtherscan
+
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
+
+class OptimismInquirer(EvmNodeInquirer):
+
+    def __init__(
+            self,
+            greenlet_manager: GreenletManager,
+            database: 'DBHandler',
+            etherscan: 'OptimismEtherscan',
+            connect_at_start: Sequence[WeightedNode],
+            rpc_timeout: int = DEFAULT_EVM_RPC_TIMEOUT,
+    ) -> None:
+        super().__init__(
+            greenlet_manager=greenlet_manager,
+            database=database,
+            etherscan=etherscan,
+            blockchain=SupportedBlockchain.OPTIMISM,
+            etherscan_node=OPTIMISM_ETHERSCAN_NODE,
+            etherscan_node_name=OPTIMISM_ETHERSCAN_NODE_NAME,
+            contracts=EvmContracts(  # TODO: Change
+                chain_id=ChainID.OPTIMISM,
+                contracts_filename='eth_contracts.json',
+                abi_filename='eth_abi.json',
+            ),
+            connect_at_start=connect_at_start,
+            rpc_timeout=rpc_timeout,
+        )
+        self.etherscan = cast('OptimismEtherscan', self.etherscan)
+
+    # -- Implementation of EvmNodeInquirer base methods --
+
+    def query_highest_block(self) -> BlockNumber:
+        block_number = self.etherscan.get_latest_block_number()
+        log.debug('Optimism highest block result', block=block_number)
+        return BlockNumber(block_number)
+
+    def have_archive(self, requery: bool = False) -> bool:
+        return False
+
+    def get_blocknumber_by_time(self, ts: Timestamp, etherscan: bool = True) -> int:
+        """Searches for the blocknumber of a specific timestamp
+
+        May raise RemoteError
+        """
+        return self.etherscan.get_blocknumber_by_time(ts)
