fdcfcf89d6b0740765d41278b327dc483622fe83
Lefteris Karapetsas <lefteris@refu.co>
2022-12-19 10:46:55 +0100

    Fix tests after bugfixes merge to develop

--- rotkehlchen/tests/unit/test_tokens.py
+++ rotkehlchen/tests/unit/test_tokens.py
@@ -10,6 +10,7 @@ from rotkehlchen.chain.evm.types import string_to_evm_address
 from rotkehlchen.constants.assets import A_OMG
 from rotkehlchen.fval import FVal
 from rotkehlchen.tests.utils.constants import A_LPT
+from rotkehlchen.types import ChainID
 from rotkehlchen.utils.misc import ts_now
 
 
@@ -77,26 +78,26 @@ def test_generate_chunks():
     assert generated_chunks == expected_chunks
 
 
-def test_last_queried_ts(evmtokens, freezer):
+def test_last_queried_ts(tokens, freezer):
     """
     Checks that after detecting evm tokens last_queried_timestamp is updated and there
     are no duplicates.
     """
     # We don't need to query the chain here, so mock tokens list
     evm_tokens_patch = patch(
-        'rotkehlchen.globaldb.handler.GlobalDBHandler.get_ethereum_tokens',
-        new=lambda _, exceptions=None, except_protocols=None, protocol=None: [],
+        'rotkehlchen.globaldb.handler.GlobalDBHandler.get_evm_tokens',
+        new=lambda _, chain_id=ChainID.ETHEREUM, exceptions=None, except_protocols=None, protocol=None: [],  # noqa: E501
     )
     beginning = ts_now()
     address = '0x4bBa290826C253BD854121346c370a9886d1bC26'
     with evm_tokens_patch:
         # Detect for the first time
-        evmtokens.detect_tokens(
+        tokens.detect_tokens(
             only_cache=False,
             addresses=[address],
         )
-        after_first_query = evmtokens.db.conn.execute(
-            'SELECT key, value FROM accounts_details',
+        after_first_query = tokens.db.conn.execute(
+            'SELECT key, value FROM evm_accounts_details',
         ).fetchall()
         assert len(after_first_query) == 1
         assert after_first_query[0][0] == 'last_queried_timestamp'
@@ -105,13 +106,13 @@ def test_last_queried_ts(evmtokens, freezer):
         continuation = beginning + 10
         freezer.move_to(datetime.fromtimestamp(continuation))
         # Detect again
-        evmtokens.detect_tokens(
+        tokens.detect_tokens(
             only_cache=False,
             addresses=['0x4bBa290826C253BD854121346c370a9886d1bC26'],
         )
         # Check that last_queried_timestamp was updated and that there are no duplicates
-        after_second_query = evmtokens.db.conn.execute(
-            'SELECT key, value FROM accounts_details',
+        after_second_query = tokens.db.conn.execute(
+            'SELECT key, value FROM evm_accounts_details',
         ).fetchall()
         assert len(after_second_query) == 1
         assert after_second_query[0][0] == 'last_queried_timestamp'
