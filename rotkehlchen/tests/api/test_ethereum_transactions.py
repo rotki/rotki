495065f55664d829492b0c6a158707fa3e55be7c
Lefteris Karapetsas <lefteris@refu.co>
2023-01-02 11:45:19 +0100

    Use evm_chain at transaction serialization out of the API

--- rotkehlchen/tests/api/test_ethereum_transactions.py
+++ rotkehlchen/tests/api/test_ethereum_transactions.py
@@ -64,7 +64,7 @@ if TYPE_CHECKING:
 
 EXPECTED_AFB7_TXS = [{
     'tx_hash': '0x13684203a4bf07aaed0112983cb380db6004acac772af2a5d46cb2a28245fbad',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1439984408,
     'block_number': 111083,
     'from_address': '0xC47Aaa860008be6f65B58c6C6E02a84e666EfE31',
@@ -77,7 +77,7 @@ EXPECTED_AFB7_TXS = [{
     'nonce': 100,
 }, {
     'tx_hash': '0xe58af420fd8430c061303e4c5bd2668fafbc0fd41078fa6aa01d7781c1dadc7a',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1461221228,
     'block_number': 1375816,
     'from_address': '0x9e6316f44BaEeeE5d41A1070516cc5fA47BAF227',
@@ -90,7 +90,7 @@ EXPECTED_AFB7_TXS = [{
     'nonce': 326,
 }, {
     'tx_hash': '0x0ae8b470b4a69c7f6905b9ec09f50c8772821080d11ba0acc83ac23a7ccb4ad8',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1461399856,
     'block_number': 1388248,
     'from_address': '0xaFB7ed3beBE50E0b62Fa862FAba93e7A46e59cA7',
@@ -103,7 +103,7 @@ EXPECTED_AFB7_TXS = [{
     'nonce': 0,
 }, {
     'tx_hash': '0x2f6f167e32e9cb1bef40b92e831c3f1d1cd0348bb72dcc723bde94f51944ebd6',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1494458609,
     'block_number': 3685519,
     'from_address': '0x4aD11d04CCd80A83d48096478b73D1E8e0ed49D6',
@@ -116,7 +116,7 @@ EXPECTED_AFB7_TXS = [{
     'nonce': 1,
 }, {
     'tx_hash': '0x5d81f937ad37349f89dc6e9926988855bb6c6e1e00c683ee3b7cb7d7b09b5567',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1494458861,
     'block_number': 3685532,
     'from_address': '0xaFB7ed3beBE50E0b62Fa862FAba93e7A46e59cA7',
@@ -131,7 +131,7 @@ EXPECTED_AFB7_TXS = [{
 
 EXPECTED_4193_TXS = [{
     'tx_hash': '0x2964f3a91408337b05aeb8f8f670f4107999be05376e630742404664c96a5c31',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1439979000,
     'block_number': 110763,
     'from_address': '0x976349705b839e2F5719387Fb27D2519d519da03',
@@ -144,7 +144,7 @@ EXPECTED_4193_TXS = [{
     'nonce': 30,
 }, {
     'tx_hash': '0xb99a6e0b40f38c4887617bc1df560fde1d0456b712cb2bb1b52fdb8880d3cd74',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1439984825,
     'block_number': 111111,
     'from_address': '0x4193122032b38236825BBa166F42e54fc3F4A1EE',
@@ -157,7 +157,7 @@ EXPECTED_4193_TXS = [{
     'nonce': 0,
 }, {
     'tx_hash': '0xfadf1f12281ee2c0311055848b4ffc0046ac80afae4a9d3640b5f57bb8a7795a',
-    'chain_id': 1,
+    'evm_chain': 'ethereum',
     'timestamp': 1507291254,
     'block_number': 4341870,
     'from_address': '0x4193122032b38236825BBa166F42e54fc3F4A1EE',
