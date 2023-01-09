581accae1e970dd635ed37532a9a61f3e2122f06
Lefteris Karapetsas <lefteris@refu.co>
2023-01-09 12:50:21 +0100

    Add pagination test for multiple evm chains

--- rotkehlchen/tests/api/test_evm_transactions.py
+++ rotkehlchen/tests/api/test_evm_transactions.py
@@ -57,9 +57,40 @@ def test_query_transactions(rotkehlchen_api_server: 'APIServer'):
 
     # check all expected data exists. User has done more transactions since then if we don't
     # mock network, so we need to test like this
+    last_ts = result['entries'][0]['entry']['timestamp']
     for entry in expected_data['entries']:
         assert entry in result['entries']
+        assert entry['entry']['timestamp'] <= last_ts
+        last_ts = entry['entry']['timestamp']
 
     assert result['entries_found'] >= expected_data['entries_found']
     assert result['entries_total'] >= expected_data['entries_total']
     assert result['entries_limit'] == -1
+
+    # After querying make sure pagination and only_cache work properly for multiple chains
+    response = requests.get(
+        api_url_for(
+            rotkehlchen_api_server,
+            'evmtransactionsresource',
+        ), json={
+            'async_query': False,
+            'limit': 20,
+            'offset': 0,
+            'ascending': [False],
+            'order_by_attributes': ['timestamp'],
+        },
+    )
+    result = assert_proper_response_with_result(response)
+    last_ts = result['entries'][0]['entry']['timestamp']
+    ethereum_found = False
+    optimism_found = False
+    for entry in result['entries']:
+        assert entry['entry']['timestamp'] <= last_ts
+        last_ts = entry['entry']['timestamp']
+        if entry['entry']['evm_chain'] == 'ethereum':
+            ethereum_found = True
+        elif entry['entry']['evm_chain'] == 'optimism':
+            optimism_found = True
+
+    assert optimism_found is True
+    assert ethereum_found is True
