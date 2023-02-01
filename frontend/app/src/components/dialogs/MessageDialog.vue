8abf75df43cb2bb0ccb51362e50219bd802e0409
Luki Centuri <lukicenturi@gmail.com>
2023-02-01 11:35:51 +0700

    feat: clear saved newly detected tokens on upgrade

--- frontend/app/src/components/dialogs/MessageDialog.vue
+++ frontend/app/src/components/dialogs/MessageDialog.vue
@@ -23,44 +23,54 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <v-dialog v-model="visible" persistent max-width="500" class="message-dialog">
-    <v-card>
-      <v-card-title
-        :class="{ 'green--text': success, 'red--text': !success }"
-        class="text-h5 message-dialog__title"
-      >
-        {{ title }}
-      </v-card-title>
-      <v-row align="center" class="mx-0 message-dialog__body">
-        <v-col cols="1">
-          <v-icon
-            size="40"
-            class="dialog-icon"
-            :class="{ 'green--text': success, 'red--text': !success }"
-          >
-            {{ icon }}
-          </v-icon>
-        </v-col>
-        <v-col cols="11">
-          <v-card-text class="message-dialog__message">
-            {{ message }}
-          </v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-card-actions>
-        <v-spacer />
-        <v-btn
-          :color="success ? 'green' : 'red'"
-          text
-          class="message-dialog__buttons__confirm"
-          @click="dismiss()"
+  <div>
+    <v-dialog
+      :value="visible"
+      max-width="500"
+      class="message-dialog"
+      persistent
+      @close="dismiss"
+      @keydown.esc="dismiss"
+      @keydown.enter="dismiss"
+    >
+      <v-card>
+        <v-card-title
+          :class="{ 'green--text': success, 'red--text': !success }"
+          class="text-h5 message-dialog__title"
         >
-          {{ t('common.actions.ok') }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+          {{ title }}
+        </v-card-title>
+        <v-row align="center" class="mx-0 message-dialog__body">
+          <v-col cols="1">
+            <v-icon
+              size="40"
+              class="dialog-icon"
+              :class="{ 'green--text': success, 'red--text': !success }"
+            >
+              {{ icon }}
+            </v-icon>
+          </v-col>
+          <v-col cols="11">
+            <v-card-text class="message-dialog__message">
+              {{ message }}
+            </v-card-text>
+          </v-col>
+        </v-row>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            :color="success ? 'green' : 'red'"
+            text
+            class="message-dialog__buttons__confirm"
+            @click="dismiss()"
+          >
+            {{ t('common.actions.ok') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <style scoped lang="scss">
