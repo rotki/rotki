f49c192616937029af8ec768af3e92fad4b64fd8
Lefteris Karapetsas <lefteris@refu.co>
2023-02-05 23:11:35 +0100

    Activate ruff RSE rules and implement fixes

--- rotkehlchen/chain/ethereum/modules/l2/loopring.py
+++ rotkehlchen/chain/ethereum/modules/l2/loopring.py
@@ -356,11 +356,11 @@ class Loopring(ExternalServiceWithApiKey, EthereumModule, LockableQueryMixIn):
                         # From the documentation the code returned when the ApiKey
                         # is not valid is 104002 https://docs3.loopring.io/en/?q=104002
                         if code == 104002:
-                            raise LoopringInvalidApiKey()
+                            raise LoopringInvalidApiKey
                         # This code is returned when an user is not found at loopring
                         # https://docs3.loopring.io/en/?q=101002
                         if code == 101002:
-                            raise LoopringUserNotFound()
+                            raise LoopringUserNotFound
                 # else just let it hit the generic remote error below
 
         if response.status_code != HTTPStatus.OK:
