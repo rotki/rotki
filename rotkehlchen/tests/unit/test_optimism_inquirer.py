c28d95349a9464ac34cf74522fdd8cd70c016aa6
Isaac Adewumi <ienioladewumi@gmail.com>
2023-02-16 08:37:20 +0100

    add tests to verify pruned nodes are handled in queries properly

--- rotkehlchen/tests/unit/test_optimism_inquirer.py
+++ rotkehlchen/tests/unit/test_optimism_inquirer.py
@@ -4,6 +4,11 @@ from rotkehlchen.tests.utils.ethereum import wait_until_all_nodes_connected
 from rotkehlchen.tests.utils.optimism import OPTIMISM_NODES_PARAMETERS_WITH_PRUNED_AND_NOT_ARCHIVED
 
 
+@pytest.mark.vcr(
+    match_on=['uri', 'method', 'raw_body'],
+    filter_query_parameters=['apikey'],
+    allow_playback_repeats=True,
+)
 @pytest.mark.parametrize(*OPTIMISM_NODES_PARAMETERS_WITH_PRUNED_AND_NOT_ARCHIVED)
 def test_optimism_nodes_prune_and_archive_status(
         optimism_manager_connect_at_start,
@@ -29,3 +34,5 @@ def test_optimism_nodes_prune_and_archive_status(
             assert web3_node.is_archive
         else:
             raise AssertionError(f'Unknown node {node_name} encountered.')
+
+    assert len(optimism_inquirer.web3_mapping) == len(optimism_manager_connect_at_start)
--- rotkehlchen/tests/utils/evm.py
+++ rotkehlchen/tests/utils/evm.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+from rotkehlchen.chain.evm.types import Web3Node
 from rotkehlchen.tests.utils.mock import (
     MOCK_WEB3_LAST_BLOCK_INT,
     patch_etherscan_request,
@@ -21,7 +22,11 @@ def maybe_mock_evm_inquirer(
     # we have to mock connect to given nodes, and patch their requests
     for mocked_node in manager_connect_at_start:
         web3, _ = evm_inquirer._init_web3(mocked_node.node_info)
-        evm_inquirer.web3_mapping[mocked_node.node_info] = web3
+        evm_inquirer.web3_mapping[mocked_node.node_info] = Web3Node(
+            web3_instance=web3,
+            is_pruned=False,
+            is_archive=True,
+        )
         parent_stack.enter_context(patch_web3_request(web3, mock_data))
     parent_stack.enter_context(patch.object(
         evm_inquirer,
