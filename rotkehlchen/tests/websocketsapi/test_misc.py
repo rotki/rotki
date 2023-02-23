8aa068946e7f8323d50646cfc48f296c1b5a0984
Isaac Adewumi <ienioladewumi@gmail.com>
2023-02-23 18:44:10 +0100

    fix: adjust websocket test to avoid failure in macos

--- rotkehlchen/tests/websocketsapi/test_misc.py
+++ rotkehlchen/tests/websocketsapi/test_misc.py
@@ -1,15 +1,17 @@
+import platform
+
 import gevent
 import pytest
 
 
-def _send_stuff(msg_aggregator, websocket_connection):
+def _send_stuff(msg_aggregator, websocket_connection, string_len):
     for _ in range(10):
         # We need big strings in order to replicate. Small messages do not hit it
         # But not too big since it can cause `WebSocketPayloadException` and that
         # can make this test run forever
         #  <bound method WebsocketReader.read_forever of <rotkehlchen.tests.fixtures.websockets.WebsocketReader object at 0x7ff6f3136f70>>> failed with WebSocketPayloadException  # noqa: E501
-        msg_aggregator.add_error('x' * 100000)
-        msg_aggregator.add_warning('y' * 100000)
+        msg_aggregator.add_error('x' * string_len)
+        msg_aggregator.add_warning('y' * string_len)
         if websocket_connection.messages_num() != 0:
             websocket_connection.pop_message()
 
@@ -20,14 +22,15 @@ def test_websockets_concurrent_use(rotkehlchen_api_server, websocket_connection)
     very heavy and specific circumstances cause concurrent websocket access from multiple
     greenlets.
 
-    This test replicates that scenario and it fails before the addition of the lock.
+    This test replicates that scenario, and it fails before the addition of the lock.
     Serves as a regression test. Should fail if locks are removed in websockets.
     """
     rotki = rotkehlchen_api_server.rest_api.rotkehlchen
+    string_len = 27000 if platform.system() == 'Darwin' else 100000
     with gevent.Timeout(10):
-        g1 = gevent.spawn(_send_stuff, rotki.msg_aggregator, websocket_connection)
-        _send_stuff(rotki.msg_aggregator, websocket_connection)
-        g2 = gevent.spawn(_send_stuff, rotki.msg_aggregator, websocket_connection)
+        g1 = gevent.spawn(_send_stuff, rotki.msg_aggregator, websocket_connection, string_len)
+        _send_stuff(rotki.msg_aggregator, websocket_connection, string_len)
+        g2 = gevent.spawn(_send_stuff, rotki.msg_aggregator, websocket_connection, string_len)
         gevent.joinall([g1, g2])
         assert all(
             isinstance(x.exception, gevent.exceptions.ConcurrentObjectUseError) is False
