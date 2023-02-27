fc8ef6b3e328579582b44b4874f5e3c55903b65f
Lefteris Karapetsas <lefteris@refu.co>
2023-02-27 22:41:25 +0100

    Ignore specific settings keys that should be cache
    When retreiving settings from the DB, ignore specific settings keys
    that should have been in the not-yet existing cache table.
    
    Related issue: https://github.com/rotki/rotki/issues/5684
    
    This elif check should be temporary until that issue is complete.

--- rotkehlchen/chain/constants.py
+++ rotkehlchen/chain/constants.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 from rotkehlchen.types import SUPPORTED_BLOCKCHAIN_TO_CHAINID, SupportedBlockchain
 
 DEFAULT_EVM_RPC_TIMEOUT = 10
@@ -7,3 +9,5 @@ NON_BITCOIN_CHAINS = [
     SupportedBlockchain.ETHEREUM_BEACONCHAIN,
     SupportedBlockchain.KUSAMA,
 ] + list(SUPPORTED_BLOCKCHAIN_TO_CHAINID.keys())
+
+LAST_EVM_ACCOUNTS_DETECT_KEY: Final = 'last_evm_accounts_detect_ts'
