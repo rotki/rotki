e79da22c35e1a091ff0ee1517aea5a3faf8ee570
Lefteris Karapetsas <lefteris@refu.co>
2023-02-28 22:20:00 +0100

    Claiming COMP after comptroller refill correct decoding
    This fixes a bug where there was COMP reward accumulated in the
    controll but failed to be paid out in an older transaction due
    to (probably) being out of COMP.
    
    Code is here:
    https://etherscan.io/address/0xBafE01ff935C7305907c33BF824352eE5979B526#code
    
    After being refilled the Distributed[Supplier/Borrower] events won't
    match the transfers.
    
    This commit fixes that bug by forcing the transfer search even if
    compdelta is zero, as accumulation of rewards can have happend in a
    previous transaction.

--- rotkehlchen/tests/unit/decoders/test_compound.py
+++ rotkehlchen/tests/unit/decoders/test_compound.py
@@ -9,10 +9,11 @@ from rotkehlchen.constants.assets import A_CDAI, A_CETH, A_COMP, A_DAI, A_ETH
 from rotkehlchen.constants.misc import ZERO
 from rotkehlchen.fval import FVal
 from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
-from rotkehlchen.types import Location, deserialize_evm_tx_hash
+from rotkehlchen.types import Location, Timestamp, deserialize_evm_tx_hash
 
 ADDY = '0x5727c0481b90a129554395937612d8b9301D6c7b'
 ADDY2 = '0x87Dd56068Af560B0D8472C4EF41CB902FCbF5ebE'
+ADDY3 = '0xb99CC7e10Fe0Acc68C50C7829F473d81e23249cc'
 
 
 @pytest.mark.vcr()
@@ -187,6 +188,97 @@ def test_compound_deposit_with_comp_claim(
             balance=Balance(amount=wrapped_amount),
             location_label=ADDY2,
             notes=f'Receive {wrapped_amount} cDAI from compound',
+            counterparty=CPT_COMPOUND,
+        )]
+    assert events == expected_events
+
+
+@pytest.mark.parametrize('ethereum_accounts', [[ADDY3]])
+def test_compound_multiple_comp_claim(database, ethereum_inquirer):
+    """Test that a transaction with multiple comp claims decodes all of them as rewards
+
+    This is to test against a regression of a bug that decoded the last reward claim
+    as a simple receive.
+    """
+    tx_hash = deserialize_evm_tx_hash('0x25d341421044fa27006c0ec8df11067d80f69b2d2135065828f1992fa6868a49')  # noqa: E501
+    timestamp = Timestamp(1622430975000)
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    expected_events = [
+        HistoryBaseEntry(
+            event_identifier=tx_hash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal('0.074799254'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Burned 0.074799254 ETH for gas',
+            counterparty=CPT_GAS,
+        ), HistoryBaseEntry(
+            event_identifier=tx_hash,
+            sequence_index=25,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.REWARD,
+            asset=A_COMP,
+            balance=Balance(amount=FVal('3.037897781413961524'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Collect 3.037897781413961524 COMP from compound',
+            counterparty=CPT_COMPOUND,
+        ), HistoryBaseEntry(
+            event_identifier=tx_hash,
+            sequence_index=29,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.REWARD,
+            asset=A_COMP,
+            balance=Balance(amount=FVal('0.03855352439614718'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Collect 0.03855352439614718 COMP from compound',
+            counterparty=CPT_COMPOUND,
+        ), HistoryBaseEntry(
+            event_identifier=tx_hash,
+            sequence_index=36,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.REWARD,
+            asset=A_COMP,
+            balance=Balance(amount=FVal('0.000186677589499495'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Collect 0.000186677589499495 COMP from compound',
+            counterparty=CPT_COMPOUND,
+        ), HistoryBaseEntry(
+            event_identifier=tx_hash,
+            sequence_index=39,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.REWARD,
+            asset=A_COMP,
+            balance=Balance(amount=FVal('0.001710153220923912'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Collect 0.001710153220923912 COMP from compound',
+            counterparty=CPT_COMPOUND,
+        ), HistoryBaseEntry(  # this appeared as Receive at time of writing the test
+            event_identifier=tx_hash,
+            sequence_index=44,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.REWARD,
+            asset=A_COMP,
+            balance=Balance(amount=FVal('0.000000000000000015'), usd_value=ZERO),
+            location_label=ADDY3,
+            notes='Collect 0.000000000000000015 COMP from compound',
             counterparty=CPT_COMPOUND,
         )]
     assert events == expected_events
