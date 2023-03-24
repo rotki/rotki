0a8ce26c138e54c2200645e44ba6624ec7a386b2
Joseph Ojoko <entewshi@gmail.com>
2023-03-24 16:25:05 +0100

    feat: add api handlers

---
+++ frontend/app/tests/unit/setup-files/handlers/asset-movements.ts
@@ -0,0 +1,10 @@
+import { rest } from 'msw';
+import assetMovements from '../../fixtures/asset-movements.json';
+
+const backendUrl = process.env.VITE_BACKEND_URL;
+
+export default [
+  rest.get(`${backendUrl}/api/1/asset_movements`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(assetMovements));
+  })
+];
---
+++ frontend/app/tests/unit/setup-files/handlers/ledger-actions.ts
@@ -0,0 +1,10 @@
+import { rest } from 'msw';
+import ledgerActions from '../../fixtures/ledger-actions.json';
+
+const backendUrl = process.env.VITE_BACKEND_URL;
+
+export default [
+  rest.get(`${backendUrl}/api/1/ledgeractions`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(ledgerActions));
+  })
+];
