f3887abe43427a970079f6673329ecf6bbf52173
Lefteris Karapetsas <lefteris@refu.co>
2023-05-23 13:02:21 +0200

    Decode locked GNO transactions

---
+++ rotkehlchen/chain/ethereum/modules/lockedgno/constants.py
@@ -0,0 +1,4 @@
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+CPT_LOCKEDGNO = 'Locked GNO'
+LOCKED_GNO_ADDRESS = string_to_evm_address('0x4f8AD938eBA0CD19155a835f617317a6E788c868')
---
+++ rotkehlchen/chain/ethereum/modules/lockedgno/decoder.py
@@ -0,0 +1,141 @@
+import logging
+from typing import TYPE_CHECKING, Any
+
+from rotkehlchen.accounting.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.assets.utils import get_or_create_evm_token
+from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
+from rotkehlchen.chain.evm.decoding.structures import (
+    DEFAULT_DECODING_OUTPUT,
+    ActionItem,
+    DecoderContext,
+    DecodingOutput,
+)
+from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails, EventCategory
+from rotkehlchen.chain.evm.types import string_to_evm_address
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import ChainID, ChecksumEvmAddress, DecoderEventMappingType
+
+from .constants import CPT_LOCKEDGNO, LOCKED_GNO_ADDRESS
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.ethereum.node_inquirer import EthereumInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
+
+class LockedgnoDecoder(DecoderInterface):
+
+    def __init__(
+            self,
+            ethereum_inquirer: 'EthereumInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=ethereum_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+        )
+        self.gno = get_or_create_evm_token(
+            userdb=self.base.database,
+            evm_address=string_to_evm_address('0x6810e776880C02933D47DB1b9fc05908e5386b96'),
+            chain_id=ChainID.ETHEREUM,
+            evm_inquirer=ethereum_inquirer,
+        )
+        self.lgno = get_or_create_evm_token(
+            userdb=self.base.database,
+            evm_address=string_to_evm_address('0x4f8AD938eBA0CD19155a835f617317a6E788c868'),
+            chain_id=ChainID.ETHEREUM,
+            evm_inquirer=ethereum_inquirer,
+        )
+
+    def _decode_events(self, context: DecoderContext) -> DecodingOutput:
+        for event in context.decoded_events:
+            if (
+                    event.event_type == HistoryEventType.SPEND and
+                    event.asset == self.gno and
+                    event.address == LOCKED_GNO_ADDRESS
+            ):
+                event.event_type = HistoryEventType.DEPOSIT
+                event.event_subtype = HistoryEventSubType.DEPOSIT_ASSET
+                event.counterparty = CPT_LOCKEDGNO
+                event.notes = f'Deposit {event.balance.amount} GNO to the locking contract'
+
+                # also create an action item for the receive of the locked gno tokens.
+                # Funny thing is that the log shows transfer from the user to the contract
+                # though it's the other way around in reality.
+                action_item = ActionItem(
+                    action='transform',
+                    sequence_index=context.tx_log.log_index,
+                    from_event_type=HistoryEventType.SPEND,
+                    from_event_subtype=HistoryEventSubType.NONE,
+                    asset=self.lgno,
+                    amount=event.balance.amount,
+                    to_event_type=HistoryEventType.RECEIVE,
+                    to_event_subtype=HistoryEventSubType.RECEIVE_WRAPPED,
+                    to_notes=f'Receive {event.balance.amount} locked GNO from the locking contract',  # noqa: E501
+                    to_counterparty=CPT_LOCKEDGNO,
+                )
+                return DecodingOutput(action_items=[action_item])
+
+            if (
+                    event.event_type == HistoryEventType.RECEIVE and
+                    event.asset == self.gno and
+                    event.address == LOCKED_GNO_ADDRESS
+            ):
+                event.event_type = HistoryEventType.WITHDRAWAL
+                event.event_subtype = HistoryEventSubType.REMOVE_ASSET
+                event.counterparty = CPT_LOCKEDGNO
+                event.notes = f'Receive {event.balance.amount} GNO back from the locking contract'
+
+                # also create an action item for the returning of the locked gno tokens.
+                # Funny thing is that the log shows transfer from the contract to the user
+                # though it's the other way around in reality.
+                action_item = ActionItem(
+                    action='transform',
+                    sequence_index=context.tx_log.log_index,
+                    from_event_type=HistoryEventType.RECEIVE,
+                    from_event_subtype=HistoryEventSubType.NONE,
+                    asset=self.lgno,
+                    amount=event.balance.amount,
+                    to_event_type=HistoryEventType.SPEND,
+                    to_event_subtype=HistoryEventSubType.RETURN_WRAPPED,
+                    to_notes=f'Return {event.balance.amount} locked GNO to the locking contract',
+                    to_counterparty=CPT_LOCKEDGNO,
+                )
+                return DecodingOutput(action_items=[action_item])
+
+        return DEFAULT_DECODING_OUTPUT
+
+    # -- DecoderInterface methods
+
+    def possible_events(self) -> DecoderEventMappingType:
+        return {
+            CPT_LOCKEDGNO: {
+                HistoryEventType.RECEIVE: {
+                    HistoryEventSubType.RECEIVE_WRAPPED: EventCategory.RECEIVE,
+                },
+                HistoryEventType.SPEND: {
+                    HistoryEventSubType.RETURN_WRAPPED: EventCategory.SEND,
+                },
+                HistoryEventType.DEPOSIT: {
+                    HistoryEventSubType.DEPOSIT_ASSET: EventCategory.DEPOSIT,
+                },
+                HistoryEventType.WITHDRAWAL: {
+                    HistoryEventSubType.REMOVE_ASSET: EventCategory.WITHDRAW,
+                },
+            },
+        }
+
+    def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
+        return {LOCKED_GNO_ADDRESS: (self._decode_events,)}
+
+    def counterparties(self) -> list[CounterpartyDetails]:
+        return [CounterpartyDetails(
+            identifier=CPT_LOCKEDGNO,
+            label='Locked GNO',
+            image='gnosis.svg',
+        )]
