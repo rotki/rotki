3a723c69574fd3692f388f8fa1e4b8def1846419
Joseph Ojoko <entewshi@gmail.com>
2023-06-02 14:14:40 +0100

    feat: update blockchain dropdown, use auto complete, allow search by chain or chain name

--- frontend/app/src/components/accounts/blockchain/ChainSelect.vue
+++ frontend/app/src/components/accounts/blockchain/ChainSelect.vue
@@ -20,18 +20,18 @@ const props = withDefaults(
 const rootAttrs = useAttrs();
 
 const emit = defineEmits<{
-  (e: 'update:model-value', blockchain: Blockchain): void;
+  (e: 'update:model-value', blockchain: Blockchain | null): void;
 }>();
 
-const updateBlockchain = (blockchain: Blockchain) => {
-  emit('update:model-value', blockchain);
-};
-
-const { evmOnly } = toRefs(props);
+const { evmOnly, modelValue } = toRefs(props);
 
 const { isModuleEnabled } = useModules();
 
 const { isEvm, supportedChains } = useSupportedChains();
+
+const { t } = useI18n();
+
+const search = ref<string | null>(null);
 
 const items = computed(() => {
   const isEth2Enabled = get(isModuleEnabled(Module.ETH2));
@@ -49,27 +49,61 @@ const items = computed(() => {
   return data;
 });
 
-const { t } = useI18n();
+const clearSearch = () => {
+  set(search, '');
+};
+
+const updateBlockchain = (blockchain: Blockchain) => {
+  clearSearch();
+  emit('update:model-value', blockchain);
+};
+
+const filter = (chain: Blockchain, queryText: string) => {
+  const item = get(supportedChains).find(blockchain => blockchain.id === chain);
+  if (!item) {
+    return false;
+  }
+
+  const nameIncludes = item.name
+    .toLocaleLowerCase()
+    .includes(queryText.toLocaleLowerCase());
+
+  const idIncludes = item.id
+    .toLocaleLowerCase()
+    .includes(queryText.toLocaleLowerCase());
+
+  return nameIncludes || idIncludes;
+};
 </script>
 
 <template>
-  <v-select
-    :value="modelValue"
-    data-cy="account-blockchain-field"
-    outlined
-    class="account-form__chain"
+  <v-autocomplete
+    :dense="dense"
+    :disabled="disabled"
+    :filter="filter"
     :items="items"
     :label="t('account_form.labels.blockchain')"
-    :disabled="disabled"
-    :dense="dense"
+    :search-input.sync="search"
+    :value="modelValue"
+    class="account-form__chain"
+    clearable
+    data-cy="account-blockchain-field"
+    outlined
+    single-line
     v-bind="rootAttrs"
     @change="updateBlockchain($event)"
+    @blur="clearSearch()"
   >
     <template #selection="{ item }">
-      <chain-display :chain="item" :dense="dense" />
+      <chain-display
+        v-if="!search"
+        :chain="item"
+        :dense="dense"
+        :full-width="false"
+      />
     </template>
     <template #item="{ item }">
       <chain-display :chain="item" />
     </template>
-  </v-select>
+  </v-autocomplete>
 </template>
