4506e2b64bb49a2187cdfe2756aac4eb09573e31
Lefteris Karapetsas <lefteris@refu.co>
2023-06-03 23:35:29 +0200

    Could 1.26.16 be the cause?
    We ran 1.26.15 8 times and no failure here:
    https://github.com/rotki/rotki/pull/6202
    
    So let's test with 1.26.16 now
    
    And now after the discussion with urllib3 people try to add something
    to pytestgeventwrapper

--- pytestgeventwrapper.py
+++ pytestgeventwrapper.py
@@ -1,5 +1,11 @@
 from gevent import monkey  # isort:skip # noqa
 monkey.patch_all()  # isort:skip # noqa
+import atexit
+import weakref
+
+weakref_finalize = weakref.finalize
+atexit.register(weakref_finalize._exitfunc)
+weakref_finalize._registered_with_atexit = True
 
 import sys
 
