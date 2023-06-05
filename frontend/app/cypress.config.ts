25358a1df223c88fb79e67be795f20cb76afb7c8
Konstantinos Paparas <kelsos86@gmail.com>
2023-06-05 20:49:21 +0200

    ci(frontend): delete videos for passed cypress specs (#6213)

--- frontend/app/cypress.config.ts
+++ frontend/app/cypress.config.ts
@@ -1,4 +1,5 @@
 import * as process from 'node:process';
+import fs from 'node:fs';
 import { defineConfig } from 'cypress';
 
 const group = process.env.GROUP ? `${process.env.GROUP}/` : '';
@@ -15,6 +16,21 @@ export default defineConfig({
     supportFile: 'tests/e2e/support/index.ts',
     testIsolation: false,
     setupNodeEvents(on, config) {
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some(test =>
+              test.attempts.some(attempt => attempt.state === 'failed')
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('@cypress/code-coverage/task')(on, config);
       // include any other plugin code...
