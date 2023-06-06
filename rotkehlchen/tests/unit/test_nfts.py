efc8d904008fc61aed690256653fdac1a29ebede
Lefteris Karapetsas <lefteris@refu.co>
2023-06-06 20:32:15 +0200

    Fix test_addresses_queried_for_nfts
    yabir got a new nft

--- rotkehlchen/tests/unit/test_nfts.py
+++ rotkehlchen/tests/unit/test_nfts.py
@@ -1,4 +1,5 @@
 import pytest
+
 from rotkehlchen.db.filtering import NFTFilterQuery
 
 TEST_ACC1 = '0xc37b40ABdB939635068d3c5f13E7faF686F03B65'  # yabir.eth
@@ -11,6 +12,8 @@ TEST_ACC2 = '0x2B888954421b424C5D3D9Ce9bB67c9bD47537d12'  # lefteris.eth
 def test_addresses_queried_for_nfts(blockchain):
     """Tests that nfts are only queried for addresses stored in the database preventing the
     IntegrityError described in https://github.com/rotki/rotki/issues/4456
+
+    TODO: May make sense to VCR this as it makes multiple opensea queries.
     """
     nft_module = blockchain.get_module('nfts')
     nft_module.query_balances(
@@ -18,4 +21,4 @@ def test_addresses_queried_for_nfts(blockchain):
         uniswap_nfts=None,
     )
     balances = nft_module.get_db_nft_balances(filter_query=NFTFilterQuery.make())['entries']
-    assert len(balances) == 1 and balances[0]['name'] == 'yabir.eth'
+    assert any(x['name'] == 'yabir.eth' for x in balances)
