97c5ce8b65c152d1e84e71a9cd7bdde5587e8615
Konstantinos Paparas <kelsos86@gmail.com>
2023-06-06 18:08:14 +0200

    fix(frontend): date-display will not show timezone

--- frontend/app/src/components/display/DateDisplay.vue
+++ frontend/app/src/components/display/DateDisplay.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { displayDateFormatter } from '@/data/date_formatter';
 
-const props = defineProps({
-  timestamp: { required: true, type: Number },
-  noTimezone: { required: false, type: Boolean, default: false },
-  noTime: { required: false, type: Boolean, default: false }
-});
+const props = withDefaults(
+  defineProps<{
+    timestamp: number;
+    showTimezone?: boolean;
+    noTime?: boolean;
+  }>(),
+  {
+    showTimezone: false,
+    noTime: false
+  }
+);
 
-const { timestamp, noTimezone, noTime } = toRefs(props);
+const { timestamp, showTimezone, noTime } = toRefs(props);
 const { dateDisplayFormat } = storeToRefs(useGeneralSettingsStore());
 const { scrambleData, shouldShowAmount } = storeToRefs(
   useSessionSettingsStore()
 );
 
 const dateFormat = computed<string>(() => {
-  const display = get(noTimezone)
-    ? get(dateDisplayFormat).replace('%z', '').replace('%Z', '')
-    : get(dateDisplayFormat);
+  const display = get(showTimezone)
+    ? get(dateDisplayFormat)
+    : get(dateDisplayFormat).replace('%z', '').replace('%Z', '');
 
   if (get(noTime)) {
     return display.split(' ')[0];
@@ -33,18 +39,34 @@ const displayTimestamp = computed<number>(() => {
   return new Date(start + Math.random() * (now - start)).getTime() / 1000;
 });
 
-const formattedDate = computed<string>(() =>
-  displayDateFormatter.format(
-    new Date(get(displayTimestamp) * 1000),
-    get(dateFormat)
-  )
-);
+const date = computed(() => new Date(get(displayTimestamp) * 1000));
+const format = (date: Ref<Date>, format: Ref<string>) =>
+  computed(() => displayDateFormatter.format(get(date), get(format)));
+
+const formattedDate = format(date, dateFormat);
+const formattedDateWithTimezone = format(date, dateDisplayFormat);
+
+const showTooltip = computed(() => {
+  const timezone = get(showTimezone);
+  const format = get(dateDisplayFormat);
+  return !timezone && (format.includes('%z') || format.includes('%Z'));
+});
 </script>
 
 <template>
-  <span class="date-display" :class="{ 'blur-content': !shouldShowAmount }">
-    {{ formattedDate }}
-  </span>
+  <v-tooltip top open-delay="400" :disabled="!showTooltip">
+    <template #activator="{ on, attrs }">
+      <span
+        class="date-display"
+        :class="{ 'blur-content': !shouldShowAmount }"
+        v-bind="attrs"
+        v-on="on"
+      >
+        {{ formattedDate }}
+      </span>
+    </template>
+    <span> {{ formattedDateWithTimezone }} </span>
+  </v-tooltip>
 </template>
 
 <style scoped lang="scss">
