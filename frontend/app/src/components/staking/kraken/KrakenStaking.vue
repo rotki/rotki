67e16d613755381f721a79ad0741384cfea3a207
lukicenturi <lukicenturi@gmail.com>
2023-06-06 19:54:46 +0700

    fix(frontend): kraken staking events

--- frontend/app/src/components/staking/kraken/KrakenStaking.vue
+++ frontend/app/src/components/staking/kraken/KrakenStaking.vue
@@ -18,9 +18,13 @@ const { events } = toRefs(useKrakenStakingStore());
         <kraken-staking-received :received="events.received" />
       </v-col>
     </v-row>
+
+    <!-- as an exception here we specify event-types to only include staking events  -->
+    <!-- if an alternative way becomes possible we can use that -->
     <history-events-view
       use-external-account-filter
       location="kraken"
+      :event-types="['staking']"
       :entry-types="[HistoryEventEntryType.HISTORY_EVENT]"
     />
   </fragment>
