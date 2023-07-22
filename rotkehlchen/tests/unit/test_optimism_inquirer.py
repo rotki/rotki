0a0f21194bb8b042bfa62102aac3818e05d8a179
Lefteris Karapetsas <lefteris@refu.co>
2023-07-22 17:45:37 +0200

    Ankr optimism node is now running an archive node

--- rotkehlchen/tests/unit/test_optimism_inquirer.py
+++ rotkehlchen/tests/unit/test_optimism_inquirer.py
@@ -16,10 +16,10 @@ def test_optimism_nodes_prune_and_archive_status(
     to the nodes not being able to be replicated in a reproducible way in the cassetes.
     """  # noqa: E501
     for node_name, web3_node in optimism_inquirer.web3_mapping.items():
-        if node_name.endpoint in ('https://rpc.ankr.com/optimism', 'https://opt-mainnet.nodereal.io/v1/e85935b614124789b99aa92930aca9a4'):  # noqa: E501
+        if node_name.endpoint == 'https://opt-mainnet.nodereal.io/v1/e85935b614124789b99aa92930aca9a4':  # noqa: E501
             assert not web3_node.is_pruned
             assert not web3_node.is_archive
-        elif node_name.endpoint == 'https://mainnet.optimism.io':
+        elif node_name.endpoint in ('https://mainnet.optimism.io', 'https://rpc.ankr.com/optimism'):
             assert not web3_node.is_pruned
             assert web3_node.is_archive
         elif node_name.endpoint == 'https://optimism-mainnet.public.blastapi.io':
