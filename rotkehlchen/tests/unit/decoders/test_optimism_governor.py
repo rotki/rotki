bfc5ad420e8e5797651c68de8dde748cae722e55
Lefteris Karapetsas <lefteris@refu.co>
2023-07-27 13:32:29 +0200

    Properly decode optimism governance votes with parameters

--- rotkehlchen/tests/unit/decoders/test_optimism_governor.py
+++ rotkehlchen/tests/unit/decoders/test_optimism_governor.py
@@ -53,3 +53,46 @@ def test_vote_cast(database, optimism_inquirer, optimism_accounts):
             address=GOVERNOR_ADDRESS,
         ),
     ]
+
+
+@pytest.mark.vcr()
+@pytest.mark.parametrize('optimism_accounts', [['0x2B888954421b424C5D3D9Ce9bB67c9bD47537d12']])
+def test_vote_cast_with_params(database, optimism_inquirer, optimism_accounts):
+    """Data is taken from
+    https://optimistic.etherscan.io/tx/0x7f54f0d15d1790ca2dd3c4870d9421f09a52f5bbe7f09472f864dc248f90f412
+    """
+    evmhash = deserialize_evm_tx_hash('0x7f54f0d15d1790ca2dd3c4870d9421f09a52f5bbe7f09472f864dc248f90f412')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=optimism_inquirer,
+        database=database,
+        tx_hash=evmhash,
+    )
+    user_address = optimism_accounts[0]
+    assert events == [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=TimestampMS(1688979323000),
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=Asset('ETH'),
+            balance=Balance(amount=FVal('0.000033338918413158')),
+            location_label=user_address,
+            notes='Burned 0.000033338918413158 ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=20,
+            timestamp=TimestampMS(1688979323000),
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.INFORMATIONAL,
+            event_subtype=HistoryEventSubType.GOVERNANCE,
+            asset=Asset('ETH'),
+            balance=Balance(),
+            location_label=user_address,
+            notes='Voted AGAINST optimism governance proposal https://vote.optimism.io/proposals/16633367863894036056841722161407059007904922838583677995599242776177398115322',  # noqa: E501
+            counterparty=CPT_OPTIMISM,
+            address=GOVERNOR_ADDRESS,
+        ),
+    ]
