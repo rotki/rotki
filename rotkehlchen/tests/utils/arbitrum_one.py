f4c1b323dcb1e1cb0915bb9484f5fb4ac32a577c
Lefteris Karapetsas <lefteris@refu.co>
2023-08-11 11:28:25 +0200

    Fix arbitrum airdrop claim test
    Simply re-record the cassete with a single arbitrum node being used.

--- rotkehlchen/tests/utils/arbitrum_one.py
+++ rotkehlchen/tests/utils/arbitrum_one.py
@@ -2,19 +2,25 @@ from rotkehlchen.chain.evm.types import NodeName, WeightedNode
 from rotkehlchen.fval import FVal
 from rotkehlchen.types import SupportedBlockchain
 
+
+def get_arbitrum_allthatnode(weight: FVal) -> WeightedNode:
+    return WeightedNode(
+        node_info=NodeName(
+            name='allthatnode',
+            endpoint='https://arbitrum-one-archive.allthatnode.com',
+            owned=False,
+            blockchain=SupportedBlockchain.ARBITRUM_ONE,
+        ),
+        active=True,
+        weight=weight,
+    )
+
+
 ARBITRUM_ONE_NODES_PARAMETERS_WITH_PRUNED_AND_NOT_ARCHIVED: tuple[str, list[tuple]] = (
     'arbitrum_one_manager_connect_at_start',
     [(
+        get_arbitrum_allthatnode(FVal('0.5')),
         WeightedNode(
-            node_info=NodeName(
-                name='allthatnode',
-                endpoint='https://arbitrum-one-archive.allthatnode.com',
-                owned=False,
-                blockchain=SupportedBlockchain.ARBITRUM_ONE,
-            ),
-            active=True,
-            weight=FVal('0.5'),
-        ), WeightedNode(
             node_info=NodeName(
                 name='blockpi',
                 endpoint='https://arbitrum.blockpi.network/v1/rpc/public',
