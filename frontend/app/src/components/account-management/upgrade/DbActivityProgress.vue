607b500c9dead62d58859ae0fbf27233324cb84d
Joseph Ojoko <entewshi@gmail.com>
2023-09-20 08:34:17 +0100

    refactor(DbActivityProgress): migrate content from vuetify to ui-lib (#6613)

--- frontend/app/src/components/account-management/upgrade/DbActivityProgress.vue
+++ frontend/app/src/components/account-management/upgrade/DbActivityProgress.vue
@@ -25,53 +25,60 @@ const multipleUpgrades = computed(() => {
 </script>
 
 <template>
-  <Card v-if="progress" flat :outlined="false">
-    <template #title>
+  <RuiCard
+    v-if="progress"
+    variant="flat"
+    class="max-w-[29rem] mx-auto !bg-transparent"
+  >
+    <template #header>
       <span v-if="dataMigration">{{ t('login.migrating_data.title') }} </span>
       <span v-else> {{ t('login.upgrading_db.title') }}</span>
     </template>
-    <VRow class="my-2">
-      <VCol cols="auto">
-        <div class="mr-2">
-          <VProgressCircular
-            rounded
+    <div class="flex space-x-4">
+      <div class="flex flex-col">
+        <div class="relative inline-flex rotate-90">
+          <RuiProgress
             :value="progress.percentage"
-            size="45"
-            width="4"
             color="primary"
-          >
-            <div v-if="multipleUpgrades">
-              <VProgressCircular
-                :value="progress.totalPercentage"
-                color="primary"
-              />
-            </div>
-          </VProgressCircular>
+            size="45"
+            circular
+          />
+          <RuiProgress
+            v-if="multipleUpgrades"
+            :value="progress.totalPercentage"
+            class="top-[0.40625rem] left-[0.40625rem] absolute"
+            color="primary"
+            circular
+          />
         </div>
-      </VCol>
-      <VCol class="text-body-1">
-        <div v-if="!dataMigration">
+      </div>
+      <div class="text-body-1">
+        <template v-if="!dataMigration">
           <div>
             {{ t('login.upgrading_db.warning', { ...progress }) }}
           </div>
-          <VDivider class="my-2" />
+          <div
+            class="my-2 border-t border-black/[.12] dark:border-white/[.12]"
+          />
           <div>
             {{ t('login.upgrading_db.current', { ...progress }) }}
           </div>
-        </div>
-        <div v-else>
+        </template>
+        <template v-else>
           <div>
             {{ t('login.migrating_data.warning', { ...progress }) }}
           </div>
-          <VDivider class="my-2" />
+          <div
+            class="my-2 border-t border-black/[.12] dark:border-white/[.12]"
+          />
           <div>
             {{ t('login.migrating_data.current', { ...progress }) }}
           </div>
-          <ul v-if="progress.description" class="ml-n2">
+          <ul v-if="progress.description" class="-ml-2">
             <li>{{ progress.description }}</li>
           </ul>
-        </div>
-      </VCol>
-    </VRow>
-  </Card>
+        </template>
+      </div>
+    </div>
+  </RuiCard>
 </template>
