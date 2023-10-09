c73cc2422c978d27aa60b47b65bf75e5a71bbf72
d347h-eth <wagdie.dev@gmail.com>
2023-10-09 15:24:30 +0200

    add accounting for The Graph staking

---
+++ rotkehlchen/chain/ethereum/modules/thegraph/accountant.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+from collections.abc import Iterator
+from typing import TYPE_CHECKING, cast
+
+from rotkehlchen.accounting.mixins.event import AccountingEventType
+from rotkehlchen.accounting.structures.base import get_event_type_identifier
+from rotkehlchen.accounting.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.chain.ethereum.modules.thegraph.constants import CPT_THEGRAPH
+from rotkehlchen.chain.evm.accounting.interfaces import ModuleAccountantInterface
+from rotkehlchen.chain.evm.accounting.structures import TxEventSettings
+from rotkehlchen.constants import ZERO
+from rotkehlchen.fval import FVal
+
+if TYPE_CHECKING:
+    from rotkehlchen.accounting.pot import AccountingPot
+    from rotkehlchen.accounting.structures.evm_event import EvmEvent
+    from rotkehlchen.types import ChecksumEvmAddress
+
+
+class ThegraphAccountant(ModuleAccountantInterface):
+    def reset(self) -> None:
+        self.assets_supplied: dict[ChecksumEvmAddress, FVal] = defaultdict(FVal)
+
+    def _process_deposit(
+            self,
+            pot: 'AccountingPot',  # pylint: disable=unused-argument
+            event: 'EvmEvent',
+            other_events: Iterator['EvmEvent'],  # pylint: disable=unused-argument
+    ) -> None:
+        self.assets_supplied[event.location_label] += event.balance.amount  # type: ignore[index]
+
+    def _process_withdraw(
+            self,
+            pot: 'AccountingPot',
+            event: 'EvmEvent',
+            other_events: Iterator['EvmEvent'],  # pylint: disable=unused-argument
+    ) -> None:
+        address = cast('ChecksumEvmAddress', event.location_label)
+        self.assets_supplied[address] -= event.balance.amount
+        if self.assets_supplied[address] < ZERO:
+            gain = -1 * self.assets_supplied[address]
+            resolved_asset = event.asset.resolve_to_asset_with_symbol()
+            pot.add_acquisition(
+                event_type=AccountingEventType.TRANSACTION_EVENT,
+                notes=f'Gained {gain} {resolved_asset.symbol} as delegation reward for {address}',
+                location=event.location,
+                timestamp=event.get_timestamp_in_sec(),
+                asset=event.asset,
+                amount=gain,
+                taxable=True,
+                extra_data={'tx_hash': event.tx_hash.hex()},
+            )
+            self.assets_supplied[address] = ZERO
+
+    def event_settings(self, pot: 'AccountingPot') -> dict[int, TxEventSettings]:  # pylint: disable=unused-argument
+        return {
+            get_event_type_identifier(HistoryEventType.STAKING, HistoryEventSubType.DEPOSIT_ASSET, CPT_THEGRAPH): TxEventSettings(  # noqa: E501
+                taxable=False,
+                count_entire_amount_spend=False,
+                count_cost_basis_pnl=False,
+                method='spend',
+                accountant_cb=self._process_deposit,
+            ),
+            get_event_type_identifier(HistoryEventType.SPEND, HistoryEventSubType.FEE, CPT_THEGRAPH): TxEventSettings(  # noqa: E501
+                taxable=True,
+                count_entire_amount_spend=True,
+                count_cost_basis_pnl=pot.settings.include_crypto2crypto,
+                method='spend',
+            ),
+            get_event_type_identifier(HistoryEventType.STAKING, HistoryEventSubType.REMOVE_ASSET, CPT_THEGRAPH): TxEventSettings(  # noqa: E501
+                taxable=False,
+                count_entire_amount_spend=False,
+                count_cost_basis_pnl=False,
+                method='acquisition',
+                accountant_cb=self._process_withdraw,
+            ),
+        }
---
+++ rotkehlchen/tests/unit/accounting/evm/test_thegraph.py
@@ -0,0 +1,186 @@
+from typing import TYPE_CHECKING
+
+import pytest
+from rotkehlchen.accounting.cost_basis.base import (
+    AssetAcquisitionEvent,
+    CostBasisInfo,
+    MatchedAcquisition,
+)
+
+from rotkehlchen.accounting.mixins.event import AccountingEventType
+from rotkehlchen.accounting.pnl import PNL
+from rotkehlchen.accounting.structures.balance import Balance
+from rotkehlchen.accounting.structures.evm_event import EvmEvent
+from rotkehlchen.accounting.structures.processed_event import ProcessedAccountingEvent
+from rotkehlchen.accounting.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.chain.ethereum.modules.thegraph.accountant import ThegraphAccountant
+from rotkehlchen.chain.ethereum.modules.thegraph.constants import CPT_THEGRAPH
+from rotkehlchen.constants.assets import A_GRT
+from rotkehlchen.constants.misc import ONE, ZERO
+from rotkehlchen.fval import FVal
+from rotkehlchen.tests.utils.factories import make_evm_address, make_evm_tx_hash
+from rotkehlchen.types import Location, Price, Timestamp
+from rotkehlchen.utils.misc import ts_sec_to_ms
+
+if TYPE_CHECKING:
+    from rotkehlchen.accounting.accountant import Accountant
+    from rotkehlchen.chain.evm.node_inquirer import EvmNodeInquirer
+
+TS1, TS2, TS3 = Timestamp(1), Timestamp(2), Timestamp(3)
+TSMS1, TSMS2, TSMS3 = ts_sec_to_ms(TS1), ts_sec_to_ms(TS2), ts_sec_to_ms(TS3)
+USER_ADDRESS = make_evm_address()
+HASH1, HASH2, HASH3 = make_evm_tx_hash(), make_evm_tx_hash(), make_evm_tx_hash()
+
+
+@pytest.mark.parametrize('default_mock_price_value', [ONE])
+@pytest.mark.parametrize('accounting_initialize_parameters', [True])
+def test_delegation_reward(accountant: 'Accountant', ethereum_inquirer: 'EvmNodeInquirer'):
+    pot = accountant.pots[0]
+    events_accountant = pot.events_accountant
+    thegraph_accountant = ThegraphAccountant(
+        node_inquirer=ethereum_inquirer,
+        msg_aggregator=ethereum_inquirer.database.msg_aggregator,
+    )
+    thegraph_settings = thegraph_accountant.event_settings(pot)
+    events_accountant.rules_manager.event_settings.update(thegraph_settings)
+    events_iterator = iter([EvmEvent(
+        tx_hash=HASH1,
+        sequence_index=358,
+        timestamp=TSMS1,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.RECEIVE,
+        event_subtype=HistoryEventSubType.NONE,
+        asset=A_GRT,
+        balance=Balance(amount=FVal('1000')),
+        location_label=USER_ADDRESS,
+        notes='Acquire 1000 GRT',
+        counterparty=None,
+        address=None,
+    ), EvmEvent(
+        tx_hash=HASH2,
+        sequence_index=359,
+        timestamp=TSMS2,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.STAKING,
+        event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+        asset=A_GRT,
+        balance=Balance(amount=FVal('995')),
+        location_label=USER_ADDRESS,
+        notes='Delegate 995 GRT to indexer',
+        counterparty=CPT_THEGRAPH,
+        address=None,
+    ), EvmEvent(
+        tx_hash=HASH2,
+        sequence_index=360,
+        timestamp=TSMS2,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.SPEND,
+        event_subtype=HistoryEventSubType.FEE,
+        asset=A_GRT,
+        balance=Balance(amount=FVal('5')),
+        location_label=USER_ADDRESS,
+        notes='Burn 5 GRT as delegation tax',
+        counterparty=CPT_THEGRAPH,
+        address=None,
+    ), EvmEvent(
+        tx_hash=HASH3,
+        sequence_index=208,
+        timestamp=TSMS3,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.STAKING,
+        event_subtype=HistoryEventSubType.REMOVE_ASSET,
+        asset=A_GRT,
+        balance=Balance(amount=FVal('1005')),
+        location_label=USER_ADDRESS,
+        notes='Withdraw 1005 GRT from indexer',
+        counterparty=CPT_THEGRAPH,
+        address=None,
+    )])
+    for event in events_iterator:
+        pot.events_accountant.process(event=event, events_iterator=events_iterator)
+
+    matched_acquisitions = [MatchedAcquisition(
+        amount=FVal('5'),
+        event=AssetAcquisitionEvent(amount=FVal('1000'), timestamp=TS1, rate=Price(ONE), index=0),
+        taxable=True,
+    )]
+    matched_acquisitions[0].event.remaining_amount = FVal('995')  # can't be set by init
+    expected_events = [
+        ProcessedAccountingEvent(
+            type=AccountingEventType.TRANSACTION_EVENT,
+            notes='Acquire 1000 GRT',
+            location=Location.ETHEREUM,
+            timestamp=TS1,
+            asset=A_GRT,
+            free_amount=ZERO,
+            taxable_amount=FVal('1000'),
+            price=Price(ONE),
+            pnl=PNL(taxable=FVal(1000), free=ZERO),
+            cost_basis=None,
+            index=0,
+            extra_data={'tx_hash': HASH1.hex()},  # pylint: disable=no-member
+        ), ProcessedAccountingEvent(
+            type=AccountingEventType.TRANSACTION_EVENT,
+            notes='Delegate 995 GRT to indexer',
+            location=Location.ETHEREUM,
+            timestamp=TS2,
+            asset=A_GRT,
+            free_amount=ZERO,
+            taxable_amount=FVal('995'),
+            price=Price(ONE),
+            pnl=PNL(taxable=ZERO, free=ZERO),  # Deposits are not taxable
+            cost_basis=None,
+            index=1,
+            extra_data={'tx_hash': HASH2.hex()},  # pylint: disable=no-member
+        ), ProcessedAccountingEvent(
+            type=AccountingEventType.TRANSACTION_EVENT,
+            notes='Burn 5 GRT as delegation tax',
+            location=Location.ETHEREUM,
+            timestamp=TS2,
+            asset=A_GRT,
+            free_amount=ZERO,
+            taxable_amount=FVal('5'),
+            price=Price(ONE),
+            pnl=PNL(taxable=FVal('-5'), free=ZERO),
+            cost_basis=CostBasisInfo(
+                taxable_amount=FVal('5'),
+                taxable_bought_cost=FVal('5'),
+                taxfree_bought_cost=ZERO,
+                matched_acquisitions=matched_acquisitions,
+                is_complete=True,
+            ),
+            index=2,
+            extra_data={'tx_hash': HASH2.hex()},  # pylint: disable=no-member
+        ), ProcessedAccountingEvent(
+            type=AccountingEventType.TRANSACTION_EVENT,
+            notes=f'Gained 10 GRT as delegation reward for {USER_ADDRESS}',
+            location=Location.ETHEREUM,
+            timestamp=TS3,
+            asset=A_GRT,
+            free_amount=ZERO,
+            taxable_amount=FVal('10'),
+            price=Price(ONE),
+            pnl=PNL(taxable=FVal('10'), free=ZERO),
+            cost_basis=None,
+            index=3,
+            extra_data={'tx_hash': HASH3.hex()},  # pylint: disable=no-member
+        ), ProcessedAccountingEvent(
+            type=AccountingEventType.TRANSACTION_EVENT,
+            notes='Withdraw 1005 GRT from indexer',
+            location=Location.ETHEREUM,
+            timestamp=TS3,
+            asset=A_GRT,
+            free_amount=FVal('1005'),
+            taxable_amount=ZERO,
+            price=Price(ONE),
+            pnl=PNL(taxable=ZERO, free=ZERO),
+            cost_basis=None,
+            index=4,
+            extra_data={'tx_hash': HASH3.hex()},  # pylint: disable=no-member
+        ),
+    ]
+    expected_events[0].count_cost_basis_pnl = True  # can't be set by init()
+    expected_events[2].count_cost_basis_pnl = True  # can't be set by init()
+    expected_events[2].count_entire_amount_spend = True  # can't be set by init()
+    expected_events[3].count_cost_basis_pnl = True  # can't be set by init()
+    assert pot.processed_events == expected_events
