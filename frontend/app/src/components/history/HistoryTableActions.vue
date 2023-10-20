91966f3bcb6af0650e392d703a962d031ea2e505
Konstantinos Paparas <kelsos86@gmail.com>
2023-10-20 16:14:24 +0200

    fix(frontend): update filter size in history events

--- frontend/app/src/components/history/HistoryTableActions.vue
+++ frontend/app/src/components/history/HistoryTableActions.vue
@@ -1,8 +1,12 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{ hideDivider?: boolean }>(), { hideDivider: false });
+</script>
+
 <template>
   <div class="flex flex-row items-center flex-wrap gap-2 mb-4">
     <slot />
-    <div class="grow xs:hidden md:block" />
-    <div class="md:w-[350px] w-full lg:w-[400px]">
+    <div v-if="!hideDivider" class="grow xs:hidden md:block" />
+    <div class="flex-1 md:min-w-[20rem]">
       <slot name="filter" />
     </div>
   </div>
