218c5058b3ec2b4ab103903ce142d7dd3a63cf70
Konstantinos Paparas <kelsos86@gmail.com>
2023-10-20 17:23:33 +0200

    refactor(frontend): update layout of import page

--- frontend/app/src/pages/import/index.vue
+++ frontend/app/src/pages/import/index.vue
@@ -3,26 +3,31 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <VContainer>
-    <CardTitle class="mt-2">{{ t('import_data.title') }}</CardTitle>
-    <div class="text-rui-text-secondary">
-      {{ t('import_data.description') }}
-    </div>
-    <RuiAlert
-      outlined
-      type="warning"
-      class="mt-4"
-      :title="t('import_data.notice_warning')"
-    >
-      <i18n tag="span" path="import_data.notice">
-        <template #link>
-          <ExternalLink url="https://github.com/rotki/rotki/issues/new/choose">
-            {{ t('import_data.notice_link') }}
-          </ExternalLink>
-        </template>
-      </i18n>
-    </RuiAlert>
+  <div class="p-4">
+    <TablePageLayout :title="[t('import_data.title')]">
+      <template #buttons>
+        <HintMenuIcon>
+          {{ t('import_data.description') }}
+        </HintMenuIcon>
+      </template>
 
-    <GroupedImport class="mt-8" />
-  </VContainer>
+      <RuiAlert
+        outlined
+        type="warning"
+        :title="t('import_data.notice_warning')"
+      >
+        <i18n tag="span" path="import_data.notice">
+          <template #link>
+            <ExternalLink
+              url="https://github.com/rotki/rotki/issues/new/choose"
+            >
+              {{ t('import_data.notice_link') }}
+            </ExternalLink>
+          </template>
+        </i18n>
+      </RuiAlert>
+
+      <GroupedImport />
+    </TablePageLayout>
+  </div>
 </template>
