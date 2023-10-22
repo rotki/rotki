e3f16072fa1e9bd28978c06ca0ff5556237a5b4f
Lefteris Karapetsas <lefteris@refu.co>
2023-10-22 22:45:47 +0200

    Initialize EVMContracts common abis in more places in tests

--- rotkehlchen/tests/fixtures/contracts.py
+++ rotkehlchen/tests/fixtures/contracts.py
@@ -6,12 +6,6 @@ from rotkehlchen.chain.evm.contracts import EvmContracts
 from rotkehlchen.types import ChainID
 
 
-@pytest.fixture(name='evm_contracts')
-def fixture_evm_contracts():  # noqa: PT004  # adding leading underscore does not return it
-    """Initialize the common EvmContracts ABIs"""
-    EvmContracts.initialize_common_abis()
-
-
 @pytest.fixture(name='ethereum_contracts')
-def fixture_ethereum_contracts(evm_contracts):  # pylint: disable=unused-argument
+def fixture_ethereum_contracts():
     return EvmContracts[Literal[ChainID.ETHEREUM]](ChainID.ETHEREUM)
