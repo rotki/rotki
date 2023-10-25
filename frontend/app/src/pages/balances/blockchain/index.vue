d9dbc00de27055e052161a7549eb34a47beaca56
lukicenturi <lukicenturi@gmail.com>
2023-10-25 15:31:31 +0700

    fix(frontend): overall balance style bug

--- frontend/app/src/pages/balances/blockchain/index.vue
+++ frontend/app/src/pages/balances/blockchain/index.vue
@@ -162,211 +162,200 @@ const showDetectEvmAccountsButton: Readonly<Ref<boolean>> = computedEager(
       </RuiButton>
     </template>
 
-    <RuiCard>
-      <template #header>
-        <CardTitle>{{ t('blockchain_balances.title') }}</CardTitle>
-      </template>
+    <div class="flex flex-col gap-8">
+      <RuiCard>
+        <template #header>
+          <CardTitle>{{ t('blockchain_balances.title') }}</CardTitle>
+        </template>
 
-      <AccountDialog :context="context" />
-      <AssetBalances
-        data-cy="blockchain-asset-balances"
-        :loading="isBlockchainLoading"
-        :title="t('blockchain_balances.per_asset.title')"
-        :balances="blockchainAssets"
+        <AccountDialog :context="context" />
+        <AssetBalances
+          data-cy="blockchain-asset-balances"
+          :loading="isBlockchainLoading"
+          :title="t('blockchain_balances.per_asset.title')"
+          :balances="blockchainAssets"
+        />
+      </RuiCard>
+
+      <div>
+        <DetectEvmAccounts v-if="showDetectEvmAccountsButton" />
+      </div>
+
+      <AccountBalances
+        v-if="ethAccounts.length > 0 || busy.eth.value"
+        v-intersect="{
+          handler: observers.eth,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.eth')"
+        :blockchain="Blockchain.ETH"
+        :balances="ethAccounts"
+        @edit-account="editAccount($event)"
       />
-    </RuiCard>
 
-    <div class="mt-8">
-      <DetectEvmAccounts v-if="showDetectEvmAccountsButton" />
+      <AccountBalances
+        v-if="eth2Accounts.length > 0 || busy.eth2.value"
+        v-intersect="{
+          handler: observers.eth2,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.eth2')"
+        :blockchain="Blockchain.ETH2"
+        :balances="eth2Accounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="btcAccounts.length > 0 || busy.btc.value"
+        v-intersect="{
+          handler: observers.btc,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.btc')"
+        :blockchain="Blockchain.BTC"
+        :balances="btcAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="bchAccounts.length > 0 || busy.bch.value"
+        v-intersect="{
+          handler: observers.bch,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.bch')"
+        :blockchain="Blockchain.BCH"
+        :balances="bchAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="ksmAccounts.length > 0 || busy.ksm.value"
+        v-intersect="{
+          handler: observers.ksm,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.ksm')"
+        :blockchain="Blockchain.KSM"
+        :balances="ksmAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="dotAccounts.length > 0 || busy.dot.value"
+        v-intersect="{
+          handler: observers.dot,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.dot')"
+        :blockchain="Blockchain.DOT"
+        :balances="dotAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="avaxAccounts.length > 0 || busy.avax.value"
+        v-intersect="{
+          handler: observers.avax,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.avax')"
+        :blockchain="Blockchain.AVAX"
+        :balances="avaxAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="optimismAccounts.length > 0 || busy.optimism.value"
+        v-intersect="{
+          handler: observers.optimism,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.optimism')"
+        :blockchain="Blockchain.OPTIMISM"
+        :balances="optimismAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="polygonAccounts.length > 0 || busy.polygon_pos.value"
+        v-intersect="{
+          handler: observers.polygon_pos,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.polygon_pos')"
+        :blockchain="Blockchain.POLYGON_POS"
+        :balances="polygonAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="arbitrumAccounts.length > 0 || busy.arbitrum_one.value"
+        v-intersect="{
+          handler: observers.arbitrum_one,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.arbitrum_one')"
+        :blockchain="Blockchain.ARBITRUM_ONE"
+        :balances="arbitrumAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="baseAccounts.length > 0 || busy.base.value"
+        v-intersect="{
+          handler: observers.base,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.base')"
+        :blockchain="Blockchain.BASE"
+        :balances="baseAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="gnosisAccounts.length > 0 || busy.gnosis.value"
+        v-intersect="{
+          handler: observers.gnosis,
+          options: {
+            threshold
+          }
+        }"
+        :title="t('blockchain_balances.balances.gnosis')"
+        :blockchain="Blockchain.GNOSIS"
+        :balances="gnosisAccounts"
+        @edit-account="editAccount($event)"
+      />
+
+      <AccountBalances
+        v-if="loopringAccounts.length > 0"
+        loopring
+        :title="t('blockchain_balances.balances.loopring')"
+        :blockchain="Blockchain.ETH"
+        :balances="loopringAccounts"
+      />
     </div>
-
-    <AccountBalances
-      v-if="ethAccounts.length > 0 || busy.eth.value"
-      v-intersect="{
-        handler: observers.eth,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.eth')"
-      :blockchain="Blockchain.ETH"
-      :balances="ethAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="eth2Accounts.length > 0 || busy.eth2.value"
-      v-intersect="{
-        handler: observers.eth2,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.eth2')"
-      :blockchain="Blockchain.ETH2"
-      :balances="eth2Accounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="btcAccounts.length > 0 || busy.btc.value"
-      v-intersect="{
-        handler: observers.btc,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.btc')"
-      :blockchain="Blockchain.BTC"
-      :balances="btcAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="bchAccounts.length > 0 || busy.bch.value"
-      v-intersect="{
-        handler: observers.bch,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.bch')"
-      :blockchain="Blockchain.BCH"
-      :balances="bchAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="ksmAccounts.length > 0 || busy.ksm.value"
-      v-intersect="{
-        handler: observers.ksm,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.ksm')"
-      :blockchain="Blockchain.KSM"
-      :balances="ksmAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="dotAccounts.length > 0 || busy.dot.value"
-      v-intersect="{
-        handler: observers.dot,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.dot')"
-      :blockchain="Blockchain.DOT"
-      :balances="dotAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="avaxAccounts.length > 0 || busy.avax.value"
-      v-intersect="{
-        handler: observers.avax,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.avax')"
-      :blockchain="Blockchain.AVAX"
-      :balances="avaxAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="optimismAccounts.length > 0 || busy.optimism.value"
-      v-intersect="{
-        handler: observers.optimism,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.optimism')"
-      :blockchain="Blockchain.OPTIMISM"
-      :balances="optimismAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="polygonAccounts.length > 0 || busy.polygon_pos.value"
-      v-intersect="{
-        handler: observers.polygon_pos,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.polygon_pos')"
-      :blockchain="Blockchain.POLYGON_POS"
-      :balances="polygonAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="arbitrumAccounts.length > 0 || busy.arbitrum_one.value"
-      v-intersect="{
-        handler: observers.arbitrum_one,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.arbitrum_one')"
-      :blockchain="Blockchain.ARBITRUM_ONE"
-      :balances="arbitrumAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="baseAccounts.length > 0 || busy.base.value"
-      v-intersect="{
-        handler: observers.base,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.base')"
-      :blockchain="Blockchain.BASE"
-      :balances="baseAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="gnosisAccounts.length > 0 || busy.gnosis.value"
-      v-intersect="{
-        handler: observers.gnosis,
-        options: {
-          threshold
-        }
-      }"
-      class="mt-8"
-      :title="t('blockchain_balances.balances.gnosis')"
-      :blockchain="Blockchain.GNOSIS"
-      :balances="gnosisAccounts"
-      @edit-account="editAccount($event)"
-    />
-
-    <AccountBalances
-      v-if="loopringAccounts.length > 0"
-      loopring
-      class="mt-8"
-      :title="t('blockchain_balances.balances.loopring')"
-      :blockchain="Blockchain.ETH"
-      :balances="loopringAccounts"
-    />
   </TablePageLayout>
 </template>
