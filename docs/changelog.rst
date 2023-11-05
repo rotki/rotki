1590461a406f41dde4e0ec8d6db0ba097b3d45e2
Lefteris Karapetsas <lefteris@refu.co>
2023-11-05 13:49:47 +0100

    VCR cassette for blockchain/test_bitcoin.py

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -2,6 +2,7 @@
 Changelog
 =========
 
+* :bug:`6871` Ethereum staking withdrawals querying should now work fine again.
 * :feature:`6439` Users can filter historical events to display only customized events.
 * :fix:`6548` Users will no longer be blocked by a persistent modal dialog while premium sync is uploading.
 * :fix:`-` Replaces snowtrace.io with avascan.info as the default explorer for Avalanche C-Chain
--- rotkehlchen/tests/api/blockchain/test_bitcoin.py
+++ rotkehlchen/tests/api/blockchain/test_bitcoin.py
@@ -1,25 +1,24 @@
-import random
-
 import pytest
 import requests
 
 from rotkehlchen.tests.utils.api import (
     api_url_for,
     assert_ok_async_response,
-    assert_proper_response_with_result,
     wait_for_async_task_with_result,
 )
 from rotkehlchen.tests.utils.ens import ENS_BRUNO, ENS_BRUNO_BTC_ADDR
 from rotkehlchen.types import SupportedBlockchain
 
 
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('network_mocking', [False])
+@pytest.mark.freeze_time('2023-11-05 11:40:00 GMT')
 @pytest.mark.parametrize('number_of_eth_accounts', [0])
 def test_add_btc_blockchain_account_ens_domain(rotkehlchen_api_server):
     """Test adding a Bitcoin blockchain account via ENS domain when there is none
     in the db works as expected.
     """
     rotki = rotkehlchen_api_server.rest_api.rotkehlchen
-    async_query = random.choice([False, True])
     response = requests.put(
         api_url_for(
             rotkehlchen_api_server,
@@ -28,14 +27,11 @@ def test_add_btc_blockchain_account_ens_domain(rotkehlchen_api_server):
         ),
         json={
             'accounts': [{'address': ENS_BRUNO}],
-            'async_query': async_query,
+            'async_query': True,
         },
     )
-    if async_query:
-        task_id = assert_ok_async_response(response)
-        result = wait_for_async_task_with_result(rotkehlchen_api_server, task_id)
-    else:
-        result = assert_proper_response_with_result(response)
+    task_id = assert_ok_async_response(response)
+    result = wait_for_async_task_with_result(rotkehlchen_api_server, task_id)
 
     assert result == [ENS_BRUNO_BTC_ADDR]
     assert set(rotki.chains_aggregator.accounts.btc) == {ENS_BRUNO_BTC_ADDR}
