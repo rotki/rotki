cf1624033f0efbce25749b8b6bd394e1a81636c2
Konstantinos Paparas <kelsos86@gmail.com>
2023-11-06 16:54:41 +0100

    fix: correct logic on deposits yearn filter

--- frontend/app/src/components/defi/Deposits.vue
+++ frontend/app/src/components/defi/Deposits.vue
@@ -90,7 +90,7 @@ const isCompound = isProtocol(DefiProtocol.COMPOUND);
 const isAave = isProtocol(DefiProtocol.AAVE);
 const isYearnVaults = isProtocol(DefiProtocol.YEARN_VAULTS);
 const isYearnVaultsV2 = isProtocol(DefiProtocol.YEARN_VAULTS_V2);
-const isYearn = logicAnd(isYearnVaults, isYearnVaultsV2);
+const isYearn = logicOr(isYearnVaults, isYearnVaultsV2);
 const noProtocolSelection = computed(() => get(selectedProtocols).length === 0);
 
 const yearnVersion = computed(() => {
