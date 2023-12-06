8a2d744c633aa119ba1170f205537bb92a82ad63
Yabir Benchakhtir <yabirg@protonmail.com>
2023-12-06 11:12:01 +0100

    Send ws message when detecting spam tokens
    This PR adds a ws message to refresh balances in chains where spam tokens were detected.
    This will update the UI and hide any possible detected spam asset from the dashboard if
    any was detected by the background task

--- rotkehlchen/tasks/assets.py
+++ rotkehlchen/tasks/assets.py
@@ -1,32 +1,46 @@
+import logging
+from rotkehlchen.api.websockets.typedefs import WSMessageType
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.assets.utils import check_if_spam_token
 from rotkehlchen.constants.misc import LAST_SPAM_ASSETS_DETECT_KEY
 from rotkehlchen.db.dbhandler import DBHandler
 from rotkehlchen.globaldb.handler import GlobalDBHandler
-from rotkehlchen.types import SPAM_PROTOCOL
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import CHAINID_TO_SUPPORTED_BLOCKCHAIN, SPAM_PROTOCOL, ChainID
 from rotkehlchen.utils.misc import ts_now
 
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
 SYMBOL_AND_NAME_ASSETS_QUERY = (
-    'SELECT C.symbol, A.name, A.identifier FROM evm_tokens as B LEFT JOIN '
+    'SELECT C.symbol, A.name, A.identifier, B.chain FROM evm_tokens as B LEFT JOIN '
     'common_asset_details AS C ON C.identifier = B.identifier JOIN assets as A on '
     'A.identifier=B.identifier WHERE B.PROTOCOL IS NOT ? ORDER BY C.symbol'
 )
 
 
 def autodetect_spam_assets_in_db(user_db: DBHandler) -> None:
-    """Autodetect spam tokens already in the globaldb and ignore them"""
+    """
+    Autodetect spam tokens already in the globaldb and ignore them.
+    If any token gets detected a ws message will be sent to refresh balances for the chain
+    where it was detected.
+    """
     globaldb = GlobalDBHandler()
     detected_spam_assets = []
+    chains_to_refresh: set[ChainID] = set()
 
     # Check if any tokens in the global DB not marked as spam are actually matching
     # the spam patterns
     with globaldb.conn.read_ctx() as cursor:
         cursor.execute(SYMBOL_AND_NAME_ASSETS_QUERY, (SPAM_PROTOCOL,))
-        for symbol, name, identifier in cursor:
+        for symbol, name, identifier, chain_id in cursor:
             if check_if_spam_token(symbol=symbol, name=name) is False:
                 continue
 
+            deserialized_chain_id = ChainID.deserialize_from_db(chain_id)
             detected_spam_assets.append(identifier)
+            chains_to_refresh.add(deserialized_chain_id)
+            log.debug(f'Detected spam token {identifier} at chain {deserialized_chain_id}')
 
     if len(detected_spam_assets) == 0:
         return
@@ -47,4 +61,16 @@ def autodetect_spam_assets_in_db(user_db: DBHandler) -> None:
         write_cursor.execute(  # remember last time spam detection ran
             'INSERT OR REPLACE INTO settings (name, value) VALUES (?, ?)',
             (LAST_SPAM_ASSETS_DETECT_KEY, str(ts_now())),
+        )
+
+    for chain in chains_to_refresh:
+        if chain not in CHAINID_TO_SUPPORTED_BLOCKCHAIN:
+            continue
+
+        user_db.msg_aggregator.add_message(
+            message_type=WSMessageType.REFRESH_BALANCES,
+            data={
+                'type': 'blockchain_balances',
+                'blockchain': chain.to_blockchain().serialize(),
+            },
         )
