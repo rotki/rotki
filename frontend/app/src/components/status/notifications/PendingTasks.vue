6859f29dda0fb5e863f0ca5114e4ee42fa5637a2
Konstantinos Paparas <kelsos86@gmail.com>
2023-12-08 12:39:59 +0100

    refactor: replace vvirtualscroll with useVirtualList

--- frontend/app/src/components/status/notifications/PendingTasks.vue
+++ frontend/app/src/components/status/notifications/PendingTasks.vue
@@ -6,7 +6,7 @@ const { hasRunningTasks, tasks } = storeToRefs(store);
 </script>
 
 <template>
-  <div class="pl-2 mb-2">
+  <div class="px-3.5 mb-2">
     <RuiCard v-if="hasRunningTasks" class="flex flex-col gap-2">
       <CollapsedPendingTasks v-model="expanded" :count="tasks.length" />
       <div v-if="expanded" class="flex flex-col pt-4 -mb-4">
