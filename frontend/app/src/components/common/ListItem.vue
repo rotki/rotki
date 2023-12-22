338acf68fd1087601df8d2f6dca2f3eea63fec90
lukicenturi <lukicenturi@gmail.com>
2023-12-22 19:54:55 +0700

    fix: e2e test error

--- frontend/app/src/components/common/ListItem.vue
+++ frontend/app/src/components/common/ListItem.vue
@@ -30,7 +30,7 @@ const avatarSizeClasses = computed(() => {
 
 <template>
   <div
-    class="w-full flex items-center py-2 gap-2 cursor-pointer"
+    class="flex items-center py-2 gap-2 cursor-pointer"
     :class="{
       'px-4': !noPadding,
       'hover:bg-rui-grey-100 hover:dark:bg-rui-grey-800': !noHover
