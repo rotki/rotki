6f886c784869bd849dd741f5ba171f49f01108d7
lukicenturi <lukicenturi@gmail.com>
2023-12-22 15:33:53 +0700

    feat: warning notes for bittrex import

--- frontend/app/src/components/import/BittrexImport.vue
+++ frontend/app/src/components/import/BittrexImport.vue
@@ -3,11 +3,16 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <ImportSource source="bittrex">
-    <template #upload-title>
-      <i18n tag="span" path="import_data.bittrex.import">
-        <strong>{{ t('import_data.bittrex.import_types') }}</strong>
-      </i18n>
-    </template>
-  </ImportSource>
+  <div>
+    <ImportSource source="bittrex">
+      <template #upload-title>
+        <i18n tag="span" path="import_data.bittrex.import">
+          <strong>{{ t('import_data.bittrex.import_types') }}</strong>
+        </i18n>
+      </template>
+      <div class="text-rui-text-secondary">
+        {{ t('import_data.bittrex.warning') }}
+      </div>
+    </ImportSource>
+  </div>
 </template>
