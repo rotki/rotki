4df6aafc7cbedaf1c806f3f578723e7ef5460032
Joseph Ojoko <entewshi@gmail.com>
2024-01-08 12:49:48 +0100

    chore: revert table limits defaults

--- frontend/app/src/composables/datatable.common.ts
+++ frontend/app/src/composables/datatable.common.ts
@@ -7,7 +7,7 @@ export const useFooterProps = () => {
     lastIcon: 'mdi-chevron-double-right',
     prevIcon: 'mdi-chevron-left',
     nextIcon: 'mdi-chevron-right',
-    itemsPerPageOptions: [5, 10, 15, 25, 50, 100]
+    itemsPerPageOptions: [10, 25, 50, 100]
   }));
   return {
     footerProps
