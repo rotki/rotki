06de692b51c80b3462e11dc636ae9ecb1d8c1cf3
Joseph Ojoko <entewshi@gmail.com>
2024-01-09 01:47:11 +0100

    fix: update api response code for unauthorised and premium sub

--- frontend/app/src/services/utils.ts
+++ frontend/app/src/services/utils.ts
@@ -73,7 +73,7 @@ const isValid = (validStatuses: number[], status: number): boolean =>
 /**
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 400, 409, or 502. This validation method should be used with requests that take
- * parameters or body payload (code 400), requests that require a logged in user (code 409) or
+ * parameters or body payload (code 400), requests that require a logged-in user (code 401) or
  * include the usage of external services 502.
  *
  * An example of a valid use-case for this validator would be for example adding a blockchain account.
@@ -84,13 +84,13 @@ const isValid = (validStatuses: number[], status: number): boolean =>
 export function validWithParamsSessionAndExternalService(
   status: number
 ): boolean {
-  return isValid([200, 400, 409, 502], status);
+  return isValid([200, 400, 401, 409, 502], status);
 }
 
 /**
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 409, or 502. This validation method should be used with requests that require
- * a logged in user (code 409) or include the usage of external services 502.
+ * a logged-in user (code 409) or include the usage of external services 502.
  *
  * An example of a valid use-case for this validator would be fetching all the defi data from
  * the Zerion SDK.
@@ -99,13 +99,13 @@ export function validWithParamsSessionAndExternalService(
  * @return The validity of the status code
  */
 export function validWithSessionAndExternalService(status: number): boolean {
-  return isValid([200, 409, 502], status);
+  return isValid([200, 401, 403, 409, 502], status);
 }
 
 /**
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 400, or 409. This validation method should be used with requests that take
- * parameters or body payload (code 400), and requests that require a logged in user (code 409).
+ * parameters or body payload (code 400), and requests that require a logged-in user (code 401).
  *
  * This is the most common combination of responses from the backend
  *
@@ -113,19 +113,19 @@ export function validWithSessionAndExternalService(status: number): boolean {
  * @return The validity of the status code
  */
 export function validStatus(status: number): boolean {
-  return isValid([200, 400, 409], status);
+  return isValid([200, 400, 401, 409], status);
 }
 
 /**
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 400, 409, or 507. This validation method should be used with requests that take
- * perform a file operation, take parameters, and that require a logged in user (code 409).
+ * perform a file operation, take parameters, and that require a logged-in user (code 401).
  *
  * @param status The status code received in the backend's response
  * @return The validity of the status code
  */
 export function validFileOperationStatus(status: number): boolean {
-  return isValid([200, 400, 409, 507], status);
+  return isValid([200, 400, 401, 507], status);
 }
 
 /**
@@ -145,8 +145,8 @@ export function validWithoutSessionStatus(status: number): boolean {
 
 /**
  * Used to validate a status. This validation considers valid responses the following
- * codes 200 or 409. This validation method should be used with requests that require a
- * logged in user (code 409).
+ * codes 200, 401 or 409. This validation method should be used with requests that require a
+ * logged-in user (code 401).
  *
  * This is most commonly used with get methods that accept no parameters
  *
@@ -154,7 +154,7 @@ export function validWithoutSessionStatus(status: number): boolean {
  * @return The validity of the status code
  */
 export function validWithSessionStatus(status: number): boolean {
-  return isValid([200, 409], status);
+  return isValid([200, 401, 409], status);
 }
 
 /**
@@ -173,8 +173,8 @@ export function validAccountOperationStatus(status: number): boolean {
 /**
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 400, 401 or 409. This validation method should be used with requests that
- * take parameters or body payload (code 400), and requests that require a logged
- * in user (code 409). The method also checks for authorization (code 401).
+ * take parameters or body payload (code 400), and requests that require a logged-in
+ * user (code 401). The method also checks for authorization (code 401).
  *
  * @param status The status code received in the backend's response
  * @return The validity of the status code
@@ -187,7 +187,7 @@ export function validAuthorizedStatus(status: number): boolean {
  * Used to validate a status. This validation considers valid responses the following
  * codes 200, 400, 404, 409 or 502. This validation method should be used with
  * requests that take parameters or body payload (code 400), and requests that
- * require a logged-in user (code 409).
+ * require a logged-in user (code 401).
  *
  * The method also checks for not found (code 404) and remote errors (502)
  *
@@ -197,5 +197,5 @@ export function validAuthorizedStatus(status: number): boolean {
  * @return The validity of the status code
  */
 export function validTaskStatus(status: number): boolean {
-  return isValid([200, 400, 404, 409, 502], status);
+  return isValid([200, 400, 401, 404, 502], status);
 }
