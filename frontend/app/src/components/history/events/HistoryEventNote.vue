3977c767cf59218327baea4b44b9dbfcac009763
Joseph Ojoko <entewshi@gmail.com>
2024-01-23 13:49:40 +0100

    chore: use tailwind class and wrap link text

--- frontend/app/src/components/history/events/HistoryEventNote.vue
+++ frontend/app/src/components/history/events/HistoryEventNote.vue
@@ -80,6 +80,7 @@ function isLinkType(t: any): t is keyof ExplorerUrls {
         v-else-if="note.type === NoteType.URL && note.url"
         :key="`${index}-link`"
         :url="note.url"
+        class="text-wrap"
         :text="note.word"
         color="primary"
         custom
