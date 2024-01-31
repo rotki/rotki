394cf66c8f7bb75fe8cf613ef3468d244e55aeb0
Joseph Ojoko <entewshi@gmail.com>
2024-01-31 20:27:45 +0100

    feat(frontend): add support for kucoin trades import

---
+++ frontend/app/src/components/import/KucoinImport.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+  <ImportSource source="kucoin">
+    <template #upload-title>
+      <i18n
+        tag="span"
+        path="import_data.kucoin.import"
+      >
+        <strong>{{ t('import_data.kucoin.import_types') }}</strong>
+      </i18n>
+    </template>
+    <i18n
+      tag="span"
+      path="import_data.note"
+    >
+      <strong>{{ t('import_data.kucoin.name') }}</strong>
+    </i18n>
+    <ul class="list-disc">
+      <li>{{ t('import_data.kucoin.line_one') }}</li>
+    </ul>
+  </ImportSource>
+</template>
