0959e812e28bb3edc71867b40be3c5bd7b3fa6dd
Yabir Benchakhtir <yabirg@protonmail.com>
2024-01-31 21:05:15 +0100

    Add decoder for juicebox

---
+++ rotkehlchen/chain/ethereum/modules/juicebox/decoder.py
@@ -0,0 +1,130 @@
+import logging
+from typing import Any
+
+import requests
+
+from rotkehlchen.chain.ethereum.abi import decode_event_data_abi_str
+from rotkehlchen.chain.ethereum.modules.juicebox.constants import (
+    CHARITY_PROJECTS_IDS,
+    CHARITY_TAG,
+    CPT_JUICEBOX,
+    JUICEBOX_IPFS_GATEWAY,
+    JUICEBOX_PROJECTS,
+    METADATA_CONTENT_OF_ABI,
+    PAY_ABI,
+    PAY_SIGNATURE,
+    TERMINAL_3_1_2,
+)
+from rotkehlchen.chain.ethereum.utils import token_normalized_value_decimals
+from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
+from rotkehlchen.chain.evm.decoding.structures import (
+    DEFAULT_DECODING_OUTPUT,
+    DecoderContext,
+    DecodingOutput,
+)
+from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
+from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.db.settings import CachedSettings
+from rotkehlchen.errors.misc import RemoteError
+from rotkehlchen.errors.serialization import DeserializationError
+from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import ChecksumEvmAddress
+
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
+
+class JuiceboxDecoder(DecoderInterface):
+
+    def _query_project_name(self, project_id: int) -> tuple[str | None, list[str]]:
+        """Query metadata for project id in ipfs"""
+        try:
+            ipfs_hash = self.evm_inquirer.call_contract(
+                contract_address=JUICEBOX_PROJECTS,
+                abi=METADATA_CONTENT_OF_ABI,
+                method_name='metadataContentOf',
+                arguments=[project_id, 0],  # 0 is the ipfs hash of the data
+            )
+        except RemoteError:
+            log.error(f'Failed to query ipfs hash for juicebox project {project_id}')
+            return None, []
+
+        metadata_url = JUICEBOX_IPFS_GATEWAY + ipfs_hash
+        try:
+            log.debug(f'Querying juicebox metadata at {metadata_url}')
+            metadata: dict[str, Any] = requests.get(
+                url=metadata_url,
+                timeout=CachedSettings().get_timeout_tuple(),
+            ).json()
+        except requests.RequestException as e:
+            log.error(f'Failed to query juicebox project {project_id} metadata due to {e}')
+            return None, []
+
+        return metadata.get('name'), metadata.get('tags', [])
+
+    def _decode_pay(self, context: DecoderContext) -> DecodingOutput:
+        """Decode pay with rewards in juicebox"""
+        if context.tx_log.topics[0] != PAY_SIGNATURE:
+            return DEFAULT_DECODING_OUTPUT
+
+        try:
+            topic_data, decoded_data = decode_event_data_abi_str(context.tx_log, PAY_ABI)
+        except DeserializationError as e:
+            log.error(
+                f'Failed to deserialize Juicebox event at '
+                f'{context.transaction.tx_hash.hex()} due to {e}',
+            )
+            return DEFAULT_DECODING_OUTPUT
+
+        amount = token_normalized_value_decimals(
+            token_amount=decoded_data[2],
+            token_decimals=18,  # it is always ETH
+        )
+        project_id = topic_data[2]
+        project_name, tags = self._query_project_name(project_id=project_id)
+        is_donation = project_id in CHARITY_PROJECTS_IDS or CHARITY_TAG in tags
+        for event in context.decoded_events:
+            if (
+                event.event_type == HistoryEventType.SPEND and
+                event.event_subtype == HistoryEventSubType.NONE and
+                event.asset == A_ETH and
+                event.balance.amount == amount and
+                event.location_label == decoded_data[0]
+            ):
+                event.counterparty = CPT_JUICEBOX
+                if is_donation:
+                    action_verb = 'Donate'
+                    event.event_subtype = HistoryEventSubType.DONATE
+                else:
+                    action_verb = 'Pay'
+
+                event.notes = f'{action_verb} {amount} ETH at Juicebox'
+                if project_name is not None:
+                    event.notes += f' to project {project_name}'
+                if len(decoded_data[4]) != 0:
+                    event.notes += f' with memo: "{decoded_data[4]}"'
+            elif (
+                event.event_type == HistoryEventType.RECEIVE and
+                event.event_subtype == HistoryEventSubType.NONE
+            ):
+                event.counterparty = CPT_JUICEBOX
+                action_verb = 'donating' if is_donation else 'contributing'
+                event.notes = f'Receive an NFT for {action_verb} via Juicebox'
+
+        return DEFAULT_DECODING_OUTPUT
+
+    def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
+        return {
+            TERMINAL_3_1_2: (self._decode_pay,),
+        }
+
+    @staticmethod
+    def counterparties() -> tuple[CounterpartyDetails, ...]:
+        return (
+            CounterpartyDetails(
+                identifier=CPT_JUICEBOX,
+                label='Juicebox',
+                image='juicebox.svg',
+            ),
+        )
---
+++ rotkehlchen/tests/unit/decoders/test_juicebox.py
@@ -0,0 +1,128 @@
+import pytest
+
+from rotkehlchen.accounting.structures.balance import Balance
+from rotkehlchen.assets.asset import Asset
+from rotkehlchen.chain.ethereum.modules.juicebox.constants import CPT_JUICEBOX, TERMINAL_3_1_2
+from rotkehlchen.chain.evm.constants import ZERO_ADDRESS
+from rotkehlchen.chain.evm.decoding.constants import CPT_GAS
+from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.constants.misc import ONE
+from rotkehlchen.fval import FVal
+from rotkehlchen.history.events.structures.evm_event import EvmEvent
+from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
+from rotkehlchen.types import Location, TimestampMS, deserialize_evm_tx_hash
+
+
+@pytest.mark.vcr()
+@pytest.mark.parametrize('ethereum_accounts', [['0x2B888954421b424C5D3D9Ce9bB67c9bD47537d12']])
+def test_donation(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash(
+        '0xf7873b900aa9bb0453b70bc57c7eef54af37346c58edfd4768eb74567279e06e',
+    )
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address = ethereum_accounts[0]
+    timestamp = TimestampMS(1706095919000)
+    donated_amount, gas_fees = '2', '0.00306450894012447'
+    expected_events = [EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=0,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.SPEND,
+        event_subtype=HistoryEventSubType.FEE,
+        asset=A_ETH,
+        balance=Balance(amount=FVal(gas_fees)),
+        location_label=user_address,
+        notes=f'Burned {gas_fees} ETH for gas',
+        counterparty=CPT_GAS,
+    ), EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=1,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.SPEND,
+        event_subtype=HistoryEventSubType.DONATE,
+        asset=A_ETH,
+        balance=Balance(amount=FVal(donated_amount)),
+        location_label=user_address,
+        notes=f'Donate {donated_amount} ETH at Juicebox to project Free Alexey & Roman with memo: "Opensource is not a crime. Privacy is normal. Thank you for fighting for us and good luck.\nhttps://jbm.infura-ipfs.io/ipfs/QmT9hZuHJjGidc8nrdZYskwRbr2hai9rwVodjCuvUNTvKL https://jbm.infura-ipfs.io/ipfs/QmXWAeCpUdLYrzYbpxWq4Ajnhf5trssicHPwGRkPWY5Fx9"',  # noqa: E501
+        address=TERMINAL_3_1_2,
+        counterparty=CPT_JUICEBOX,
+    ), EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=278,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.RECEIVE,
+        event_subtype=HistoryEventSubType.NONE,
+        asset=Asset('eip155:1/erc721:0x723932B58a7c6AEf036d1Fe17654E845d0f0fae5'),
+        balance=Balance(amount=ONE),
+        location_label=user_address,
+        notes='Receive an NFT for donating via Juicebox',
+        counterparty=CPT_JUICEBOX,
+        address=ZERO_ADDRESS,
+        extra_data={'token_id': 4000000011, 'token_name': 'Free Alexey & Roman NFT rewards'},
+    ), EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=280,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.RECEIVE,
+        event_subtype=HistoryEventSubType.NONE,
+        asset=Asset('eip155:1/erc721:0x723932B58a7c6AEf036d1Fe17654E845d0f0fae5'),
+        balance=Balance(amount=ONE),
+        location_label=user_address,
+        notes='Receive an NFT for donating via Juicebox',
+        counterparty=CPT_JUICEBOX,
+        address=ZERO_ADDRESS,
+        extra_data={'token_id': 3000000016, 'token_name': 'Free Alexey & Roman NFT rewards'},
+    )]
+    assert expected_events == events
+
+
+@pytest.mark.vcr()
+@pytest.mark.parametrize('ethereum_accounts', [['0x15b850a67A6ceDd218e368f1Cab11403f45a42f4']])
+def test_fund_raising(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash(
+        '0xd4b8b0857d4cce83f0ce7310a0ed1a8f6360bae331bb4e8bd9217b1370b05bee',
+    )
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address = ethereum_accounts[0]
+    timestamp = TimestampMS(1706711399000)
+    paid_amount, gas_fees = '0.4', '0.003792515741532086'
+    expected_events = [EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=0,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.SPEND,
+        event_subtype=HistoryEventSubType.FEE,
+        asset=A_ETH,
+        balance=Balance(amount=FVal(gas_fees)),
+        location_label=user_address,
+        notes=f'Burned {gas_fees} ETH for gas',
+        counterparty=CPT_GAS,
+    ), EvmEvent(
+        tx_hash=tx_hash,
+        sequence_index=1,
+        timestamp=timestamp,
+        location=Location.ETHEREUM,
+        event_type=HistoryEventType.SPEND,
+        event_subtype=HistoryEventSubType.NONE,
+        asset=A_ETH,
+        balance=Balance(amount=FVal(paid_amount)),
+        location_label=user_address,
+        notes=f'Pay {paid_amount} ETH at Juicebox to project octra NCS community raise',
+        address=TERMINAL_3_1_2,
+        counterparty=CPT_JUICEBOX,
+    )]
+    assert expected_events == events
