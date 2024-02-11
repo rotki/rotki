19aaf07492d41d053d3cd170b4a02c8d1f2b359a
Yabir Benchakhtir <yabirg@protonmail.com>
2024-02-11 21:19:51 +0100

    Add decoder for arbitrum governance

--- rotkehlchen/tests/unit/decoders/test_arbitrum_one.py
+++ rotkehlchen/tests/unit/decoders/test_arbitrum_one.py
@@ -3,9 +3,11 @@ import pytest
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.chain.arbitrum_one.constants import CPT_ARBITRUM_ONE
 from rotkehlchen.chain.arbitrum_one.modules.airdrops.decoder import ARBITRUM_ONE_AIRDROP
+from rotkehlchen.chain.arbitrum_one.modules.arbitrum_governor.decoder import GOVERNOR_ADDRESS
 from rotkehlchen.chain.evm.decoding.constants import CPT_GAS
 from rotkehlchen.constants import ONE
 from rotkehlchen.constants.assets import A_ARB, A_ETH
+from rotkehlchen.constants.misc import ZERO
 from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.evm_event import EvmEvent
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
@@ -13,13 +15,11 @@ from rotkehlchen.tests.utils.arbitrum_one import get_arbitrum_allthatnode
 from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
 from rotkehlchen.types import Location, TimestampMS, deserialize_evm_tx_hash
 
-user_address = '0x0c5b7A89b3689d86Ed473caE4E7CB00381949861'
-
 
 @pytest.mark.vcr(filter_query_parameters=['apikey'])
-@pytest.mark.parametrize('arbitrum_one_accounts', [[user_address]])
+@pytest.mark.parametrize('arbitrum_one_accounts', [['0x0c5b7A89b3689d86Ed473caE4E7CB00381949861']])
 @pytest.mark.parametrize('arbitrum_one_manager_connect_at_start', [(get_arbitrum_allthatnode(ONE),)])  # noqa: E501
-def test_arbitrum_airdrop_claim(database, arbitrum_one_inquirer):
+def test_arbitrum_airdrop_claim(database, arbitrum_one_inquirer, arbitrum_one_accounts):
     """Data taken from
     https://arbiscan.io/tx/0xa230fc4d5e61db1d9be044215b00cb6ad1775b413a240ea23a98117153f6264e
     """
@@ -29,7 +29,7 @@ def test_arbitrum_airdrop_claim(database, arbitrum_one_inquirer):
         database=database,
         tx_hash=tx_hash,
     )
-    timestamp = TimestampMS(1689935876000)
+    timestamp, user_address = TimestampMS(1689935876000), arbitrum_one_accounts[0]
     expected_events = [
         EvmEvent(
             tx_hash=tx_hash,
@@ -56,5 +56,45 @@ def test_arbitrum_airdrop_claim(database, arbitrum_one_inquirer):
             notes='Claimed 625 ARB from arbitrum airdrop',
             counterparty=CPT_ARBITRUM_ONE,
             address=ARBITRUM_ONE_AIRDROP,
+        )]
+    assert expected_events == events
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('arbitrum_one_accounts', [['0xc37b40ABdB939635068d3c5f13E7faF686F03B65']])
+def test_vote_cast(database, arbitrum_one_inquirer, arbitrum_one_accounts):
+    tx_hash = deserialize_evm_tx_hash('0x10b9c5adb845151365bf6977d76b728fad60b64372ce0bde6ae52602b01c282b')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=arbitrum_one_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    timestamp, user_address = TimestampMS(1707677598000), arbitrum_one_accounts[0]
+    expected_events = [
+        EvmEvent(
+            tx_hash=tx_hash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ARBITRUM_ONE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal('0.0000821946')),
+            location_label=user_address,
+            notes='Burned 0.0000821946 ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=tx_hash,
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.ARBITRUM_ONE,
+            event_type=HistoryEventType.INFORMATIONAL,
+            event_subtype=HistoryEventSubType.GOVERNANCE,
+            asset=A_ETH,
+            balance=Balance(amount=ZERO),
+            location_label=user_address,
+            notes='Voted FOR arbitrum_one governance proposal https://www.tally.xyz/gov/arbitrum/proposal/28300903567340237987946172947371304329455149918972967618773111648600015289785',
+            counterparty=CPT_ARBITRUM_ONE,
+            address=GOVERNOR_ADDRESS,
         )]
     assert expected_events == events
