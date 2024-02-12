450b7217f8f9be0b97f3ff82f2503ecffdff07d5
Joseph Ojoko <entewshi@gmail.com>
2024-02-12 19:36:55 +0100

    chore: add asset identifier hint, rearrange to fix lint warning

--- frontend/app/src/components/asset-manager/MergeDialog.vue
+++ frontend/app/src/components/asset-manager/MergeDialog.vue
@@ -2,6 +2,7 @@
 import useVuelidate from '@vuelidate/core';
 import { helpers, required } from '@vuelidate/validators';
 import { toMessages } from '@/utils/validation';
+import type { AssetInfoWithId } from '@/types/asset';
 
 type Errors = Partial<
   Record<'targetIdentifier' | 'sourceIdentifier', string[]>
@@ -16,11 +17,39 @@ const display = useSimpleVModel(props, emit);
 const done = ref(false);
 const errorMessages = ref<Errors>({});
 const targetIdentifier = ref('');
+const target = ref<AssetInfoWithId>();
 const sourceIdentifier = ref('');
 const pending = ref(false);
 
 const { mergeAssets } = useAssets();
 const { t } = useI18n();
+
+const rules = {
+  sourceIdentifier: {
+    required: helpers.withMessage(
+      t('merge_dialog.source.non_empty').toString(),
+      required,
+    ),
+  },
+  targetIdentifier: {
+    required: helpers.withMessage(
+      t('merge_dialog.target.non_empty').toString(),
+      required,
+    ),
+  },
+};
+
+const v$ = useVuelidate(
+  rules,
+  {
+    sourceIdentifier,
+    targetIdentifier,
+  },
+  {
+    $autoDirty: true,
+    $externalResults: errorMessages,
+  },
+);
 
 function reset() {
   set(done, false);
@@ -29,6 +58,7 @@ function reset() {
   set(pending, false);
   set(errorMessages, {});
   get(v$).$reset();
+  set(target, undefined);
 }
 
 function clearErrors() {
@@ -65,33 +95,6 @@ function input(value: boolean) {
   emit('input', value);
   setTimeout(() => reset(), 100);
 }
-
-const rules = {
-  sourceIdentifier: {
-    required: helpers.withMessage(
-      t('merge_dialog.source.non_empty').toString(),
-      required,
-    ),
-  },
-  targetIdentifier: {
-    required: helpers.withMessage(
-      t('merge_dialog.target.non_empty').toString(),
-      required,
-    ),
-  },
-};
-
-const v$ = useVuelidate(
-  rules,
-  {
-    sourceIdentifier,
-    targetIdentifier,
-  },
-  {
-    $autoDirty: true,
-    $externalResults: errorMessages,
-  },
-);
 </script>
 
 <template>
@@ -133,6 +136,9 @@ const v$ = useVuelidate(
           :error-messages="toMessages(v$.targetIdentifier)"
           :label="t('merge_dialog.target.label')"
           :disabled="pending"
+          :asset.sync="target"
+          :hint="target ? t('merge_dialog.target_hint', { identifier: target.identifier }) : ''"
+          persistent-hint
           @focus="clearErrors()"
           @blur="v$.targetIdentifier.$touch()"
         />
