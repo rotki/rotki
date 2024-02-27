cb3dabd04a28ac2eb567ab0b0e98af361753b788
Lefteris Karapetsas <lefteris@refu.co>
2024-02-27 22:07:56 +0100

    Remove atoken_to_asset
    The function is not used at all, and was problematic if both v1 and v2
    tokens were in the globalDB due to symbol conflict.
    
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/ethereum/modules/aave/common.py
+++ rotkehlchen/chain/ethereum/modules/aave/common.py
@@ -4,15 +4,12 @@ from typing import NamedTuple
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import Asset, CryptoAsset, EvmToken
 from rotkehlchen.chain.evm.constants import ETH_SPECIAL_ADDRESS
-from rotkehlchen.constants.assets import A_AETH_V1, A_AREP_V1, A_ETH, A_KNC, A_REP
-from rotkehlchen.constants.resolver import ethaddress_to_identifier
+from rotkehlchen.constants.assets import A_AETH_V1, A_ETH
 from rotkehlchen.errors.asset import UnknownAsset
 from rotkehlchen.fval import FVal
 from rotkehlchen.globaldb.handler import GlobalDBHandler
 from rotkehlchen.logging import RotkehlchenLogsAdapter
-from rotkehlchen.tests.utils.aave import A_AKNC_V1, A_AKNC_V2
 from rotkehlchen.types import ChecksumEvmAddress
-
 
 logger = logging.getLogger(__name__)
 log = RotkehlchenLogsAdapter(logger)
@@ -65,28 +62,6 @@ def asset_to_aave_reserve_address(asset: CryptoAsset) -> ChecksumEvmAddress | No
     token = EvmToken(asset.identifier)
     assert token, 'should not be a non token asset at this point'
     return token.evm_address
-
-
-def atoken_to_asset(atoken: EvmToken) -> CryptoAsset | None:
-    if atoken == A_AETH_V1:
-        return A_ETH.resolve_to_crypto_asset()
-    if atoken == A_AREP_V1:
-        return A_REP.resolve_to_crypto_asset()
-    if atoken in (A_AKNC_V1, A_AKNC_V2):
-        return A_KNC.resolve_to_crypto_asset()
-
-    asset_symbol = atoken.symbol[1:]
-    with GlobalDBHandler().conn.read_ctx() as cursor:
-        result = cursor.execute(
-            'SELECT A.address from evm_tokens as A LEFT OUTER JOIN common_asset_details as B '
-            'ON A.identifier=B.identifier WHERE A.chain=? AND B.symbol=? COLLATE NOCASE',
-            (atoken.chain_id.serialize_for_db(), asset_symbol),
-        ).fetchall()
-    if len(result) != 1:
-        log.error(f'Could not find asset from {atoken} since multiple or no results were returned')
-        return None
-
-    return EvmToken(ethaddress_to_identifier(result[0][0]))
 
 
 def asset_to_atoken(asset: CryptoAsset, version: int) -> EvmToken | None:
