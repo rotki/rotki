ed77469a5bcd99757912b8ed6ce238fde62ec737
Joseph Ojoko <entewshi@gmail.com>
2024-03-12 16:39:50 +0100

    refactor(frontend): adjust layout of single location page

--- frontend/app/src/pages/locations/[identifier].vue
+++ frontend/app/src/pages/locations/[identifier].vue
@@ -13,23 +13,28 @@ const location = locationData(identifier);
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 mx-4">
-    <div
-      v-if="location"
-      class="flex flex-row items-center my-4 gap-2"
-    >
-      <LocationIcon
-        :item="identifier"
-        icon
-        size="48px"
-      />
-      <span class="text-h5 font-medium">
-        {{ location.name }}
-      </span>
+  <TablePageLayout
+    class="p-4"
+    hide-header
+  >
+    <div class="flex flex-col gap-4 w-full">
+      <div
+        v-if="location"
+        class="flex flex-row items-center mb-4 gap-2"
+      >
+        <LocationIcon
+          :item="identifier"
+          icon
+          size="48px"
+        />
+        <span class="text-h5 font-medium">
+          {{ location.name }}
+        </span>
+      </div>
+      <LocationValueRow :identifier="identifier" />
+      <LocationAssets :identifier="identifier" />
+      <ClosedTrades :location-overview="identifier" />
+      <DepositsWithdrawalsContent :location-overview="identifier" />
     </div>
-    <LocationValueRow :identifier="identifier" />
-    <LocationAssets :identifier="identifier" />
-    <ClosedTrades :location-overview="identifier" />
-    <DepositsWithdrawalsContent :location-overview="identifier" />
-  </div>
+  </TablePageLayout>
 </template>
