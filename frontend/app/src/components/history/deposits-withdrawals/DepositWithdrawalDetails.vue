162790d2de77b688ae0f2945a9565308699caccd
Joseph Ojoko <entewshi@gmail.com>
2024-03-12 16:39:16 +0100

    refactor(frontend): migrate deposits & withdrawals table

--- frontend/app/src/components/history/deposits-withdrawals/DepositWithdrawalDetails.vue
+++ frontend/app/src/components/history/deposits-withdrawals/DepositWithdrawalDetails.vue
@@ -1,31 +1,27 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import type { AssetMovementEntry } from '@/types/history/asset-movements';
 
-defineProps<{
-  span: number;
-  item: AssetMovementEntry;
-}>();
+defineProps<{ item: AssetMovementEntry }>();
 
 const { t } = useI18n();
 </script>
 
 <template>
-  <TableExpandContainer
-    visible
-    :colspan="span"
-  >
-    <template #title>
+  <div>
+    <h6 class="text-h6 mb-4">
       {{ t('deposits_withdrawals.details.title') }}
-    </template>
-    <MovementLinks
-      v-if="item.address || item.transactionId"
-      :item="item"
-    />
-    <div
-      v-else
-      class="font-medium"
-    >
-      {{ t('deposits_withdrawals.details.no_details') }}
-    </div>
-  </TableExpandContainer>
+    </h6>
+    <RuiCard>
+      <MovementLinks
+        v-if="item.address || item.transactionId"
+        :item="item"
+      />
+      <div
+        v-else
+        class="font-medium"
+      >
+        {{ t('deposits_withdrawals.details.no_details') }}
+      </div>
+    </RuiCard>
+  </div>
 </template>
