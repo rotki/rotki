9924da3bc2c67972ce084d62942819b80183a9c5
yetyear <flite@outlook.com>
2024-03-13 10:54:45 +0800

    chore: remove repetitive words
    Signed-off-by: yetyear <flite@outlook.com>

--- rotkehlchen/api/v1/parser.py
+++ rotkehlchen/api/v1/parser.py
@@ -31,7 +31,7 @@ class ResourceReadingParser(FlaskParser):
         Edited from core parser to include the resource object.
 
         When allow_async_validation is set to true we avoid doing validation for async requests
-        before spawning the the task and instead any validation error will appear in the async
+        before spawning the task and instead any validation error will appear in the async
         task output.
         """
         location = location or self.location
