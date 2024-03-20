e19f0a84f62180dc86b08b8fda5516833b5cf64f
OjusWiZard <ojuswimail@gmail.com>
2024-03-20 22:17:53 +0530

    Chore (VCR): Cassettes for scroll inquirer test
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- rotkehlchen/tests/unit/test_scroll_inquirer.py
+++ rotkehlchen/tests/unit/test_scroll_inquirer.py
@@ -1,6 +1,5 @@
 import pytest
 
-from rotkehlchen.tests.utils.factories import make_evm_address
 from rotkehlchen.tests.utils.scroll import (
     ANKR_SCROLL_NODE,
     BLOCKPI_SCROLL_NODE,
@@ -9,15 +8,16 @@ from rotkehlchen.tests.utils.scroll import (
 )
 
 
+@pytest.mark.vcr()
 @pytest.mark.parametrize(*SCROLL_NODES_PARAMETERS_WITH_PRUNED_AND_NOT_ARCHIVED)
-@pytest.mark.parametrize('scroll_accounts', [[make_evm_address()]])  # to connect to nodes
+@pytest.mark.parametrize('scroll_accounts', [['0xCace5b3c29211740E595850E80478416eE77cA21']])  # to connect to nodes  # noqa: E501
 def test_scroll_nodes_prune_and_archive_status(
         scroll_manager_connect_at_start,
         scroll_inquirer,
 ):
-    """Checks that connecting to a set of scroll nodes, the capabilities of those nodes are
-    known and stored. This test is sort of fast and is not VCRed due to the randomness of the
-    connection to the nodes not being able to be replicated in a reproducible way in the cassetes.
+    """Checks that connecting to a set of scroll nodes, the capabilities of those nodes are known
+    and stored. It tests the nodes one by one to avoid the randomness of the connections to
+    the nodes while running with the VCR cassettes.
     """
     for node_name, web3_node in scroll_inquirer.web3_mapping.items():
         if node_name in {BLOCKPI_SCROLL_NODE, ANKR_SCROLL_NODE, ONE_RPC_SCROLL_NODE}:
