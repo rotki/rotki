e813cc0965fba312ccc95d3ccd3ab870f21fc55a
Lefteris Karapetsas <lefteris@refu.co>
2024-03-29 14:58:00 +0100

    VCR api test_ethereum_tokens_detection
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/tests/api/test_balances.py
+++ rotkehlchen/tests/api/test_balances.py
@@ -820,7 +820,9 @@ def test_query_avax_balances(rotkehlchen_api_server: 'APIServer') -> None:
     assert FVal(total_avax['usd_value']) >= ZERO
 
 
-@pytest.mark.parametrize('number_of_eth_accounts', [1])
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.freeze_time('2024-03-29 13:00:00 GMT')
+@pytest.mark.parametrize('ethereum_accounts', [['0x2B888954421b424C5D3D9Ce9bB67c9bD47537d12']])
 def test_ethereum_tokens_detection(
         rotkehlchen_api_server,
         ethereum_accounts,
