ff029e2f40bf5909dd7f49af3a8fdfac72bcee36
Joseph Ojoko <entewshi@gmail.com>
2024-04-10 15:30:08 +0100

    chore(frontend): extend autocomplete stub to show messages and handle array value

--- frontend/app/tests/unit/specs/stubs/VAutocomplete.ts
+++ frontend/app/tests/unit/specs/stubs/VAutocomplete.ts
@@ -2,19 +2,35 @@ const VAutocompleteStub = {
   template: `
     <div>
       <div>
-        <input :value="value" class="input-value" type="text" @input="$emit('input', $event.value)">
+        <input :value="value?.toString()" class="input-value" type="text" @input="$emit('input', $event.value)">
       </div>
       <div class="selections">
         <span v-for="item in items">
           {{ item[itemValue] ?? item }}
         </span>
       </div>
+      <div v-if="error || message" class="details">
+        {{ error || message }}
+      </div>
     </div>
   `,
   props: {
-    value: { type: String },
+    value: { type: [String, Array<string>] },
+    successMessages: { type: [String, Array<string>], default: [] },
+    errorMessages: { type: [String, Array<string>], default: [] },
     items: { type: Array<any> },
     itemValue: { type: String },
+  },
+  inheritAttrs: false,
+  computed: {
+    message(): string {
+      const m: string | string[] = this.successMessages;
+      return Array.isArray(m) ? m[0] : m;
+    },
+    error(): string {
+      const m: string | string[] = this.errorMessages;
+      return Array.isArray(m) ? m[0] : m;
+    },
   },
 };
 
