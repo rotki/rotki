1568500e4b2726d6bca1b1615eadf64c0d3d19e8
Konstantinos Paparas <kelsos86@gmail.com>
2024-04-10 14:07:52 +0200

    chore: bump minor dependencies

--- frontend/common/src/settings/index.ts
+++ frontend/common/src/settings/index.ts
@@ -32,11 +32,11 @@ export enum TimeUnit {
   DAY = 'day',
 }
 
-export const SELECTED_THEME = 'selectedTheme' as const;
+export const SELECTED_THEME = 'selectedTheme';
 
-export const LIGHT_THEME = 'lightTheme' as const;
+export const LIGHT_THEME = 'lightTheme';
 
-export const DARK_THEME = 'darkTheme' as const;
+export const DARK_THEME = 'darkTheme';
 
 interface FrontendSettings {
   readonly [SELECTED_THEME]: Theme;
