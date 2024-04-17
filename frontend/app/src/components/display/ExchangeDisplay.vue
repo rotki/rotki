6e829f1fc0c0df8b138489637bcccd65d5f3affd
Joseph Ojoko <entewshi@gmail.com>
2024-04-17 11:37:21 +0100

    chore(frontend): add prop for setting icon size in exchange display

--- frontend/app/src/components/display/ExchangeDisplay.vue
+++ frontend/app/src/components/display/ExchangeDisplay.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   exchange: string;
-}>();
+  size?: string;
+}>(), { size: '1.5rem' });
 
 const { exchange } = toRefs(props);
 const { locationData } = useLocations();
@@ -12,18 +13,15 @@ const image = useRefMap(location, location => location?.image ?? undefined);
 </script>
 
 <template>
-  <div class="flex flex-row items-center shrink">
+  <div class="flex flex-row gap-2 items-center shrink">
     <AdaptiveWrapper>
       <AppImage
-        width="24px"
-        height="24px"
+        :width="size"
+        :height="size"
         contain
         :src="image"
       />
     </AdaptiveWrapper>
-    <div
-      class="ml-2"
-      v-text="name"
-    />
+    <div v-text="name" />
   </div>
 </template>
