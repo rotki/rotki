efaa41bd45dc881abb90d7b7edd092722c014c26
Lefteris Karapetsas <lefteris@refu.co>
2024-04-20 14:38:46 +0200

    Implement decoder for OMNI claimAndStake
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/tests/unit/decoders/test_omni.py
+++ rotkehlchen/tests/unit/decoders/test_omni.py
@@ -17,7 +17,7 @@ from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
 from rotkehlchen.types import Location, TimestampMS, deserialize_evm_tx_hash
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
 @pytest.mark.parametrize('ethereum_accounts', [['0x9211043D7012457a51caB901e5b184dA2Ef8b245']])
 def test_claim(database, ethereum_inquirer, ethereum_accounts):
     tx_hex = deserialize_evm_tx_hash('0xc626898273896eb771e9725137849dd104e388aad49687068a7681b5c54893fe')  # noqa: E501
@@ -72,7 +72,7 @@ def test_claim(database, ethereum_inquirer, ethereum_accounts):
     assert events == expected_events
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
 @pytest.mark.parametrize('ethereum_accounts', [['0x361D6a0A6ce25c833081DAa7C31c416D4295eC5A']])
 def test_stake(database, ethereum_inquirer, ethereum_accounts):
     tx_hex = deserialize_evm_tx_hash('0x66f47dc448d7371eeccaa500af1aea76a1620de56c621187be83b1cc19bf861c')  # noqa: E501
@@ -111,6 +111,74 @@ def test_stake(database, ethereum_inquirer, ethereum_accounts):
         ), EvmEvent(
             tx_hash=evmhash,
             sequence_index=103,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.STAKING,
+            event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+            asset=Asset(OMNI_TOKEN_ID),
+            balance=Balance(amount=FVal(omni_str)),
+            location_label=ethereum_accounts[0],
+            notes=f'Stake {omni_str} OMNI',
+            counterparty=CPT_OMNI,
+            address=OMNI_STAKING_CONTRACT,
+        ),
+    ]
+    assert events == expected_events
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('ethereum_accounts', [['0x77782FcAFAF67576b7423ae5173CDe6D83695796']])
+def test_claim_and_stake(database, ethereum_inquirer, ethereum_accounts):
+    tx_hex = deserialize_evm_tx_hash('0x630c32c20d75c51ffe7b1db1cb3d82d357a8db4da10c1c5b212e01f8e92f6310')  # noqa: E501
+    evmhash = deserialize_evm_tx_hash(tx_hex)
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hex,
+    )
+    timestamp, gas_str, omni_str = TimestampMS(1713602555000), '0.000645708531342875', '15.3275649'
+    expected_events = [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_str)),
+            location_label=ethereum_accounts[0],
+            notes=f'Burned {gas_str} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal('0.0005')),
+            location_label=ethereum_accounts[0],
+            notes='Spend 0.0005 ETH as a fee to claim the Omni genesis airdrop',
+            counterparty=CPT_OMNI,
+            address=OMNI_AIDROP_CONTRACT,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=2,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.AIRDROP,
+            asset=Asset(OMNI_TOKEN_ID),
+            balance=Balance(amount=FVal(omni_str)),
+            location_label=ethereum_accounts[0],
+            notes=f'Claim {omni_str} OMNI from the Omni genesis airdrop',
+            counterparty=CPT_OMNI,
+            address=OMNI_AIDROP_CONTRACT,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=3,
             timestamp=timestamp,
             location=Location.ETHEREUM,
             event_type=HistoryEventType.STAKING,
