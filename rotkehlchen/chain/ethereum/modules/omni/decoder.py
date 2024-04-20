bc818090664c19a8d1ef0b1dcd1ff9941449ef24
Lefteris Karapetsas <lefteris@refu.co>
2024-04-20 14:38:46 +0200

    Implement decoder for OMNI claimAndStake
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/ethereum/modules/omni/decoder.py
+++ rotkehlchen/chain/ethereum/modules/omni/decoder.py
@@ -1,6 +1,8 @@
 import logging
 from typing import Any
 
+from rotkehlchen.accounting.structures.balance import Balance
+from rotkehlchen.assets.asset import Asset
 from rotkehlchen.chain.ethereum.utils import token_normalized_value_decimals
 from rotkehlchen.chain.evm.decoding.constants import STAKED
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
@@ -10,7 +12,9 @@ from rotkehlchen.chain.evm.decoding.structures import (
     DecodingOutput,
 )
 from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
+from rotkehlchen.chain.evm.decoding.utils import maybe_reshuffle_events
 from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.types import ChecksumEvmAddress
@@ -41,6 +45,8 @@ class OmniDecoder(DecoderInterface):
             token_amount=hex_or_bytes_to_int(context.tx_log.data),
             token_decimals=18,  # omni has 18 decimals
         )
+        transfer_found = False
+        notes = f'Claim {claimed_amount} OMNI from the Omni genesis airdrop'
         for event in context.decoded_events:
             if (
                 event.event_type == HistoryEventType.RECEIVE and
@@ -51,7 +57,8 @@ class OmniDecoder(DecoderInterface):
                 event.event_type = HistoryEventType.RECEIVE
                 event.event_subtype = HistoryEventSubType.AIRDROP
                 event.counterparty = CPT_OMNI
-                event.notes = f'Claim {event.balance.amount} OMNI from the Omni genesis airdrop'
+                event.notes = notes
+                transfer_found = True
             elif (
                 event.event_type == HistoryEventType.SPEND and
                 event.address == OMNI_AIDROP_CONTRACT and
@@ -60,6 +67,38 @@ class OmniDecoder(DecoderInterface):
                 event.event_subtype = HistoryEventSubType.FEE
                 event.counterparty = CPT_OMNI
                 event.notes = f'Spend {event.balance.amount} ETH as a fee to claim the Omni genesis airdrop'  # noqa: E501
+
+        if not transfer_found:  # it's claim and stake, so transfer is direct to stake
+            claim_event = self.base.make_event_from_transaction(
+                transaction=context.transaction,
+                tx_log=context.tx_log,
+                event_type=HistoryEventType.RECEIVE,
+                event_subtype=HistoryEventSubType.AIRDROP,
+                asset=Asset(OMNI_TOKEN_ID),
+                balance=Balance(amount=FVal(claimed_amount)),
+                location_label=claiming_address,
+                notes=notes,
+                counterparty=CPT_OMNI,
+                address=OMNI_AIDROP_CONTRACT,
+            )
+            for event in context.decoded_events:  # find the stake event and assure comes after
+                if (
+                        event.event_type == HistoryEventType.STAKING and
+                        event.event_subtype == HistoryEventSubType.DEPOSIT_ASSET and
+                        event.location_label == claiming_address and
+                        event.asset.identifier == OMNI_TOKEN_ID and
+                        event.balance.amount == claimed_amount and
+                        event.address == OMNI_STAKING_CONTRACT
+                ):
+                    maybe_reshuffle_events(
+                        ordered_events=[claim_event, event],
+                        events_list=context.decoded_events + [claim_event],
+                    )
+                    break
+            else:
+                log.error(f'Could not find stake event for an OMNI stake and claim action {context.transaction.tx_hash.hex()}')  # noqa: E501
+
+            return DecodingOutput(event=claim_event)
 
         return DEFAULT_DECODING_OUTPUT
 
@@ -74,6 +113,8 @@ class OmniDecoder(DecoderInterface):
             token_amount=hex_or_bytes_to_int(context.tx_log.data),
             token_decimals=18,  # omni has 18 decimals
         )
+        transfer_found = False
+        notes = f'Stake {staked_amount} OMNI'
         for event in context.decoded_events:
             if (
                 event.event_type == HistoryEventType.SPEND and
@@ -85,7 +126,22 @@ class OmniDecoder(DecoderInterface):
                 event.event_type = HistoryEventType.STAKING
                 event.event_subtype = HistoryEventSubType.DEPOSIT_ASSET
                 event.counterparty = CPT_OMNI
-                event.notes = f'Stake {event.balance.amount} OMNI'
+                event.notes = notes
+                transfer_found = True
+
+        if not transfer_found:  # it's claim and stake so transfer is direct from airdrop contract
+            return DecodingOutput(event=self.base.make_event_from_transaction(
+                transaction=context.transaction,
+                tx_log=context.tx_log,
+                event_type=HistoryEventType.STAKING,
+                event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+                asset=Asset(OMNI_TOKEN_ID),
+                balance=Balance(amount=FVal(staked_amount)),
+                location_label=user_address,
+                notes=notes,
+                counterparty=CPT_OMNI,
+                address=OMNI_STAKING_CONTRACT,
+            ))
 
         return DEFAULT_DECODING_OUTPUT
 
