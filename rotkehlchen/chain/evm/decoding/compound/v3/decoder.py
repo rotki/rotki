2ecb04d0211e491566727e599c40769016a0dfc0
OjusWiZard <ojuswimail@gmail.com>
2024-05-01 19:22:58 +0530

    Fix (Compound v3): Remap the event decoders
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- rotkehlchen/chain/evm/decoding/compound/v3/decoder.py
+++ rotkehlchen/chain/evm/decoding/compound/v3/decoder.py
@@ -2,9 +2,12 @@ import logging
 from collections.abc import Callable
 from typing import TYPE_CHECKING, Any
 
+from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import EvmToken
 from rotkehlchen.assets.utils import get_or_create_evm_token
 from rotkehlchen.chain.ethereum.utils import asset_normalized_value
+from rotkehlchen.chain.evm.constants import ZERO_ADDRESS
+from rotkehlchen.chain.evm.decoding.constants import ERC20_OR_ERC721_TRANSFER
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
 from rotkehlchen.chain.evm.decoding.structures import (
     DEFAULT_DECODING_OUTPUT,
@@ -13,6 +16,7 @@ from rotkehlchen.chain.evm.decoding.structures import (
     DecodingOutput,
 )
 from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
+from rotkehlchen.chain.evm.decoding.utils import maybe_reshuffle_events
 from rotkehlchen.constants.resolver import evm_address_to_identifier
 from rotkehlchen.errors.asset import UnknownAsset, WrongAssetType
 from rotkehlchen.globaldb.handler import GlobalDBHandler
@@ -106,68 +110,6 @@ class Compoundv3CommonDecoder(DecoderInterface):
 
         return DEFAULT_DECODING_OUTPUT
 
-    def _decode_supply_or_withdraw_event(
-            self,
-            context: DecoderContext,
-            compound_token: EvmToken,
-    ) -> DecodingOutput:
-        """Decode a compound v3 supply or withdraw event. Takes decoder context and
-        the compound v3 wrapped token of the supplied/withdrawn underlying token"""
-        if (underlying_token := self._get_compound_underlying_token(compound_token)) is None:
-            log.error(
-                f'At compound v3 supply/withdraw decoding of tx '
-                f'{context.transaction.tx_hash.hex()} the underlying token was not found.',
-            )
-            return DEFAULT_DECODING_OUTPUT
-
-        amount = asset_normalized_value(
-            amount=hex_or_bytes_to_int(context.tx_log.data),
-            asset=underlying_token,
-        )
-        paired_event, action_from_event_type, action_to_event_subtype, action_to_notes = None, None, None, None  # noqa: E501
-        for event in context.decoded_events:
-            if (
-                event.event_subtype == HistoryEventSubType.NONE and
-                event.asset == underlying_token and amount == event.balance.amount
-            ):
-                if event.event_type == HistoryEventType.SPEND:
-                    event.event_type = HistoryEventType.DEPOSIT
-                    event.event_subtype = HistoryEventSubType.DEPOSIT_ASSET
-                    event.notes = f'Deposit {amount} {underlying_token.symbol} to compound v3'
-                    action_from_event_type = HistoryEventType.RECEIVE
-                    action_to_event_subtype = HistoryEventSubType.RECEIVE_WRAPPED
-                    action_to_notes = f'Receive {{amount}} {compound_token.symbol} from compound v3'  # {amount} to be replaced in post decoding  # noqa: E501
-                else:
-                    event.event_type = HistoryEventType.WITHDRAWAL
-                    event.event_subtype = HistoryEventSubType.REMOVE_ASSET
-                    event.notes = f'Withdraw {amount} {underlying_token.symbol} from compound v3'
-                    action_from_event_type = HistoryEventType.SPEND
-                    action_to_event_subtype = HistoryEventSubType.RETURN_WRAPPED
-                    action_to_notes = f'Return {{amount}} {compound_token.symbol} to compound v3'
-
-                event.counterparty = CPT_COMPOUND_V3
-                paired_event = event
-                break
-        else:  # did not break/find anything
-            log.error(
-                'At compound v3 supply/withdraw decoding of tx '
-                f'{context.transaction.tx_hash.hex()} the action item data was not found.',
-            )
-            return DEFAULT_DECODING_OUTPUT
-
-        # also create an action item for the receive/return of the cTokens
-        action_item = ActionItem(
-            action='transform',
-            from_event_type=action_from_event_type,
-            from_event_subtype=HistoryEventSubType.NONE,
-            asset=compound_token,
-            to_event_subtype=action_to_event_subtype,
-            to_notes=action_to_notes,
-            to_counterparty=CPT_COMPOUND_V3,
-            paired_event_data=(paired_event, paired_event.event_type == HistoryEventType.DEPOSIT),
-        )
-        return DecodingOutput(action_items=[action_item])
-
     def _correct_supply_or_withdraw_event(
             self,
             decoded_events: list['EvmEvent'],
@@ -187,32 +129,210 @@ class Compoundv3CommonDecoder(DecoderInterface):
                 break
         return decoded_events
 
-    def _decode_repay_or_borrow_event(self, context: DecoderContext) -> DecodingOutput:
-        """Decode a compound v3 repay or borrow event. Takes only the decoder context."""
+    def _decode_supply_or_repay_event(
+            self,
+            context: DecoderContext,
+            compound_token: 'EvmToken',
+    ) -> DecodingOutput:
+        """Decode a compound v3 supply or repay event. Takes decoder context and
+        the compound v3 wrapped token of the supplied/withdrawn underlying token."""
+        if (underlying_token := self._get_compound_underlying_token(compound_token)) is None:
+            log.error(
+                f'At compound v3 supply/withdraw decoding of tx '
+                f'{context.transaction.tx_hash.hex()} the underlying token was not found.',
+            )
+            return DEFAULT_DECODING_OUTPUT
+
+        receiving_ctoken = False
+        for tx_log in context.all_logs:
+            if (
+                tx_log.address == compound_token.evm_address and
+                tx_log.topics[0] == ERC20_OR_ERC721_TRANSFER and
+                hex_or_bytes_to_address(tx_log.topics[1]) == ZERO_ADDRESS and  # from
+                hex_or_bytes_to_address(tx_log.topics[2]) == hex_or_bytes_to_address(context.tx_log.topics[2])  # to  # noqa: E501
+            ):
+                receiving_ctoken = True
+                break
+
+        amount = asset_normalized_value(
+            amount=hex_or_bytes_to_int(context.tx_log.data),
+            asset=underlying_token,
+        )
+        paired_event, action_from_event_type, action_to_event_subtype, action_to_notes = None, None, None, None  # noqa: E501
         for event in context.decoded_events:
             if (
+                event.event_type == HistoryEventType.SPEND and
                 event.event_subtype == HistoryEventSubType.NONE and
-                event.asset.identifier == evm_address_to_identifier(
-                    address=hex_or_bytes_to_address(context.tx_log.topics[3]),
-                    chain_id=self.evm_inquirer.chain_id,
-                    token_type=EvmTokenKind.ERC20,
-                )
+                event.asset == underlying_token and amount == event.balance.amount and
+                event.address == compound_token.evm_address
             ):
-                if event.event_type == HistoryEventType.SPEND:
+                event.counterparty = CPT_COMPOUND_V3
+                if receiving_ctoken:
+                    event.event_type = HistoryEventType.DEPOSIT
+                    event.event_subtype = HistoryEventSubType.DEPOSIT_ASSET
+                    event.notes = f'Deposit {amount} {underlying_token.symbol} into Compound v3'
+                    paired_event = event
+                    action_from_event_type = HistoryEventType.RECEIVE
+                    action_to_event_subtype = HistoryEventSubType.RECEIVE_WRAPPED
+                    action_to_notes = f'Receive {{amount}} {compound_token.symbol} from Compound v3'  # {amount} to be replaced in post decoding  # noqa: E501
+                else:
+                    event.event_type = HistoryEventType.SPEND
                     event.event_subtype = HistoryEventSubType.PAYBACK_DEBT
-                    event.notes = f'Repay {event.balance.amount} {event.asset.symbol_or_name()} to compound v3'  # noqa: E501
+                    event.notes = f'Repay {amount} {underlying_token.symbol} on Compound v3'
+
+                break
+        else:  # did not break/find anything
+            log.error(
+                'At compound v3 supply/withdraw decoding of tx '
+                f'{context.transaction.tx_hash.hex()} the action item data was not found.',
+            )
+            return DEFAULT_DECODING_OUTPUT
+
+        action_items = []  # also create an action item for the receive of the cTokens
+        if paired_event is not None and action_from_event_type is not None:
+            action_items.append(ActionItem(
+                action='transform',
+                from_event_type=action_from_event_type,
+                from_event_subtype=HistoryEventSubType.NONE,
+                asset=compound_token,
+                to_event_subtype=action_to_event_subtype,
+                to_notes=action_to_notes,
+                to_counterparty=CPT_COMPOUND_V3,
+                paired_event_data=(paired_event, True),
+            ))
+
+        return DecodingOutput(action_items=action_items)
+
+    def _decode_withdraw_or_borrow_event(
+            self,
+            context: DecoderContext,
+            compound_token: EvmToken,
+    ) -> DecodingOutput:
+        """Decode a compound v3 withdraw or borrow event. Takes decoder context and
+        the compound v3 wrapped token of the withdrawn/borrowed underlying token."""
+        if (underlying_token := self._get_compound_underlying_token(compound_token)) is None:
+            log.error(
+                f'At compound v3 supply/withdraw decoding of tx '
+                f'{context.transaction.tx_hash.hex()} the underlying token was not found.',
+            )
+            return DEFAULT_DECODING_OUTPUT
+
+        sending_ctoken = False
+        for tx_log in context.all_logs:
+            if (
+                tx_log.address == compound_token.evm_address and
+                tx_log.topics[0] == ERC20_OR_ERC721_TRANSFER and
+                hex_or_bytes_to_address(tx_log.topics[1]) == hex_or_bytes_to_address(context.tx_log.topics[2]) and  # from  # noqa: E501
+                hex_or_bytes_to_address(tx_log.topics[2]) == ZERO_ADDRESS  # to
+            ):
+                sending_ctoken = True
+                break
+
+        paired_event, action_from_event_type, action_to_event_subtype, action_to_notes = None, None, None, None  # noqa: E501
+        for event in context.decoded_events:
+            if (
+                event.event_type == HistoryEventType.RECEIVE and
+                event.event_subtype == HistoryEventSubType.NONE and
+                event.asset.identifier == underlying_token.identifier and
+                event.address == compound_token.evm_address
+            ):
+                if sending_ctoken:
+                    event.event_type = HistoryEventType.WITHDRAWAL
+                    event.event_subtype = HistoryEventSubType.REMOVE_ASSET
+                    event.notes = f'Withdraw {event.balance.amount} {event.asset.symbol_or_name()} from Compound v3'  # noqa: E501
+                    paired_event = event
+                    action_from_event_type = HistoryEventType.SPEND
+                    action_to_event_subtype = HistoryEventSubType.RETURN_WRAPPED
+                    action_to_notes = f'Return {{amount}} {compound_token.symbol} to Compound v3'  # {amount} to be replaced in post decoding  # noqa: E501
                 else:
                     event.event_subtype = HistoryEventSubType.GENERATE_DEBT
-                    event.notes = f'Borrow {event.balance.amount} {event.asset.symbol_or_name()} from compound v3'  # noqa: E501
+                    event.notes = f'Borrow {event.balance.amount} {event.asset.symbol_or_name()} from Compound v3'  # noqa: E501
 
                 event.counterparty = CPT_COMPOUND_V3
                 break
         else:
             log.error(
-                f'Could not find any compound v3 repay or borrow event in tx '
+                f'Could not find any compound v3 withdraw or borrow event in tx '
                 f'{context.transaction.tx_hash.hex()}.',
             )
 
+        action_items = []  # also create an action item for the spend of the cTokens
+        if paired_event is not None and action_from_event_type is not None:
+            action_items.append(ActionItem(
+                action='transform',
+                from_event_type=action_from_event_type,
+                from_event_subtype=HistoryEventSubType.NONE,
+                asset=compound_token,
+                to_event_subtype=action_to_event_subtype,
+                to_notes=action_to_notes,
+                to_counterparty=CPT_COMPOUND_V3,
+                paired_event_data=(paired_event, False),
+            ))
+
+        return DecodingOutput(action_items=action_items)
+
+    def _decode_collateral_movemement(
+            self,
+            context: DecoderContext,
+            compound_token: EvmToken,
+    ) -> DecodingOutput:
+        """Decode compound v3 supply/withdraw collateral events"""
+        collateral_asset = EvmToken(evm_address_to_identifier(
+            address=hex_or_bytes_to_address(context.tx_log.topics[3]),
+            chain_id=self.evm_inquirer.chain_id,
+            token_type=EvmTokenKind.ERC20,
+        ))
+        collateral_amount = asset_normalized_value(
+            amount=hex_or_bytes_to_int(context.tx_log.data),
+            asset=collateral_asset,
+        )
+
+        for event in context.decoded_events:
+            if (
+                event.event_type in {HistoryEventType.SPEND, HistoryEventType.RECEIVE} and
+                event.event_subtype == HistoryEventSubType.NONE and
+                event.balance.amount == collateral_amount and
+                event.asset == collateral_asset and
+                event.address == compound_token.evm_address
+            ):
+                transfer_event = event
+                event.counterparty = CPT_COMPOUND_V3
+                if event.event_type == HistoryEventType.SPEND:
+                    notes_action = 'Enable'
+                    event.event_type = HistoryEventType.DEPOSIT
+                    event.event_subtype = HistoryEventSubType.DEPOSIT_ASSET
+                    event.notes = f'Deposit {collateral_amount} {collateral_asset.symbol} into Compound v3'  # noqa: E501
+                else:
+                    notes_action = 'Disable'
+                    event.event_type = HistoryEventType.WITHDRAWAL
+                    event.event_subtype = HistoryEventSubType.REMOVE_ASSET
+                    event.notes = f'Withdraw {collateral_amount} {collateral_asset.symbol} from Compound v3'  # noqa: E501
+
+                collateral_event = self.base.make_event_next_index(
+                    tx_hash=context.transaction.tx_hash,
+                    timestamp=context.transaction.timestamp,
+                    event_type=HistoryEventType.INFORMATIONAL,
+                    event_subtype=HistoryEventSubType.NONE,
+                    balance=Balance(),
+                    asset=event.asset,
+                    location_label=event.location_label,
+                    counterparty=event.counterparty,
+                    notes=f'{notes_action} {collateral_amount} {collateral_asset.symbol} as collateral on Compound v3',  # noqa: E501
+                    address=event.address,
+                )
+                break
+        else:
+            log.error(
+                f'Could not find any compound v3 supply/withdraw collateral event in tx '
+                f'{context.transaction.tx_hash.hex()}.',
+            )
+            return DEFAULT_DECODING_OUTPUT
+
+        context.decoded_events.append(collateral_event)
+        maybe_reshuffle_events(
+            events_list=context.decoded_events,
+            ordered_events=[transfer_event, collateral_event],
+        )
         return DEFAULT_DECODING_OUTPUT
 
     def decode_compound_token_movement(
@@ -232,13 +352,21 @@ class Compoundv3CommonDecoder(DecoderInterface):
         ):
             return DEFAULT_DECODING_OUTPUT
 
-        if context.tx_log.topics[0] in {COMPOUND_V3_SUPPLY, COMPOUND_V3_WITHDRAW}:
-            return self._decode_supply_or_withdraw_event(
+        if context.tx_log.topics[0] == COMPOUND_V3_SUPPLY:
+            return self._decode_supply_or_repay_event(
+                context=context,
+                compound_token=compound_token,
+            )
+        elif context.tx_log.topics[0] == COMPOUND_V3_WITHDRAW:
+            return self._decode_withdraw_or_borrow_event(
                 context=context,
                 compound_token=compound_token,
             )
         else:
-            return self._decode_repay_or_borrow_event(context=context)
+            return self._decode_collateral_movemement(
+                context=context,
+                compound_token=compound_token,
+            )
 
     # -- DecoderInterface methods
 
