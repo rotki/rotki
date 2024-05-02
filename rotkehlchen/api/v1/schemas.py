47bd4c20701dc25721b9f8ca7573c8e4969189da
Mithil Poojary <mithil467@gmail.com>
2024-05-03 04:54:59 +0530

    Enable sync_method_upon_size_discrepancy setting in API

--- rotkehlchen/api/v1/schemas.py
+++ rotkehlchen/api/v1/schemas.py
@@ -118,6 +118,7 @@ from rotkehlchen.types import (
     OptionalBlockchainAddress,
     OptionalChainAddress,
     SupportedBlockchain,
+    SyncMethodUponSizeDiscrepancy,
     Timestamp,
     TradeType,
     UserNote,
@@ -1332,6 +1333,10 @@ class ModifiableSettingsSchema(Schema):
     )
     auto_delete_calendar_entries = fields.Boolean(load_default=None)
     auto_create_calendar_reminders = fields.Boolean(load_default=None)
+    sync_method_upon_size_discrepancy = SerializableEnumField(
+        enum_class=SyncMethodUponSizeDiscrepancy,
+        load_default=None,
+    )
 
     @validates_schema
     def validate_settings_schema(
@@ -1392,6 +1397,7 @@ class ModifiableSettingsSchema(Schema):
             oracle_penalty_duration=data['oracle_penalty_duration'],
             auto_delete_calendar_entries=data['auto_delete_calendar_entries'],
             auto_create_calendar_reminders=data['auto_create_calendar_reminders'],
+            sync_method_upon_size_discrepancy=data['sync_method_upon_size_discrepancy'],
         )
 
 
