8e72f2482b4bed536aa5ff08717d20853b5411d4
Mohit Sahoo <git@mohitsahoo.com>
2024-05-10 16:26:37 +0530

    fix(balances): fix graph balance querying and detection

--- rotkehlchen/chain/evm/decoding/thegraph/constants.py
+++ rotkehlchen/chain/evm/decoding/thegraph/constants.py
@@ -7,3 +7,43 @@ THEGRAPH_CPT_DETAILS: Final = CounterpartyDetails(
     label='The Graph',
     image='thegraph.svg',
 )
+
+GRAPH_DELEGATION_TRANSFER_ABI: Final = [
+    {
+        'anonymous': 'false',
+        'inputs': [
+            {
+                'indexed': 'true',
+                'internalType': 'address',
+                'name': 'delegator',
+                'type': 'address',
+            },
+            {
+                'indexed': 'true',
+                'internalType': 'address',
+                'name': 'l2Delegator',
+                'type': 'address',
+            },
+            {
+                'indexed': 'true',
+                'internalType': 'address',
+                'name': 'indexer',
+                'type': 'address',
+            },
+            {
+                'indexed': 'false',
+                'internalType': 'address',
+                'name': 'l2Indexer',
+                'type': 'address',
+            },
+            {
+                'indexed': 'false',
+                'internalType': 'uint256',
+                'name': 'transferredDelegationTokens',
+                'type': 'uint256',
+            },
+        ],
+        'name': 'DelegationTransferredToL2',
+        'type': 'event',
+    },
+]
--- rotkehlchen/chain/evm/decoding/thegraph/decoder.py
+++ rotkehlchen/chain/evm/decoding/thegraph/decoder.py
@@ -3,8 +3,7 @@ from typing import TYPE_CHECKING, Any, Final
 
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import Asset
-from rotkehlchen.chain.ethereum.modules.thegraph.constants import GRAPH_L1_LOCK_TRANSFER_TOOL
-from rotkehlchen.chain.ethereum.utils import asset_normalized_value, token_normalized_value
+from rotkehlchen.chain.ethereum.utils import token_normalized_value
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
 from rotkehlchen.chain.evm.decoding.structures import (
     DEFAULT_DECODING_OUTPUT,
@@ -14,14 +13,13 @@ from rotkehlchen.chain.evm.decoding.structures import (
 )
 from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
 from rotkehlchen.chain.evm.decoding.utils import maybe_reshuffle_events
-from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.db.cache import DBCacheStatic
 from rotkehlchen.errors.misc import RemoteError
-from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.evm_event import EvmProduct
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.serialization.deserialize import deserialize_evm_address
-from rotkehlchen.types import ChecksumEvmAddress
+from rotkehlchen.types import ChecksumEvmAddress, Timestamp
 from rotkehlchen.utils.misc import hex_or_bytes_to_address, hex_or_bytes_to_int
 
 from .constants import CPT_THEGRAPH, THEGRAPH_CPT_DETAILS
@@ -37,21 +35,12 @@ log = RotkehlchenLogsAdapter(logger)
 TOPIC_STAKE_DELEGATED: Final = b'\xcd\x03f\xdc\xe5$}\x87O\xfc`\xa7b\xaaz\xbb\xb8,\x16\x95\xbb\xb1q`\x9c\x1b\x88a\xe2y\xebs'  # noqa: E501
 TOPIC_STAKE_DELEGATED_LOCKED: Final = b'\x040\x18?\x84\xd9\xc4P#\x86\xd4\x99\xda\x80eC\xde\xe1\xd9\xde\x83\xc0\x8b\x01\xe3\x9am!\x16\xc4;%'  # noqa: E501
 TOPIC_STAKE_DELEGATED_WITHDRAWN: Final = b'\x1b.w7\xe0C\xc5\xcf\x1bX|\xebM\xae\xb7\xae\x00\x14\x8b\x9b\xda\x8fy\xf1\t>\xea\xd0\x8f\x14\x19R'  # noqa: E501
-ETH_DEPOSITED: Final = b'lp7\x91\xf3\x99U\x88\x07BOH\x9c\xcd\x81\x1cr\xb4\xff\x0bt\xafTrd\xfa\xd7\xc6Fwm\xf0'  # noqa: E501
-DELEGATION_TRANSFERRED_TO_L2: Final = b'#\x1e\\\xfe\xffwY\xa4h$\x1d\x93\x9a\xb0J`\xd6\x03\xb1~5\x90W\xab\xbb\x8fR\xaf\xc3\xe4\x98k'  # noqa: E501
 
 GRAPH_TOKEN_LOCK_WALLET_ABI: Final = [
     {
         'inputs': [],
         'name': 'beneficiary',
         'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}],
-        'stateMutability': 'view',
-        'type': 'function',
-    },
-    {
-        'inputs': [],
-        'name': 'totalOutstandingAmount',
-        'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
         'stateMutability': 'view',
         'type': 'function',
     },
@@ -82,16 +71,14 @@ class ThegraphCommonDecoder(DecoderInterface):
             return self._decode_stake_locked(context)
         elif context.tx_log.topics[0] == TOPIC_STAKE_DELEGATED_WITHDRAWN:
             return self._decode_stake_withdrawn(context)
-        elif context.tx_log.topics[0] == DELEGATION_TRANSFERRED_TO_L2:
-            return self._decode_delegation_transferred_to_l2(context)
 
         return DEFAULT_DECODING_OUTPUT
 
-    def _get_user_address(self, address: ChecksumEvmAddress) -> ChecksumEvmAddress | None:
-        """Get the user address (benficiary) from the vesting contract if it exists"""
+    def get_user_address(self, address: ChecksumEvmAddress, address_l2: ChecksumEvmAddress | None = None) -> ChecksumEvmAddress | None:  # noqa: E501
+        """Get the user address (benficiary) from the vesting contract on L2 if it exists"""
         if self.evm_inquirer.get_code(address) != '0x':
             try:
-                raw_beneficiary_address = self.base.evm_inquirer.call_contract(
+                raw_beneficiary_address = self.evm_inquirer.call_contract(
                     contract_address=address,  # the vesting contract
                     abi=GRAPH_TOKEN_LOCK_WALLET_ABI,
                     method_name='beneficiary',
@@ -100,58 +87,7 @@ class ThegraphCommonDecoder(DecoderInterface):
             except RemoteError as e:
                 log.error(f'During delegation transfer to arbitrum got error calling {address} beneficiary(): {e!s}')  # noqa: E501
                 return None
-        return address
-
-    def _decode_delegation_transferred_to_l2(self, context: DecoderContext) -> DecodingOutput:
-        """Decode a transfer delegating GRT from ethereum to arbitrum"""
-        user_address = self._get_user_address(hex_or_bytes_to_address(context.tx_log.topics[1]))
-        if not user_address or not self.base.is_tracked(user_address):
-            return DEFAULT_DECODING_OUTPUT
-
-        delegator_l2 = hex_or_bytes_to_address(context.tx_log.topics[2])
-        indexer = hex_or_bytes_to_address(context.tx_log.topics[3])
-        l2_indexer = hex_or_bytes_to_address(context.tx_log.data[:32])
-        transferred_delegation_tokens = token_normalized_value(
-            token_amount=hex_or_bytes_to_int(context.tx_log.data[32:]),
-            token=self.token,
-        )
-        event = self.base.make_event_from_transaction(
-            transaction=context.transaction,
-            tx_log=context.tx_log,
-            event_type=HistoryEventType.INFORMATIONAL,
-            event_subtype=HistoryEventSubType.NONE,
-            asset=self.token,
-            balance=Balance(amount=FVal(transferred_delegation_tokens)),
-            notes=f'Delegation of {transferred_delegation_tokens} GRT transferred from indexer {indexer} to L2 indexer {l2_indexer}.',  # noqa: E501
-            location_label=context.transaction.from_address,
-            counterparty=CPT_THEGRAPH,
-            address=context.transaction.to_address,
-            extra_data={'delegator_l2': delegator_l2},
-        )
-        return DecodingOutput(event=event)
-
-    def _decode_contract_deposit(self, context: DecoderContext) -> DecodingOutput:
-        """Decode a deposit of ETH to cover the arbitrum fees of delegating GRT"""
-        user_address = self._get_user_address(hex_or_bytes_to_address(context.tx_log.topics[1]))
-        if not user_address or not self.base.is_tracked(user_address):
-            return DEFAULT_DECODING_OUTPUT
-
-        indexer = hex_or_bytes_to_address(context.tx_log.topics[1])
-        raw_amount = hex_or_bytes_to_int(context.tx_log.data)
-        amount = asset_normalized_value(amount=raw_amount, asset=A_ETH.resolve_to_crypto_asset())
-        for event in context.decoded_events:
-            if (
-                event.event_type == HistoryEventType.SPEND and
-                event.event_subtype == HistoryEventSubType.NONE and
-                event.location_label == context.transaction.from_address and
-                event.balance.amount == amount
-            ):
-                event.event_subtype = HistoryEventSubType.FEE
-                event.notes = f'Deposit {event.balance.amount} ETH to {event.address} contract to pay for the gas in L2.'  # noqa: E501
-                event.counterparty = CPT_THEGRAPH
-                event.extra_data = {'indexer': indexer}
-                break
-        return DEFAULT_DECODING_OUTPUT
+        return address_l2 or address
 
     def _decode_stake_delegated(self, context: DecoderContext) -> DecodingOutput:
         deposit_event, burn_event = None, None
@@ -204,6 +140,16 @@ class ThegraphCommonDecoder(DecoderInterface):
                     )
                     return DecodingOutput(event=burn_event)
                 break
+
+        # Reset the LAST_GRAPH_DELEGATIONS_CHECK_TS to Timestamp(0) to ensure the task runs more
+        # frequently on decoding related events, rather than making users wait a whole day
+        # to query event logs and get the balances detected.
+        with self.evm_inquirer.database.user_write() as write_cursor:
+            self.evm_inquirer.database.set_static_cache(
+                write_cursor=write_cursor,
+                name=DBCacheStatic.LAST_GRAPH_DELEGATIONS_CHECK_TS,
+                value=Timestamp(0),
+            )
 
         return DEFAULT_DECODING_OUTPUT
 
@@ -268,7 +214,4 @@ class ThegraphCommonDecoder(DecoderInterface):
         return (THEGRAPH_CPT_DETAILS,)
 
     def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
-        return {
-            self.staking_contract: (self._decode_delegator_staking,),
-            GRAPH_L1_LOCK_TRANSFER_TOOL: (self._decode_contract_deposit,),
-        }
+        return {self.staking_contract: (self._decode_delegator_staking,)}
