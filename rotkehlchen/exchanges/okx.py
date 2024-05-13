7e679ebbe223141ce329d16f833905e1eedeee66
İzzet Emre Demir <izzetemredemir@gmail.com>
2024-05-13 20:26:25 +0300

    Okx frozen balance bug fix #7915 (#7928)
    * Okx frozen balance bug fix #7915
    
    * Update docs/changelog.rst

--- rotkehlchen/exchanges/okx.py
+++ rotkehlchen/exchanges/okx.py
@@ -273,7 +273,7 @@ class Okx(ExchangeInterface):
                 continue
 
             try:
-                amount = deserialize_asset_amount(currency_data['availBal'])
+                amount = deserialize_asset_amount(currency_data['availBal']) + deserialize_asset_amount(currency_data['frozenBal'])  # noqa: E501
             except DeserializationError as e:
                 self.msg_aggregator.add_error(
                     f'Error processing {self.name} {asset.name} balance result due to inability '
