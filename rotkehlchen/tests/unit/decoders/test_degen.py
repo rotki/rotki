12406190b9c3977c2e4d55ec77e66fb696a9eaa4
Lefteris Karapetsas <lefteris@refu.co>
2024-05-14 22:14:13 +0200

    Decode degen airdrop 2 season 3
    Fix https://github.com/orgs/rotki/projects/11/views/2?pane=issue&itemId=62563387
    
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/tests/unit/decoders/test_degen.py
+++ rotkehlchen/tests/unit/decoders/test_degen.py
@@ -1,11 +1,17 @@
+from typing import Final
+
 import pytest
 
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import Asset
-from rotkehlchen.chain.base.modules.degen.constants import CPT_DEGEN
+from rotkehlchen.chain.base.modules.degen.constants import (
+    CLAIM_AIRDROP_2_CONTRACT,
+    CLAIM_AIRDROP_3_CONTRACT,
+    CPT_DEGEN,
+    DEGEN_TOKEN_ID,
+)
 from rotkehlchen.chain.evm.decoding.constants import CPT_GAS
 from rotkehlchen.chain.evm.decoding.decoder import EVMTransactionDecoder
-from rotkehlchen.chain.evm.types import string_to_evm_address
 from rotkehlchen.constants.assets import A_ETH
 from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.evm_event import EvmEvent
@@ -13,10 +19,12 @@ from rotkehlchen.history.events.structures.types import HistoryEventSubType, His
 from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
 from rotkehlchen.types import ChecksumEvmAddress, Location, TimestampMS, deserialize_evm_tx_hash
 
+DEGEN_TOKEN: Final = Asset(DEGEN_TOKEN_ID)
+
 
 @pytest.mark.vcr()
 @pytest.mark.parametrize('base_accounts', [['0xc37b40ABdB939635068d3c5f13E7faF686F03B65']])
-def test_claim_airdrop_1(
+def test_claim_airdrop_2(
         base_accounts: list[ChecksumEvmAddress],
         base_transaction_decoder: EVMTransactionDecoder,
 ):
@@ -27,9 +35,7 @@ def test_claim_airdrop_1(
         database=base_transaction_decoder.database,
         tx_hash=evmhash,
     )
-    degen_token = Asset('eip155:8453/erc20:0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed')
-    timestamp = TimestampMS(1709555247000)
-    gas_amount, claimed_amount = '0.000443147649294366', '100'
+    timestamp, gas_amount, claimed_amount = TimestampMS(1709555247000), '0.000443147649294366', '100'  # noqa: E501
     expected_events = [
         EvmEvent(
             tx_hash=evmhash,
@@ -50,12 +56,57 @@ def test_claim_airdrop_1(
             location=Location.BASE,
             event_type=HistoryEventType.RECEIVE,
             event_subtype=HistoryEventSubType.AIRDROP,
-            asset=degen_token,
+            asset=DEGEN_TOKEN,
             balance=Balance(FVal(claimed_amount)),
             location_label=user_address,
             counterparty=CPT_DEGEN,
-            address=string_to_evm_address('0x88d42b6DBc10D2494A0c6c189CeFC7573a6dCE62'),
+            address=CLAIM_AIRDROP_2_CONTRACT,
             notes=f'Claim {claimed_amount} DEGEN from Degen airdrop 2',
+        ),
+    ]
+    assert events == expected_events
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('base_accounts', [['0x80c008A7c9ec056158cB1F64024e710C8398048A']])
+def test_claim_airdrop_3(
+        base_accounts: list[ChecksumEvmAddress],
+        base_transaction_decoder: EVMTransactionDecoder,
+):
+    evmhash = deserialize_evm_tx_hash('0x40920bf5416e9bd756d1c57f04e1b978e838efb42e7c2b07c4e9aaa8eb0da2ef ')  # noqa: E501
+    user_address = base_accounts[0]
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=base_transaction_decoder.evm_inquirer,
+        database=base_transaction_decoder.database,
+        tx_hash=evmhash,
+    )
+    timestamp, gas_amount, claimed_amount = TimestampMS(1715696797000), '0.000016768741928411', '1649'  # noqa: E501
+    expected_events = [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(FVal(gas_amount)),
+            location_label=user_address,
+            counterparty=CPT_GAS,
+            notes=f'Burned {gas_amount} ETH for gas',
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=121,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.AIRDROP,
+            asset=DEGEN_TOKEN,
+            balance=Balance(FVal(claimed_amount)),
+            location_label=user_address,
+            counterparty=CPT_DEGEN,
+            address=CLAIM_AIRDROP_3_CONTRACT,
+            notes=f'Claim {claimed_amount} DEGEN from Degen airdrop 3',
         ),
     ]
     assert events == expected_events
