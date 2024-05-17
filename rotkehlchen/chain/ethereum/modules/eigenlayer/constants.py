ee0e9b2fa8713125b6f2193e8aca37b2250b0929
Lefteris Karapetsas <lefteris@refu.co>
2024-05-17 11:19:36 +0200

    Decode eigenpod deployment events
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/ethereum/modules/eigenlayer/constants.py
+++ rotkehlchen/chain/ethereum/modules/eigenlayer/constants.py
@@ -1,13 +1,15 @@
 from typing import Final
+
 from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
 from rotkehlchen.chain.evm.types import string_to_evm_address
 
-
 CPT_EIGENLAYER: Final = 'eigenlayer'
+EIGENPOD_MANAGER: Final = string_to_evm_address('0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338')
 EIGENLAYER_STRATEGY_MANAGER: Final = string_to_evm_address('0x858646372CC42E1A627fcE94aa7A7033e7CF075A')  # noqa: E501
 EIGENLAYER_AIRDROP_DISTRIBUTOR: Final = string_to_evm_address('0x035bdAeaB85E47710C27EdA7FD754bA80aD4ad02')  # noqa: E501
 DEPOSIT_TOPIC: Final = b'|\xff\xf9\x08\xa4\xb5\x83\xf3d0\xb2]u\x96LE\x8d\x8e\xde\x8a\x99\xbda\xbeu\x0e\x97\xee\x1b/:\x96'  # noqa: E501
 WITHDRAWAL_COMPLETE_TOPIC: Final = b"\xe7\xeb\x0c\xa1\x1b\x83tN\xce=x\xe9\xbe\x01\xb9\x13B_\xba\xe7\x0c2\xce'rm\x0e\xcd\xe9.\xf8\xd2"  # noqa: E501
+POD_DEPLOYED: Final = b'!\xc9\x9d\r\xb0"\x13\xc3/\xff[\x05\xcf\nq\x8a\xb5\xf8X\x80+\x91I\x8f\x80\xd8"p(\x9d\x85j'  # noqa: E501
 EIGENLAYER_CPT_DETAILS: Final = CounterpartyDetails(
     identifier=CPT_EIGENLAYER,
     label='EigenLayer',
