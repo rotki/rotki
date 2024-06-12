85d591c6589c1e62020615f394452bfcd30bd726
Mohit Sahoo <git@mohitsahoo.com>
2024-06-12 13:42:44 +0530

    feat(gearbox): decode staking events for Gear

--- rotkehlchen/chain/ethereum/modules/gearbox/decoder.py
+++ rotkehlchen/chain/ethereum/modules/gearbox/decoder.py
@@ -1,5 +1,23 @@
+from typing import TYPE_CHECKING
+from rotkehlchen.chain.ethereum.modules.gearbox.constants import GEAR_STAKING_CONTRACT
 from rotkehlchen.chain.evm.decoding.gearbox.decoder import GearboxCommonDecoder
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.ethereum.node_inquirer import EthereumInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
 
 
 class GearboxDecoder(GearboxCommonDecoder):
-    ...
+    def __init__(
+            self,
+            ethereum_inquirer: 'EthereumInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=ethereum_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            staking_contract=GEAR_STAKING_CONTRACT,
+        )
--- rotkehlchen/chain/optimism/modules/gearbox/decoder.py
+++ rotkehlchen/chain/optimism/modules/gearbox/decoder.py
@@ -1,5 +1,24 @@
+from typing import TYPE_CHECKING
+
 from rotkehlchen.chain.evm.decoding.gearbox.decoder import GearboxCommonDecoder
+from rotkehlchen.chain.optimism.modules.gearbox.constants import GEAR_STAKING_CONTRACT
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.chain.optimism.node_inquirer import OptimismInquirer
+    from rotkehlchen.user_messages import MessagesAggregator
 
 
 class GearboxDecoder(GearboxCommonDecoder):
-    ...
+    def __init__(
+            self,
+            ethereum_inquirer: 'OptimismInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=ethereum_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            staking_contract=GEAR_STAKING_CONTRACT,
+        )
