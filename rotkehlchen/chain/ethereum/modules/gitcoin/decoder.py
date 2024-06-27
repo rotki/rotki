4c658b36505307518376149e35eb841d242bda24
Lefteris Karapetsas <lefteris@refu.co>
2024-06-27 16:06:01 +0200

    Abstract gitcoin cgrants to be multievm chain capable
    Also handle the other kind of funds claimed possible event log which
    unfortunately has the same topics[0] hash
    
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/ethereum/modules/gitcoin/decoder.py
+++ rotkehlchen/chain/ethereum/modules/gitcoin/decoder.py
@@ -2,21 +2,15 @@ import logging
 from collections.abc import Callable
 from typing import TYPE_CHECKING, Any
 
-from rotkehlchen.assets.asset import Asset
 from rotkehlchen.chain.ethereum.modules.gitcoin.constants import (
-    FUNDS_CLAIMED,
     GITCOIN_GOVERNOR_ALPHA,
     GITCOIN_GRANTS_OLD1,
 )
-from rotkehlchen.chain.ethereum.utils import asset_normalized_value
 from rotkehlchen.chain.evm.decoding.constants import CPT_GITCOIN
 from rotkehlchen.chain.evm.decoding.gitcoin.decoder import GitcoinOldCommonDecoder
 from rotkehlchen.chain.evm.decoding.interfaces import GovernableDecoderInterface
 from rotkehlchen.chain.evm.decoding.structures import (
-    DEFAULT_DECODING_OUTPUT,
     FAILED_ENRICHMENT_OUTPUT,
-    DecoderContext,
-    DecodingOutput,
     EnricherContext,
     TransferEnrichmentOutput,
 )
@@ -25,7 +19,6 @@ from rotkehlchen.constants.assets import A_DAI, A_GRT
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.types import ChecksumEvmAddress
-from rotkehlchen.utils.misc import hex_or_bytes_to_address, hex_or_bytes_to_int
 
 if TYPE_CHECKING:
     from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
@@ -58,6 +51,11 @@ class GitcoinDecoder(GovernableDecoderInterface, GitcoinOldCommonDecoder):
             base_tools=base_tools,
             msg_aggregator=msg_aggregator,
             bulkcheckout_address=string_to_evm_address('0x7d655c57f71464B6f83811C55D84009Cd9f5221C'),
+            matching_contracts=[
+                (string_to_evm_address('0xC8AcA0b50F3Ca9A0cBe413d8a110a7aab7d4C1aE'), 'grants 15 main round', A_DAI),  # noqa: E501
+                (string_to_evm_address('0x2878883dD4345C7b35c13FefC5096dd400814D91'), 'grants 14 main round', A_DAI),  # noqa: E501
+                (string_to_evm_address('0xa640830aFAa6455E198eDa49E085C4C377789ddd'), 'grants 14 graph protocol round', A_GRT),  # noqa: E501
+            ],
         )
 
     def _maybe_enrich_gitcoin_transfers(
@@ -87,56 +85,11 @@ class GitcoinDecoder(GovernableDecoderInterface, GitcoinOldCommonDecoder):
         context.event.counterparty = CPT_GITCOIN
         return TransferEnrichmentOutput(matched_counterparty=CPT_GITCOIN)
 
-    def _decode_matching_claim(self, context: DecoderContext, name: str, asset: Asset) -> DecodingOutput:  # noqa: E501
-        """Decode the matching funds claim based on the given name and asset. We need
-        to provide the name and the asset as this is based per contract and does not change.
-
-        For the token we could query it but the name we can't. Still since it's hard
-        coded per contract and we have a hard coded list it's best to not ask the chain
-        and do an extra network query since this is immutable."""
-        if context.tx_log.topics[0] != FUNDS_CLAIMED:
-            return DEFAULT_DECODING_OUTPUT
-
-        if not self.base.is_tracked(claimee := hex_or_bytes_to_address(context.tx_log.topics[1])):
-            return DEFAULT_DECODING_OUTPUT
-
-        asset = asset.resolve_to_crypto_asset()
-        amount = asset_normalized_value(
-            amount=hex_or_bytes_to_int(context.tx_log.topics[2]),
-            asset=asset,
-        )
-        for event in context.decoded_events:
-            if (
-                    event.event_type == HistoryEventType.RECEIVE and
-                    event.event_subtype == HistoryEventSubType.NONE and
-                    event.asset == asset and
-                    event.balance.amount == amount and
-                    event.location_label == claimee
-            ):
-                event.event_subtype = HistoryEventSubType.DONATE
-                event.counterparty = CPT_GITCOIN
-                event.notes = f'Claim {amount} {asset.symbol} as matching funds payout for gitcoin {name}'  # noqa: E501
-                break
-
-        else:
-            log.error(
-                f'Failed to find the gitcoin matching receive transfer for {self.evm_inquirer.chain_name} transaction {context.transaction.tx_hash.hex()}.',  # noqa: E501
-            )
-
-        return DEFAULT_DECODING_OUTPUT
-
     # -- DecoderInterface methods
 
     def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
         return {
             GITCOIN_GOVERNOR_ALPHA: (self._decode_governance,),
-        } | {
-            address: (self._decode_matching_claim, name, asset) for address, name, asset in [
-                (string_to_evm_address('0xC8AcA0b50F3Ca9A0cBe413d8a110a7aab7d4C1aE'), 'grants 15 main round', A_DAI),  # noqa: E501
-                (string_to_evm_address('0x2878883dD4345C7b35c13FefC5096dd400814D91'), 'grants 14 main round', A_DAI),  # noqa: E501
-                (string_to_evm_address('0xa640830aFAa6455E198eDa49E085C4C377789ddd'), 'grants 14 graph protocol round', A_GRT),  # noqa: E501
-            ]
-
         } | super().addresses_to_decoders()
 
     def enricher_rules(self) -> list[Callable]:
--- rotkehlchen/chain/evm/decoding/gitcoin/decoder.py
+++ rotkehlchen/chain/evm/decoding/gitcoin/decoder.py
@@ -1,6 +1,7 @@
 import logging
 from typing import TYPE_CHECKING, Any
 
+from rotkehlchen.assets.asset import Asset
 from rotkehlchen.chain.ethereum.utils import asset_normalized_value
 from rotkehlchen.chain.evm.decoding.constants import CPT_GITCOIN, GITCOIN_CPT_DETAILS
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
@@ -16,9 +17,10 @@ from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.types import ChecksumEvmAddress
 from rotkehlchen.utils.misc import hex_or_bytes_to_address, hex_or_bytes_to_int
 
-from .constants import DONATION_SENT
+from .constants import DONATION_SENT, FUNDS_CLAIMED
 
 if TYPE_CHECKING:
+
     from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
     from rotkehlchen.chain.evm.node_inquirer import EvmNodeInquirer
     from rotkehlchen.user_messages import MessagesAggregator
@@ -34,14 +36,25 @@ class GitcoinOldCommonDecoder(DecoderInterface):
             evm_inquirer: 'EvmNodeInquirer',
             base_tools: 'BaseDecoderTools',
             msg_aggregator: 'MessagesAggregator',
-            bulkcheckout_address: ChecksumEvmAddress,
+            bulkcheckout_address: ChecksumEvmAddress | None = None,
+            matching_contracts: list[tuple[ChecksumEvmAddress, str, Asset]] | None = None,
+            matching_contracts2: list[tuple[ChecksumEvmAddress, str, Asset]] | None = None,
     ) -> None:
+        """
+        - bulkcheckout address is the bulk checkout address for the network
+        - matching_contracts is a list containing the matching contracts data
+        from which donors can claim the matching funds.
+        - matching_contract2 is the same but for contracts containing a different
+        funds claimed log event structure
+        """
         super().__init__(
             evm_inquirer=evm_inquirer,
             base_tools=base_tools,
             msg_aggregator=msg_aggregator,
         )
         self.bulkcheckout_address = bulkcheckout_address
+        self.matching_contracts = matching_contracts
+        self.matching_contracts2 = matching_contracts2
 
     def _decode_bulkcheckout(self, context: DecoderContext) -> DecodingOutput:
         if context.tx_log.topics[0] != DONATION_SENT:
@@ -110,12 +123,98 @@ class GitcoinOldCommonDecoder(DecoderInterface):
 
         return DEFAULT_DECODING_OUTPUT
 
+    def _decode_matching_claim_common(
+            self,
+            context: DecoderContext,
+            name: str,
+            asset: Asset,
+            claimee_raw: bytes,
+            amount_raw: bytes,
+    ) -> DecodingOutput:
+        """Decode the matching funds claim based on the given name and asset. We need
+        to provide the name and the asset as this is based per contract and does not change.
+
+        For the token we could query it but the name we can't. Still since it's hard
+        coded per contract and we have a hard coded list it's best to not ask the chain
+        and do an extra network query since this is immutable."""
+        if context.tx_log.topics[0] != FUNDS_CLAIMED:
+            return DEFAULT_DECODING_OUTPUT
+
+        if not self.base.any_tracked([claimee := hex_or_bytes_to_address(claimee_raw), context.transaction.from_address]):  # noqa: E501
+            return DEFAULT_DECODING_OUTPUT
+
+        asset = asset.resolve_to_crypto_asset()
+        amount = asset_normalized_value(
+            amount=hex_or_bytes_to_int(amount_raw),
+            asset=asset,
+        )
+        for event in context.decoded_events:
+            if (
+                    event.event_type == HistoryEventType.RECEIVE and
+                    event.event_subtype == HistoryEventSubType.NONE and
+                    event.asset == asset and
+                    event.balance.amount == amount and
+                    event.location_label == claimee
+            ):
+                event.event_subtype = HistoryEventSubType.DONATE
+                event.counterparty = CPT_GITCOIN
+                event.notes = f'Claim {amount} {asset.symbol} as matching funds payout of gitcoin {name}'  # noqa: E501
+                if context.transaction.from_address != claimee:
+                    event.notes += f' for {claimee}'
+                break
+
+        else:
+            log.error(
+                f'Failed to find the gitcoin matching receive transfer for {self.evm_inquirer.chain_name} transaction {context.transaction.tx_hash.hex()}.',  # noqa: E501
+            )
+
+        return DEFAULT_DECODING_OUTPUT
+
+    def _decode_matching_claim(self, context: DecoderContext, name: str, asset: Asset) -> DecodingOutput:  # noqa: E501
+        """The normal case seen in mainnet. Example transaction:
+        https://etherscan.io/tx/0xc7ba01598f7fee42bb3923af95355d676ad38ec0aebdcdf49eaf7cb74d2150b2
+        """
+        return self._decode_matching_claim_common(
+            context=context,
+            name=name,
+            asset=asset,
+            claimee_raw=context.tx_log.topics[1],
+            amount_raw=context.tx_log.topics[2],
+        )
+
+    def _decode_matching_claim_voteoptonindex(self, context: DecoderContext, name: str, asset: Asset) -> DecodingOutput:  # noqa: E501
+        """
+        Decode matching funds claimed with different event structure:
+        https://arbiscan.io/tx/0x5236f217873582446398c9b427a80a669be90b8a17fb5ed842f8d5d2925f3e7f#eventlog
+        """
+        return self._decode_matching_claim_common(
+            context=context,
+            name=name,
+            asset=asset,
+            claimee_raw=context.tx_log.topics[2],
+            amount_raw=context.tx_log.data,
+        )
+
     # -- DecoderInterface methods
 
     def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
-        return {
-            self.bulkcheckout_address: (self._decode_bulkcheckout,),
-        }
+        mapping: dict[ChecksumEvmAddress, tuple[Any, ...]] = {}
+        if self.bulkcheckout_address:
+            mapping = {
+                self.bulkcheckout_address: (self._decode_bulkcheckout,),
+            }
+
+        if self.matching_contracts:
+            mapping |= {
+                address: (self._decode_matching_claim, name, asset) for address, name, asset in self.matching_contracts  # noqa: E501
+            }
+
+        if self.matching_contracts2:
+            mapping |= {
+                address: (self._decode_matching_claim_voteoptonindex, name, asset) for address, name, asset in self.matching_contracts2  # noqa: E501
+            }
+
+        return mapping
 
     @staticmethod
     def counterparties() -> tuple[CounterpartyDetails, ...]:
