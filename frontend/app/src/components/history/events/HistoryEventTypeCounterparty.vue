948a2e33bc0dc19cb1fca815f85a0d10be6d070f
Konstantinos Paparas <kelsos86@gmail.com>
2024-07-01 15:13:10 +0200

    refactor: small component restructure to reduce dom elements

--- frontend/app/src/components/history/events/HistoryEventTypeCounterparty.vue
+++ frontend/app/src/components/history/events/HistoryEventTypeCounterparty.vue
@@ -12,55 +12,54 @@ const imagePath = '/assets/images/protocols/';
 </script>
 
 <template>
-  <div>
-    <template v-if="counterparty || event.address">
-      <RuiBadge
-        class="[&_span]:!px-0"
-        color="default"
-        offset-x="-4"
-        offset-y="4"
+  <RuiBadge
+    v-if="counterparty || event.address"
+    class="[&_span]:!px-0"
+    color="default"
+    offset-x="-4"
+    offset-y="4"
+  >
+    <template #icon>
+      <RuiTooltip
+        :popper="{ placement: 'top' }"
+        :open-delay="400"
       >
-        <template #icon>
-          <RuiTooltip
-            :popper="{ placement: 'top' }"
-            :open-delay="400"
-          >
-            <template #activator>
-              <div class="rounded-full overflow-hidden bg-white">
-                <div v-if="counterparty">
-                  <RuiIcon
-                    v-if="counterparty.icon"
-                    :name="counterparty.icon"
-                    :color="counterparty.color"
-                  />
+        <template #activator>
+          <div class="rounded-full overflow-hidden bg-white">
+            <template v-if="counterparty">
+              <RuiIcon
+                v-if="counterparty.icon"
+                :name="counterparty.icon"
+                :color="counterparty.color"
+              />
 
-                  <AppImage
-                    v-else-if="counterparty.image"
-                    :src="`${imagePath}${counterparty.image}`"
-                    contain
-                    size="20px"
-                  />
+              <AppImage
+                v-else-if="counterparty.image"
+                :src="`${imagePath}${counterparty.image}`"
+                contain
+                size="20px"
+              />
 
-                  <EnsAvatar
-                    v-else
-                    size="20px"
-                    :address="counterparty.label"
-                  />
-                </div>
-                <EnsAvatar
-                  v-else-if="event.address"
-                  size="20px"
-                  :address="event.address"
-                  avatar
-                />
-              </div>
+              <EnsAvatar
+                v-else
+                size="20px"
+                :address="counterparty.label"
+              />
             </template>
-            <div>{{ counterparty?.label || event?.address }}</div>
-          </RuiTooltip>
+            <EnsAvatar
+              v-else-if="event.address"
+              size="20px"
+              :address="event.address"
+              avatar
+            />
+          </div>
         </template>
-        <slot />
-      </RuiBadge>
+        <div>{{ counterparty?.label || event?.address }}</div>
+      </RuiTooltip>
     </template>
-    <slot v-else />
+    <slot />
+  </RuiBadge>
+  <div v-else>
+    <slot />
   </div>
 </template>
