29024d6641bc6d93a5ac74ddee17048bac575fa8
OjusWiZard <ojuswimail@gmail.com>
2024-07-18 21:13:15 +0530

    Feat (Odos v2): Support for all the chains
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

---
+++ rotkehlchen/chain/scroll/modules/odos/v2/constants.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+ODOS_V2_ROUTER: Final = string_to_evm_address('0xbFe03C9E20a9Fc0b37de01A172F207004935E0b1')
