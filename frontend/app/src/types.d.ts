9c3957fd08f6d98f51c49c6b4162fe140f67757d
lukicenturi <lukicenturi@gmail.com>
2024-07-29 18:42:14 +0700

    fix: metamask issue if other wallet is installed

--- frontend/app/src/types.d.ts
+++ frontend/app/src/types.d.ts
@@ -5,19 +5,12 @@ interface Request {
   readonly params: Record<string, any>[];
 }
 
-interface Caveat {
-  readonly name: string;
-  readonly value: string[];
-}
-
 interface Permission {
   readonly parentCapability: string;
-  readonly caveats: Caveat[];
 }
 
 interface Provider {
   readonly isMetaMask?: boolean;
-  readonly request: (request: Request) => Promise<Permission[]>;
 }
 
 declare global {
@@ -25,6 +18,45 @@ declare global {
     interop?: Interop;
     ethereum?: Provider;
   }
+
+  interface WindowEventMap {
+    'eip6963:announceProvider': CustomEvent;
+  }
+}
+
+// Handle Metamask and other wallet extension import
+export interface EIP6963ProviderInfo {
+  rdns: string;
+  uuid: string;
+  name: string;
+  icon: string;
+}
+
+export interface EIP6963ProviderDetail {
+  info: EIP6963ProviderInfo;
+  provider: EIP1193Provider;
+}
+
+export interface EIP6963AnnounceProviderEvent {
+  detail: {
+    info: EIP6963ProviderInfo;
+    provider: Readonly<EIP1193Provider>;
+  };
+}
+
+export interface EIP1193Provider {
+  isStatus?: boolean;
+  host?: string;
+  path?: string;
+  sendAsync?: (
+    request: { method: string; params?: Array<unknown> },
+    callback: (error: Error | null, response: unknown) => void
+  ) => void;
+  send?: (
+    request: { method: string; params?: Array<unknown> },
+    callback: (error: Error | null, response: unknown) => void
+  ) => void;
+  request: (request: Request) => Promise<Permission[] | string[]>;
 }
 
 export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
