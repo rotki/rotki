1071a9cb646564e5e05fd8485f80bf775f4ec00a
Nicholas Yoder <nicholasyoder@proton.me>
2024-08-15 11:35:41 -0400

    Add Gnosis chain omnibridge decoder

---
+++ rotkehlchen/chain/evm/decoding/omnibridge/decoder.py
@@ -0,0 +1,130 @@
+import abc
+import logging
+from typing import TYPE_CHECKING, Any, Final
+
+from rotkehlchen.assets.utils import get_or_create_evm_token
+from rotkehlchen.chain.ethereum.decoding.constants import GNOSIS_CPT_DETAILS
+from rotkehlchen.chain.ethereum.utils import asset_normalized_value
+from rotkehlchen.chain.evm.constants import ZERO_ADDRESS
+from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
+from rotkehlchen.chain.evm.decoding.structures import (
+    DEFAULT_DECODING_OUTPUT,
+    DecoderContext,
+    DecodingOutput,
+)
+from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
+from rotkehlchen.chain.evm.decoding.utils import bridge_match_transfer, bridge_prepare_data
+from rotkehlchen.constants.assets import A_ETH, A_WETH
+from rotkehlchen.history.events.structures.types import HistoryEventType
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import ChainID, ChecksumEvmAddress
+from rotkehlchen.utils.misc import hex_or_bytes_to_address, hex_or_bytes_to_int
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.chain.evm.node_inquirer import EvmNodeInquirer
+    from rotkehlchen.user_messages import MessagesAggregator
+
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
+TOKENS_BRIDGING_INITIATED: Final = b'Y\xa9\xa8\x02{\x9c\x87\xb9a\xe2T\x89\x98!\xc9\xa2v\xb5\xef\xc3]\x1ft\t\xeaO)\x14p\xf1b\x9a'  # noqa: E501
+TOKENS_BRIDGED: Final = b'\x9a\xfdG\x90~%\x02\x8c\xda\xca\x89\xd1\x93Q\x8c0+\xbb\x12\x86\x17\xd5\xa9\x92\xc5\xab\xd4X\x15Re\x93'  # noqa: E501
+
+
+class OmnibridgeCommonDecoder(DecoderInterface, abc.ABC):
+
+    def __init__(
+            self,
+            evm_inquirer: 'EvmNodeInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+            bridge_address: ChecksumEvmAddress,
+            source_chain: ChainID,
+            target_chain: ChainID,
+    ) -> None:
+        super().__init__(
+            evm_inquirer=evm_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+        )
+        self.bridge_address = bridge_address
+        self.source_chain = source_chain
+        self.target_chain = target_chain
+
+    def _decode_bridge_tokens(self, context: DecoderContext) -> DecodingOutput:
+        """Decodes a bridging event for tokens. Either a deposit or a withdrawal."""
+        if context.tx_log.topics[0] == TOKENS_BRIDGING_INITIATED:
+            from_address = context.transaction.from_address
+            to_address = from_address  # We have no to_address information
+        elif context.tx_log.topics[0] == TOKENS_BRIDGED:
+            to_address = hex_or_bytes_to_address(context.tx_log.topics[2])
+            from_address = to_address  # We have no from_address information
+        else:
+            return DEFAULT_DECODING_OUTPUT
+
+        bridged_asset = get_or_create_evm_token(
+            userdb=self.evm_inquirer.database,
+            evm_address=hex_or_bytes_to_address(context.tx_log.topics[1]),
+            chain_id=self.evm_inquirer.chain_id,
+            evm_inquirer=self.evm_inquirer,
+        )
+        amount = asset_normalized_value(
+            amount=hex_or_bytes_to_int(context.tx_log.data[0:32]),
+            asset=bridged_asset,
+        )
+        expected_event_type, new_event_type, from_chain, to_chain, expected_location_label = bridge_prepare_data(  # noqa: E501
+            tx_log=context.tx_log,
+            deposit_topics=(TOKENS_BRIDGING_INITIATED,),
+            source_chain=self.source_chain,
+            target_chain=self.target_chain,
+            from_address=from_address,
+            to_address=to_address,
+        )
+
+        for event in context.decoded_events:
+            if bridged_asset == A_WETH and event.asset == A_ETH and event.balance.amount == amount:
+                if event.event_type == HistoryEventType.RECEIVE:
+                    # Use transaction.from_address in ETH withdrawals since tx_log.topics[2] has other data  # noqa: E501
+                    expected_location_label = context.transaction.from_address
+                    from_address = expected_location_label
+                    to_address = expected_location_label
+                expected_address = hex_or_bytes_to_address(context.tx_log.topics[2])
+                expected_asset = A_ETH
+            else:
+                expected_address = self.bridge_address
+                expected_asset = bridged_asset
+
+            if (
+                event.event_type == expected_event_type and
+                event.location_label == expected_location_label and
+                event.address in (expected_address, ZERO_ADDRESS) and
+                event.asset == expected_asset and
+                event.balance.amount == amount
+            ):
+                bridge_match_transfer(
+                    event=event,
+                    from_address=from_address,
+                    to_address=to_address,
+                    from_chain=from_chain,
+                    to_chain=to_chain,
+                    amount=event.balance.amount,
+                    asset=bridged_asset,
+                    expected_event_type=expected_event_type,
+                    new_event_type=new_event_type,
+                    counterparty=GNOSIS_CPT_DETAILS,
+                )
+                break
+        else:
+            log.error(f'Could not find the transfer event for bridging to {to_address} in {context.transaction}')  # noqa: E501
+
+        return DEFAULT_DECODING_OUTPUT
+
+    # -- DecoderInterface methods
+
+    def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
+        return {self.bridge_address: (self._decode_bridge_tokens,)}
+
+    @staticmethod
+    def counterparties() -> tuple[CounterpartyDetails, ...]:
+        return (GNOSIS_CPT_DETAILS,)
---
+++ rotkehlchen/tests/unit/decoders/test_omnibridge.py
@@ -0,0 +1,247 @@
+import pytest
+
+from rotkehlchen.accounting.structures.balance import Balance
+from rotkehlchen.chain.ethereum.decoding.constants import CPT_GNOSIS_CHAIN
+from rotkehlchen.chain.ethereum.modules.omnibridge.decoder import (
+    BRIDGE_ADDRESS as ETHEREUM_BRIDGE_ADDRESS,
+)
+from rotkehlchen.chain.evm.constants import ZERO_ADDRESS
+from rotkehlchen.chain.evm.decoding.constants import CPT_GAS
+from rotkehlchen.chain.evm.types import string_to_evm_address
+from rotkehlchen.chain.gnosis.modules.omnibridge.decoder import (
+    BRIDGE_ADDRESS as GNOSIS_BRIDGE_ADDRESS,
+)
+from rotkehlchen.constants.assets import A_ETH, A_GNO, A_USDT, A_XDAI, Asset
+from rotkehlchen.fval import FVal
+from rotkehlchen.history.events.structures.evm_event import EvmEvent
+from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
+from rotkehlchen.types import Location, TimestampMS, deserialize_evm_tx_hash
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('ethereum_accounts', [['0xcA74F404E0C7bfA35B13B511097df966D5a65597']])
+def test_omnibridge_ethereum_token_deposit(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash('0x34b2058dd53b09c49fb415c402427f142d4da2789dff01655cc7254b949ec32d')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, gas_amount, bridge_amount = ethereum_accounts[0], TimestampMS(1718409971000), '0.00097359584767956', '32.614964635297737403'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_amount)),
+            location_label=user_address,
+            notes=f'Burned {gas_amount} ETH for gas',
+            tx_hash=tx_hash,
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            sequence_index=2998,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=A_GNO,
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} GNO from Ethereum to Gnosis via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=ETHEREUM_BRIDGE_ADDRESS,
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('ethereum_accounts', [['0x683fC308f72491B33D200b4EB10Fa9780904173d']])
+def test_omnibridge_ethereum_eth_deposit(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash('0xa19b48afa68a21ed7076559f7f6234c25d11645ea757f0d66f960056dc263a67')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, gas_amount, bridge_amount = ethereum_accounts[0], TimestampMS(1723812767000), '0.000927846227501566', '0.4'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_amount)),
+            location_label=user_address,
+            notes=f'Burned {gas_amount} ETH for gas',
+            tx_hash=tx_hash,
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} WETH from Ethereum to Gnosis via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=string_to_evm_address('0xa6439Ca0FCbA1d0F80df0bE6A17220feD9c9038a'),
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('gnosis_accounts', [['0xD2e5D774BE8dF6A2e5f091812e10149BbB12702f']])
+def test_omnibridge_gnosis_token_deposit(database, gnosis_inquirer, gnosis_accounts):
+    tx_hash = deserialize_evm_tx_hash('0x36dfd776bcc430609ea47ea74893095f27e62e691d8622569cc43ea70cec3dd4')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=gnosis_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, gas_amount, bridge_amount = gnosis_accounts[0], TimestampMS(1723743095000), '0.000318556', '8416.007021'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_XDAI,
+            balance=Balance(amount=FVal(gas_amount)),
+            location_label=user_address,
+            notes=f'Burned {gas_amount} XDAI for gas',
+            tx_hash=tx_hash,
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            sequence_index=2,
+            timestamp=timestamp,
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=Asset('eip155:100/erc20:0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83'),
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} USDC from Gnosis to Ethereum via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=GNOSIS_BRIDGE_ADDRESS,
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('ethereum_accounts', [['0x9164dDF23c029E30b2eB0EbacbB4dEa3C0E1ac43']])
+def test_omnibridge_ethereum_token_withdrawal(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash('0xa56d72db76b6c7cfd3db1d2d51902b6f611f5124157ae50a6fd2f5dac7bbfa54')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, gas_amount, bridge_amount = ethereum_accounts[0], TimestampMS(1723719059000), '0.000740577652951968', '8420.660675'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_amount)),
+            location_label=user_address,
+            notes=f'Burned {gas_amount} ETH for gas',
+            tx_hash=tx_hash,
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            sequence_index=415,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.WITHDRAWAL,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=A_USDT,
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} USDT from Gnosis to Ethereum via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=ETHEREUM_BRIDGE_ADDRESS,
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('ethereum_accounts', [['0x8D1D236F45a7DEcB769D88963b0F699cDBE5D833']])
+def test_omnibridge_ethereum_eth_withdrawal(database, ethereum_inquirer, ethereum_accounts):
+    tx_hash = deserialize_evm_tx_hash('0x0d5b64cb8b79e4a728b22caf7ac51a0a595278fd6b2265775a4429a204e4e5e3')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=ethereum_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, gas_amount, bridge_amount = ethereum_accounts[0], TimestampMS(1723786739000), '0.000333774965537772', '24.42848'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_amount)),
+            location_label=user_address,
+            notes=f'Burned {gas_amount} ETH for gas',
+            tx_hash=tx_hash,
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.ETHEREUM,
+            event_type=HistoryEventType.WITHDRAWAL,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} WETH from Gnosis to Ethereum via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=string_to_evm_address('0xa6439Ca0FCbA1d0F80df0bE6A17220feD9c9038a'),
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('gnosis_accounts', [['0xcc2c6D82e00E14f81FFc8e4F6A26c4522adA5a34']])
+def test_omnibridge_gnosis_token_withdrawal(database, gnosis_inquirer, gnosis_accounts):
+    tx_hash = deserialize_evm_tx_hash('0x6d4006bc0927588758911407d0f01124056453ab8828897a1c4a125a5f574520')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=gnosis_inquirer,
+        database=database,
+        tx_hash=tx_hash,
+    )
+    user_address, timestamp, bridge_amount = gnosis_accounts[0], TimestampMS(1598480695000), '300'
+    assert events == [
+        EvmEvent(
+            sequence_index=4,
+            timestamp=timestamp,
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.WITHDRAWAL,
+            event_subtype=HistoryEventSubType.BRIDGE,
+            asset=Asset('eip155:100/erc20:0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83'),
+            balance=Balance(amount=FVal(bridge_amount)),
+            location_label=user_address,
+            notes=f'Bridge {bridge_amount} USDC from Ethereum to Gnosis via Gnosis Chain bridge',
+            tx_hash=tx_hash,
+            counterparty=CPT_GNOSIS_CHAIN,
+            address=ZERO_ADDRESS,
+        ),
+    ]
