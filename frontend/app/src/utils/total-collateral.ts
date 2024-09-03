d6e5c900088d1818e2600dc3e58ff9aa22ba3609
Konstantinos Paparas <kelsos86@gmail.com>
2024-09-03 16:04:58 +0200

    refactor: introduces some types

--- frontend/app/src/utils/total-collateral.ts
+++ frontend/app/src/utils/total-collateral.ts
@@ -2,5 +2,5 @@ import type { BigNumber } from '@rotki/common';
 import type { Collateral, CollateralizedLoan } from '@/types/defi';
 
 export function totalCollateral(loan: Ref<CollateralizedLoan<Collateral[]>>): ComputedRef<BigNumber> {
-  return computed(() => get(loan).collateral.reduce((previous, current) => previous.plus(current.usdValue), Zero));
+  return computed<BigNumber>(() => get(loan).collateral.reduce((previous, current) => previous.plus(current.usdValue), Zero));
 }
