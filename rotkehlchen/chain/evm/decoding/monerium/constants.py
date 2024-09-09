1ee5d18339e19d5cc1fb09832485a5a9a8584797
Lefteris Karapetsas <lefteris@refu.co>
2024-09-09 23:02:55 +0200

    Properly decode monerium v2 burn events
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/evm/decoding/monerium/constants.py
+++ rotkehlchen/chain/evm/decoding/monerium/constants.py
@@ -3,4 +3,5 @@ from typing import Final
 
 CPT_MONERIUM: Final = 'monerium'
 BURN_MONERIUM_SIGNATURE: Final = b'8#\xca\xec'
+BURN_MONERIUM_SIGNATURE_V2: Final = b'\x8d\x12G\xba'
 BURNFROM_MONERIUM_SIGNATURE: Final = b'\xef1\xf7\xaa'
