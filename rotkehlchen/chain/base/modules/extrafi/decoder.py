7177a49f571c9d4cc76f1ec22db459239f0e2fee
Yabir Benchakhtir <yabirg@protonmail.com>
2024-09-10 11:46:44 +0200

    Add extrafi base balances

---
+++ rotkehlchen/chain/base/modules/extrafi/decoder.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.extrafi.decoder import ExtrafiCommonDecoder
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.base.node_inquirer import BaseInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class ExtrafiDecoder(ExtrafiCommonDecoder):
+
+    def __init__(
+            self,
+            optimism_inquirer: 'BaseInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=optimism_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            extra_token_identifier='eip155:8453/erc20:0x2dad3a13ef0c6366220f989157009e501e7938f8',
+        )
--- rotkehlchen/tests/unit/decoders/test_extrafi.py
+++ rotkehlchen/tests/unit/decoders/test_extrafi.py
@@ -5,14 +5,14 @@ import pytest
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.chain.evm.decoding.constants import CPT_GAS
-from rotkehlchen.chain.evm.types import string_to_evm_address
-from rotkehlchen.chain.optimism.modules.extrafi.decoder import (
+from rotkehlchen.chain.evm.decoding.extrafi.decoder import (
     CPT_EXTRAFI,
     EXTRAFI_FARMING_CONTRACT,
     EXTRAFI_POOL_CONTRACT,
     EXTRAFI_STAKING_CONTRACT,
     VOTE_ESCROW,
 )
+from rotkehlchen.chain.evm.types import string_to_evm_address
 from rotkehlchen.constants.assets import A_ETH, A_OP
 from rotkehlchen.constants.misc import ZERO
 from rotkehlchen.fval import FVal
@@ -70,7 +70,7 @@ def test_extrafi_deposit_and_stake(
             asset=Asset('eip155:10/erc20:0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db'),
             balance=Balance(amount=FVal(deposited_amount)),
             location_label=optimism_accounts[0],
-            notes=f'Deposit {deposited_amount} VELO into Extrafi',
+            notes=f'Deposit {deposited_amount} VELO into Extrafi lend',
             counterparty=CPT_EXTRAFI,
             address=string_to_evm_address('0xC89Bddb6E5aada6Ef9e15eeA77E9C5e0dB9dAe5D'),
             extra_data={'reserve_index': 35},
@@ -110,7 +110,7 @@ def test_extrafi_unstake_and_withdraw(
             asset=Asset('eip155:10/erc20:0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85'),
             balance=Balance(amount=FVal(deposited_amount)),
             location_label=optimism_accounts[0],
-            notes=f'Withdraw {deposited_amount} USDC from Extrafi',
+            notes=f'Withdraw {deposited_amount} USDC from Extrafi lend',
             address=string_to_evm_address('0x85B16A35d310Db338D7bA35b85F83ea44182A396'),
             counterparty=CPT_EXTRAFI,
         ),
@@ -253,7 +253,7 @@ def test_extrafi_repay(
             asset=A_ETH,
             balance=Balance(amount=FVal(repaid_amount)),
             location_label=optimism_accounts[0],
-            notes=f'Repay {repaid_amount} ETH in Extrafi',
+            notes=f'Repay {repaid_amount} ETH in Extrafi EXA-WETH farm',
             address=EXTRAFI_FARMING_CONTRACT,
             counterparty=CPT_EXTRAFI,
         ), EvmEvent(
@@ -317,7 +317,7 @@ def test_extrafi_repay_with_token(
             asset=Asset('eip155:10/erc20:0x7F5c764cBc14f9669B88837ca1490cCa17c31607'),
             balance=Balance(amount=FVal(repaid_amount)),
             location_label=optimism_accounts[0],
-            notes=f'Repay {repaid_amount} USDC.e in Extrafi',
+            notes=f'Repay {repaid_amount} USDC.e in Extrafi USDC.e-wUSDR farm',
             address=string_to_evm_address('0x5f88d6f7beD0538Ca825404Baf20C846b4073e5D'),
             counterparty=CPT_EXTRAFI,
         ), EvmEvent(
@@ -369,7 +369,7 @@ def test_close_position(
             asset=Asset('eip155:10/erc20:0x1e925De1c68ef83bD98eE3E130eF14a50309C01B'),
             balance=Balance(amount=FVal(withdrawn_amount)),
             location_label=optimism_accounts[0],
-            notes=f"Withdraw {withdrawn_amount} EXA from Extrafi's farm",
+            notes=f'Withdraw {withdrawn_amount} EXA from Extrafi EXA-WETH farm',
             counterparty=CPT_EXTRAFI,
             address=string_to_evm_address('0x9558FF42E95dcA076A8DEB67c8FF8B86f52b2f8C'),
         ),
@@ -384,7 +384,7 @@ def test_farm_investment(
 ):
     tx_hash = deserialize_evm_tx_hash('0xf1f2de24757b57df7b53090369020a527f9ea54d5a876fe85fe972643c50a7e2')  # noqa: E501
     events, _ = get_decoded_events_of_transaction(evm_inquirer=optimism_inquirer, tx_hash=tx_hash)
-    timestamp, fee_amount, deposited_amount = TimestampMS(1722792861000), '0.000027486262250944', '8.230157245731733013'  # noqa: E501
+    timestamp, fee_amount, deposited_amount, borrow_amount = TimestampMS(1722792861000), '0.000027486262250944', '8.230157245731733013', '0.002920375680424896'  # noqa: E501
     assert events == [
         EvmEvent(
             tx_hash=tx_hash,
@@ -420,9 +420,170 @@ def test_farm_investment(
             asset=Asset('eip155:10/erc20:0x1e925De1c68ef83bD98eE3E130eF14a50309C01B'),
             balance=Balance(amount=FVal(deposited_amount)),
             location_label=optimism_accounts[0],
-            notes=f'Deposit {deposited_amount} EXA in Extrafi',
+            notes=f'Deposit {deposited_amount} EXA in Extrafi EXA-WETH farm',
             counterparty=CPT_EXTRAFI,
             extra_data={'vault_id': 70, 'vault_position': 511},
             address=string_to_evm_address('0x9558FF42E95dcA076A8DEB67c8FF8B86f52b2f8C'),
+        ), EvmEvent(
+            tx_hash=tx_hash,
+            sequence_index=50,
+            timestamp=timestamp,
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.GENERATE_DEBT,
+            asset=Asset('eip155:10/erc20:0x4200000000000000000000000000000000000006'),
+            balance=Balance(amount=FVal(borrow_amount)),
+            location_label=optimism_accounts[0],
+            notes=f'Borrow {borrow_amount} WETH in Extrafi EXA-WETH farm',
+            counterparty=CPT_EXTRAFI,
+            address=string_to_evm_address('0xf9cFB8a62f50e10AdDE5Aa888B44cF01C5957055'),
+        ), EvmEvent(
+            tx_hash=tx_hash,
+            sequence_index=51,
+            timestamp=timestamp,
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+            asset=Asset('eip155:10/erc20:0x4200000000000000000000000000000000000006'),
+            balance=Balance(amount=FVal(borrow_amount)),
+            location_label=optimism_accounts[0],
+            notes=f'Deposit {borrow_amount} WETH in Extrafi EXA-WETH farm',
+            counterparty=CPT_EXTRAFI,
+            address=string_to_evm_address('0xf9cFB8a62f50e10AdDE5Aa888B44cF01C5957055'),
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('base_accounts', [['0x007183900fBbe3e7815b278074a49B8C7319EDba']])
+def test_new_farm_borrow_position_on_base(base_inquirer, base_accounts):
+    evmhash = deserialize_evm_tx_hash('0xb9479f2e21100ddbba10395d76abb2fb4e151b2142ba90c91151a10fcb5cfbc7')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(evm_inquirer=base_inquirer, tx_hash=evmhash)
+    timestamp, borrow_amount, gas_fees = TimestampMS(1725972969000), '4025.689364', '0.000006907473477667'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_fees)),
+            location_label=base_accounts[0],
+            notes=f'Burned {gas_fees} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.GENERATE_DEBT,
+            asset=Asset('eip155:8453/erc20:0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'),
+            balance=Balance(amount=FVal(borrow_amount)),
+            location_label=base_accounts[0],
+            notes=f'Borrow {borrow_amount} USDC in Extrafi USDz-USDC farm',
+            counterparty=CPT_EXTRAFI,
+            address=EXTRAFI_FARMING_CONTRACT,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=2,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+            asset=Asset('eip155:8453/erc20:0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'),
+            balance=Balance(amount=FVal(borrow_amount)),
+            location_label=base_accounts[0],
+            notes=f'Deposit {borrow_amount} USDC in Extrafi USDz-USDC farm',
+            counterparty=CPT_EXTRAFI,
+            address=EXTRAFI_FARMING_CONTRACT,
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('base_accounts', [['0x007183900fBbe3e7815b278074a49B8C7319EDba']])
+def test_new_farm_position_on_base(base_inquirer, base_accounts):
+    evmhash = deserialize_evm_tx_hash('0xf0458b2c208fa7362669b6430277808a2bda527fcbe5dd3514a5879c445311cc')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(evm_inquirer=base_inquirer, tx_hash=evmhash)
+    timestamp, deposit_amount, gas_fees = TimestampMS(1725309783000), '5042.114438', '0.000003258939143014'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_fees)),
+            location_label=base_accounts[0],
+            notes=f'Burned {gas_fees} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=390,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+            asset=Asset('eip155:8453/erc20:0xB79DD08EA68A908A97220C76d19A6aA9cBDE4376'),
+            balance=Balance(amount=FVal(deposit_amount)),
+            location_label=base_accounts[0],
+            notes=f'Deposit {deposit_amount} USD+ in Extrafi OVN-USD+ farm',
+            counterparty=CPT_EXTRAFI,
+            address=string_to_evm_address('0x88F6e275dD40250782ff48c9b561C8a875934043'),
+            extra_data={'vault_id': 20, 'vault_position': 3408},
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('base_accounts', [['0xDbA7bB7Ee25d259e0a14880Ef107A7c5106A716d']])
+def test_vested_extra_base(base_inquirer, base_accounts):
+    evmhash = deserialize_evm_tx_hash('0x560a5e279a7f1b9c89dca3d7f93da11c9418037c0362f01b443d50967a719d5d')  # noqa: E501
+    events, _ = get_decoded_events_of_transaction(evm_inquirer=base_inquirer, tx_hash=evmhash)
+    timestamp, locked_amount, gas_fees = TimestampMS(1722581379000), '8.004925206880061111', '0.000002859618316575'  # noqa: E501
+    assert events == [
+        EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=0,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            balance=Balance(amount=FVal(gas_fees)),
+            location_label=base_accounts[0],
+            notes=f'Burned {gas_fees} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=53,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.INFORMATIONAL,
+            event_subtype=HistoryEventSubType.APPROVE,
+            asset=Asset('eip155:8453/erc20:0x2dAD3a13ef0C6366220f989157009e501e7938F8'),
+            balance=Balance(amount=ZERO),
+            location_label=base_accounts[0],
+            notes=f'Revoke EXTRA spending approval of {base_accounts[0]} by 0xe0BeC4F45aEF64CeC9dCB9010d4beFfB13e91466',  # noqa: E501
+            address=VOTE_ESCROW,
+        ), EvmEvent(
+            tx_hash=evmhash,
+            sequence_index=54,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_ASSET,
+            asset=Asset('eip155:8453/erc20:0x2dAD3a13ef0C6366220f989157009e501e7938F8'),
+            balance=Balance(amount=FVal(locked_amount)),
+            location_label=base_accounts[0],
+            notes=f'Lock {locked_amount} EXTRA until 08/08/2024 00:00:00',
+            counterparty=CPT_EXTRAFI,
+            address=VOTE_ESCROW,
+            product=EvmProduct.STAKING,
         ),
     ]
