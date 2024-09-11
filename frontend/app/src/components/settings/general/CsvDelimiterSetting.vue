4ba08c32fb88f0d509cb129e3f20f01cd37ac60c
cleanerzkp <0xCleaner@proton.me>
2024-09-11 16:49:33 +0200

    style: fixed lint

--- frontend/app/src/components/settings/general/CsvDelimiterSetting.vue
+++ frontend/app/src/components/settings/general/CsvDelimiterSetting.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import useVuelidate from '@vuelidate/core';
-import { helpers, required, maxLength } from '@vuelidate/validators';
+import { helpers, maxLength, required } from '@vuelidate/validators';
 import { useFrontendSettingsStore } from '@/store/settings/frontend';
 import { toMessages } from '@/utils/validation';
 
@@ -15,7 +15,7 @@ const rules = {
     required: helpers.withMessage(t('general_settings.validation.csv_delimiter.empty'), required),
     singleCharacter: helpers.withMessage(
       t('general_settings.validation.csv_delimiter.single_character'),
-      maxLength(1)
+      maxLength(1),
     ),
   },
 };
@@ -34,32 +34,27 @@ function successMessage(delimiter: string) {
 onMounted(() => {
   resetCsvDelimiter();
 });
-
-function focusInput(event: Event) {
-  const input = event.target as HTMLInputElement;
-  input.setSelectionRange(input.value.length, input.value.length);
-}
 </script>
+
 <template>
-    <SettingsOption
-      #default="{ error, success, updateImmediate }"
-      setting="csvSeparator"
-      frontend-setting
-      :error-message="t('general_settings.validation.csv_delimiter.error')"
-      :success-message="successMessage"
-      @finished="resetCsvDelimiter()"
-    >
-      <RuiTextField
-        v-model="csvDelimiter"
-        variant="outlined"
-        color="primary"
-        maxlength="1"
-        class="general-settings__fields__csv-delimiter"
-        :label="t('general_settings.labels.csv_delimiter')"
-        :success-messages="success ? [success] : []"
-        :error-messages="error ? [error] : toMessages(v$.csvDelimiter)"
-        @update:model-value="callIfValid($event, updateImmediate)"
-        @focus="focusInput"
-      />
-    </SettingsOption>
-  </template>
+  <SettingsOption
+    #default="{ error, success, updateImmediate }"
+    setting="csvSeparator"
+    frontend-setting
+    :error-message="t('general_settings.validation.csv_delimiter.error')"
+    :success-message="successMessage"
+    @finished="resetCsvDelimiter()"
+  >
+    <RuiTextField
+      v-model="csvDelimiter"
+      variant="outlined"
+      color="primary"
+      maxlength="1"
+      class="general-settings__fields__csv-delimiter"
+      :label="t('general_settings.labels.csv_delimiter')"
+      :success-messages="success ? [success] : []"
+      :error-messages="error ? [error] : toMessages(v$.csvDelimiter)"
+      @update:model-value="callIfValid($event, updateImmediate)"
+    />
+  </SettingsOption>
+</template>
--- frontend/app/tests/unit/specs/types/user.spec.ts
+++ frontend/app/tests/unit/specs/types/user.spec.ts
@@ -77,7 +77,7 @@ describe('types/user', () => {
       shouldRefreshValidatorDailyStats: false,
       unifyAccountsTable: false,
       savedFilters: {},
-      csvSeparator: ','
+      csvSeparator: ',',
     };
 
     const raw = {
