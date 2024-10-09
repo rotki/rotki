e399e0eac90eb338acc3aaceab6c1f2d9ce35729
cleanerzkp <0xCleaner@proton.me>
2024-10-09 20:52:46 +0200

    refactor: database page

--- frontend/app/src/components/settings/data-security/data-management/PurgeImagesCache.vue
+++ frontend/app/src/components/settings/data-security/data-management/PurgeImagesCache.vue
@@ -66,74 +66,61 @@ const { status, pending, showConfirmation } = useCacheClear<PurgeableImageCache>
 </script>
 
 <template>
-  <div>
-    <RuiCardHeader class="p-0 mb-4">
-      <template #header>
-        {{ t('data_management.purge_images_cache.title') }}
-      </template>
-      <template #subheader>
-        {{ t('data_management.purge_images_cache.subtitle') }}
-      </template>
-    </RuiCardHeader>
-
-    <div class="flex items-center gap-4">
-      <div class="flex flex-col md:flex-row md:gap-4 flex-1">
-        <RuiAutoComplete
-          v-model="source"
-          class="flex-1"
-          variant="outlined"
-          :label="t('data_management.purge_images_cache.select_image_source')"
-          :options="purgable"
-          text-attr="text"
-          key-attr="id"
-          :disabled="pending"
-        />
-        <AssetSelect
-          v-if="source === PurgeableImageCache.ASSET_ICONS"
-          v-model="assetToClear"
-          class="flex-1"
-          outlined
-          :label="t('data_management.purge_images_cache.label.asset_to_clear')"
-          :hint="t('data_management.purge_images_cache.hint')"
-        />
-        <RuiAutoComplete
-          v-else
-          v-model="ensToClear"
-          class="flex-1"
-          :options="ensNamesList"
-          variant="outlined"
-          chips
-          clearable
-          custom-value
-          :label="t('data_management.purge_images_cache.label.ens_to_clear')"
-          :hint="t('data_management.purge_images_cache.hint')"
-        />
-      </div>
-
-      <RuiTooltip
-        :popper="{ placement: 'top' }"
-        :open-delay="400"
-        class="-mt-6"
-      >
-        <template #activator>
-          <RuiButton
-            variant="text"
-            icon
-            :disabled="!source || pending"
-            :loading="pending"
-            @click="showConfirmation(source)"
-          >
-            <RuiIcon name="delete-bin-line" />
-          </RuiButton>
-        </template>
-        <span> {{ t('data_management.purge_images_cache.tooltip') }} </span>
-      </RuiTooltip>
+  <div class="flex items-center gap-4">
+    <div class="flex flex-col md:flex-col md:gap-4 flex-1">
+      <RuiAutoComplete
+        v-model="source"
+        class="flex-1"
+        variant="outlined"
+        :label="t('data_management.purge_images_cache.select_image_source')"
+        :options="purgable"
+        text-attr="text"
+        key-attr="id"
+        :disabled="pending"
+      />
+      <AssetSelect
+        v-if="source === PurgeableImageCache.ASSET_ICONS"
+        v-model="assetToClear"
+        class="flex-1"
+        outlined
+        :label="t('data_management.purge_images_cache.label.asset_to_clear')"
+        :hint="t('data_management.purge_images_cache.hint')"
+      />
+      <RuiAutoComplete
+        v-else
+        v-model="ensToClear"
+        class="flex-1"
+        :options="ensNamesList"
+        variant="outlined"
+        chips
+        clearable
+        custom-value
+        :label="t('data_management.purge_images_cache.label.ens_to_clear')"
+        :hint="t('data_management.purge_images_cache.hint')"
+      />
     </div>
-
-    <ActionStatusIndicator
-      v-if="status"
-      class="mt-4"
-      :status="status"
-    />
+    <RuiTooltip
+      :popper="{ placement: 'top' }"
+      :open-delay="400"
+      class="-mt-6"
+    >
+      <template #activator>
+        <RuiButton
+          variant="text"
+          icon
+          :disabled="!source || pending"
+          :loading="pending"
+          @click="showConfirmation(source)"
+        >
+          <RuiIcon name="delete-bin-line" />
+        </RuiButton>
+      </template>
+      <span> {{ t('data_management.purge_images_cache.tooltip') }} </span>
+    </RuiTooltip>
   </div>
+  <ActionStatusIndicator
+    v-if="status"
+    class="mt-4"
+    :status="status"
+  />
 </template>
