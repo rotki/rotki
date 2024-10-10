25a38e4339f9c39df0b0405f2c8c91cb064b7271
lukicenturi <lukicenturi@gmail.com>
2024-10-10 17:00:28 +0700

    fix(frontend): check 'icon_url' and 'icon' from response to show icon

--- frontend/app/src/components/defi/airdrops/AirdropDisplay.vue
+++ frontend/app/src/components/defi/airdrops/AirdropDisplay.vue
@@ -2,21 +2,29 @@
 const props = defineProps<{
   source: string;
   iconUrl?: string;
+  icon?: string;
 }>();
 
 const { getAirdropName, getAirdropImageUrl, loading } = useAirdropsMetadata();
 
-const { source } = toRefs(props);
+const { source, icon } = toRefs(props);
 
 const name = getAirdropName(source);
 const image = getAirdropImageUrl(source);
+const imageFromIconName = computed(() => {
+  const iconVal = get(icon);
+  if (!iconVal)
+    return undefined;
+
+  return `./assets/images/protocols/${iconVal}`;
+});
 </script>
 
 <template>
   <div class="flex items-center gap-4">
     <AdaptiveWrapper>
       <AppImage
-        :src="iconUrl || image"
+        :src="iconUrl || imageFromIconName || image"
         width="1.5rem"
         height="1.5rem"
         contain
