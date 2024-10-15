7974741e29e1a1a19e6c01eaa955e50298695879
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-10-15 15:52:27 +0200

    Revamp blockchain address aliases setting (#8747)

--- frontend/app/src/components/settings/frontend/AliasSettingsCategory.vue
+++ frontend/app/src/components/settings/frontend/AliasSettingsCategory.vue
@@ -7,7 +7,22 @@ const { t } = useI18n();
     <template #title>
       {{ t('frontend_settings.subtitle.blockchain_address_aliases') }}
     </template>
-    <EnableEnsNamesSetting />
-    <AddressNamePrioritySetting />
+    <RuiAlert
+      type="info"
+      class="my-4"
+    >
+      {{ t('frontend_settings.alias_names.title') }}
+    </RuiAlert>
+    <RuiCard
+      rounded="md"
+      :no-padding="true"
+      variant="flat"
+      class="overflow-hidden"
+    >
+      <div class="pl-8 pt-2">
+        <EnableEnsNamesSetting />
+      </div>
+      <AddressNamePrioritySetting />
+    </RuiCard>
   </SettingCategory>
 </template>
