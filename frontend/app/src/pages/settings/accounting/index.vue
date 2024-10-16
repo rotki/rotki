6aa4aa52708982d125989f1f1f747d76b91aeed2
cleanerzkp <0xCleaner@proton.me>
2024-10-16 14:27:26 +0200

    feat: add AccountRuleSection category

--- frontend/app/src/pages/settings/accounting/index.vue
+++ frontend/app/src/pages/settings/accounting/index.vue
@@ -9,6 +9,7 @@ definePage({
 });
 
 enum Category {
+  ACCOUNTING_RULE = 'accounting-rule',
   TRADE_SETTINGS = 'trade-settings',
   CSV_EXPORT = 'csv-export',
 }
@@ -16,26 +17,15 @@ enum Category {
 const { t } = useI18n();
 
 const navigation = [
+  { id: Category.ACCOUNTING_RULE, label: t('accounting_settings.rule.title') },
   { id: Category.TRADE_SETTINGS, label: t('accounting_settings.trade.title') },
   { id: Category.CSV_EXPORT, label: t('account_settings.csv_export_settings.title') },
 ];
 </script>
 
 <template>
-  <div>
-    <div class="py-4 border-b border-default flex flex-wrap gap-4 items-center justify-between">
-      <SettingCategoryHeader>
-        <template #title>
-          {{ t('accounting_settings.rule.title') }}
-        </template>
-      </SettingCategoryHeader>
-    </div>
-    <div class="py-4">
-      <AccountingRuleSetting />
-    </div>
-  </div>
-
   <SettingsPage :navigation="navigation">
+    <AccountingRuleSetting :id="Category.ACCOUNTING_RULE" />
     <AccountingTradeSettings :id="Category.TRADE_SETTINGS" />
     <AccountingCsvExportSetting :id="Category.CSV_EXPORT" />
   </SettingsPage>
