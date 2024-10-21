4a4920c00ccb1df7b3d10cd6572ea4163ec79c2a
Lefteris Karapetsas <lefteris@refu.co>
2024-10-21 14:35:12 +0200

    Add more gitcoin v2 addresses for Q1 2023 rounds
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

--- rotkehlchen/chain/ethereum/modules/gitcoinv2/decoder.py
+++ rotkehlchen/chain/ethereum/modules/gitcoinv2/decoder.py
@@ -10,10 +10,7 @@ if TYPE_CHECKING:
 
 
 class Gitcoinv2Decoder(GitcoinV2CommonDecoder):
-    """This is the gitcoin v2 (allo protocol) decoder for Ethereum
-
-    No gitcoin v1 in optimism since v1 was only on mainnet and zksync lite (maybe polygon too?)
-    """
+    """This is the gitcoin v2 (allo protocol) decoder for Ethereum"""
 
     def __init__(  # pylint: disable=super-init-not-called
             self,
@@ -29,13 +26,16 @@ class Gitcoinv2Decoder(GitcoinV2CommonDecoder):
             round_impl_addresses=[
                 string_to_evm_address('0xe575282b376E3c9886779A841A2510F1Dd8C2CE4'),
                 string_to_evm_address('0xdf22a2C8F6BA9376fF17EE13E6154B784ee92094'),
+                string_to_evm_address('0x64E5b2228eF31437909900B38fC42Dd5E4B83147'),
             ],
             payout_strategy_addresses=[  # they match to the above round_impl addresses. Can be found by roundimpl.payoutStrategy()  # noqa: E501
                 string_to_evm_address('0xc41bBa19D78242C141D229e5Fe9078def93f304f'),
                 string_to_evm_address('0xebaF311F318b5426815727101fB82f0Af3525d7b'),
+                string_to_evm_address('0xD32c97242c30AcFFbd30808A31a785772640C6Db'),
             ],
             voting_impl_addresses=[
                 string_to_evm_address('0xDA2F26B30e8f5aa9cbE9c5B7Ed58E1cA81D0EbF2'),
                 string_to_evm_address('0x8fBEa07446DdF4518b1a7BA2B4f11Bd140a8DF41'),
+                string_to_evm_address('0xA09a77809785452c7a5E3d087aFa4B6Bc66Ec881'),
             ],
         )
