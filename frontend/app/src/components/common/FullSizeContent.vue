3d08093c065e63767c246acd01c60d3bf5f8b7af
Luki Centuri <lukicenturi@gmail.com>
2024-10-24 14:23:59 +0700

    style: fix full style content bug (#8801)

--- frontend/app/src/components/common/FullSizeContent.vue
+++ frontend/app/src/components/common/FullSizeContent.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 const wrapper = ref();
-const { top } = useElementBounding(wrapper);
+const topOffset = ref<number>(0);
+
+useResizeObserver(wrapper, (a) => {
+  if (a.length > 0)
+    set(topOffset, a[0].contentRect.top + 156);
+});
+
+onMounted(() => {
+  if (get(wrapper))
+    set(topOffset, get(wrapper).offsetTop + 156);
+});
 </script>
 
 <template>
   <div
     ref="wrapper"
     class="flex flex-col items-center justify-center text-center p-2"
-    :style="`height: calc(100vh - ${top + 92}px);`"
+    :style="{
+      height: `calc(100vh - ${topOffset}px)`,
+    }"
   >
     <slot />
   </div>
