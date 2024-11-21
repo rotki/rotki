bc934663b4e4d8a1f269c1ca0632149e65589a8a
Donny <130464015+defitricks@users.noreply.github.com>
2024-11-21 21:16:58 +0200

    Fix version comparison issue with Node.js version
    This pull request addresses a small but important issue related to the version comparison logic in the code. Specifically, it fixes an issue with the comparison of Node.js versions by removing the "v" prefix from process.version before passing it to the semver.satisfies function.

--- frontend/scripts/check-versions.js
+++ frontend/scripts/check-versions.js
@@ -12,18 +12,19 @@ const version = version => `\u001B[33m\u001B[40m${version}\u001B[0m`;
 
 if (!semver.satisfies(pnpmVersion, requiredPnpmVersion)) {
   console.error(
-    `${error('ERROR!')} ${requiredPnpmVersion} of pnpm is required. The current pnpm version ${version(
+    `${error('ERROR!')} ${requiredPnpmVersion} of pnpm is required. Your current pnpm version ${version(
       pnpmVersion,
     )} does not satisfy the required version.\n\n`,
   );
   process.exit(1);
 }
 
-if (!semver.satisfies(process.version, requiredNodeVersion)) {
+if (!semver.satisfies(process.version.slice(1), requiredNodeVersion)) {
   console.error(
-    `${error('ERROR!')} ${requiredNodeVersion} of node is required. The current node version ${version(
+    `${error('ERROR!')} ${requiredNodeVersion} of node is required. Your current node version ${version(
       process.version,
     )} does not satisfy the required version.\n\n`,
   );
   process.exit(1);
 }
+
