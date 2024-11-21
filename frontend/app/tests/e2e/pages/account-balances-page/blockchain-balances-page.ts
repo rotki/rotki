d2a477ea0573044548d837f5437d3046882a6978
Konstantinos Paparas <kelsos86@gmail.com>
2024-11-21 10:34:19 +0100

    test: apply balances test change

--- frontend/app/tests/e2e/pages/account-balances-page/blockchain-balances-page.ts
+++ frontend/app/tests/e2e/pages/account-balances-page/blockchain-balances-page.ts
@@ -121,12 +121,13 @@ export class BlockchainBalancesPage {
         .find('tbody')
         .find('tr[class^="_tr_"]:not(tr[class*="_group_"]')
         .each((row) => {
-          const usdValue = row.find('td').eq(5).find('[data-cy="display-amount"]').text();
-          const asset = row.find('td').eq(2).find('.account-chain').eq(0).attr('data-chain');
-          if (!usdValue || !asset)
+          const value = row.find('[data-cy=display-amount]').text();
+          const asset = row.find('.account-chain').eq(0).attr('data-chain');
+
+          if (!value || !asset)
             return;
 
-          updateLocationBalance(usdValue, balances, asset.trim().toLowerCase());
+          updateLocationBalance(value, balances, asset.trim().toLowerCase());
         });
     });
 
