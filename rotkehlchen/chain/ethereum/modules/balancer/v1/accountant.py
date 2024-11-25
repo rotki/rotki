edfa5ca1c1c533fc735f27bd89499bcf823cac15
Isaac Adewumi <ienioladewumi@gmail.com>
2024-11-25 11:19:53 +0100

    added balancer v2 accountant

--- rotkehlchen/chain/ethereum/modules/balancer/v1/accountant.py
+++ rotkehlchen/chain/ethereum/modules/balancer/v1/accountant.py
@@ -1,5 +1,15 @@
-from rotkehlchen.chain.evm.decoding.balancer.v1.accountant import Balancerv1CommonAccountant
+from rotkehlchen.chain.evm.accounting.interfaces import DepositableAccountantInterface
+from rotkehlchen.chain.evm.accounting.structures import EventsAccountantCallback
+from rotkehlchen.chain.evm.decoding.balancer.constants import CPT_BALANCER_V1
+from rotkehlchen.history.events.structures.base import get_event_type_identifier
+from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 
 
-class Balancerv1Accountant(Balancerv1CommonAccountant):
-    ...
+class Balancerv1Accountant(DepositableAccountantInterface):
+
+    def event_callbacks(self) -> dict[int, tuple[int, EventsAccountantCallback]]:
+        """Being defined at function call time is fine since this function is called only once"""
+        return {
+            get_event_type_identifier(HistoryEventType.RECEIVE, HistoryEventSubType.RECEIVE_WRAPPED, CPT_BALANCER_V1): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
+            get_event_type_identifier(HistoryEventType.SPEND, HistoryEventSubType.RETURN_WRAPPED, CPT_BALANCER_V1): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
+        }
--- rotkehlchen/chain/ethereum/modules/balancer/v2/accountant.py
+++ rotkehlchen/chain/ethereum/modules/balancer/v2/accountant.py
@@ -1,15 +1,15 @@
 from rotkehlchen.chain.evm.accounting.interfaces import DepositableAccountantInterface
 from rotkehlchen.chain.evm.accounting.structures import EventsAccountantCallback
-from rotkehlchen.chain.evm.decoding.balancer.constants import CPT_BALANCER_V1
+from rotkehlchen.chain.evm.decoding.balancer.constants import CPT_BALANCER_V2
 from rotkehlchen.history.events.structures.base import get_event_type_identifier
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 
 
-class Balancerv1CommonAccountant(DepositableAccountantInterface):
+class Balancerv2Accountant(DepositableAccountantInterface):
 
     def event_callbacks(self) -> dict[int, tuple[int, EventsAccountantCallback]]:
         """Being defined at function call time is fine since this function is called only once"""
         return {
-            get_event_type_identifier(HistoryEventType.RECEIVE, HistoryEventSubType.RECEIVE_WRAPPED, CPT_BALANCER_V1): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
-            get_event_type_identifier(HistoryEventType.SPEND, HistoryEventSubType.RETURN_WRAPPED, CPT_BALANCER_V1): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
+            get_event_type_identifier(HistoryEventType.RECEIVE, HistoryEventSubType.RECEIVE_WRAPPED, CPT_BALANCER_V2): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
+            get_event_type_identifier(HistoryEventType.SPEND, HistoryEventSubType.RETURN_WRAPPED, CPT_BALANCER_V2): (-1, self._process_deposit_or_withdrawal),  # noqa: E501
         }
