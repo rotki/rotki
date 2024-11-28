87dc0db0a46e67c1497ff0ed0abbab4697352348
azerv1 <119133160+azerv1@users.noreply.github.com>
2024-11-28 13:18:15 +0200

    Typing api l (#8963)
    Fix typing in tests/api/test_l

--- rotkehlchen/tests/api/test_locations.py
+++ rotkehlchen/tests/api/test_locations.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 import pytest
 import requests
 
@@ -8,7 +10,19 @@ from rotkehlchen.exchanges.data_structures import Trade
 from rotkehlchen.fval import FVal
 from rotkehlchen.tests.utils.api import api_url_for, assert_proper_sync_response_with_result
 from rotkehlchen.tests.utils.exchanges import mock_exchange_data_in_db
-from rotkehlchen.types import AssetAmount, Fee, Location, Price, Timestamp, TradeType
+from rotkehlchen.types import (
+    EVM_LOCATIONS_TYPE,
+    AssetAmount,
+    ChecksumEvmAddress,
+    Fee,
+    Location,
+    Price,
+    Timestamp,
+    TradeType,
+)
+
+if TYPE_CHECKING:
+    from rotkehlchen.api.server import APIServer
 
 UNISWAP_ADDR = string_to_evm_address('0xcaf012cB72f2c7152b255E091837E3a628F739e7')
 
@@ -18,11 +32,11 @@ UNISWAP_ADDR = string_to_evm_address('0xcaf012cB72f2c7152b255E091837E3a628F739e7
 @pytest.mark.parametrize('ethereum_modules', [['uniswap']])
 @pytest.mark.parametrize('start_with_valid_premium', [True])
 def test_get_associated_locations(
-        rotkehlchen_api_server_with_exchanges,
-        added_exchanges,
-        ethereum_accounts,  # pylint: disable=unused-argument
-        start_with_valid_premium,  # pylint: disable=unused-argument
-):
+        rotkehlchen_api_server_with_exchanges: 'APIServer',
+        added_exchanges: list[EVM_LOCATIONS_TYPE],
+        ethereum_accounts: list[ChecksumEvmAddress],  # pylint: disable=unused-argument
+        start_with_valid_premium: bool,  # pylint: disable=unused-argument
+) -> None:
     rotki = rotkehlchen_api_server_with_exchanges.rest_api.rotkehlchen
     mock_exchange_data_in_db(added_exchanges, rotki)
     db = rotki.data.db
