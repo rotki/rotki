b3dcc77dab4596c6f71792a0061aef8b6d484bf4
Nicholas Yoder <nicholasyoder@proton.me>
2024-12-19 10:32:30 -0500

    Add evm event redecode trigger to db upgrade

--- rotkehlchen/db/upgrades/v45_v46.py
+++ rotkehlchen/db/upgrades/v45_v46.py
@@ -7,6 +7,7 @@ from rotkehlchen.assets.asset import Asset
 from rotkehlchen.constants import ALLASSETIMAGESDIR_NAME, ASSETIMAGESDIR_NAME, IMAGESDIR_NAME
 from rotkehlchen.constants.assets import A_COW, A_GNOSIS_COW, A_LQTY
 from rotkehlchen.data_import.utils import maybe_set_transaction_extra_data
+from rotkehlchen.db.constants import HISTORY_MAPPING_KEY_STATE, HISTORY_MAPPING_STATE_CUSTOMIZED
 from rotkehlchen.errors.asset import UnknownAsset
 from rotkehlchen.errors.serialization import DeserializationError
 from rotkehlchen.fval import FVal
@@ -68,11 +69,7 @@ def upgrade_v45_to_v46(db: 'DBHandler', progress_handler: 'DBUpgradeProgressHand
     @progress_step(description='Moving EVM event extra data to the history_events table.')
     def _move_extra_data(write_cursor: 'DBCursor') -> None:
         write_cursor.execute('ALTER TABLE history_events ADD COLUMN extra_data TEXT;')
-        write_cursor.execute(
-            'UPDATE history_events SET extra_data = '
-            '(SELECT extra_data FROM evm_events_info '
-            'WHERE evm_events_info.identifier = history_events.identifier);',
-        )
+        # no need to migrate the data since evm events are to be redecoded after this upgrade.
         write_cursor.execute('ALTER TABLE evm_events_info DROP COLUMN extra_data;')
 
     @progress_step(description='Converting asset movements to history events')
@@ -151,5 +148,32 @@ def upgrade_v45_to_v46(db: 'DBHandler', progress_handler: 'DBUpgradeProgressHand
         write_cursor.execute('DROP TABLE asset_movements')
         write_cursor.execute('DROP TABLE asset_movement_category')
         write_cursor.execute("DELETE FROM settings WHERE name='account_for_assets_movements'")
+
+    @progress_step(description='Resetting decoded events.')
+    def _reset_decoded_events(write_cursor: 'DBCursor') -> None:
+        """Reset all decoded evm events except for the customized ones and those in zksync lite.
+        Code taken from previous upgrade
+        """
+        if write_cursor.execute('SELECT COUNT(*) FROM evm_transactions').fetchone()[0] > 0:
+            customized_events = write_cursor.execute(
+                'SELECT COUNT(*) FROM history_events_mappings WHERE name=? AND value=?',
+                (HISTORY_MAPPING_KEY_STATE, HISTORY_MAPPING_STATE_CUSTOMIZED),
+            ).fetchone()[0]
+            querystr = (
+                "DELETE FROM history_events WHERE identifier IN ("
+                "SELECT H.identifier from history_events H INNER JOIN evm_events_info E "
+                "ON H.identifier=E.identifier AND E.tx_hash IN "
+                "(SELECT tx_hash FROM evm_transactions) AND H.location != 'o')"  # location 'o' is zksync lite  # noqa: E501
+            )
+            bindings: tuple = ()
+            if customized_events != 0:
+                querystr += ' AND identifier NOT IN (SELECT parent_identifier FROM history_events_mappings WHERE name=? AND value=?)'  # noqa: E501
+                bindings = (HISTORY_MAPPING_KEY_STATE, HISTORY_MAPPING_STATE_CUSTOMIZED)
+
+            write_cursor.execute(querystr, bindings)
+            write_cursor.execute(
+                'DELETE from evm_tx_mappings WHERE tx_id IN (SELECT identifier FROM evm_transactions) AND value=?',  # noqa: E501
+                (0,),  # decoded tx state
+            )
 
     perform_userdb_upgrade_steps(db=db, progress_handler=progress_handler, should_vacuum=True)
