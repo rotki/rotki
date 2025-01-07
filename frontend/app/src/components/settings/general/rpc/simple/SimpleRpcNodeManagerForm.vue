d87740312db2c30261886f4a2916c20b5d8b91d1
lukicenturi <lukicenturi@gmail.com>
2025-01-07 18:09:02 +0700

    fix: show add node button for ksm, dot, and beaconchain

---
+++ frontend/app/src/components/settings/general/rpc/simple/SimpleRpcNodeManagerForm.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
+import { useFormStateWatcher } from '@/composables/form';
+import { toMessages } from '@/utils/validation';
+import type { RuiTextField } from '@rotki/ui-library';
+import type { ValidationErrors } from '@/types/api/errors';
+
+const errors = defineModel<ValidationErrors>('errorMessages', { required: true });
+const stateUpdated = defineModel<boolean>('stateUpdated', { default: false, required: false });
+const modelValue = defineModel<string>({ required: true });
+
+const { t } = useI18n();
+
+const rules = {
+  modelValue: { required },
+};
+
+const states = {
+  modelValue,
+};
+
+const v$ = useVuelidate(
+  rules,
+  states,
+  {
+    $autoDirty: true,
+    $externalResults: errors,
+  },
+);
+
+useFormStateWatcher(states, stateUpdated);
+
+defineExpose({
+  validate: async () => await get(v$).$validate(),
+});
+</script>
+
+<template>
+  <RuiTextField
+    v-model="modelValue"
+    variant="outlined"
+    color="primary"
+    class="pt-2"
+    :label="t('general_settings.labels.node_rpc_endpoint')"
+    type="text"
+    clearable
+    :error-messages="toMessages(v$.modelValue)"
+  />
+</template>
