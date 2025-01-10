09b7934fd8d244e201df90d67ff54c0362e41f40
Nicholas Yoder <nicholasyoder@proton.me>
2025-01-10 14:40:44 -0500

    Add paraswap decoders to BSC

---
+++ rotkehlchen/chain/binance_sc/modules/paraswap/v5/constants.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+PARASWAP_FEE_CLAIMER: Final = string_to_evm_address('0x2DF17455B96Dde3618FD6B1C3a9AA06D6aB89347')
---
+++ rotkehlchen/chain/binance_sc/modules/paraswap/v5/decoder.py
@@ -0,0 +1,27 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.binance_sc.modules.paraswap.v5.constants import PARASWAP_FEE_CLAIMER
+from rotkehlchen.chain.ethereum.modules.paraswap.v5.constants import PARASWAP_AUGUSTUS_ROUTER
+from rotkehlchen.chain.evm.decoding.paraswap.v5.decoder import Paraswapv5CommonDecoder
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.binance_sc.node_inquirer import BinanceSCInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class Paraswapv5Decoder(Paraswapv5CommonDecoder):
+
+    def __init__(
+            self,
+            evm_inquirer: 'BinanceSCInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=evm_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            router_address=PARASWAP_AUGUSTUS_ROUTER,
+            fee_receiver_address=PARASWAP_FEE_CLAIMER,
+        )
