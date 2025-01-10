341761e049af401eb7c997b9b0220c6734aecfbf
Nicholas Yoder <nicholasyoder@proton.me>
2025-01-10 11:37:57 -0500

    Add odos decoders to BSC

---
+++ rotkehlchen/chain/binance_sc/modules/odos/v1/decoder.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.odos.v1.decoder import Odosv1DecoderBase
+
+from .constants import ODOS_V1_ROUTER
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.binance_sc.node_inquirer import BinanceSCInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class Odosv1Decoder(Odosv1DecoderBase):
+    def __init__(
+            self,
+            evm_inquirer: 'BinanceSCInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=evm_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            router_address=ODOS_V1_ROUTER,
+        )
---
+++ rotkehlchen/chain/binance_sc/modules/odos/v2/constants.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+ODOS_V2_ROUTER: Final = string_to_evm_address('0x89b8AA89FDd0507a99d334CBe3C808fAFC7d850E')
