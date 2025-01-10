29aed7f28c64d363616e7d5b4adf54af009b7371
Nicholas Yoder <nicholasyoder@proton.me>
2025-01-10 13:48:51 -0500

    Add firebird finance decoder to BSC

---
+++ rotkehlchen/chain/binance_sc/modules/firebird_finance/decoder.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.firebird_finance.decoder import FirebirdFinanceCommonDecoder
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.binance_sc.node_inquirer import BinanceSCInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class FirebirdFinanceDecoder(FirebirdFinanceCommonDecoder):
+
+    def __init__(
+            self,
+            evm_inquirer: 'BinanceSCInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=evm_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            router_address=string_to_evm_address('0x92e4F29Be975C1B1eB72E77De24Dccf11432a5bd'),
+        )
