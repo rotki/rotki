0d3055ec1d93ce77c1676e12f18273ce5016a412
Nicholas Yoder <nicholasyoder@proton.me>
2025-01-10 10:36:48 -0500

    Add curve decoder to BSC

---
+++ rotkehlchen/chain/binance_sc/modules/curve/decoder.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.binance_sc.modules.curve.constants import (
+    CURVE_SWAP_ROUTER_NG,
+    DEPOSIT_AND_STAKE_ZAP,
+)
+from rotkehlchen.chain.evm.decoding.curve.decoder import CurveCommonDecoder
+from rotkehlchen.constants.assets import A_BSC_BNB
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.binance_sc.node_inquirer import BinanceSCInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class CurveDecoder(CurveCommonDecoder):
+
+    def __init__(
+            self,
+            evm_inquirer: 'BinanceSCInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=evm_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            native_currency=A_BSC_BNB,
+            aave_pools=set(),
+            curve_deposit_contracts={DEPOSIT_AND_STAKE_ZAP},
+            curve_swap_routers={CURVE_SWAP_ROUTER_NG},
+        )
