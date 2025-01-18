682ba2410fac1c29086347e7da42c1eaf4c28004
Yabir Benchakhtir <yabirg@protonmail.com>
2025-01-18 18:57:27 +0100

    Handle user db in colibri
    also makes use multi thread runtime
    
    https://docs.rs/tokio/latest/tokio/runtime/index.html#multi-threaded-runtime-behavior-at-the-time-of-writing

--- colibri/src/api.rs
+++ colibri/src/api.rs
@@ -1,42 +1,17 @@
-use axum::{extract::Json, extract::State, response::IntoResponse};
-use serde::Deserialize;
+pub mod database;
+pub mod icons;
+mod utils;
+
 use std::path::PathBuf;
 use std::sync::Arc;
+use tokio::sync::RwLock;
 
 use crate::coingecko;
-use crate::icons;
+use crate::database::DBHandler;
 
 #[derive(Clone)]
 pub struct AppState {
     pub data_dir: PathBuf,
     pub coingecko: Arc<coingecko::Coingecko>,
-}
-
-#[derive(Deserialize)]
-pub struct AssetIconRequest {
-    asset_id: String,
-    match_header: Option<String>,
-}
-
-/// The handler for the get icon endpoint
-///
-/// Gets the given icon from the user's system if it's already
-/// downloaded or asks for it from the icon sources. Returns it
-/// if found and a 404 if not
-pub async fn get_icon(
-    State(state): State<AppState>,
-    Json(payload): Json<AssetIconRequest>,
-) -> impl IntoResponse {
-    match icons::get_or_query_icon(
-        state.coingecko,
-        state.data_dir,
-        &payload.asset_id,
-        payload.match_header,
-    )
-    .await
-    {
-        (status, Some(headers), Some(bytes)) => (status, headers, bytes).into_response(),
-        (status, Some(headers), None) => (status, headers).into_response(),
-        (status, _, _) => status.into_response(),
-    }
+    pub userdb: Arc<RwLock<DBHandler>>,
 }
