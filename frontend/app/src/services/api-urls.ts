c314227dcc3214b949f488649e670fcc30dd58d4
Konstantinos Paparas <kelsos86@gmail.com>
2025-01-22 12:52:03 +0100

    feat: enables colibri runtime in docker

--- frontend/app/src/services/api-urls.ts
+++ frontend/app/src/services/api-urls.ts
@@ -12,8 +12,20 @@ function getDefaultCoreUrl(): string {
   return '';
 }
 
+function getDefaultColibriUrl(): string {
+  if (import.meta.env.VITE_COLIBRI_URL)
+    return import.meta.env.VITE_COLIBRI_URL as string;
+
+  if (import.meta.env.VITE_PUBLIC_PATH) {
+    const pathname = window.location.pathname;
+    return `${pathname.endsWith('/') ? pathname.slice(0, -1) : pathname}/colibri`;
+  }
+
+  return '';
+}
+
 export const defaultApiUrls: Readonly<ApiUrls> = {
-  colibriApiUrl: import.meta.env.VITE_COLIBRI_URL as string,
+  colibriApiUrl: getDefaultColibriUrl(),
   coreApiUrl: getDefaultCoreUrl(),
 };
 
