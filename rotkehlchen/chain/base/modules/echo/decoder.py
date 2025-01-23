bf21d135f9015bb1865bf4c3151c647ce1371eb3
GUNBOATs <92352063+gunboatsss@users.noreply.github.com>
2025-01-23 20:17:42 +0700

    lint stuff

--- rotkehlchen/chain/base/modules/echo/decoder.py
+++ rotkehlchen/chain/base/modules/echo/decoder.py
@@ -1,45 +1,40 @@
 import logging
-from typing import TYPE_CHECKING, Any
-
 from collections.abc import Callable
+from typing import TYPE_CHECKING, Any
 
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import EvmToken
-from rotkehlchen.chain.ethereum.utils import asset_normalized_value, token_normalized_value_decimals
 from rotkehlchen.chain.base.modules.echo.constants import (
-    FUNDING_CONDUIT,
-    FUNDER_REGISTRY,
-    FUNDER_DEREGISTERED,
+    CPT_ECHO,
     DEAL_ABI,
     DEAL_FUNDED,
-    DEAL_REFUNDED,
-    CPT_ECHO,
     ECHO_CPT_DETAILS,
     FEE_PAID,
-    POOL_REFUNDED
+    FUNDER_DEREGISTERED,
+    FUNDER_REGISTRY,
+    FUNDING_CONDUIT,
+    POOL_REFUNDED,
 )
-from rotkehlchen.chain.evm.decoding.cctp.constants import (
-    USDC_DECIMALS
-)
+from rotkehlchen.chain.ethereum.utils import asset_normalized_value
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
 from rotkehlchen.chain.evm.decoding.structures import (
-    ActionItem,
     DEFAULT_DECODING_OUTPUT,
+    ActionItem,
     DecoderContext,
     DecodingOutput,
 )
-
-from rotkehlchen.chain.evm.structures import EvmTxReceiptLog
 from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
 from rotkehlchen.constants.resolver import evm_address_to_identifier
-from rotkehlchen.history.events.structures.evm_event import EvmEvent
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.logging import RotkehlchenLogsAdapter
-from rotkehlchen.types import ChecksumEvmAddress, EvmTransaction, EvmTokenKind
+from rotkehlchen.types import ChecksumEvmAddress, EvmTokenKind, EvmTransaction
 from rotkehlchen.utils.misc import bytes_to_address
+
 if TYPE_CHECKING:
     from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
     from rotkehlchen.chain.evm.node_inquirer import EvmNodeInquirer
+    from rotkehlchen.chain.evm.structures import EvmTxReceiptLog
+    from rotkehlchen.history.events.structures.evm_event import EvmEvent
     from rotkehlchen.user_messages import MessagesAggregator
 
 logger = logging.getLogger(__name__)
@@ -55,24 +50,24 @@ class EchoDecoder(DecoderInterface):
         super().__init__(
             evm_inquirer=evm_inquirer,
             base_tools=base_tools,
-            msg_aggregator=msg_aggregator
+            msg_aggregator=msg_aggregator,
         )
 
     def _decode_fee_paid(self, context: DecoderContext) -> DecodingOutput:
-        if not context.tx_log.topics[0] == FEE_PAID:
+        if context.tx_log.topics[0] != FEE_PAID:
             return DEFAULT_DECODING_OUTPUT
         if not self.base.is_tracked(bytes_to_address(context.tx_log.topics[1])):
             return DEFAULT_DECODING_OUTPUT
-        
+
         for event in context.decoded_events:
-            if(
+            if (
                 event.sequence_index == context.tx_log.log_index - 1 and
                 event.event_type == HistoryEventType.INFORMATIONAL and
                 event.event_subtype == HistoryEventSubType.APPROVE
             ):
-                fee_amount = token_normalized_value_decimals(
-                    token_amount=int.from_bytes(context.tx_log.data[0:32]),
-                    token_decimals=USDC_DECIMALS # Echo is using USDC for now
+                fee_amount = asset_normalized_value(
+                    amount=int.from_bytes(context.tx_log.data[0:32]),
+                    asset=event.asset,
                 )
                 new_event = self.base.make_event_from_transaction(
                     transaction=context.transaction,
@@ -83,30 +78,33 @@ class EchoDecoder(DecoderInterface):
                     balance=Balance(amount=fee_amount),
                     address=FUNDING_CONDUIT,
                     notes=f'Paid {fee_amount} USDC as part of funding an Echo deal',
-                    counterparty=CPT_ECHO
+                    counterparty=CPT_ECHO,
                 )
                 context.decoded_events.append(new_event)
                 break
         return DEFAULT_DECODING_OUTPUT
-    
+
     def _mark_for_refund(self, context: DecoderContext) -> DecodingOutput:
-        if not context.tx_log.topics[0] == FUNDER_DEREGISTERED:
+        if context.tx_log.topics[0] != FUNDER_DEREGISTERED:
             return DEFAULT_DECODING_OUTPUT
         user_address = bytes_to_address(context.tx_log.topics[2])
         if not self.base.is_tracked(user_address):
             return DEFAULT_DECODING_OUTPUT
-        for _log in context.all_logs:
+        for tx_log in context.all_logs:
             if (
-                _log.topics[0] == POOL_REFUNDED and
-                user_address == bytes_to_address(_log.topics[2])
+                tx_log.topics[0] == POOL_REFUNDED and
+                user_address == bytes_to_address(tx_log.topics[2])
             ):
                 raw_token_address = self.evm_inquirer.call_contract(
-                    contract_address=_log.address,
+                    contract_address=tx_log.address,
                     abi=DEAL_ABI,
-                    method_name='token'
+                    method_name='token',
                 )
-                token = EvmToken(evm_address_to_identifier(raw_token_address, self.evm_inquirer.chain_id, EvmTokenKind.ERC20))
-                token_amount = int.from_bytes(_log.data[:32])
+                token = EvmToken(evm_address_to_identifier(
+                    raw_token_address,
+                    self.evm_inquirer.chain_id,
+                    EvmTokenKind.ERC20))
+                token_amount = int.from_bytes(tx_log.data[:32])
                 amount = asset_normalized_value(token_amount, token)
                 context.action_items.append(ActionItem(
                     action='transform',
@@ -117,13 +115,13 @@ class EchoDecoder(DecoderInterface):
                     asset=token,
                     amount=amount,
                     location_label=user_address,
-                    address=_log.address,
+                    address=tx_log.address,
                     to_counterparty=CPT_ECHO,
-                    to_notes=f'Refund {amount} USDC from {_log.address} on Echo'
+                    to_notes=f'Refund {amount} USDC from {tx_log.address} on Echo',
                 ))
                 break
         return DEFAULT_DECODING_OUTPUT
-        
+
     def _process_funding(
             self,
             transaction: 'EvmTransaction',  # pylint: disable=unused-argument
@@ -134,7 +132,7 @@ class EchoDecoder(DecoderInterface):
         deal_address = None
         user_address = None
         for tx_log in all_logs:
-            if(
+            if (
                 tx_log.topics[0] == DEAL_FUNDED and
                 self.base.is_tracked(user_address := bytes_to_address(tx_log.topics[2]))
             ):
@@ -143,7 +141,7 @@ class EchoDecoder(DecoderInterface):
                 break
 
         for event in decoded_events:
-            if(
+            if (
                 event.event_type == HistoryEventType.SPEND and
                 event.event_subtype == HistoryEventSubType.NONE and
                 event.balance.amount == asset_normalized_value(fund_amount, event.asset)
@@ -153,23 +151,23 @@ class EchoDecoder(DecoderInterface):
                 event.location_label = user_address
                 event.address = deal_address
                 event.counterparty = CPT_ECHO
-                event.notes = f'Fund {event.balance.amount} {event.asset.symbol_or_name()} to {deal_address} on Echo'
+                event.notes = f'Fund {event.balance.amount} {event.asset.symbol_or_name()} to {deal_address} on Echo'  # noqa:E501
                 break
         return decoded_events
 
     @staticmethod
     def counterparties() -> tuple[CounterpartyDetails, ...]:
         return (ECHO_CPT_DETAILS, )
-    
+
     def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
         return {
             FUNDING_CONDUIT: (self._decode_fee_paid,),
             FUNDER_REGISTRY: (self._mark_for_refund,),
         }
-    
+
     def addresses_to_counterparties(self) -> dict['ChecksumEvmAddress', str]:
         return {FUNDING_CONDUIT: CPT_ECHO}
-    
+
     def post_decoding_rules(self) -> dict[str, list[tuple[int, Callable]]]:
         return {
             CPT_ECHO: [(0, self._process_funding)],
--- rotkehlchen/tests/unit/decoders/test_echo.py
+++ rotkehlchen/tests/unit/decoders/test_echo.py
@@ -1,34 +1,26 @@
-from typing import TYPE_CHECKING
 
 import pytest
 
 from rotkehlchen.accounting.structures.balance import Balance
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.chain.base.modules.echo.constants import CPT_ECHO
-from rotkehlchen.chain.base.modules.echo.decoder import (
-    FUNDING_CONDUIT
-)
+from rotkehlchen.chain.base.modules.echo.decoder import FUNDING_CONDUIT
 from rotkehlchen.chain.evm.types import string_to_evm_address
 from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.evm_event import EvmEvent
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.tests.utils.ethereum import get_decoded_events_of_transaction
-from rotkehlchen.types import (Location, TimestampMS, deserialize_evm_tx_hash)
-
-if TYPE_CHECKING:
-    from rotkehlchen.chain.base.node_inquirer import BaseInquirer
-    from rotkehlchen.types import ChecksumEvmAddress
+from rotkehlchen.types import Location, TimestampMS, deserialize_evm_tx_hash
 
 USDC_TOKEN = Asset('eip155:8453/erc20:0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913')
+
 
 @pytest.mark.vcr(filter_query_parameters=['apikey'])
 @pytest.mark.parametrize('base_accounts', [['0xB196cd166602BfcD879Bf12925e348689F6881B8']])
 def test_echo_fund(base_inquirer, base_accounts):
     evmhash = deserialize_evm_tx_hash('0x6f16132d373dbba06259d04bd1e83771343c7ac17ac0a4e74fad645ed9d809de')  # noqa: E501
     events, _ = get_decoded_events_of_transaction(evm_inquirer=base_inquirer, tx_hash=evmhash)
-    timestamp, fund_amount, deal_address = TimestampMS(1736535363000), '5887', string_to_evm_address('0xeC14eD2470FaCe0a60a41c945Cc586aFbcc3273C') # noqa: E501
-    for event in events:
-        print(event.serialize())
+    timestamp, fund_amount, deal_address = TimestampMS(1736535363000), '5887', string_to_evm_address('0xeC14eD2470FaCe0a60a41c945Cc586aFbcc3273C')  # noqa: E501
     assert events == [
         EvmEvent(
             tx_hash=evmhash,
@@ -40,8 +32,8 @@ def test_echo_fund(base_inquirer, base_accounts):
             asset=USDC_TOKEN,
             balance=Balance(amount=FVal(5887)),
             location_label=base_accounts[0],
-            notes=f'Set USDC spending approval of {base_accounts[0]} by {FUNDING_CONDUIT} to {fund_amount}',
-            address=FUNDING_CONDUIT
+            notes=f'Set USDC spending approval of {base_accounts[0]} by {FUNDING_CONDUIT} to {fund_amount}',  # noqa:E501
+            address=FUNDING_CONDUIT,
         ),
         EvmEvent(
             tx_hash=evmhash,
@@ -54,8 +46,8 @@ def test_echo_fund(base_inquirer, base_accounts):
             balance=Balance(amount=FVal(0)),
             location_label=None,
             counterparty=CPT_ECHO,
-            notes=f'Paid 0 USDC as part of funding an Echo deal',
-            address=FUNDING_CONDUIT
+            notes='Paid 0 USDC as part of funding an Echo deal',
+            address=FUNDING_CONDUIT,
         ),
         EvmEvent(
             tx_hash=evmhash,
@@ -69,16 +61,17 @@ def test_echo_fund(base_inquirer, base_accounts):
             location_label=base_accounts[0],
             counterparty=CPT_ECHO,
             notes=f'Fund {fund_amount} USDC to {deal_address} on Echo',
-            address=deal_address
+            address=deal_address,
         ),
     ]
+
 
 @pytest.mark.vcr(filter_query_parameters=['apikey'])
 @pytest.mark.parametrize('base_accounts', [['0x867eaD8851912894a20fCFe55AFefB33BD30fBc6']])
 def test_echo_refund(base_inquirer, base_accounts):
     evmhash = deserialize_evm_tx_hash('0xfaeea0e01cecda7849efb3f27aa216c567366e10237b717a75587ff05ad7a7c6')  # noqa: E501
     events, _ = get_decoded_events_of_transaction(evm_inquirer=base_inquirer, tx_hash=evmhash)
-    timestamp, deal_address = TimestampMS(1730227259000), string_to_evm_address('0x241F8222D1c80a4FDc25f9FC00181Eec770131A2')
+    timestamp, deal_address = TimestampMS(1730227259000), string_to_evm_address('0x241F8222D1c80a4FDc25f9FC00181Eec770131A2')  # noqa: E501
     assert events == [
         EvmEvent(
             tx_hash=evmhash,
@@ -92,7 +85,6 @@ def test_echo_refund(base_inquirer, base_accounts):
             location_label=base_accounts[0],
             counterparty=CPT_ECHO,
             notes=f'Refund 2000 USDC from {deal_address} on Echo',
-            address=deal_address
-        )
+            address=deal_address,
+        ),
     ]
-
