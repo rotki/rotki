22caaf6dd0813df6e8bf4c923630f0eb3013c033
lukicenturi <lukicenturi@gmail.com>
2025-01-28 09:45:14 +0700

    fix: conditional for the show networth on tray

--- frontend/app/electron/main/app-config.ts
+++ frontend/app/electron/main/app-config.ts
@@ -2,6 +2,7 @@ import type { ApiUrls } from '@shared/ipc';
 
 export interface AppConfig {
   readonly isDev: boolean;
+  readonly isLinux: boolean;
   readonly isMac: boolean;
   readonly urls: ApiUrls;
   readonly ports: {
