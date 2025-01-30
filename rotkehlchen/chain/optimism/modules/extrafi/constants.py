7bafbc6f5dd20e204c152474ae38d683a54411e0
Lefteris Karapetsas <lefteris@refu.co>
2025-01-30 23:41:59 +0100

    Extrafi optimism rewards sent to the address will now be recognized
    Signed-off-by: Lefteris Karapetsas <lefteris@refu.co>

---
+++ rotkehlchen/chain/optimism/modules/extrafi/constants.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+EXTRAFI_COMMUNITY_FUND: Final = string_to_evm_address('0x89F0885DA2553232aeEf201692F8C97E24715c83')
