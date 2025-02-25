13f0558cf2d66a2fabb13147e3dc98c38588d705
Fallengirl <155266340+Fallengirl@users.noreply.github.com>
2025-02-25 18:10:58 +0100

    Update utils.ts

--- frontend/app/src/services/utils.ts
+++ frontend/app/src/services/utils.ts
@@ -1,5 +1,5 @@
 import { ApiValidationError } from '@/types/api/errors';
-import { snakeCaseTransformer } from '@/services/axios-tranformers';
+import { snakeCaseTransformer } from '@/services/axios-transformers';
 import type { ActionResult } from '@rotki/common';
 import type { AxiosInstance, AxiosResponse, ParamsSerializerOptions } from 'axios';
 import type { PendingTask } from '@/types/task';
