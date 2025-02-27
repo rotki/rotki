361fc7020466997cf0de3f95f7f856e09c253a11
Isaac Izard <loopyloopy811@gmail.com>
2025-02-27 17:06:57 +0300

    fix: update broken link psl.ts

--- frontend/app/src/data/psl.ts
+++ frontend/app/src/data/psl.ts
@@ -1,5 +1,5 @@
 // Public suffixes list
-// Source: https://github.com/lupomontero/psl/blob/main/data/rules.json
+// Source: https://github.com/lupomontero/psl/blob/main/data/rules.js
 import psl from '@/data/psl.json';
 
 export const pslSuffixes: Set<string> = new Set(psl.psl);
