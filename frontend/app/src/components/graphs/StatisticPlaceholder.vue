c89e75ef2137f78e5e6fcc5f54a07e21261402b9
lukicenturi <lukicenturi@gmail.com>
2025-02-27 17:16:19 +0700

    fix: update statistic placeholder screenshot

--- frontend/app/src/components/graphs/StatisticPlaceholder.vue
+++ frontend/app/src/components/graphs/StatisticPlaceholder.vue
@@ -3,6 +3,11 @@ import AppImage from '@/components/common/AppImage.vue';
 import GetPremiumPlaceholder from '@/components/graphs/GetPremiumPlaceholder.vue';
 
 const { t } = useI18n();
+const { isMdAndDown } = useBreakpoint();
+
+function getFullPath(fileName: string) {
+  return `./assets/images/placeholder/${fileName}`;
+}
 </script>
 
 <template>
@@ -11,11 +16,17 @@ const { t } = useI18n();
       class="absolute z-1 pt-40 top-0 left-1/2 transform -translate-x-1/2"
       :title="t('premium_settings.chart_limit.statistic')"
     />
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <AppImage src="./assets/images/placeholder/net_value_graph_placeholder.png" />
-      <AppImage src="./assets/images/placeholder/asset_amount_and_value_graph.png" />
-      <AppImage src="./assets/images/placeholder/asset_distribution_by_location.png" />
-      <AppImage src="./assets/images/placeholder/asset_distribution_by_asset.png" />
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2">
+      <AppImage
+        class="lg:col-span-2"
+        :src="isMdAndDown ? getFullPath('net_value_graph_placeholder_small.png') : getFullPath('net_value_graph_placeholder.png')"
+      />
+      <AppImage
+        class="lg:col-span-2"
+        :src="isMdAndDown ? getFullPath('asset_amount_and_value_graph_small.png') : getFullPath('asset_amount_and_value_graph.png')"
+      />
+      <AppImage :src="getFullPath('asset_distribution_by_location.png')" />
+      <AppImage :src="getFullPath('asset_distribution_by_asset.png')" />
     </div>
   </div>
 </template>
