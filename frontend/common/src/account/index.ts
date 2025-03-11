5629cf69dfe43663a0caca3bca3646fa28b5f325
Konstantinos Paparas <kelsos86@gmail.com>
2025-03-11 18:19:09 +0100

    refactor: remove or hide unused exports in common

--- frontend/common/src/account/index.ts
+++ frontend/common/src/account/index.ts
@@ -2,13 +2,3 @@ export interface Account {
   readonly chain: string;
   readonly address: string;
 }
-
-export interface GeneralAccount {
-  readonly data: { address: string };
-  readonly tags?: string[];
-  readonly label?: string;
-  readonly chain: string;
-  readonly nativeAsset: string;
-  readonly groupId?: string;
-  readonly groupHeader?: boolean;
-}
--- frontend/common/src/utils/index.ts
+++ frontend/common/src/utils/index.ts
@@ -1,9 +1,5 @@
 export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 
-export type Properties<TObj, TResult> = {
-  [K in keyof TObj]: TObj[K] extends TResult ? K : never;
-}[keyof TObj];
-
 export type Nullable<T> = T | null;
 
 export type SemiPartial<T, Ps extends keyof T> = Pick<T, Ps> & Partial<T>;
@@ -16,10 +12,6 @@ export type Awaitable = VoidFunction | PromiseFunction;
 
 export type MaybePromise<T> = T | Promise<T>;
 
-export type Diff<T, U> = T extends U ? never : T;
-
 export function onlyIfTruthy<T>(value: T): T | undefined {
   return value || undefined;
 }
-
-export type AddressIndexed<T> = Readonly<Record<string, T>>;
