0f5efc96e6d5322977316d7d7931b95065ac1b01
Konstantinos Paparas <kelsos86@gmail.com>
2025-03-25 14:30:35 +0100

    feat: add initial model support for swap events (#9680)

---
+++ frontend/app/src/components/history/events/forms/SwapEventForm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { EventData, SwapEvent } from '@/types/history/events';
+
+withDefaults(defineProps<{
+  data?: EventData<SwapEvent>;
+}>(), {
+  data: undefined,
+});
+</script>
+
+<template>
+  <div />
+</template>
