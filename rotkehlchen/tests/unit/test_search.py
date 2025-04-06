d4894034ab58d63d9a8ac2b59470d4222106384e
Lefteris Karapetsas <lefteris@refu.co>
2025-04-06 18:31:00 +0200

    Bump ruff to 0.11.4

--- rotkehlchen/tests/unit/test_search.py
+++ rotkehlchen/tests/unit/test_search.py
@@ -57,7 +57,7 @@ def test_db_persistence_after_search(messages_aggregator):
     - Add a manual price for the custom asset
     - Crash the application
     - Check that the manual price is still in the global database"""
-    rotki_process = subprocess.Popen(  # noqa: S603
+    rotki_process = subprocess.Popen(
         [  # noqa: S607  # is only used to execute rotki code here
             'python',
             '-m'
