c89e87ce404c082ea0f952fea5705a12f47c7ef0
Lefteris Karapetsas <lefteris@refu.co>
2025-04-12 11:39:28 +0200

    Decode impact donation minting transactions for Gitcoin

--- rotkehlchen/chain/arbitrum_one/modules/gitcoin/decoder.py
+++ rotkehlchen/chain/arbitrum_one/modules/gitcoin/decoder.py
@@ -1,12 +1,26 @@
-from typing import TYPE_CHECKING
+import logging
+from typing import TYPE_CHECKING, Any, Final
 
+from rotkehlchen.chain.evm.decoding.constants import CPT_GITCOIN
 from rotkehlchen.chain.evm.decoding.gitcoinv2.decoder import GitcoinV2CommonDecoder
+from rotkehlchen.chain.evm.decoding.structures import DEFAULT_DECODING_OUTPUT, DecodingOutput
 from rotkehlchen.chain.evm.types import string_to_evm_address
+from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
+from rotkehlchen.logging import RotkehlchenLogsAdapter
+from rotkehlchen.types import ChecksumEvmAddress
+from rotkehlchen.utils.misc import bytes_to_address, from_wei
 
 if TYPE_CHECKING:
     from rotkehlchen.chain.arbitrum_one.node_inquirer import ArbitrumOneInquirer
     from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.chain.evm.decoding.structures import DecoderContext
     from rotkehlchen.user_messages import MessagesAggregator
+
+logger = logging.getLogger(__name__)
+log = RotkehlchenLogsAdapter(logger)
+
+ON_ATTESTED: Final = b';\xdb\xa5P\x8aX\xb2%\x9e\xce\x9bEU\x84;\x90W\xcb+\xfa\xb8\xf3\x84\x0bm\xf3P\xda`k(q'  # noqa: E501
 
 
 class GitcoinDecoder(GitcoinV2CommonDecoder):
@@ -45,3 +59,31 @@ class GitcoinDecoder(GitcoinV2CommonDecoder):
             ],
             retro_funding_strategy_addresses=[string_to_evm_address('0x2Caa214E2de4b05A9E0E1a1cCfDb3c673a28acCf')],
         )
+
+    def _decode_donation_impact_minting(self, context: 'DecoderContext') -> DecodingOutput:
+        if context.tx_log.topics[0] != ON_ATTESTED:
+            return DEFAULT_DECODING_OUTPUT
+
+        if not self.base.is_tracked(recipient := bytes_to_address(context.tx_log.topics[2])):
+            return DEFAULT_DECODING_OUTPUT
+
+        amount = from_wei(int.from_bytes(context.tx_log.data[:32]))
+        for event in context.decoded_events:
+            if event.event_type == HistoryEventType.SPEND and event.event_subtype == HistoryEventSubType.NONE and event.asset == A_ETH and event.location_label == recipient and event.amount == amount:  # noqa: E501
+                event.counterparty = CPT_GITCOIN
+                event.event_type = HistoryEventType.MINT
+                event.event_subtype = HistoryEventSubType.FEE
+                event.notes = f'Mint donation impact attestation 0x{context.tx_log.topics[1].hex()} for a fee of {amount} ETH'  # noqa: E501
+                break
+
+        else:
+            log.error(f'In minting donation impact could not find the ETH fee transfer in {context.transaction}')  # noqa: E501
+
+        return DEFAULT_DECODING_OUTPUT
+
+    # -- DecoderInterface methods
+
+    def addresses_to_decoders(self) -> dict[ChecksumEvmAddress, tuple[Any, ...]]:
+        return super().addresses_to_decoders() | {
+            string_to_evm_address('0x2ce7E4cB5Edb140A9327e67De85463186E757C8f'): (self._decode_donation_impact_minting,),  # noqa: E501
+        }
