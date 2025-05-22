baeec9a19706349ba88e2e3d9c18539935ac5747
Konstantinos Paparas <kelsos86@gmail.com>
2025-05-22 17:27:58 +0200

    feat: remove pwa related dependencies from frontend

--- frontend/app/src/env.d.ts
+++ frontend/app/src/env.d.ts
@@ -1,15 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 declare const __APP_VERSION__: string;
-
-// Importing the client rules in types seems to create build errors.
-declare module 'virtual:pwa-register' {
-  export interface RegisterSWOptions {
-    immediate?: boolean;
-    onNeedRefresh?: () => void;
-    onOfflineReady?: () => void;
-    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisterError?: (error: any) => void;
-  }
-
-  export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
-}
