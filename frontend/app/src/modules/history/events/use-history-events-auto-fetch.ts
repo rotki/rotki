e35de49163c4ce2f4ae7855e52d7cd693956c67a
Konstantinos Paparas <kelsos86@gmail.com>
2025-05-27 17:02:16 +0200

    chore: increase history event auto-refresh to 60 seconds (#10031)

--- frontend/app/src/modules/history/events/use-history-events-auto-fetch.ts
+++ frontend/app/src/modules/history/events/use-history-events-auto-fetch.ts
@@ -4,8 +4,11 @@ import { type Ref, ref } from 'vue';
 /**
  * The refresh interval between the calls to fetch events and data.
  * It should be bigger than the request timeout to avoid queuing to many requests.
+ *
+ * After measurement the query to fetch the events page can take up to 20 seconds,
+ * so we set the refresh interval to 60 seconds to avoid queuing too many requests.
  */
-const REFRESH_INTERVAL = 35_000;
+const REFRESH_INTERVAL = 60_000;
 
 export function useHistoryEventsAutoFetch(shouldFetch: Ref<boolean>, fetchFunction: () => Promise<void>): void {
   const isFetching = ref(false);
