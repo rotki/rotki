f110695f51bea84d69ca04c3d3f209548ee40b44
Isaac Adewumi <ienioladewumi@gmail.com>
2025-05-29 20:45:23 +0100

    prevent odos swaps from being misclassified as curve events

--- rotkehlchen/chain/polygon_pos/modules/curve/decoder.py
+++ rotkehlchen/chain/polygon_pos/modules/curve/decoder.py
@@ -6,6 +6,7 @@ from rotkehlchen.chain.evm.decoding.curve.constants import (
     DEPOSIT_AND_STAKE_ZAP,
 )
 from rotkehlchen.chain.evm.decoding.curve.decoder import CurveCommonDecoder
+from rotkehlchen.chain.evm.types import string_to_evm_address
 from rotkehlchen.constants.assets import A_POLYGON_POS_MATIC
 
 from .constants import AAVE_POOLS
@@ -30,7 +31,15 @@ class CurveDecoder(CurveCommonDecoder):
             msg_aggregator=msg_aggregator,
             native_currency=A_POLYGON_POS_MATIC,
             aave_pools=AAVE_POOLS,
-            curve_deposit_contracts={DEPOSIT_AND_STAKE_ZAP},
+            curve_deposit_contracts={
+                DEPOSIT_AND_STAKE_ZAP,
+                string_to_evm_address('0x3FCD5De6A9fC8A99995c406c77DDa3eD7E406f81'),  # ATriCrypto Zap  # noqa: E501
+                string_to_evm_address('0x1d8b86e3D88cDb2d34688e87E72F388Cb541B7C8'),  # ATriCrypto3 Zap  # noqa: E501
+                string_to_evm_address('0x225FB4176f0E20CDb66b4a3DF70CA3063281E855'),  # EURTUSD Zap
+                string_to_evm_address('0x4DF7eF55E99a56851187822d96B4E17D98A47DeD'),  # EURs Zap
+                string_to_evm_address('0x5ab5C56B9db92Ba45a0B46a207286cD83C15C939'),  # MetaUSD Zap
+                string_to_evm_address('0xE2e6DC1708337A6e59f227921db08F21e3394723'),  # MetaBTC Zap
+            },
             curve_swap_routers={CURVE_SWAP_ROUTER_NG},
             crv_minter_addresses={CHILD_LIQUIDITY_GAUGE_FACTORY},
         )
