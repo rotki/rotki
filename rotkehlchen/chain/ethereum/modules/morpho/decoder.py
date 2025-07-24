6f3fc037b27cb800bb4baa90f2853d26b1014a84
Lefteris Karapetsas <lefteris@refu.co>
2025-07-24 23:22:00 +0200

    Add a missing morpho bundler for ethereum mainnet

--- rotkehlchen/chain/ethereum/modules/morpho/decoder.py
+++ rotkehlchen/chain/ethereum/modules/morpho/decoder.py
@@ -23,6 +23,7 @@ class MorphoDecoder(MorphoCommonDecoder):
             base_tools=base_tools,
             msg_aggregator=msg_aggregator,
             bundlers={
+                string_to_evm_address('0x6566194141eefa99Af43Bb5Aa71460Ca2Dc90245'),  # EthereumBundlerV3  # noqa: E501
                 string_to_evm_address('0x4095F064B8d3c3548A3bebfd0Bbfd04750E30077'),  # EthereumBundlerV2  # noqa: E501
                 string_to_evm_address('0xa7995f71aa11525DB02Fc2473c37Dee5dbf55107'),  # EthereumBundler  # noqa: E501
                 string_to_evm_address('0xb3dCc75DB379925edFd3007511A8CE0cB4aa8e76'),  # AaveV2MigrationBundler  # noqa: E501
