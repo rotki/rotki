aca98beb90ef4f26e192d5e7453ad4cc789c6e5c
Konstantinos Paparas <kelsos86@gmail.com>
2025-07-30 17:02:25 +0200

    refactor: modularize wallet connection logic, add status visualization

---
+++ frontend/app/src/modules/onchain/wallet-bridge/ProviderIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  metadata?: {
+    icon: string;
+    name: string;
+  };
+  size?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  metadata: undefined,
+  size: '16px',
+});
+</script>
+
+<template>
+  <img
+    v-if="metadata"
+    :src="metadata.icon"
+    :alt="metadata.name"
+    :style="{ width: size, height: size }"
+  />
+</template>
