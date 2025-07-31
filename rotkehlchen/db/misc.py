e207256456b1e36c6adc7db633b75a2f984a209d
Yabir Benchakhtir <git@yabirgb.com>
2025-07-31 18:14:31 +0200

    Log version of special dependencies

--- rotkehlchen/db/misc.py
+++ rotkehlchen/db/misc.py
@@ -7,16 +7,18 @@ import re
 from pysqlcipher3 import dbapi2 as sqlcipher
 
 
-def detect_sqlcipher_version() -> int:
-    """Returns the major part of the version of the system's sqlcipher package"""
+def get_sqlcipher_version_string() -> str:
+    """Retrieve SQLCipher version used by the app"""
     conn = sqlcipher.connect(':memory:')  # pylint: disable=no-member
-    query = conn.execute('PRAGMA cipher_version;')
-    version = query.fetchall()[0][0]
-
-    match = re.search(r'(\d+).(\d+).(\d+)', version)
-    if not match:
-        raise ValueError(f'Could not process the version returned by sqlcipher: {version}')
-
-    sqlcipher_version = int(match.group(1))
+    version = conn.execute('PRAGMA cipher_version;').fetchone()[0]
     conn.close()
-    return sqlcipher_version
+    return version
+
+
+def detect_sqlcipher_version() -> int:
+    """Returns the major part of the version for SQLCipher"""
+    version = get_sqlcipher_version_string()
+    if not (match := re.search(r'(\d+).(\d+).(\d+)', version)):
+        raise ValueError(f'Could not process the version returned by SQLCipher: {version}')
+
+    return int(match.group(1))
