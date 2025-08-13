f9bf52ecb5bed4eaf7e338cc64b25111f1987195
lukicenturi <lukicenturi@gmail.com>
2025-08-13 23:28:47 +0700

    fix: event analysis zero timestamp

--- frontend/app/src/pages/statistics/history-events/index.vue
+++ frontend/app/src/pages/statistics/history-events/index.vue
@@ -16,7 +16,7 @@ const premium = usePremium();
 
 <template>
   <div class="container">
-    <RuiCard class="max-w-[820px] mx-auto">
+    <RuiCard class="max-w-[920px] mx-auto">
       <WrappedContainer v-if="premium" />
       <WrappedContainerPlaceholder v-else />
     </RuiCard>
