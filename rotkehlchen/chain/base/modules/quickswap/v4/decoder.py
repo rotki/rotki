bee374eb0359226082020fc76d05071c0c6b4041
Nicholas Yoder <nicholasyoder@proton.me>
2025-09-01 10:51:38 -0400

    Add quickswap v4 swap decoding

---
+++ rotkehlchen/chain/base/modules/quickswap/v4/decoder.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.quickswap.v4.decoder import Quickswapv4CommonDecoder
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.base.node_inquirer import BaseInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class Quickswapv4Decoder(Quickswapv4CommonDecoder):
+
+    def __init__(
+            self,
+            base_inquirer: 'BaseInquirer',
+            base_tools: 'BaseDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=base_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            swap_router=string_to_evm_address('0xe6c9bb24ddB4aE5c6632dbE0DE14e3E474c6Cb04'),
+        )
--- rotkehlchen/chain/evm/decoding/uniswap/v4/decoder.py
+++ rotkehlchen/chain/evm/decoding/uniswap/v4/decoder.py
@@ -1,13 +1,10 @@
 import logging
-from collections import defaultdict
 from collections.abc import Callable
 from typing import TYPE_CHECKING, Any
 
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.assets.utils import CHAIN_TO_WRAPPED_TOKEN
-from rotkehlchen.chain.ethereum.utils import asset_normalized_value, asset_raw_value
 from rotkehlchen.chain.evm.constants import ZERO_ADDRESS
-from rotkehlchen.chain.evm.decoding.constants import ERC20_OR_ERC721_TRANSFER
 from rotkehlchen.chain.evm.decoding.interfaces import DecoderInterface
 from rotkehlchen.chain.evm.decoding.structures import (
     DEFAULT_DECODING_OUTPUT,
@@ -19,18 +16,15 @@ from rotkehlchen.chain.evm.decoding.types import CounterpartyDetails
 from rotkehlchen.chain.evm.decoding.uniswap.constants import CPT_UNISWAP_V4, UNISWAP_ICON
 from rotkehlchen.chain.evm.decoding.uniswap.utils import (
     decode_uniswap_v3_like_position_create_or_exit,
-    get_uniswap_swap_amounts,
 )
 from rotkehlchen.chain.evm.decoding.uniswap.v3.constants import SWAP_SIGNATURE as V3_SWAP_TOPIC
-from rotkehlchen.chain.evm.decoding.utils import maybe_reshuffle_events
-from rotkehlchen.errors.asset import WrongAssetType
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.serialization.deserialize import deserialize_evm_address
 from rotkehlchen.types import ChecksumEvmAddress
-from rotkehlchen.utils.misc import bytes_to_address
 
 from .constants import CPT_UNISWAP_V4_LP, MODIFY_LIQUIDITY, POSITION_MANAGER_ABI, V4_SWAP_TOPIC
+from .utils import decode_uniswap_v4_like_swaps
 
 if TYPE_CHECKING:
     from rotkehlchen.chain.evm.decoding.base import BaseDecoderTools
@@ -157,117 +151,16 @@ class Uniswapv4CommonDecoder(DecoderInterface):
             all_logs: list['EvmTxReceiptLog'],
     ) -> list['EvmEvent']:
         """Decode swaps routed through the Uniswap V4 universal router."""
-        amounts_received, amounts_sent, pools_used, possible_fees = set(), set(), set(), defaultdict(list)  # noqa: E501
-        # Since tokens may be swapped multiple times before reaching the desired token, we must
-        # check the amounts from multiple swap logs if present.
-        for tx_log in all_logs:
-            if tx_log.topics[0] in (V4_SWAP_TOPIC, V3_SWAP_TOPIC):
-                amount_received, amount_sent = get_uniswap_swap_amounts(tx_log=tx_log)
-                amounts_received.add(amount_received)
-                amounts_sent.add(amount_sent)
-                pools_used.add(tx_log.address)
-            elif (
-                tx_log.topics[0] == ERC20_OR_ERC721_TRANSFER and
-                ((from_addr := bytes_to_address(tx_log.topics[1])) == self.universal_router or
-                from_addr in pools_used) and
-                not self.base.is_tracked(bytes_to_address(tx_log.topics[2]))
-            ):
-                possible_fees[tx_log.address].append(int.from_bytes(tx_log.data[:32]))
-
-        if len(amounts_received) == 0:
-            log.error(f'Could not find Uniswap swap log in {transaction}')
-            return decoded_events
-
-        spend_event, receive_event, fee_event = None, None, None
-        for event in decoded_events:
-            if not (event.address == self.universal_router or event.address in pools_used):
-                continue
-
-            if (
-                ((
-                    event.event_type == HistoryEventType.SPEND and
-                    event.event_subtype == HistoryEventSubType.NONE
-                ) or (
-                    event.event_type == HistoryEventType.TRADE and
-                    event.event_subtype == HistoryEventSubType.SPEND
-                )) and
-                asset_raw_value(
-                    amount=event.amount,
-                    asset=(resolved_asset := event.asset.resolve_to_crypto_asset()),
-                ) in amounts_sent
-            ):
-                event.counterparty = CPT_UNISWAP_V4
-                event.event_type = HistoryEventType.TRADE
-                event.event_subtype = HistoryEventSubType.SPEND
-                event.notes = f'Swap {event.amount} {resolved_asset.symbol} in Uniswap V4'
-                spend_event = event
-            elif ((
-                event.event_type == HistoryEventType.RECEIVE and
-                event.event_subtype == HistoryEventSubType.NONE
-            ) or (
-                event.event_type == HistoryEventType.TRADE and
-                event.event_subtype == HistoryEventSubType.RECEIVE
-            )):
-                if (event_raw_amount := asset_raw_value(
-                    amount=event.amount,
-                    asset=(resolved_asset := event.asset.resolve_to_crypto_asset()),
-                )) not in amounts_received:
-                    # In some cases a fee is deducted from the receive amount before it is sent to
-                    # the user. Check if this receive event actually has the right amount to match
-                    # this swap after adding the fee back on.
-                    try:  # First, get the received token's address
-                        fee_token_address = (  # Fee will be in the wrapped version of the native currency.  # noqa: E501
-                            event.asset if event.asset != self.native_currency
-                            else self.wrapped_native_currency
-                        ).resolve_to_evm_token().evm_address
-                    except WrongAssetType:
-                        log.error(
-                            f'Uniswap V4 swap receive asset {event.asset} is not the native '
-                            f'currency or an EVM token in {transaction}. Should not happen.')
-                        continue
-
-                    # Match against the amounts for possible fee transfers of this token
-                    for amount in possible_fees[fee_token_address]:
-                        if event_raw_amount + amount in amounts_received:
-                            fee_amount = asset_normalized_value(
-                                asset=resolved_asset,
-                                amount=amount,
-                            )
-                            break
-                    else:
-                        continue  # this receive is not related to this swap
-
-                    event.amount += fee_amount
-                    fee_event = self.base.make_event_next_index(
-                        tx_hash=event.tx_hash,
-                        timestamp=transaction.timestamp,
-                        event_type=HistoryEventType.TRADE,
-                        event_subtype=HistoryEventSubType.FEE,
-                        asset=resolved_asset,
-                        amount=fee_amount,
-                        location_label=event.location_label,
-                        notes=f'Spend {fee_amount} {resolved_asset.symbol} as a Uniswap V4 fee',
-                        counterparty=CPT_UNISWAP_V4,
-                        address=event.address,
-                    )
-
-                event.counterparty = CPT_UNISWAP_V4
-                event.event_type = HistoryEventType.TRADE
-                event.event_subtype = HistoryEventSubType.RECEIVE
-                event.notes = f'Receive {event.amount} {resolved_asset.symbol} after a swap in Uniswap V4'  # noqa: E501
-                receive_event = event
-
-        if spend_event is None or receive_event is None:
-            log.error(f'Failed to find both out and in events for Uniswap V4 swap {transaction}')
-            return decoded_events
-
-        ordered_events = [spend_event, receive_event]
-        if fee_event is not None:
-            decoded_events.append(fee_event)
-            ordered_events.append(fee_event)
-
-        maybe_reshuffle_events(ordered_events=ordered_events, events_list=decoded_events)
-        return decoded_events
+        return decode_uniswap_v4_like_swaps(
+            transaction=transaction,
+            decoded_events=decoded_events,
+            all_logs=all_logs,
+            base_tools=self.base,
+            swap_topics=(V4_SWAP_TOPIC, V3_SWAP_TOPIC),
+            counterparty=CPT_UNISWAP_V4,
+            router_address=self.universal_router,
+            wrapped_native_currency=self.wrapped_native_currency,
+        )
 
     def _lp_post_decoding(
             self,
