49d04401d520fffc994fae6c792134e535da550e
Isaac Adewumi <ienioladewumi@gmail.com>
2025-09-12 22:51:35 +0100

    add more 0x settler addresses

--- rotkehlchen/chain/arbitrum_one/modules/zerox/constants.py
+++ rotkehlchen/chain/arbitrum_one/modules/zerox/constants.py
@@ -10,4 +10,5 @@ SETTLER_ROUTERS: Final = {
     string_to_evm_address('0xf3E01012Ce60BB95AE294D5b24a9Fc3af245b53b'),  # V1.7 commit: 0xa6f39ee20f0c4dfe1265f5d203dfc4f3f05ca003000000000000000000000000  # noqa: E501
     string_to_evm_address('0x6dB4d3B89b06d3C8Bd2074Ee1F1B440baf122eAd'),  # V1.8 commit: 0xffc129424fbe525c124e52cff5225afbfb610534000000000000000000000000  # noqa: E501
     string_to_evm_address('0xB254ee265261675528bdDb0796741c0C65a4C158'),  # V1.9 commit: 0xed3a62a794e1598537d99e3f7d44d08f2ff2cac8000000000000000000000000  # noqa: E501
+    string_to_evm_address('0x246475E1F63d8e26D6f4fB6029033da8831eD396'),  # commit: 0x2ddbef2c14ee27f3dafcfdb3d376dad949d7a86e000000000000000000000000  # noqa: E501
 }
