2e10131cbc15c1a9f0c578e375d4cb66c896b1fd
Yabir Benchakhtir <git@yabirgb.com>
2025-10-24 12:04:48 +0200

    Notify users about outdated rust versions

---
+++ colibri/build.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+use rustc_version::{version, Version};
+
+/// Warn users about outdated rust versions
+fn main() {
+    const MIN_RUST: &str = "1.86.0";
+    let min_version = Version::parse(MIN_RUST).expect("invalid minimum Rust version");
+    let current_version = version().expect("failed to read rustc version");
+
+    if current_version < min_version {
+        eprintln!(
+            "\x1b[1;33mwarning:\x1b[0m Minimum required version is {} but found {}. Make sure you are running the latest version of Rust. If you have installed Rust using rustup, simply run rustup update.",
+            min_version,
+            current_version,
+        );
+        io::stderr().flush().unwrap();
+        std::process::exit(1);
+    }
+}
