ccafbfa88461c3e3a43fe7851f3498436cb5cf4f
Nicholas Yoder <nicholasyoder@proton.me>
2025-12-03 16:24:00 -0500

    Properly handle routescan per minute/day rate limits

--- rotkehlchen/externalapis/routescan.py
+++ rotkehlchen/externalapis/routescan.py
@@ -1,7 +1,10 @@
 import logging
 from typing import TYPE_CHECKING, Final
 
-from rotkehlchen.errors.misc import ChainNotSupported
+import gevent
+from requests import Response
+
+from rotkehlchen.errors.misc import ChainNotSupported, RemoteError
 from rotkehlchen.externalapis.etherscan_like import EtherscanLikeApi
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.types import SUPPORTED_CHAIN_IDS, ApiKey, ChainID, ExternalService
@@ -52,3 +55,45 @@ class Routescan(EtherscanLikeApi):
     ) -> dict[str, str]:
         """Routescan doesn't need chainid in params since it's in the URL."""
         return {'module': module, 'action': action, 'apikey': api_key}
+
+    def _handle_rate_limit(
+            self,
+            response: Response,
+            current_backoff: int,
+            backoff_limit: int,
+            chain_id: ChainID,
+    ) -> int:
+        """Handles rate limit responses from routescan. Returns the new backoff time.
+        May raise RemoteError if the time until reset for the rate-limited time period is greater
+        than the backoff limit, or if the response is not as expected.
+        """
+        for remaining_key, reset_key, display_name in (
+            ('x-ratelimit-rpd-remaining', 'x-ratelimit-rpd-reset', 'Daily'),  # day
+            ('x-ratelimit-rpm-remaining', 'x-ratelimit-rpm-reset', 'Per minute'),  # minute
+        ):
+            try:
+                if int(response.headers[remaining_key]) > 0:
+                    continue  # there are still requests remaining for this time period.
+
+                time_until_reset = int(response.headers[reset_key])
+            except (KeyError, ValueError, TypeError):
+                continue
+
+            if time_until_reset >= backoff_limit:
+                raise RemoteError(
+                    f'{display_name} rate limit reached for {self.name} on '
+                    f'{chain_id.name.capitalize()} with {time_until_reset} seconds until reset '
+                    f'while max backoff is {backoff_limit} seconds.',
+                )
+            else:
+                gevent.sleep(time_until_reset)
+                return time_until_reset
+
+        # If the ratelimit headers are missing or still have requests remaining (shouldn't happen),
+        # log the response info and fail with a RemoteError.
+        msg = f'Unexpected rate limit response from {self.name} on {chain_id.name.capitalize()}.'
+        log.error(
+            f'{msg} Expected rate limit headers with no remaining requests but got: '
+            f'{response.headers}. Response body: {response.text}.',
+        )
+        raise RemoteError(f'{msg} Check logs for details.')
