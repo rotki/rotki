2b2c7f0958cda587e6d12193190169f286a76944
Nicholas Yoder <nicholasyoder@proton.me>
2026-02-23 15:12:12 -0500

    Add WOOFi supercharger deposit/withdraw decoding

---
+++ rotkehlchen/chain/base/modules/woo_fi/decoder.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.woo_fi.decoder import WooFiCommonDecoder, WooVaultInfo
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.base.node_inquirer import BaseInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseEvmDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class WooFiDecoder(WooFiCommonDecoder):
+
+    def __init__(
+            self,
+            base_inquirer: 'BaseInquirer',
+            base_tools: 'BaseEvmDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=base_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            earn_vaults=[WooVaultInfo(  # USDC
+                token=string_to_evm_address('0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'),
+                supercharger=string_to_evm_address('0x44dF096D2600C6a6db77899dB3DE3AeCff746cb8'),
+                withdraw_manager=string_to_evm_address('0xa1bb8a8ED84A37a8c93a10Df5153E612f58e34E5'),
+            ), WooVaultInfo(  # WETH/ETH
+                token=string_to_evm_address('0x4200000000000000000000000000000000000006'),
+                supercharger=string_to_evm_address('0xb772122C4a37fe1754B46AB1799b909351e8Cb43'),
+                withdraw_manager=string_to_evm_address('0xe61Acb121a2B538dF495A85C4E50dD8581de4ed0'),
+            ), WooVaultInfo(  # cbBTC
+                token=string_to_evm_address('0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf'),
+                supercharger=string_to_evm_address('0x8C603050D7a913b6f63836e07ebF385a4A5736E7'),
+                withdraw_manager=string_to_evm_address('0xEc054126922a9a1918435c9072c32f1B60cB2B90'),
+            )],
+        )
---
+++ rotkehlchen/chain/binance_sc/modules/woo_fi/decoder.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING
+
+from rotkehlchen.chain.evm.decoding.woo_fi.decoder import WooFiCommonDecoder, WooVaultInfo
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+if TYPE_CHECKING:
+    from rotkehlchen.chain.binance_sc.node_inquirer import BinanceSCInquirer
+    from rotkehlchen.chain.evm.decoding.base import BaseEvmDecoderTools
+    from rotkehlchen.user_messages import MessagesAggregator
+
+
+class WooFiDecoder(WooFiCommonDecoder):
+
+    def __init__(
+            self,
+            binance_sc_inquirer: 'BinanceSCInquirer',
+            base_tools: 'BaseEvmDecoderTools',
+            msg_aggregator: 'MessagesAggregator',
+    ) -> None:
+        super().__init__(
+            evm_inquirer=binance_sc_inquirer,
+            base_tools=base_tools,
+            msg_aggregator=msg_aggregator,
+            earn_vaults=[WooVaultInfo(  # BNB
+                token=string_to_evm_address('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'),
+                supercharger=string_to_evm_address('0x7eb8D4CcFDBD9dF8d3520E9C5b5edf6a5Cbe4CaD'),
+                withdraw_manager=string_to_evm_address('0x2698946AD5988759fa29093e9aF99eeA12a31bb4'),
+            ), WooVaultInfo(  # USDT
+                token=string_to_evm_address('0x55d398326f99059fF775485246999027B3197955'),
+                supercharger=string_to_evm_address('0x5CB9ba4a6f05c4125D61172E1b2C1DBe3afb3158'),
+                withdraw_manager=string_to_evm_address('0x3cBB7F9a4e1E8a8430f1d400DF269B80B6872DeB'),
+            )],
+        )
