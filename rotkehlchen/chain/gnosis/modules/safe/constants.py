5d4b9293ba756d3d87efaec32443215ecba7dd0e
Lefteris Karapetsas <lefteris@refu.co>
2026-03-04 10:32:03 +0000

    Decode SafeDAO Token distribution claim

---
+++ rotkehlchen/chain/gnosis/modules/safe/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+from rotkehlchen.chain.evm.types import string_to_evm_address
+
+SAFE_CLAIM_DISTRIBUTOR: Final = string_to_evm_address('0xEc279b435068412589b0a6179710D1A300E5F58E')
+SAFE_TOKEN_ID_GNOSIS: Final = 'eip155:100/erc20:0x4d18815D14fe5c3304e87B3FA18318baa5c23820'
