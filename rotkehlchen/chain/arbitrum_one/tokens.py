8cc1466a06dd5a168f1b1bc9b3238200d3950d38
Yabir Benchakhtir <git@yabirgb.com>
2026-04-21 18:42:08 +0200

    Fresh account doesnt use RPC nodes to query tokens
    A new account that didn´t query the chain before would have no connected nodes and the logic
    for token detection would use the indexer only to query tokens. This caused premature Etherscan/indexer usage instead of lazy RPC-first behavior.

--- rotkehlchen/chain/arbitrum_one/tokens.py
+++ rotkehlchen/chain/arbitrum_one/tokens.py
@@ -1,5 +1,5 @@
-from rotkehlchen.chain.evm.tokens import EvmTokens
+from rotkehlchen.chain.evm.tokens import ARBISCAN_MAX_ARGUMENTS_TO_CONTRACT, EvmTokens
 
 
 class ArbitrumOneTokens(EvmTokens):
-    ...
+    INDEXER_CHUNK_SIZE = ARBISCAN_MAX_ARGUMENTS_TO_CONTRACT
