bb8be3c956d046b7567a640a0e8f2a49dbfcaad3
Lefteris Karapetsas <lefteris@refu.co>
2026-07-04 20:15:08 +0000

    chore: freeze time in gns calendar reminder test
    Without it the test would start failing once lefteris.gwei expires in
    July 2027, since past expiries are skipped by the reminder creator.

--- rotkehlchen/tests/unit/test_calendar_reminders.py
+++ rotkehlchen/tests/unit/test_calendar_reminders.py
@@ -196,6 +196,7 @@ def test_ens_expiry_calendar_reminders(
         assert reminders[1].secs_before == WEEK_IN_SECONDS
 
 
+@pytest.mark.freeze_time('2026-07-04 18:00:00 GMT')  # keep current_ts before the name expiries
 @pytest.mark.parametrize('ethereum_accounts', [[
     '0x2B888954421b424C5D3D9Ce9bB67c9bD47537d12',  # registered lefteris.gwei
     '0x9398084E888CB5B5c126240439054b57C10138E7',  # renewed aiiiden.gwei
