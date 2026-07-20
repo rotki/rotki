413c9a5d9491ca9379846f88bca0fea97adb98f8
Lefteris Karapetsas <lefteris@refu.co>
2026-07-20 12:10:46 +0000

    feat: record laposte bridge data for exact matching

--- rotkehlchen/chain/evm/decoding/stakedao/v2/constants.py
+++ rotkehlchen/chain/evm/decoding/stakedao/v2/constants.py
@@ -7,6 +7,8 @@ VOTEMARKET_CLAIM_TOPIC: Final = b'\x31\x8e\x0a\x24\xa7\xfc\x05\xb1\x2e\x35\x89\x
 LAPOSTE_MESSAGE_SENT_TOPIC: Final = b'\xc3\xfe\x0c\xec\x81\xa9\xf3\x5c\xd4\xd9\x04\xd9\xbd\xc8\xa8\x07\xc1\xdf\xb9\xc7\x7c\x51\x1f\xa8\x76\x89\x9e\xb0\xb1\xa4\xc9\x29'  # noqa: E501
 LAPOSTE_MESSAGE_RECEIVED_TOPIC: Final = b'\xe1\x20\x70\xab\x31\xc3\x54\xe3\x2f\x75\x59\x84\x62\x8c\xc3\xaf\x26\x01\x21\xee\x90\x15\xcf\x34\x63\x7b\x82\x4e\x34\xd0\x0b\xc7'  # noqa: E501
 
+LAPOSTE_MESSAGE_ABI: Final = '(uint256,address,address,(address,uint256)[],(string,string,uint8)[],bytes,uint256)'  # (destinationChainId, to, sender, tokens[], tokenMetadata[], payload, nonce)  # noqa: E501
+
 LAPOSTE_ADDRESS: Final = string_to_evm_address('0xF0000058000021003E4754dCA700C766DE7601C2')
 LAPOSTE_BUNDLER_ADDRESS: Final = string_to_evm_address(
     '0x67346f8b9B7dDA4639600C190DDaEcDc654359c8',
