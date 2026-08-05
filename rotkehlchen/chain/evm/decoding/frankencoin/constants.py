44a86764e6ac3c3bc4de88a9b0642746347f72db
borosorus <diego.boros@protonmail.com>
2026-08-05 15:44:41 +0200

    test: cover Frankencoin savings across EVM chains

--- rotkehlchen/chain/evm/decoding/frankencoin/constants.py
+++ rotkehlchen/chain/evm/decoding/frankencoin/constants.py
@@ -17,9 +17,9 @@ FRANKENCOIN_COUNTERPARTY_DETAILS: Final = CounterpartyDetails(
 
 ZCHF_ADDRESS: Final[dict[ChainID, ChecksumEvmAddress]] = {
     ChainID.ETHEREUM: string_to_evm_address('0xB58E61C3098d85632Df34EecfB899A1Ed80921cB'),
-    ChainID.ARBITRUM_ONE: string_to_evm_address('0xB33c4255938de7A6ec1200d397B2b2F329397F9B'),
+    ChainID.ARBITRUM_ONE: string_to_evm_address('0xD4dD9e2F021BB459D5A5f6c24C12fE09c5D45553'),
     ChainID.BASE: string_to_evm_address('0xD4dD9e2F021BB459D5A5f6c24C12fE09c5D45553'),
     ChainID.GNOSIS: string_to_evm_address('0xD4dD9e2F021BB459D5A5f6c24C12fE09c5D45553'),
-    ChainID.POLYGON_POS: string_to_evm_address('0x02567e4b14b25549331fCEe2B56c647A8bAB16FD'),
-    ChainID.OPTIMISM: string_to_evm_address('0x4F8a84C442F9675610c680990EdDb2CCDDB8aB6f'),
+    ChainID.POLYGON_POS: string_to_evm_address('0xD4dD9e2F021BB459D5A5f6c24C12fE09c5D45553'),
+    ChainID.OPTIMISM: string_to_evm_address('0xD4dD9e2F021BB459D5A5f6c24C12fE09c5D45553'),
 }
--- rotkehlchen/tests/unit/decoders/test_frankencoin_savings.py
+++ rotkehlchen/tests/unit/decoders/test_frankencoin_savings.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -18,7 +18,7 @@ from rotkehlchen.chain.evm.decoding.structures import (
     DecoderContext,
 )
 from rotkehlchen.chain.evm.structures import EvmTxReceiptLog
-from rotkehlchen.constants.assets import A_ETH
+from rotkehlchen.constants.assets import A_ETH, A_POL, A_XDAI
 from rotkehlchen.fval import FVal
 from rotkehlchen.history.events.structures.evm_event import EvmEvent
 from rotkehlchen.history.events.structures.types import HistoryEventSubType, HistoryEventType
@@ -321,6 +321,207 @@ def test_interest_collection(ethereum_inquirer, ethereum_accounts):
             event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
             asset=zchf,
             amount=FVal(deposit_amount := '1797.322805687710429906'),
+            location_label=user_address,
+            notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=savings_address,
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('arbitrum_one_accounts', [['0xEf5ec96BB5761853682Ac486842f59aB7E663552']])
+def test_deposit_arbitrum_one(arbitrum_one_inquirer, arbitrum_one_accounts):
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=arbitrum_one_inquirer,
+        tx_hash=(tx_hash := deserialize_evm_tx_hash('0xa5e84fbb22e1273b842319a089e7574363d16b06a70dc6666543c119e8b28630')),  # noqa: E501
+    )
+    assert events == [
+        EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=0,
+            timestamp=(timestamp := TimestampMS(1768655289000)),
+            location=Location.ARBITRUM_ONE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            amount=FVal(gas_amount := '0.000001778793984'),
+            location_label=(user_address := arbitrum_one_accounts[0]),
+            notes=f'Burn {gas_amount} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.ARBITRUM_ONE,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
+            asset=Asset(f'eip155:42161/erc20:{ZCHF_ADDRESS[ChainID.ARBITRUM_ONE]}'),
+            amount=FVal(deposit_amount := '1701.360556746607878987'),
+            location_label=user_address,
+            notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=SAVINGS_CONTRACT_ADDRESS[ChainID.ARBITRUM_ONE],
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('base_accounts', [['0xAAafdC589d2222cE3b794876c768Eb540230aB11']])
+def test_deposit_base(base_inquirer, base_accounts):
+    with patch(
+        'rotkehlchen.chain.evm.transactions.EvmTransactions._query_and_save_internal_transactions_for_parent_hash',
+    ):
+        events, _ = get_decoded_events_of_transaction(
+            evm_inquirer=base_inquirer,
+            tx_hash=(tx_hash := deserialize_evm_tx_hash('0x2aa42477bb168eb4532368a94c65506420abf8ea4071d96c9355032939e89d79')),  # noqa: E501
+        )
+    assert events == [
+        EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=0,
+            timestamp=(timestamp := TimestampMS(1785923517000)),
+            location=Location.BASE,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            amount=FVal(gas_amount := '0.000000430021640992'),
+            location_label=(user_address := base_accounts[0]),
+            notes=f'Burn {gas_amount} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=322,
+            timestamp=timestamp,
+            location=Location.BASE,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
+            asset=Asset(f'eip155:8453/erc20:{ZCHF_ADDRESS[ChainID.BASE]}'),
+            amount=FVal(deposit_amount := '5'),
+            location_label=user_address,
+            notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=SAVINGS_CONTRACT_ADDRESS[ChainID.BASE],
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('optimism_accounts', [['0xc35A45BcF42BeE24bB62D512b8aA08660cc8a3d3']])
+def test_deposit_optimism(optimism_inquirer, optimism_accounts):
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=optimism_inquirer,
+        tx_hash=(tx_hash := deserialize_evm_tx_hash('0xbe24c79aa348b94e116dd58cc14134acdb703ac1b5d8136113fa295ffd86d816')),  # noqa: E501
+    )
+    assert events == [
+        EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=0,
+            timestamp=(timestamp := TimestampMS(1785774967000)),
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_ETH,
+            amount=FVal(gas_amount := '0.000001133643733036'),
+            location_label=(user_address := optimism_accounts[0]),
+            notes=f'Burn {gas_amount} ETH for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=1,
+            timestamp=timestamp,
+            location=Location.OPTIMISM,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
+            asset=Asset(f'eip155:10/erc20:{ZCHF_ADDRESS[ChainID.OPTIMISM]}'),
+            amount=FVal(deposit_amount := '1'),
+            location_label=user_address,
+            notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=SAVINGS_CONTRACT_ADDRESS[ChainID.OPTIMISM],
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('polygon_pos_accounts', [['0x726397F95bcE98505d67480457071896962f85AC']])
+def test_deposit_polygon_pos(polygon_pos_inquirer, polygon_pos_accounts):
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=polygon_pos_inquirer,
+        tx_hash=(tx_hash := deserialize_evm_tx_hash('0x439c5f7f120f8fce87c589b83211527085d04c0e093748511c5df63f5df9a245')),  # noqa: E501
+    )
+    assert events == [
+        EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=0,
+            timestamp=(timestamp := TimestampMS(1783018649000)),
+            location=Location.POLYGON_POS,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_POL,
+            amount=FVal(gas_amount := '0.050978100548580012'),
+            location_label=(user_address := polygon_pos_accounts[0]),
+            notes=f'Burn {gas_amount} POL for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=1166,
+            timestamp=timestamp,
+            location=Location.POLYGON_POS,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
+            asset=Asset(f'eip155:137/erc20:{ZCHF_ADDRESS[ChainID.POLYGON_POS]}'),
+            amount=FVal(deposit_amount := '1'),
+            location_label=user_address,
+            notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=SAVINGS_CONTRACT_ADDRESS[ChainID.POLYGON_POS],
+        ),
+    ]
+
+
+@pytest.mark.vcr(filter_query_parameters=['apikey'])
+@pytest.mark.parametrize('gnosis_accounts', [['0x5D0cE9De9F4a26e3999dCE56Ff62cA5Db97608e3']])
+def test_deposit_gnosis(gnosis_inquirer, gnosis_accounts):
+    events, _ = get_decoded_events_of_transaction(
+        evm_inquirer=gnosis_inquirer,
+        tx_hash=(tx_hash := deserialize_evm_tx_hash('0x1bf9048db6bc58f5c3b9f34656992e39111cbef83b088eeb54b49cb09189ce82')),  # noqa: E501
+    )
+    assert events == [
+        EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=0,
+            timestamp=(timestamp := TimestampMS(1785931065000)),
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.SPEND,
+            event_subtype=HistoryEventSubType.FEE,
+            asset=A_XDAI,
+            amount=FVal(gas_amount := '0.0000871440130716'),
+            location_label=(user_address := gnosis_accounts[0]),
+            notes=f'Burn {gas_amount} XDAI for gas',
+            counterparty=CPT_GAS,
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=13,
+            timestamp=timestamp,
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.RECEIVE,
+            event_subtype=HistoryEventSubType.INTEREST,
+            asset=(zchf := Asset(f'eip155:100/erc20:{ZCHF_ADDRESS[ChainID.GNOSIS]}')),
+            amount=FVal(interest_amount := '32.403386488099250454'),
+            location_label=user_address,
+            notes=f'Received {interest_amount} zCHF as interests in Frankencoin Savings Module',
+            counterparty=CPT_FRANKENCOIN,
+            address=(savings_address := SAVINGS_CONTRACT_ADDRESS[ChainID.GNOSIS]),
+        ), EvmEvent(
+            tx_ref=tx_hash,
+            sequence_index=14,
+            timestamp=timestamp,
+            location=Location.GNOSIS,
+            event_type=HistoryEventType.DEPOSIT,
+            event_subtype=HistoryEventSubType.DEPOSIT_TO_PROTOCOL,
+            asset=zchf,
+            amount=FVal(deposit_amount := '11279.78'),
             location_label=user_address,
             notes=f'Deposit {deposit_amount} zCHF in Frankencoin Savings Module',
             counterparty=CPT_FRANKENCOIN,
